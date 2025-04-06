3cb3ecbe6b00c8911068eb64c022b14322751f81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-06 15:04:25 +0000

    Add package PyQWebWindow/0.4.0/pyqwebwindow-0.4.0.tar.gz

---
+++ packages/PyQWebWindow/pyqwebwindow-0.4.0.tar.gz/pyqwebwindow-0.4.0/src/PyQWebWindow/__init__.py
@@ -0,0 +1,5 @@
+from PySide6.QtCore import Slot
+from .QAppManager import QAppManager
+from .QWebWindow  import QWebWindow
+from .controllers.BindingController import Serializable, SerializableCallable
+from .controllers.IpcController import IpcServer, IpcClient, IpcSerializable, IpcSerializableCallable
---
+++ packages/PyQWebWindow/pyqwebwindow-0.4.0.tar.gz/pyqwebwindow-0.4.0/src/PyQWebWindow/controllers/__init__.py
@@ -0,0 +1,3 @@
+from .WebViewController import WebViewController
+from .BindingController import BindingController
+from .WindowController import WindowController
