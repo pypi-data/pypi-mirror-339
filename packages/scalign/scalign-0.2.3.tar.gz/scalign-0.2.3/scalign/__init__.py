004ca37696ea1e50347e39815c4e407b09449b02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-06 15:04:28 +0000

    Add package scalign/0.2.3/scalign-0.2.3.tar.gz

---
+++ packages/scalign/scalign-0.2.3.tar.gz/scalign-0.2.3/scalign/__init__.py
@@ -0,0 +1,10 @@
+
+from scalign.reference import (
+    reference, 
+    default_encoder, 
+    make_reference, 
+    rebuild_umap_embedder,
+    rebuild_expression
+)
+from scalign.parametric import pumap, load_pumap
+from scalign.encoders import resnet_encoder
---
+++ packages/scalign/scalign-0.2.3.tar.gz/scalign-0.2.3/setup.py
@@ -0,0 +1,26 @@
+
+from setuptools import setup
+
+setup(
+    name                 = 'scalign',
+    version              = '0.2.3',
+    description          = 'align query dataset to reference atlases.',
+    author               = 'Zheng Yang',
+    author_email         = 'xornent@outlook.com',
+    license              = 'MIT',
+    packages             = ['scalign'],
+    install_requires     = [
+        'umap-learn >= 0.5.0',
+        'scvi-tools >= 1.3.0',
+        'scanpy',
+        'pandas',
+        'numpy',
+        'matplotlib',
+        'seaborn',
+        'dill',
+        'torch >= 2.0',
+        'pytorch_lightning >= 2.0'
+    ],
+    include_package_data = True,
+    zip_safe             = False
+)
