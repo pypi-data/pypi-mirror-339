ad09d524ae1e6b6a26940be9b391cfd8ead09774
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-06 04:03:48 +0000

    Add package beautools/1.1.0/beautools-1.1.0-py2.py3-none-any.whl

---
+++ packages/beautools/beautools-1.1.0-py2.py3-none-any.whl/beautools/decor.py
@@ -0,0 +1,63 @@
+import functools
+import logging
+import time
+import traceback
+
+
+
+def catch_print_swallow_exc(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except:
+            traceback.print_exc()
+    
+    
+    return wrapper
+
+
+def a_catch_print_swallow_exc(func):
+    @functools.wraps(func)
+    async def wrapper(*args, **kwargs):
+        try:
+            return await func(*args, **kwargs)
+        except:
+            traceback.print_exc()
+    
+    
+    return wrapper
+
+
+def log_execution_time(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        start_time = time.perf_counter()
+        try:
+            result = func(*args, **kwargs)
+            return result
+        except:
+            traceback.print_exc()
+        finally:
+            end_time = time.perf_counter()
+            logging.warning(f"---- {func.__name__} executed in {end_time - start_time:.6f} seconds")
+    
+    
+    return wrapper
+
+
+def a_log_execution_time(func, level=logging.INFO):
+    @functools.wraps(func)
+    async def wrapper(*args, **kwargs):
+        start_time = time.perf_counter()
+        try:
+            result = await func(*args, **kwargs)
+            return result
+        except:
+            traceback.print_exc()
+        finally:
+            end_time = time.perf_counter()
+            logging.log(level, f"---- {func.__name__} executed in {end_time - start_time:.6f} seconds")
+    
+    
+    return wrapper
