5aa90439f4d203d0f96aa8d46fed6c6f9e6c3036
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-06 15:04:37 +0000

    Add package exppandas/0.1.0/exppandas-0.1.0.tar.gz

---
+++ packages/exppandas/exppandas-0.1.0.tar.gz/exppandas-0.1.0/README.md
@@ -0,0 +1,52 @@
+# exppandas
+
+[![PyPI version](https://img.shields.io/pypi/v/exppandas.svg)](https://pypi.org/project/exppandas/)
+[![Python Versions](https://img.shields.io/pypi/pyversions/exppandas.svg)](https://pypi.org/project/exppandas/)
+[![License](https://img.shields.io/github/license/Sumedh1599/exppandas.svg)](https://github.com/Sumedh1599/exppandas/blob/main/LICENSE)
+[![GitHub Issues](https://img.shields.io/github/issues/Sumedh1599/exppandas.svg)](https://github.com/Sumedh1599/exppandas/issues)
+[![Tests](https://github.com/Sumedh1599/exppandas/workflows/Tests/badge.svg)](https://github.com/Sumedh1599/exppandas/actions)
+[![Documentation Status](https://readthedocs.org/projects/exppandas/badge/?version=latest)](https://exppandas.readthedocs.io/en/latest/?badge=latest)
+
+## Overview
+
+Enhanced version of Pandas with improved data manipulation capabilities
+
+This library is an enhanced version of pandas with additional features and improvements.
+
+## Features
+
+- All features from the original pandas
+- Faster data processing
+- Advanced data cleaning
+- Better visualization
+- Seamless integration with other exp libraries
+
+## Installation
+
+```bash
+pip install exppandas
+```
+
+## Quick Start
+
+```python
+import exppandas as exp
+
+# Example code
+# Example code for exppandas
+result = exp.function()
+```
+
+## Documentation
+
+For full documentation, visit [exppandas.readthedocs.io](https://exppandas.readthedocs.io/).
+
+## Contributing
+
+Contributions are welcome! Please feel free to submit a Pull Request.
+
+## License
+
+Copyright (c) 2025 Sumedh Patil, Aipresso Limited, UK
+
+This project is licensed under the MIT License - see the LICENSE file for details.
---
+++ packages/exppandas/exppandas-0.1.0.tar.gz/exppandas-0.1.0/setup.py
@@ -0,0 +1,55 @@
+import os
+from setuptools import setup, find_packages
+
+# Read the contents of your README file
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+# Read version from version.py
+about = {}
+with open(os.path.join('src', 'exppandas', 'version.py'), 'r', encoding='utf-8') as f:
+    exec(f.read(), about)
+
+setup(
+    name='exppandas',
+    version=about['__version__'],
+    author='Sumedh Patil',
+    author_email='sumedh@aipresso.com',
+    description='Enhanced version of Pandas with improved data manipulation capabilities',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/Sumedh1599/exppandas',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
+    python_requires='>=3.7',
+    install_requires=[
+        'pandas>=1.0.0',
+        'numpy>=1.20.0',
+        'scipy>=1.7.0'
+    ],
+    extras_require={
+        'dev': [
+            'pytest>=7.0.0',
+            'pytest-cov>=4.0.0',
+            'black>=23.0.0',
+            'isort>=5.0.0',
+            'sphinx>=5.0.0',
+            'sphinx-rtd-theme>=1.0.0',
+        ],
+    },
+    project_urls={
+        'Bug Reports': 'https://github.com/Sumedh1599/exppandas/issues',
+        'Source': 'https://github.com/Sumedh1599/exppandas',
+        'Documentation': 'https://exppandas.readthedocs.io/',
+    },
+)
