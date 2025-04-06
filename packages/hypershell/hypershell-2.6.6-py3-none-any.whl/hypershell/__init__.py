29faed8f6f97a778cbab7c484bac1401a2ef2803
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-06 05:03:23 +0000

    Add package hypershell/2.6.6/hypershell-2.6.6-py3-none-any.whl

---
+++ packages/hypershell/hypershell-2.6.6-py3-none-any.whl/hypershell/__init__.py
@@ -0,0 +1,118 @@
+# SPDX-FileCopyrightText: 2025 Geoffrey Lentner
+# SPDX-License-Identifier: Apache-2.0
+
+"""Initialization and entry-point for console application."""
+
+
+# Path sanitizer must happen first
+import hypershell.core.sys
+
+# Standard libs
+import sys
+from importlib.metadata import version as get_version
+from platform import python_version
+
+# External libs
+from cmdkit.app import Application, ApplicationGroup
+from cmdkit.cli import Interface
+
+# Internal libs
+from hypershell.core.logging import Logger, initialize_logging
+from hypershell.core.signal import register_handlers
+from hypershell.submit import SubmitApp
+from hypershell.server import ServerApp
+from hypershell.client import ClientApp
+from hypershell.cluster import ClusterApp
+from hypershell.task import TaskGroupApp
+from hypershell.config import ConfigApp
+from hypershell.data import InitDBApp
+
+# Public interface
+__all__ = ['HyperShellApp', 'main', '__version__']
+
+# project metadata
+__version__     = get_version('hypershell')
+__website__     = 'https://github.com/hypershell/hypershell'
+__description__ = 'Process shell commands over a distributed, asynchronous queue.'
+__citation__    = """\
+@inproceedings{lentner_2022,
+    author = {Lentner, Geoffrey and Gorenstein, Lev},
+    title = {HyperShell v2: Distributed Task Execution for HPC},
+    year = {2022},
+    isbn = {9781450391610},
+    publisher = {Association for Computing Machinery},
+    url = {https://doi.org/10.1145/3491418.3535138},
+    doi = {10.1145/3491418.3535138},
+    booktitle = {Practice and Experience in Advanced Research Computing},
+    articleno = {80},
+    numpages = {3},
+    series = {PEARC '22}
+}\
+"""
+
+# Initialize logger
+log = Logger.with_name('hypershell')
+
+
+# inject logger setup into command-line framework
+Application.log_critical = log.critical
+Application.log_exception = log.exception
+
+
+APP_NAME = 'hs'
+APP_USAGE = f"""\
+Usage:
+  {APP_NAME} [-h] [-v] <command> [<args>...]
+  {__description__}\
+"""
+
+APP_HELP = f"""\
+{APP_USAGE}
+
+Commands:
+  config                 {ConfigApp.__doc__}
+  submit                 {SubmitApp.__doc__}
+  server                 {ServerApp.__doc__}
+  client                 {ClientApp.__doc__}
+  cluster                {ClusterApp.__doc__} (recommended)
+  task                   {TaskGroupApp.__doc__}
+  initdb                 {InitDBApp.__doc__}
+
+Options:
+  -h, --help             Show this message and exit.
+  -v, --version          Show the version and exit.
+      --citation         Show citation info and exit.
+
+Issue tracking at:
+{__website__}
+
+If this software has helped in your research please consider
+citing us (see --citation).\
+"""
+
+
+class HyperShellApp(ApplicationGroup):
+    """Top-level application class for console application."""
+
+    interface = Interface(APP_NAME, APP_USAGE, APP_HELP)
+    interface.add_argument('-v', '--version', action='version',
+                           version=f'HyperShell v{__version__} (Python {python_version()})')
+    interface.add_argument('--citation', action='version', version=__citation__)
+    interface.add_argument('command')
+
+    commands = {
+        'submit': SubmitApp,
+        'server': ServerApp,
+        'client': ClientApp,
+        'cluster': ClusterApp,
+        'task': TaskGroupApp,
+        'config': ConfigApp,
+        'initdb': InitDBApp,
+    }
+
+
+def main() -> int:
+    """Entry-point for console application."""
+    initialize_logging()
+    register_handlers()
+    return HyperShellApp.main(sys.argv[1:])
---
+++ packages/hypershell/hypershell-2.6.6-py3-none-any.whl/hypershell/data/__init__.py
@@ -0,0 +1,137 @@
+# SPDX-FileCopyrightText: 2025 Geoffrey Lentner
+# SPDX-License-Identifier: Apache-2.0
+
+"""Database interface, models, and methods."""
+
+
+# Type annotations
+from __future__ import annotations
+from typing import Final
+
+# Standard libs
+import sys
+import functools
+
+# External libs
+from cmdkit.app import Application, exit_status
+from cmdkit.cli import Interface
+from cmdkit.config import ConfigurationError
+from sqlalchemy import inspect
+from sqlalchemy.orm import close_all_sessions
+from sqlalchemy.exc import OperationalError
+
+# Internal libs
+from hypershell.core.logging import Logger
+from hypershell.core.config import config
+from hypershell.core.exceptions import handle_exception, DatabaseUninitialized, get_shared_exception_mapping
+from hypershell.data.core import engine, in_memory, schema
+from hypershell.data.model import Entity, Task
+
+# Public interface
+__all__ = ['InitDBApp', 'initdb', 'truncatedb', 'checkdb', 'ensuredb', 'DATABASE_ENABLED', ]
+
+# Initialize logger
+log = Logger.with_name(__name__)
+
+
+DATABASE_ENABLED: Final[bool] = not in_memory
+"""Set if database has been configured."""
+
+
+def initdb() -> None:
+    """Initialize database tables."""
+    Entity.metadata.create_all(engine)
+
+
+def truncatedb() -> None:
+    """Truncate database tables."""
+    # NOTE: We still might hang here if other sessions exist outside this app instance
+    close_all_sessions()
+    log.trace('Dropping all tables')
+    Entity.metadata.drop_all(engine)
+    log.trace('Creating all tables')
+    Entity.metadata.create_all(engine)
+    log.warning(f'Truncated database')
+
+
+def checkdb() -> None:
+    """Ensure database connection and tables exist."""
+    if not inspect(engine).has_table('task', schema=schema):
+        raise DatabaseUninitialized('Use \'initdb\' to initialize the database')
+
+
+def ensuredb(auto_init: bool = False) -> None:
+    """
+    Ensure database configuration before applying any operations.
+
+    If SQLite and `auto_init` we run :meth:`initdb`, else :meth:`checkdb`.
+    """
+    db = config.database.get('file', None) or config.database.get('database', None)
+    if config.database.provider == 'sqlite' and db in ('', ':memory:', None):
+        raise ConfigurationError('Missing database configuration')
+    if config.database.provider == 'sqlite' or auto_init is True:
+        initdb()
+    else:
+        checkdb()
+
+
+INITDB_PROGRAM = 'hs initdb'
+INITDB_USAGE = f"""\
+Usage:
+  {INITDB_PROGRAM} [-h] [--truncate [--yes]]
+
+  Initialize database (not needed for SQLite).
+  Use --truncate to zero out the task metadata.\
+"""
+
+INITDB_HELP = f"""\
+{INITDB_USAGE}
+
+Options:
+  -t, --truncate       Truncate database (task metadata will be lost).
+  -y, --yes            Auto-confirm truncation (default will prompt).
+  -h, --help           Show this message and exit.\
+"""
+
+
+class InitDBApp(Application):
+    """Initialize database (not needed for SQLite)."""
+
+    interface = Interface(INITDB_PROGRAM, INITDB_USAGE, INITDB_HELP)
+
+    ALLOW_NOARGS = True
+
+    truncate: bool = False
+    interface.add_argument('-t', '--truncate', action='store_true')
+
+    auto_confirm: bool = False
+    interface.add_argument('-y', '--yes', action='store_true', dest='auto_confirm')
+
+    exceptions = {
+        OperationalError: functools.partial(handle_exception, logger=log, status=exit_status.runtime_error),
+        **get_shared_exception_mapping(__name__),
+    }
+
+    def run(self: InitDBApp) -> None:
+        """Business logic for `initdb`."""
+        if not DATABASE_ENABLED:
+            raise ConfigurationError('No database configured')
+        elif not self.truncate:
+            initdb()
+        elif self.auto_confirm:
+            truncatedb()
+        elif not sys.stdout.isatty():
+            raise RuntimeError('Non-interactive prompt cannot confirm --truncate (see --yes).')
+        else:
+            if config.database.provider == 'sqlite':
+                site = config.database.file
+            else:
+                site = config.database.get('host', 'localhost')
+            print(f'Connected to: {config.database.provider} ({site})')
+            response = input(f'Truncate database ({Task.count()} tasks)? [Y]es/no: ').strip()
+            if response.lower() in ['', 'y', 'yes']:
+                truncatedb()
+            elif response.lower() in ['n', 'no']:
+                print('Stopping')
+            else:
+                raise RuntimeError(f'Stopping (invalid response: "{response}")')
