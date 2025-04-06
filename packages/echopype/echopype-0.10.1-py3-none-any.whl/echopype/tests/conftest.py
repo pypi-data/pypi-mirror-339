52db21725ece5cd47b7ec7ffe032374ffed2d29f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-06 14:03:36 +0000

    Add package echopype/0.10.1/echopype-0.10.1-py3-none-any.whl

---
+++ packages/echopype/echopype-0.10.1-py3-none-any.whl/echopype/tests/conftest.py
@@ -0,0 +1,42 @@
+"""``pytest`` configuration."""
+
+import pytest
+
+
+from echopype.testing import TEST_DATA_FOLDER
+
+
+@pytest.fixture(scope="session")
+def dump_output_dir():
+    return TEST_DATA_FOLDER / "dump"
+
+
+@pytest.fixture(scope="session")
+def test_path():
+    return {
+        "ROOT": TEST_DATA_FOLDER,
+        "EA640": TEST_DATA_FOLDER / "ea640",
+        "EK60": TEST_DATA_FOLDER / "ek60",
+        "EK60_MISSING_CHANNEL_POWER": TEST_DATA_FOLDER / "ek60_missing_channel_power",
+        "EK80": TEST_DATA_FOLDER / "ek80",
+        "EK80_NEW": TEST_DATA_FOLDER / "ek80_new",
+        "ES60": TEST_DATA_FOLDER / "es60",
+        "ES70": TEST_DATA_FOLDER / "es70",
+        "ES80": TEST_DATA_FOLDER / "es80",
+        "AZFP": TEST_DATA_FOLDER / "azfp",
+        "AZFP6": TEST_DATA_FOLDER / "azfp6",
+        "AD2CP": TEST_DATA_FOLDER / "ad2cp",
+        "EK80_CAL": TEST_DATA_FOLDER / "ek80_bb_with_calibration",
+        "EK80_EXT": TEST_DATA_FOLDER / "ek80_ext",
+        "ECS": TEST_DATA_FOLDER / "ecs",
+        "LEGACY_DATATREE": TEST_DATA_FOLDER / "legacy_datatree",
+    }
+
+
+@pytest.fixture(scope="session")
+def minio_bucket():
+    return dict(
+        client_kwargs=dict(endpoint_url="http://localhost:9000/"),
+        key="minioadmin",
+        secret="minioadmin",
+    )
