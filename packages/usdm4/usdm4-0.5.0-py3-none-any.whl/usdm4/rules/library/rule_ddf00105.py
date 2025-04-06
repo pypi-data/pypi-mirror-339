cc3c36ba58f5102ea390ce40f0be20226c892384
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-06 04:03:39 +0000

    Add package usdm4/0.5.0/usdm4-0.5.0-py3-none-any.whl

---
+++ packages/usdm4/usdm4-0.5.0-py3-none-any.whl/usdm4/rules/library/rule_ddf00105.py
@@ -0,0 +1,5 @@
+from usdm3.rules.library.rule_ddf00105 import RuleDDF00105 as V3Rule
+
+
+class RuleDDF00105(V3Rule):
+    pass
---
+++ packages/usdm4/usdm4-0.5.0-py3-none-any.whl/usdm4/rules/library/rule_ddf00106.py
@@ -0,0 +1,5 @@
+from usdm3.rules.library.rule_ddf00106 import RuleDDF00106 as V3Rule
+
+
+class RuleDDF00106(V3Rule):
+    pass
