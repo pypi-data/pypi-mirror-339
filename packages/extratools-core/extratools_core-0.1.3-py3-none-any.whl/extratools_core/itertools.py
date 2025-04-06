7de9e4c4f58d44467875df2a7a122c782e26e7ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-06 15:04:01 +0000

    Add package extratools-core/0.1.3/extratools_core-0.1.3-py3-none-any.whl

---
+++ packages/extratools-core/extratools_core-0.1.3-py3-none-any.whl/extratools_core/itertools.py
@@ -0,0 +1,83 @@
+from collections.abc import Callable, Iterable, Sequence
+from itertools import chain, count, repeat
+from typing import Iterator, cast
+
+from toolz.itertoolz import sliding_window
+
+from .seq import iter_to_seq
+
+
+def iter_to_grams[T](
+    _iter: Iterable[T],
+    *,
+    n: int,
+    pad: T | None = None,
+) -> Iterable[Sequence[T]]:
+    if pad is not None:
+        _iter = chain(
+            repeat(pad, n - 1),
+            _iter,
+            repeat(pad, n - 1),
+        )
+
+    return sliding_window(n, _iter)
+
+
+def filter_by_positions[T](poss: Iterable[int], seq: Iterable[T]) -> Iterable[T]:
+    p: Iterator[int] = iter(poss)
+
+    pos: int | None = next(p, None)
+    if pos is None:
+        return
+
+    for i, v in enumerate(seq):
+        if i == pos:
+            yield v
+
+            pos = next(p, None)
+            if pos is None:
+                return
+
+
+def filter_by_others[T](func: Callable[[T, T], bool], _iter: Iterable[T]) -> Iterable[T]:
+    seq: Sequence[T] = iter_to_seq(_iter)
+
+    filtered_ids: set[int] = set(range(len(seq)))
+
+    for i, x in enumerate(seq):
+        remove: bool = False
+        for j in filtered_ids:
+            if i == j:
+                continue
+
+            if not func(x, seq[j]):
+                remove = True
+                break
+
+        if remove:
+            filtered_ids.remove(i)
+
+    for i in filtered_ids:
+        yield seq[i]
+
+
+def remap[KT, VT](
+    data: Iterable[KT],
+    mapping: dict[KT, VT],
+    *,
+    key: Callable[[KT], VT] | None = None,
+) -> Iterable[VT]:
+    local_key: Callable[[KT], VT]
+    if key is None:
+        c = count(start=0)
+
+        def default_key(_: KT) -> VT:
+            return cast("VT", next(c))
+
+        local_key = default_key
+    else:
+        local_key = key
+
+    k: KT
+    for k in data:
+        yield mapping.setdefault(k, local_key(k))
