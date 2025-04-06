6970e7d4e0f96d5c78a657f83522d608069fced4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-06 04:05:07 +0000

    Add package simular-evm/0.3.0/simular_evm-0.3.0-cp310-none-win32.whl

---
+++ packages/simular-evm/simular_evm-0.3.0-cp310-none-win32.whl/simular/contract.py
@@ -0,0 +1,155 @@
+"""
+Wraps pyo3 code to provide a high-level contract API
+"""
+
+from eth_utils import is_address
+import typing
+
+from .simular import PyEvm, PyAbi, TxResult
+
+
+def convert_for_soltypes(args: typing.Tuple) -> str:
+    """
+    This converts `args` into a string for processing on the Rust side
+    """
+    if not isinstance(args, tuple):
+        raise Exception("Expected tuple as arguments")
+
+    def clean(v):
+        return str(v).replace("'", "").replace("True", "true").replace("False", "false")
+
+    if len(args) == 1:
+        return f"({clean(args[0])})"
+    return clean(args)
+
+
+class Function:
+    """
+    Contains information needed to interact with a contract function. This
+    is attached automatically to the Contract based on the ABI.
+    """
+
+    def __init__(self, evm: PyEvm, abi: PyAbi, contract_address: str, name: str):
+        self.name = name
+        self.evm = evm
+        self.abi = abi
+        self.contract_address = contract_address
+
+    def call(self, *args) -> typing.Any:
+        """
+        Make a read-only call to the contract, returning any results. Solidity
+        read-only calls are marked as `view` or `pure`. Does not commit any state
+        changes to the Evm.
+
+        - `args`: 0 or more expected arguments to the function
+
+        Returns: the decoded result
+        """
+        if not self.contract_address:
+            raise Exception("missing contract address. see at() method")
+
+        stargs = convert_for_soltypes(args)
+        result = self.evm.call(self.name, stargs, self.contract_address, self.abi)
+        return result
+
+    def simulate(self, *args, caller: str = None, value: int = 0) -> "TxResult":
+        """
+        Simulate a write call to the contract w/o changing state.
+        """
+        if not self.contract_address:
+            raise Exception("missing contract address. see at() method")
+
+        if not is_address(caller):
+            raise Exception("caller is missing or is not a valid address")
+
+        stargs = convert_for_soltypes(args)
+        result = self.evm.simulate(
+            self.name, stargs, caller, self.contract_address, value, self.abi
+        )
+        return result
+
+    def transact(self, *args, caller: str = None, value: int = 0) -> "TxResult":
+        """
+        Make a write call to the contract changing the state of the Evm.
+        - `args`: 0 or more expected arguments to the function
+        - `caller`: the address of the caller. This translates to `msg.sender` in a Solidity
+        - `value` : an optional amount of Ether to send with the value ... `msg.value`
+        Returns: the decoded result
+        """
+        if not self.contract_address:
+            raise Exception("missing contract address. see at() method")
+
+        if not is_address(caller):
+            raise Exception("caller is missing or is not a valid address")
+
+        stargs = convert_for_soltypes(args)
+        result = self.evm.transact(
+            self.name, stargs, caller, self.contract_address, value, self.abi
+        )
+        return result
+
+
+class Contract:
+    def __init__(self, evm: PyEvm, abi: PyAbi):
+        """
+        Instantiate a contract from an ABI with an EVM.
+
+        Maps contract functions to this class.  Making function available
+        as attributes on the Contract.
+
+        See `utils.py` for helper functions to create a Contract
+        """
+        self.address = None
+        self.evm = evm
+        self.abi = abi
+
+    def __getattr__(self, name: str) -> Function:
+        """
+        Make solidity contract methods available as method calls. For a given function `name`,
+        return `Function`.
+
+        For example, if the ABI has the contract function 'function hello(uint256)',
+        you can invoke it by name: contract.hello.transact(10)
+        """
+        if self.abi.has_function(name):
+            return Function(self.evm, self.abi, self.address, name)
+        else:
+            raise Exception(f"contract function: '{name}' not found!")
+
+    def at(self, address: str) -> "Contract":
+        """
+        Set the contract address.
+
+        .. note::
+            this is automatically set when using deploy``
+
+        Parameters
+        ----------
+        address: str
+            the address of a deployed contract
+
+        Returns
+        -------
+            self
+        """
+        self.address = address
+        return self
+
+    def deploy(self, *args, caller: str = None, value: int = 0) -> str:
+        """
+        Deploy the contract, returning it's deployed address
+        - `args`: a list of args (if any)
+        - `caller`: the address of the requester...`msg.sender`
+        - `value`: optional amount of Ether for the contract
+        Returns the address of the deployed contract
+        """
+        if not caller:
+            raise Exception("Missing required 'caller' address")
+
+        if not is_address(caller):
+            raise Exception("'caller' is not a valid ethereum address")
+
+        stargs = convert_for_soltypes(args)
+        addr = self.evm.deploy(stargs, caller, value, self.abi)
+        self.address = addr
+        return addr
---
+++ packages/simular-evm/simular_evm-0.3.0-cp310-none-win32.whl/simular/utils.py
@@ -0,0 +1,128 @@
+"""
+Helper functions
+"""
+
+from secrets import token_hex
+from eth_utils import to_wei, is_address
+import typing
+
+from . import PyEvm, PyAbi, Contract
+
+
+def ether_to_wei(value: int) -> int:
+    """Convert ether to wei"""
+    return to_wei(value, "ether")
+
+
+def generate_random_address() -> str:
+    """Generate a random hex encoded account/wallet address
+
+    Returns: the address
+    """
+    return "0x" + token_hex(20)
+
+
+def create_account(evm: PyEvm, address: str = None, value: int = 0) -> str:
+    """
+    Create an account
+
+    - evm    : PyEvm.  the EVM client
+    - address: str  optional. if set it will be used for the account address.
+                              Otherwise a random address will be generated.
+    - value  : int  optional. create an initial balance for the account in wei
+
+    Returns: the address
+    """
+    if not isinstance(evm, PyEvm):
+        raise Exception("'evm' should be an instance of either PyEvmLocal or PyEvmFork")
+
+    if not address:
+        address = generate_random_address()
+        evm.create_account(address, value)
+        return address
+
+    if not is_address(address):
+        raise Exception("'address' does not appear to be a valid Ethereum address")
+
+    evm.create_account(address, value)
+    return address
+
+
+def create_many_accounts(evm: PyEvm, num: int, value: int = 0) -> typing.List[str]:
+    """
+    Create many accounts in the EVM
+    - evm    : PyEvm.  the EVM client
+    - num    : int  the number of accounts to create
+    - value  : int  optional. create an initial balance for each account in wei
+
+    Returns a list of addresses
+    """
+    return [create_account(evm, value=value) for _ in range(num)]
+
+
+def contract_from_raw_abi(evm: PyEvm, raw_abi: str) -> Contract:
+    """
+    Create the contract given the full ABI. Full ABI should include
+    `abi` and `bytecode`. This is usually a single json file from a compiled Solidity contract.
+
+    - `evm`     : PyEvm.  the EVM client
+    - `raw_abi` : abi file as un-parsed json
+    Returns an instance of Contract
+    """
+    if not isinstance(evm, PyEvm):
+        raise Exception("'evm' should be an instance of either PyEvmLocal or PyEvmFork")
+
+    if not isinstance(raw_abi, str):
+        raise Exception("expected a an un-parsed json file")
+
+    abi = PyAbi.from_full_json(raw_abi)
+    return Contract(evm, abi)
+
+
+def contract_from_abi_bytecode(
+    evm: PyEvm, raw_abi: str, bytecode: bytes = None
+) -> Contract:
+    """
+    Create a contract given the abi and/or bytecode.
+
+    - `evm`     : PyEvm  the EVM client
+    - `raw_abi` : abi file as un-parsed json
+    - `bytecode`: Optional bytes
+    Returns an instance of Contract
+    """
+    if not isinstance(evm, PyEvm):
+        raise Exception("'evm' should be an instance of either PyEvmLocal or PyEvmFork")
+
+    if not isinstance(raw_abi, str):
+        raise Exception("expected a an un-parsed json file")
+
+    abi = PyAbi.from_abi_bytecode(raw_abi, bytecode)
+    return Contract(evm, abi)
+
+
+def contract_from_inline_abi(evm: PyEvm, abi: typing.List[str]) -> Contract:
+    """
+    Create the contract using inline ABI.
+    - `evm` : PyEvm.  the EVM client
+    - `abi` : a list of strings that describe the solidity functions of interest.
+    Returns an instance of Contract
+
+    Function are described in the format: 'function NAME(PARAMETER TYPES) (RETURN TYPES)'
+    where:
+    `NAME` if the function name
+    `PARAMETER TYPES are 0 or more solidity types of any arguments to the function
+    `RETURN TYPES are any expected returned solidity types.  If the function does not return
+    anything, this is not needed.
+
+    Examples:
+    - "function hello(uint256,uint256)`: hello function the expects 2 int arguments and returns nothing
+    - "function hello()(uint256)"`: hello function with no arguments and return an int
+
+    abi = ['function hello()(uint256)', 'function world(string) (string)']
+
+    """
+    if not isinstance(evm, PyEvm):
+        raise Exception("'evm' should be an instance of either PyEvmLocal or PyEvmFork")
+
+    abi = PyAbi.from_human_readable(abi)
+    return Contract(evm, abi)
