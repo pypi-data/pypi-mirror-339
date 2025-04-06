6ca45e98454df64961440741b917c0a416f3d695
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-06 03:17:02 +0000

    Add package shivs-per-lib/0.2/shivs_per_lib-0.2-py3-none-any.whl

---
+++ packages/shivs-per-lib/shivs_per_lib-0.2-py3-none-any.whl/shivs_per_lib/packages.py
@@ -0,0 +1,18 @@
+def addition(a,b):
+    return a+b 
+
+def subtraction(a,b):
+    return a-b 
+
+def str_to_int(given_list):
+    int_list = []
+    for i in given_list:
+        int_list.append(int(i))
+    return int_list
+
+
+def int_to_str(given_list):
+    str_list = []
+    for i in given_list:
+        str_list.append(str(i))
+    return str_list
