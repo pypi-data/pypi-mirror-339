7438a8598adeef7de3b8aceb136bb3517afd3b10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-06 03:17:07 +0000

    Add package dhn-med-py/2.0.6/dhn_med_py-2.0.6-cp311-cp311-macosx_11_0_arm64.whl

---
+++ packages/dhn-med-py/dhn_med_py-2.0.6-cp311-cp311-macosx_11_0_arm64.whl/dhn_med_py/med_file/__init__.py
@@ -0,0 +1,23 @@
+# This file is part of the DHN-MED-Py distribution.
+# Copyright (c) 2023 Dark Horse Neuro Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+#!/usr/bin/env python3
+
+#***********************************************************************//
+#******************  DARK HORSE NEURO MED Python API  ******************//
+#***********************************************************************//
+
+# Written by Matt Stead and Dan Crepeau
+# Copyright Dark Horse Neuro Inc, 2023
