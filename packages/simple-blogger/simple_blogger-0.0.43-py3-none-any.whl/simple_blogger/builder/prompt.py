7d42b804476ddf3003ca89cbc8fc2ecc5f5acf13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-06 15:04:25 +0000

    Add package simple-blogger/0.0.43/simple_blogger-0.0.43-py3-none-any.whl

---
+++ packages/simple-blogger/simple_blogger-0.0.43-py3-none-any.whl/simple_blogger/builder/prompt.py
@@ -0,0 +1,31 @@
+from __future__ import annotations 
+import simple_blogger.builder.content
+from abc import ABC, abstractmethod
+
+class IPromptBuilder(ABC):
+    @abstractmethod
+    def build(self, *_, **__)->str:
+        """ Prompt builder method """
+
+class IdentityPromptBuilder(IPromptBuilder):
+    def __init__(self, prompt:str):
+        self.prompt = prompt
+    
+    def build(self, *_, **__):
+        return self.prompt
+    
+class TaskPromptBuilder(IPromptBuilder):
+    def __init__(self, task_builder, prompt_constructor):
+       self.task_builder=task_builder
+       self.prompt_constructor=prompt_constructor
+
+    def build(self, *_, **__):
+        task = self.task_builder.build()
+        return task and self.prompt_constructor(task=task) 
+
+class ContentBuilderPromptBuilder(IPromptBuilder):
+    def __init__(self, content_builder: simple_blogger.builder.content.IContentBuilder):
+        self.content_builder=content_builder
+    
+    def build(self):
+        return (content:=self.content_builder.build()) and content.get_file().read()
