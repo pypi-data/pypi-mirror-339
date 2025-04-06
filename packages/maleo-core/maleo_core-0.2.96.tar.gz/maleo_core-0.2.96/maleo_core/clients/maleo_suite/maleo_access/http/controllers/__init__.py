f6f88db7bb5f72687b404e3a7939c690d34fbb2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-06 04:03:44 +0000

    Add package maleo-core/0.2.96/maleo_core-0.2.96.tar.gz

---
+++ packages/maleo-core/maleo_core-0.2.96.tar.gz/maleo_core-0.2.96/maleo_core/clients/maleo_suite/maleo_access/http/controllers/__init__.py
@@ -0,0 +1,18 @@
+from .blood_type import MaleoAccessBloodTypeHTTPController
+from .gender import MaleoAccessGenderHTTPController
+from .organization_role import MaleoAccessOrganizationRoleHTTPController
+from .organization_type import MaleoAccessOrganizationTypeHTTPController
+from .organization import MaleoAccessOrganizationHTTPController
+from .system_role import MaleoAccessSystemRoleHTTPController
+from .user_type import MaleoAccessUserTypeHTTPController
+from .user import MaleoAccessUserHTTPController
+
+class MaleoAccessHTTPControllers:
+    BloodType = MaleoAccessBloodTypeHTTPController
+    Gender = MaleoAccessGenderHTTPController
+    OrganizationRole = MaleoAccessOrganizationRoleHTTPController
+    OrganizationType = MaleoAccessOrganizationTypeHTTPController
+    Organization = MaleoAccessOrganizationHTTPController
+    SystemRole = MaleoAccessSystemRoleHTTPController
+    UserType = MaleoAccessUserTypeHTTPController
+    User = MaleoAccessUserHTTPController
---
+++ packages/maleo-core/maleo_core-0.2.96.tar.gz/maleo_core-0.2.96/maleo_core/models/maleo_suite/maleo_access/transfers/parameters/client/organization_role.py
@@ -0,0 +1,6 @@
+from __future__ import annotations
+from maleo_core.models.base.transfers.parameters.client import BaseClientParameters
+
+class MaleoAccessOrganizationRoleClientParameters:
+    Get = BaseClientParameters.Get
+    GetQuery = BaseClientParameters.GetQuery
