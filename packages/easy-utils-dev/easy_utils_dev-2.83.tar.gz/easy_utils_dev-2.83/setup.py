abbabfc32ff582fb735ac9de39ed7eba678f294e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-06 05:03:32 +0000

    Add package easy-utils-dev/2.83/easy_utils_dev-2.83.tar.gz

---
+++ packages/easy-utils-dev/easy_utils_dev-2.83.tar.gz/easy_utils_dev-2.83/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+VERSION = '2.83'
+
+# Setting up
+setup(
+    name="easy_utils_dev",
+    version=VERSION,
+    packages=find_packages(),
+    install_requires=[
+            'psutil' ,
+            'ping3' , 
+            'flask' , 
+            'flask_cors' , 
+            'xmltodict' , 
+            'paramiko' ,
+            'oracledb' ,
+            'requests',
+            'flask_socketio',
+            'python-dotenv',
+            'gevent',
+            'pyzipper'
+        ],
+    keywords=['python3'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ]
+)
