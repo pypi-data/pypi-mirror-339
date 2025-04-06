032224f112c7b6a5cf755b2f847e63cd3ae227e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-06 04:04:27 +0000

    Add package grafi/0.0.8/grafi-0.0.8-py3-none-any.whl

---
+++ packages/grafi/grafi-0.0.8-py3-none-any.whl/grafi/common/models/default_id.py
@@ -0,0 +1,5 @@
+import uuid
+
+from pydantic import Field
+
+default_id = Field(default_factory=lambda: uuid.uuid4().hex)
---
+++ packages/grafi/grafi-0.0.8-py3-none-any.whl/grafi/tools/llms/llm_response_command.py
@@ -0,0 +1,39 @@
+from typing import Any, AsyncGenerator, List
+
+from pydantic import Field
+
+from grafi.common.models.command import Command
+from grafi.common.models.execution_context import ExecutionContext
+from grafi.common.models.message import Message
+from grafi.tools.llms.llm import LLM
+
+
+class LLMResponseCommand(Command):
+    llm: LLM = Field(default=None)
+
+    class Builder(Command.Builder):
+        """Concrete builder for LLMResponseCommand."""
+
+        def __init__(self):
+            self._command = self._init_command()
+
+        def _init_command(self) -> "LLMResponseCommand":
+            return LLMResponseCommand()
+
+        def llm(self, llm: LLM) -> "LLMResponseCommand.Builder":
+            self._command.llm = llm
+            return self
+
+    def execute(
+        self, execution_context: ExecutionContext, input_data: List[Message]
+    ) -> Message:
+        return self.llm.execute(execution_context, input_data)
+
+    async def a_execute(
+        self, execution_context: ExecutionContext, input_data: List[Message]
+    ) -> AsyncGenerator[Message, None]:
+        async for message in self.llm.a_execute(execution_context, input_data):
+            yield message
+
+    def to_dict(self) -> dict[str, Any]:
+        return {"llm": self.llm.to_dict()}
