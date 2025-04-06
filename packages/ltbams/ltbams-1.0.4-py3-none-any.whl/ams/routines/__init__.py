8abe3093c9a90aa296e39a8268b9c84a8f97f161
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-06 14:03:34 +0000

    Add package ltbams/1.0.4/ltbams-1.0.4-py3-none-any.whl

---
+++ packages/ltbams/ltbams-1.0.4-py3-none-any.whl/ams/routines/__init__.py
@@ -0,0 +1,23 @@
+"""
+Scheduling routines.
+"""
+
+from collections import OrderedDict
+from andes.utils.func import list_flatten
+
+all_routines = OrderedDict([
+    ('dcpf', ['DCPF']),
+    ('pflow', ['PFlow']),
+    ('cpf', ['CPF']),
+    ('acopf', ['ACOPF']),
+    ('dcopf', ['DCOPF']),
+    ('ed', ['ED', 'EDDG', 'EDES']),
+    ('rted', ['RTED', 'RTEDDG', 'RTEDES', 'RTEDVIS']),
+    ('uc', ['UC', 'UCDG', 'UCES']),
+    ('dopf', ['DOPF', 'DOPFVIS']),
+    ('pflow0', ['PFlow0']),
+    ('dcpf0', ['DCPF0']),
+])
+
+class_names = list_flatten(list(all_routines.values()))
+routine_cli = OrderedDict([(item.lower(), item) for item in class_names])
