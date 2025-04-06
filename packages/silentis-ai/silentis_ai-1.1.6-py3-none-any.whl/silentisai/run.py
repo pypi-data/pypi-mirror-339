895baa1ff069e57e8413525c348d740b8095039a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-06 05:03:33 +0000

    Add package silentis-ai/1.1.6/silentis_ai-1.1.6-py3-none-any.whl

---
+++ packages/silentis-ai/silentis_ai-1.1.6-py3-none-any.whl/silentisai/run.py
@@ -0,0 +1,23 @@
+import sys
+from silentisai.silentis import Silentis
+
+# Silentis AI - Python Plugin
+# Developed by: Silentis Team
+# MIT License | Version 1.0
+
+# Documentation: https://silentis.ai
+# Website: https://silentis.ai
+# Github: https://github.com/Silentisai
+
+# X: https://x.com/silentisproject
+# Telegram: https://t.me/SilentisAi
+
+# Support our mission: https://springboard.pancakeswap.finance/bsc/token/0x8a87562947422db0eb3070a5a1ac773c7a8d64e7
+
+def main():
+    plugin = Silentis()
+    plugin.run()
+
+
+if __name__ == "__main__":
+    main()
