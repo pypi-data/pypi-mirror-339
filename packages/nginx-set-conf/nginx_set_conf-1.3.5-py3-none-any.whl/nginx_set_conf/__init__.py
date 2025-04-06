da06f4945ea1e0800f948aa972491d6930ecb916
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-06 15:04:12 +0000

    Add package nginx-set-conf/1.3.5/nginx_set_conf-1.3.5-py3-none-any.whl

---
+++ packages/nginx-set-conf/nginx_set_conf-1.3.5-py3-none-any.whl/nginx_set_conf/__init__.py
@@ -0,0 +1,43 @@
+"""
+nginx-set-conf - Ein Werkzeug zur Verwaltung von Nginx-Konfigurationen
+"""
+
+__version__ = '1.3.5'
+
+from . import config_templates, utils
+
+def replace_cache_path(template, service_name):
+    """Replace the cache path in a template with a unique path based on service name.
+    Also replace the zone name to be unique for each service.
+    Also replace the limit_req_zone name to be unique per service.
+    
+    Args:
+        template (str): The nginx config template
+        service_name (str): Name of the service for unique path and zone
+        
+    Returns:
+        str: Template with updated cache path and zone name
+    """
+    # Create unique names based on the service
+    cache_zone_name = f"{service_name}_cache"
+    limit_zone_name = f"{service_name}_limit"
+    
+    # First replace the cache path
+    updated_template = template.replace(
+        'proxy_cache_path /tmp', 
+        f'proxy_cache_path /var/cache/nginx/{service_name}'
+    )
+    
+    # Then replace the cache zone name
+    updated_template = updated_template.replace(
+        'keys_zone=my_cache:',
+        f'keys_zone={cache_zone_name}:'
+    )
+    
+    # Finally replace the limit_req_zone name
+    updated_template = updated_template.replace(
+        'zone=iprl:',
+        f'zone={limit_zone_name}:'
+    )
+    
+    return updated_template
