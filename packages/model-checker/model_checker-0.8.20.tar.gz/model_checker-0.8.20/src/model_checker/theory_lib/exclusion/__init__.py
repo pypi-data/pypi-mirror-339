ffc7cf7277ae3155c0bf72ee62574610be24031b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-06 04:04:05 +0000

    Add package model-checker/0.8.20/model_checker-0.8.20.tar.gz

---
+++ packages/model-checker/model_checker-0.8.20.tar.gz/model_checker-0.8.20/src/model_checker/theory_lib/exclusion/__init__.py
@@ -0,0 +1,21 @@
+__version__ = "unknown"
+
+
+# Define the public API of the package
+__all__ = [
+    "ExclusionSemantics", "UnilateralProposition", "ExclusionStructure",  # semantic
+    "exclusion_operators",  # operators
+]
+
+# Import specific items from semantic
+from .semantic import (
+    ExclusionSemantics,
+    UnilateralProposition,
+    ExclusionStructure,
+)
+
+# Import all operators
+from .operators import exclusion_operators
+
+# NOTE: this will cause circular imports
+# from .examples import example_range
