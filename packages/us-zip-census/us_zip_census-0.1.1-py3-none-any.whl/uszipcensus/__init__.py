3d3667e55532580c2a97164c36cc33846a8e3799
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-06 04:03:32 +0000

    Add package us-zip-census/0.1.1/us_zip_census-0.1.1-py3-none-any.whl

---
+++ packages/us-zip-census/us_zip_census-0.1.1-py3-none-any.whl/uszipcensus/__init__.py
@@ -0,0 +1,6 @@
+"""Module providing a function mapping for convenience on package import."""
+
+from .main import UsZipCensus
+
+zip_to_division = UsZipCensus.zip_to_division
+zip_to_region = UsZipCensus.zip_to_region
