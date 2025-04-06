e2c7d3467807ea5830af3846b363fc6a088ae9cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-06 05:03:37 +0000

    Add package utils-me/0.1.3.11/utils_me-0.1.3.11-py3-none-any.whl

---
+++ packages/utils-me/utils_me-0.1.3.11-py3-none-any.whl/utilsme/compression.py
@@ -0,0 +1,17 @@
+from utilsme import timing
+
+@timing
+def zip_files(file_paths, output_zip_path):
+    pass
+
+@timing
+def unzip_file(zip_path, extract_to):
+    pass
+
+@timing
+def compress_file(file_path, output_path):
+    pass
+
+@timing
+def decompress_file(compressed_path, output_path):
+    pass
