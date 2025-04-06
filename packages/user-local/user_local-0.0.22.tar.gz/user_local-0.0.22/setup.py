855dd5c13674855a3750958f690ebf754b3d42f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-06 03:17:11 +0000

    Add package user-local/0.0.22/user_local-0.0.22.tar.gz

---
+++ packages/user-local/user_local-0.0.22.tar.gz/user_local-0.0.22/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+PACKAGE_NAME = "user-local"
+package_dir = PACKAGE_NAME.replace("-", "_")
+
+# used by python -m build
+# python -m build needs pyproject.toml or setup.py
+setuptools.setup(
+    name=PACKAGE_NAME,
+    version="0.0.22",  # https://pypi.org/project/user-local/
+    author="Circles",
+    author_email="info@circlez.ai",
+    description="PyPI Package for Circles Python user Local Python",
+    long_description="This is a package for user local python",
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/circles-zone/{PACKAGE_NAME}-python-package",
+    packages=[package_dir],
+    package_dir={package_dir: f"{package_dir}/src"},
+    package_data={package_dir: ["*.py"]},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: Other/Proprietary License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "database-mysql-local>=0.0.451",
+        "logger-local>=0.0.46",
+        "database-infrastructure-local>=0.0.9",
+        "email-address-local",
+        "location-local",
+    ],
+)
---
+++ packages/user-local/user_local-0.0.22.tar.gz/user_local-0.0.22/user_local/src/user_local.py
@@ -0,0 +1,125 @@
+from random import randint
+from typing import Tuple
+from database_infrastructure_local.number_generator import NumberGenerator
+from database_mysql_local.generic_crud import GenericCRUD
+from email_address_local.email_address import EmailAddressesLocal
+from location_local.locations_local_crud import LocationsLocal
+from logger_local.MetaLogger import MetaLogger
+
+from .constants import user_local_python_code_logger_object
+
+# TODO Adding is_same_user() which override people.is_same_enity() checking username only
+
+
+class UsersLocal(
+    GenericCRUD, metaclass=MetaLogger, object=user_local_python_code_logger_object
+):
+    def __init__(self, is_test_data: bool = False):
+        super().__init__(
+            default_schema_name="user",
+            default_view_table_name="user_not_deleted_view",
+            default_table_name="user_table",
+            default_column_name="user_id",
+            is_test_data=is_test_data,
+        )
+
+    # When inserting a deleted entity (all entities except person and tables of codes i.e. country, state, county, city, neighbourhood, street ...) and table_definition.insert_is_undelete=false, we should null all the unique fields of the deleted entity- if_same_even_deleted_return_existing_id=true  # noqa
+    def insert(
+        self,
+        *,
+        username: str,
+        main_email_address: str,  # noqa
+        first_name: str,
+        last_name: str,
+        location_id: int,
+        is_test_data: bool = False
+    ) -> int:
+        """Returns user_id (int) of the inserted user record"""
+        number = NumberGenerator.get_random_number("user", "user_table", "user_id")
+        # TODO develop GenericCRUD.__fix_data_dict() and then remove the `` from user.main_email_address
+        data_dict = {
+            "number": number,
+            "username": username,
+            "`user.main_email_address`": main_email_address,
+            "first_name": first_name,
+            "last_name": last_name,
+            "active_location_id": location_id,
+            "is_test_data": is_test_data,
+        }
+        user_id = super().insert(data_dict=data_dict)
+        return user_id
+
+    def update_by_user_id(
+        self,
+        user_id: int,
+        username: str = None,
+        main_email_address: str = None,
+        first_name: str = None,
+        last_name: str = None,
+        active_location_id: int = None,
+    ) -> int:
+        """Updates the user record with the given user_id with the given values"""
+        data_dict = {
+            k: v
+            for k, v in locals().items()
+            if k not in ("__class__", "self") and v is not None
+        }
+        assert len(data_dict) > 1, "At least one of the fields must be updated"
+        super().update_by_column_and_value(
+            column_name="user_id", column_value=user_id, data_dict=data_dict
+        )
+
+    def read_user_tuple_by_user_id(
+        self, user_id: int
+    ) -> Tuple[int, int, str, str, str, str, int]:
+        """
+        Returns a tuple of (username, user.main_email_address, first_name, last_name, active_location_id)
+        """
+        user_tuple = self.select_one_tuple_by_column_and_value(
+            select_clause_value=" username, `user.main_email_address`, first_name, last_name, active_location_id",
+            column_value=user_id,
+        )
+        return user_tuple
+
+    def read_user_dict_by_user_id(self, user_id: int) -> dict:
+        """
+        Returns a dict of user with (number, username, user.main_email_address, first_name, last_name, active_location_id)
+        """
+        user_dict = self.select_one_dict_by_column_and_value(
+            select_clause_value="number, username, `user.main_email_address`, first_name, last_name, active_location_id",
+            column_value=user_id,
+        )
+        if not user_dict:
+            user_dict = self.select_one_dict_by_column_and_value(
+                view_table_name="user_to_be_reviewed_view",
+                select_clause_value="number, username, `user.main_email_address`, first_name, last_name, active_location_id",
+                column_value=user_id,
+            )
+        return user_dict
+
+    def delete_by_user_id(self, user_id: int) -> int:
+        """
+        Updates the user end_timestamp with the given user_id
+        """
+        deleted_row = self.delete_by_column_and_value(id_column_value=user_id)
+        return deleted_row
+
+    def get_test_user_id(self):
+        username = "test username " + str(randint(1, 100000))
+        test_email_address = EmailAddressesLocal.get_test_email_address()
+        first_name = "test first name " + str(randint(1, 100000))
+        last_name = "test last name " + str(randint(1, 100000))
+        test_location_id = LocationsLocal().get_test_location_id()
+        insert_kwargs = {
+            "username": username,
+            "main_email_address": test_email_address,
+            "first_name": first_name,
+            "last_name": last_name,
+            "location_id": test_location_id,
+        }
+
+        return_result = self.get_test_entity_id(
+            entity_name="user", insert_function=self.insert, insert_kwargs=insert_kwargs
+        )
+
+        return return_result
