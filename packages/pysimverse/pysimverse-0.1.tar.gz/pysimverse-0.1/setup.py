e6ca7acf66df0c4136abe01c3eb2ef7975ff2fc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-06 04:03:26 +0000

    Add package pysimverse/0.1/pysimverse-0.1.tar.gz

---
+++ packages/pysimverse/pysimverse-0.1.tar.gz/pysimverse-0.1/setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+
+setup(
+    name='pysimverse',
+    packages=['pysimverse'],
+    version='0.1',
+    license='MIT',
+    description='Python Computer Vision and Robotics Simulator',
+    author='Computer Vision Zone',
+    author_email='contact@computervision.zone',
+    url='https://github.com/cvzone/pysimverse.git',
+    keywords=['ComputerVision', 'Simulator', 'Robotics', 'Drone'],
+    install_requires=[
+        'pyzmq',
+        'opencv-python',
+    ],
+    python_requires='>=3.7',  # Requires any version >= 3.7
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'Programming Language :: Python :: 3',
+    ],
+)
