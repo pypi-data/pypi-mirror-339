952e9238af2fe8e80ec69422b7b5883808079e3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-06 05:03:22 +0000

    Add package orshot/0.1.0/orshot-0.1.0.tar.gz

---
+++ packages/orshot/orshot-0.1.0.tar.gz/orshot-0.1.0/src/orshot/constants.py
@@ -0,0 +1,6 @@
+ORSHOT_SOURCE = "orshot-python-sdk";
+ORSHOT_API_BASE_URL = "https://api.orshot.com";
+ORSHOT_API_VERSION = "v1";
+DEFAULT_RESPONSE_TYPE = "base64";
+DEFAULT_RESPONSE_FORMAT = "png";
+DEFAULT_RENDER_TYPE = "images";
