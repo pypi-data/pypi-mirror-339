08b0b88714c4c684919d63bd44a5887de3b82ce6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-06 04:03:42 +0000

    Add package logdetective/0.5.8/logdetective-0.5.8.tar.gz

---
+++ packages/logdetective/logdetective-0.5.8.tar.gz/logdetective-0.5.8/logdetective/server/utils.py
@@ -0,0 +1,45 @@
+import logging
+import yaml
+from logdetective.server.models import Config
+
+
+def load_server_config(path: str | None) -> Config:
+    """Load configuration file for logdetective server.
+    If no path was provided, or if the file doesn't exist, return defaults.
+    """
+    if path is not None:
+        try:
+            with open(path, "r") as config_file:
+                return Config(yaml.safe_load(config_file))
+        except FileNotFoundError:
+            # This is not an error, we will fall back to default
+            print("Unable to find server config file, using default then.")
+    return Config()
+
+
+def get_log(config: Config):
+    """
+    Initialize a logger for this server
+    """
+    log = logging.getLogger(config.log.name)
+    if getattr(log, "initialized", False):
+        return log
+
+    log.setLevel(config.log.level)
+
+    # Drop the default handler, we will create it ourselves
+    log.handlers = []
+
+    # STDOUT
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(logging.Formatter(config.log.format))
+    log.addHandler(stream_handler)
+
+    # Log to file
+    if config.log.path:
+        file_handler = logging.FileHandler(config.log.path)
+        file_handler.setFormatter(logging.Formatter(config.log.format))
+        log.addHandler(file_handler)
+
+    log.initialized = True
+    return log
