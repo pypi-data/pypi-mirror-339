c413897e6ab2c20e9b994dfb28f72ccba13bacf8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-06 05:03:53 +0000

    Add package InfinityMath/0.0.7/InfinityMath-0.0.7.tar.gz

---
+++ packages/InfinityMath/InfinityMath-0.0.7.tar.gz/InfinityMath-0.0.7/InfinityMath/__init__.py
@@ -0,0 +1,9 @@
+from . import algebra
+from . import trigonometry
+from . import calculus
+from . import quadratics_math
+from . import graphics
+
+__version__ = "0.0.7"
+def show_version():
+    print(f"InfinityMath version: {__version__}")
