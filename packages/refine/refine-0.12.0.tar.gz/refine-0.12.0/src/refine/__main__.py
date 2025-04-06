13c528bb15c17a18af073e5c14e09a682bc34f14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-06 15:04:06 +0000

    Add package refine/0.12.0/refine-0.12.0.tar.gz

---
+++ packages/refine/refine-0.12.0.tar.gz/refine-0.12.0/src/refine/__main__.py
@@ -0,0 +1,209 @@
+"""
+Utility to simplify rewriting python code.
+
+It can be used for one-off rewrites, or, to maintain code styling rules.
+"""
+
+from __future__ import annotations
+
+import argparse
+import logging
+import pathlib
+import sys
+from multiprocessing import freeze_support
+from typing import NoReturn
+
+import msgspec.structs
+import py_walk
+
+from refine import __version__
+from refine.config import Config
+from refine.exc import RefineSystemExit
+from refine.processor import Processor
+from refine.registry import Registry
+
+logging.basicConfig(level=logging.INFO, stream=sys.stderr, format="%(message)s")
+
+log = logging.getLogger(__name__)
+
+
+def main() -> NoReturn:  # noqa: PLR0915,C901
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument("--version", action="version", version=__version__)
+    parser.add_argument("files", metavar="FILE", nargs="*", type=pathlib.Path, help="One or more files to process.")
+    parser.add_argument(
+        "--config",
+        type=pathlib.Path,
+        help="Path to config file. Defaults to '%(default)s' on the current directory.",
+        default=".refine.toml",
+    )
+    parser.add_argument(
+        "--respect-gitignore",
+        "--rgi",
+        action="store_true",
+        default=False,
+        help="Respect .gitignore files when searching for files to process.",
+    )
+    parser.add_argument("--quiet", "-q", action="store_true", default=False, help="Quiet down the tool output.")
+    parser.add_argument(
+        "--fail-fast",
+        "--ff",
+        action="store_true",
+        default=False,
+        help="Exit as soon as possible on the first processing error",
+    )
+    parser.add_argument(
+        "--list-codemods",
+        "--list",
+        action="store_true",
+        help="List all available codemods",
+    )
+    parser.add_argument(
+        "--exclude-codemod",
+        "--exclude",
+        default=[],
+        action="append",
+        help="Exclude codemods from available codemods.",
+    )
+    parser.add_argument(
+        "--select-codemod",
+        "--select",
+        default=[],
+        action="append",
+        help="Explicitly select codemod names from available codemods.",
+    )
+    parser.add_argument(
+        "--codemods-path",
+        type=pathlib.Path,
+        action="append",
+        default=[],
+        dest="codemod_paths",
+        help="Path to a codemods directory. Can be passed multiple times.",
+    )
+    args = parser.parse_args()
+    if args.quiet:
+        logging.getLogger().setLevel(logging.ERROR)
+    if not args.config.is_absolute():
+        config_file = pathlib.Path.cwd().joinpath(args.config).resolve()
+    else:
+        config_file = args.config
+    if config_file.exists():
+        if config_file.name == "pyproject.toml":
+            config = Config.from_pyproject_file(config_file)
+        else:
+            config = Config.from_default_file(config_file)
+    elif pathlib.Path.cwd().joinpath("pyproject.toml").exists():
+        config = Config.from_pyproject_file(pathlib.Path.cwd().joinpath("pyproject.toml"))
+    else:
+        config = Config()
+
+    if args.list_codemods:
+        # Add the additional CLI passed codemod paths
+        config.codemod_paths[:] = list(set(config.codemod_paths) | set(args.codemod_paths))
+
+    registry = Registry()
+    registry.load(config.codemod_paths)
+
+    available_codemods = {codemod.NAME: codemod.get_short_description() for codemod in registry.codemods()}
+
+    if args.select_codemod:
+        # Add any additional CLI passed selections
+        config.select[:] = list(set(config.select) | set(args.select_codemod))
+        bad_codemods = set(config.select) - set(available_codemods)
+        if bad_codemods:
+            log.error("Invalid codemods selected: %s", ", ".join(bad_codemods))
+            parser.exit(status=1)
+
+    if args.exclude_codemod:
+        # Add any additional CLI passed exclusions
+        config.exclude[:] = list(set(config.exclude) | set(args.exclude_codemod))
+        bad_codemods = set(config.exclude) - set(available_codemods)
+        if bad_codemods:
+            log.error("Invalid codemods excluded: %s", ", ".join(bad_codemods))
+            parser.exit(status=1)
+
+    if args.fail_fast:
+        config = msgspec.structs.replace(config, fail_fast=True)
+
+    if args.list_codemods:
+        log.info("Available codemods:")
+        for name, description in sorted(available_codemods.items()):
+            # In case the description is comming from the docstring, we just really want the first line.
+            log.info(" - %s: %s", name, description.strip())
+        parser.exit()
+
+    repo_root: pathlib.Path = pathlib.Path(config.repo_root)
+    paths: list[pathlib.Path] = args.files
+    if not paths:
+        paths.append(repo_root)
+
+    ignore_patterns: list[str] = config.exclude_patterns
+    gitignore_file = repo_root / ".gitignore"
+    respect_gitignore: bool = args.respect_gitignore or config.respect_gitignore
+    if respect_gitignore and gitignore_file.exists():
+        ignore_patterns.extend(
+            pattern for pattern in gitignore_file.read_text().splitlines() if pattern and not pattern.startswith("#")
+        )
+
+    files: list[pathlib.Path] = []
+    for path in paths:
+        if path.is_file():
+            if _append_path(path, repo_root=repo_root, files=files) is False:
+                parser.exit(status=1)
+            continue
+        for subpath in py_walk.walk(path, match=["*.py"], mode="only-files", ignore=ignore_patterns):
+            if _append_path(subpath, repo_root=repo_root, files=files) is False:
+                parser.exit(status=1)
+
+    codemods = list(registry.codemods(select_codemods=config.select, exclude_codemods=config.exclude))
+    if not codemods:
+        log.error("No codemods selected. Exiting.")
+        parser.exit(status=1)
+
+    log.info("Selected codemods:")
+    for codemod in codemods:
+        log.info(" - %s: %s", codemod.NAME, codemod.get_short_description())
+
+    processor = Processor(config=config, registry=registry, codemods=codemods)
+    try:
+        result = processor.process(files)
+        if result.failures:
+            parser.exit(status=1)
+    except RefineSystemExit as exc:
+        parser.exit(status=exc.code, message=exc.message)
+    except SystemExit as exc:
+        code: str | int | None = exc.code
+        if code is None:
+            parser.exit(status=1)
+        if isinstance(code, int):
+            parser.exit(status=code)
+        parser.exit(status=1, message=code)
+    except KeyboardInterrupt:
+        parser.exit(status=1)
+    parser.exit(status=0)
+
+
+def _append_path(path: pathlib.Path, repo_root: pathlib.Path, files: list[pathlib.Path]) -> bool:
+    """
+    Append a path to the list of paths if it is not already present.
+    """
+    resolved_path: pathlib.Path = path.resolve()
+    try:
+        # Check if the file is inside the repo root
+        resolved_path.relative_to(repo_root)
+    except ValueError:
+        log.error(  # noqa: TRY400
+            "File %s is not inside the repo root %s",
+            path,
+            repo_root,
+        )
+        return False
+    else:
+        if resolved_path not in files:
+            files.append(resolved_path)
+        return True
+
+
+if __name__ == "__main__":
+    freeze_support()
+    main()
---
+++ packages/refine/refine-0.12.0.tar.gz/refine-0.12.0/src/refine/abc.py
@@ -0,0 +1,78 @@
+"""
+Abstract base classes for defining codemod types and their configurations.
+"""
+
+from __future__ import annotations
+
+import fnmatch
+from abc import ABC
+from typing import TYPE_CHECKING
+from typing import ClassVar
+from typing import Generic
+from typing import TypeVar
+
+import msgspec
+from libcst.codemod import CodemodContext
+from libcst.codemod import SkipFile
+from libcst.codemod import VisitorBasedCodemodCommand
+
+
+class BaseConfig(msgspec.Struct, kw_only=True, frozen=True, forbid_unknown_fields=True):
+    """
+    Base configuration class for codemoders.
+    """
+
+    exclude: list[str] = msgspec.field(default_factory=list)
+    """
+    List of glob patterns to exclude paths from being processed.
+    """
+
+
+CodemodConfigType = TypeVar("CodemodConfigType", bound=BaseConfig)
+
+
+class BaseCodemod(VisitorBasedCodemodCommand, ABC, Generic[CodemodConfigType]):
+    """
+    Base class for codemoders.
+    """
+
+    NAME: ClassVar[str]
+    CONFIG_CLS: ClassVar[type[BaseConfig]]
+    PRIORITY: ClassVar[int] = 0
+
+    def __init__(self, context: CodemodContext, config: CodemodConfigType):
+        for pattern in config.exclude:
+            if TYPE_CHECKING:
+                assert context.filename is not None
+            if fnmatch.fnmatch(context.filename, pattern):
+                error_msg = f"Excluded by pattern '{pattern}' defined in the '{self.NAME}' codemod configuration ."
+                raise SkipFile(error_msg)
+        super().__init__(context)
+        self.config = config
+        self.__post_codemod_init__()
+
+    def __post_codemod_init__(self) -> None:
+        """
+        This method can implement additional codemod initialization.
+        """
+
+    @classmethod
+    def get_short_description(cls) -> str:
+        """
+        Return a short description of the codemod.
+
+        This short description is used in the CLI to list available codemods and should be a single line.
+
+        By default, it returns the first line of the class docstring, override this method to provide a
+        custom description.
+        """
+        doc = cls.__doc__
+        if doc is None:
+            error_msg = f"Codemod {cls.__name__} must have a docstring to be used in the CLI."
+            raise TypeError(error_msg)
+        if TYPE_CHECKING:
+            assert doc is not None
+        return doc.strip().splitlines()[0].strip()
+
+
+CodemodType = TypeVar("CodemodType", bound=BaseCodemod)
