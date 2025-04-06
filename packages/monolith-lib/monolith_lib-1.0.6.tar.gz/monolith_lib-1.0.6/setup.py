d1774a02a8baef9d013bd8fbe48e04db1691ac77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-06 03:16:57 +0000

    Add package monolith-lib/1.0.6/monolith_lib-1.0.6.tar.gz

---
+++ packages/monolith-lib/monolith_lib-1.0.6.tar.gz/monolith_lib-1.0.6/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="monolith_lib",
+    version="1.0.6",
+    author="Du Mingzhe",
+    author_email="dumingzhex@gmail.com",
+    description="A code execution engine",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url="https://github.com/Elfsong/Monolith",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
