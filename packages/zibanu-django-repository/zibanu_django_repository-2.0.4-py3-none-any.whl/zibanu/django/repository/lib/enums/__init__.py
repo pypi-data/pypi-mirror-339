86707b58f79f0cbb8a2929322a72ace7a6dc223a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-06 14:03:45 +0000

    Add package zibanu-django-repository/2.0.4/zibanu_django_repository-2.0.4-py3-none-any.whl

---
+++ packages/zibanu-django-repository/zibanu_django_repository-2.0.4-py3-none-any.whl/zibanu/django/repository/lib/enums/__init__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+#  Developed by CQ Inversiones SAS. Copyright ©. 2019 - 2025. All rights reserved.
+#  Desarrollado por CQ Inversiones SAS. Copyright ©. 2019 - 2025. Todos los derechos reservados.
+
+# ****************************************************************
+# IDE:          PyCharm
+# Developed by: macercha
+# Date:         17/02/25
+# Project:      Zibanu Django
+# Module Name:  __init__.py
+# Description:
+# ****************************************************************
+from .access_type import AccessTypeEnum
+
+__all__ = [
+    "AccessTypeEnum",
+]
