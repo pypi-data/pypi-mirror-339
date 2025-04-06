e966aa9e13092514465d067393740c74ea1e404a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-06 04:03:31 +0000

    Add package fastgear/0.1.3/fastgear-0.1.3-py3-none-any.whl

---
+++ packages/fastgear/fastgear-0.1.3-py3-none-any.whl/fastgear/constants/regex_expressions.py
@@ -0,0 +1,8 @@
+# Regular expression to match an ORDER BY clause with 'ASC' or 'DESC'.
+REGEX_ORDER_BY_QUERY = "(ASC|DESC)$"
+
+# Regular expression to match any character (zero or more times).
+REGEX_ANY_CHAR = ".*"
+
+# Regular expression to match logical operators 'AND' or 'OR'.
+REGEX_OPERATOR = "(AND|OR)$"
