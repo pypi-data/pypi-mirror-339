84cf2af2c1ff2b4f30797497b58ff757d941c47e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-06 04:03:26 +0000

    Add package EcoStock/1.5/ecostock-1.5.tar.gz

---
+++ packages/EcoStock/ecostock-1.5.tar.gz/ecostock-1.5/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='EcoStock',
+    version='1.5',
+    author="Antonio Paparo, Giovanni Paparo",
+    author_email="antoniopaparo@outlook.com", 
+    description='A Python package designed for finance professionals and economists',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Tonij10/EcoStock",
+    packages=find_packages(),
+    install_requires=[
+        'fastapi',
+        'argparse',
+        'uvicorn',
+        'gunicorn',
+        'pandas',
+        'numpy',
+        'matplotlib',
+        'yfinance',
+        'plotly',
+        'requests',
+        'seaborn',
+        'statsmodels',
+        'nbformat',
+        'scikit-learn',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.10',
+)
