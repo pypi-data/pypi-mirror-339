da1099b59734781a7897a3e0ec17eb11535a2dd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-06 03:17:04 +0000

    Add package pytusk/0.0.2/pytusk-0.0.2-py3-none-any.whl

---
+++ packages/pytusk/pytusk-0.0.2-py3-none-any.whl/bin/package-publish.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Copyright (c) Frank V. Castellucci
+# License: Apache-2.0
+
+# Setup target delivery
+
+base_dir=${PWD##*/}
+if test "$base_dir" = "pytusk";
+then
+    echo "Publishing to PyPi"
+    tout=$(twine check "dist/*")
+    if echo $tout | grep -q "PASSED"; then
+        echo "Valid build... uploading to pypi"
+        tout=$(twine upload "dist/*")
+        if echo $tout | grep -q "ERROR"; then
+            echo $tout
+            echo "Publish failed. Fix errors and rerun"
+            exit -1
+        else
+            echo "Upload passed!"
+            exit 0
+        fi
+    fi
+else
+    echo "Command must run from pytusk folder."
+fi
