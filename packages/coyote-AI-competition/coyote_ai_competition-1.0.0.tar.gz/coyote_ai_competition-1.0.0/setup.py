c08c423635b3c81be3d4e55a5ed4953bea020b0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-06 03:17:07 +0000

    Add package coyote-AI-competition/1.0.0/coyote_ai_competition-1.0.0.tar.gz

---
+++ packages/coyote-AI-competition/coyote_ai_competition-1.0.0.tar.gz/coyote_ai_competition-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='coyote_game',
+    version='0.1.0',
+    description='コヨーテボードゲームの進行管理ライブラリ',
+    author='Your Name',
+    packages=find_packages(),
+    install_requires=[
+        # 依存ライブラリがあれば(おそらくない？)
+    ],
+)
