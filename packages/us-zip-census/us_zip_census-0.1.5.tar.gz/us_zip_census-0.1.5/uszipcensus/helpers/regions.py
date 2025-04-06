0611f1d06a5efb970e9ba11f176a0d12f573305e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-06 04:03:55 +0000

    Add package us-zip-census/0.1.5/us_zip_census-0.1.5.tar.gz

---
+++ packages/us-zip-census/us_zip_census-0.1.5.tar.gz/us_zip_census-0.1.5/uszipcensus/helpers/regions.py
@@ -0,0 +1,14 @@
+"""Module providing census division to census region mapping."""
+
+division_to_region = {
+    "New England": "Northeast",
+    "Middle Atlantic": "Northeast",
+    "East North Central": "Midwest",
+    "West North Central": "Midwest",
+    "South Atlantic": "South",
+    "East South Central": "South",
+    "West South Central": "South",
+    "Mountain": "West",
+    "Pacific": "West",
+    "Puerto Rico": "Puerto Rico",
+}
