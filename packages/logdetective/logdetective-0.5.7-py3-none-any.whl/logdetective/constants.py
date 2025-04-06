3c6abea287eac0ec4c83094f7d4954f36898de85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-06 03:17:20 +0000

    Add package logdetective/0.5.7/logdetective-0.5.7-py3-none-any.whl

---
+++ packages/logdetective/logdetective-0.5.7-py3-none-any.whl/logdetective/constants.py
@@ -0,0 +1,61 @@
+# pylint: disable=line-too-long
+DEFAULT_ADVISOR = "fedora-copr/Mistral-7B-Instruct-v0.2-GGUF"
+
+PROMPT_TEMPLATE = """
+Given following log snippets, and nothing else, explain what failure, if any, occured during build of this package.
+
+Analysis of the snippets must be in a format of [X] : [Y], where [X] is a log snippet, and [Y] is the explanation.
+Snippets themselves must not be altered in any way whatsoever.
+
+Snippets are delimited with '================'.
+
+Finally, drawing on information from all snippets, provide complete explanation of the issue and recommend solution.
+
+Snippets:
+
+{}
+
+Analysis:
+
+"""
+
+SUMMARIZE_PROMPT_TEMPLATE = """
+Does following log contain error or issue?
+
+Log:
+
+{}
+
+Answer:
+
+"""
+
+SNIPPET_PROMPT_TEMPLATE = """
+Analyse following RPM build log snippet. Describe contents accurately, without speculation or suggestions for resolution.
+
+Snippet:
+
+{}
+
+Analysis:
+
+"""
+
+PROMPT_TEMPLATE_STAGED = """
+Given following log snippets, their explanation, and nothing else, explain what failure, if any, occured during build of this package.
+
+Snippets are in a format of [X] : [Y], where [X] is a log snippet, and [Y] is the explanation.
+
+Snippets are delimited with '================'.
+
+Drawing on information from all snippets, provide complete explanation of the issue and recommend solution.
+
+Snippets:
+
+{}
+
+Analysis:
+
+"""
+
+SNIPPET_DELIMITER = "================"
