0b74eb9dee4159453e6f2e10f7587a4927176cd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-06 03:17:09 +0000

    Add package maleo-core/0.2.79/maleo_core-0.2.79.tar.gz

---
+++ packages/maleo-core/maleo_core-0.2.79.tar.gz/maleo_core-0.2.79/maleo_core/models/maleo_suite/maleo_access/transfers/results/query/user.py
@@ -0,0 +1,18 @@
+from __future__ import annotations
+from pydantic import Field
+from typing import Optional
+from maleo_core.models.base.transfers.results.services.query import BaseServiceQueryResults
+from maleo_core.models.maleo_suite.maleo_access.transfers.general.user import MaleoAccessUserGeneralTransfers
+from maleo_core.models.maleo_suite.maleo_access.transfers.results.query.user_type import MaleoAccessUserTypeQueryResults
+
+class MaleoAccessUserQueryResults:
+    class Get(MaleoAccessUserGeneralTransfers.Base, BaseServiceQueryResults.Get):
+        user_type:MaleoAccessUserTypeQueryResults.Get = Field(..., description="User's type")
+
+    Fail = BaseServiceQueryResults.Fail
+
+    class SingleData(BaseServiceQueryResults.SingleData):
+        data:Optional[MaleoAccessUserQueryResults.Get]
+
+    class MultipleData(BaseServiceQueryResults.MultipleData):
+        data:list[MaleoAccessUserQueryResults.Get]
