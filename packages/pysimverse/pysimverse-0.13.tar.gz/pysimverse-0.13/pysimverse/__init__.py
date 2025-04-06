e3696f64ebeed41ccf1c5f1e37a9d16df1508a6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-06 04:05:11 +0000

    Add package pysimverse/0.13/pysimverse-0.13.tar.gz

---
+++ packages/pysimverse/pysimverse-0.13.tar.gz/pysimverse-0.13/pysimverse/__init__.py
@@ -0,0 +1,2 @@
+from pysimverse.simulators.drone.drone import Drone
+from pysimverse.simulators.drone.drone_manager import DroneManager
---
+++ packages/pysimverse/pysimverse-0.13.tar.gz/pysimverse-0.13/setup.py
@@ -0,0 +1,28 @@
+from distutils.core import setup
+
+setup(
+    name='pysimverse',
+    packages=['pysimverse'],
+    version='0.13',
+    license='MIT',
+    description='Python Computer Vision and Robotics Simulator',
+    author='Computer Vision Zone',
+    author_email='contact@computervision.zone',
+    url='https://github.com/cvzone/pysimverse.git',
+    keywords=['ComputerVision', 'Simulator', 'Robotics', 'Drone'],
+    install_requires=[
+        'pyzmq',
+        'opencv-python',
+    ],
+    python_requires='>=3.7',  # Requires any version >= 3.7
+    include_package_data=True,  # Include additional files specified in MANIFEST.in
+    package_data={
+        'pysimverse': ['simulators/*','simulators/drone/*'],  # Include the files in assets and ui folders
+    },
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'Programming Language :: Python :: 3',
+    ],
+)
