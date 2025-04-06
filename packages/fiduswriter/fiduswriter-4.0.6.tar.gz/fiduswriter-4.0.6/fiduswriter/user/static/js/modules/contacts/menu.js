4dfefac507172e2f59d3791181302f0a228ec618
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-06 03:16:49 +0000

    Add package fiduswriter/4.0.6/fiduswriter-4.0.6.tar.gz

---
+++ packages/fiduswriter/fiduswriter-4.0.6.tar.gz/fiduswriter-4.0.6/fiduswriter/user/static/js/modules/contacts/menu.js
@@ -0,0 +1,47 @@
+import {AddContactDialog} from "./add_dialog"
+import {DeleteContactDialog} from "./delete_dialog"
+
+export const bulkMenuModel = () => ({
+    content: [
+        {
+            title: gettext("Delete selected"),
+            tooltip: gettext("Delete selected contacts."),
+            action: overview => {
+                const selected = overview.getSelected()
+                if (selected.length) {
+                    const dialog = new DeleteContactDialog(selected)
+                    dialog.init().then(() => {
+                        overview.contacts = overview.contacts.filter(
+                            ocontact =>
+                                !selected.some(
+                                    scontact =>
+                                        scontact.id == ocontact.id &&
+                                        scontact.type == ocontact.type
+                                )
+                        )
+                        overview.initializeView()
+                    })
+                }
+            },
+            disabled: overview => !overview.getSelected().length
+        }
+    ]
+})
+
+export const menuModel = () => ({
+    content: [
+        {
+            type: "text",
+            title: gettext("Invite contact"),
+            keys: "Alt-i",
+            action: overview => {
+                const dialog = new AddContactDialog()
+                dialog.init().then(contacts => {
+                    contacts.forEach(contact => overview.contacts.push(contact))
+                    overview.initializeView()
+                })
+            },
+            order: 0
+        }
+    ]
+})
