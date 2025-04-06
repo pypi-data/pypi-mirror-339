0247feeeec588b177cc7067f83a0337f78e2b694
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-06 03:17:04 +0000

    Add package spforge/0.2.0/spforge-0.2.0.tar.gz

---
+++ packages/spforge/spforge-0.2.0.tar.gz/spforge-0.2.0/spforge/predictor/__init__.py
@@ -0,0 +1,8 @@
+from .predictor import (
+    SklearnPredictor,
+    GameTeamPredictor,
+    GranularityPredictor,
+    SklearnPredictor,
+)
+from ._base import BasePredictor
+from .sklearn_estimator import OrdinalClassifier, SkLearnWrapper
