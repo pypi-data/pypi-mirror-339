5cd0051b016c0c02780a474b84848eaa3b4c8af5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-06 04:04:13 +0000

    Add package py-common-func-package/1.0.7/py_common_func_package-1.0.7.tar.gz

---
+++ packages/py-common-func-package/py_common_func_package-1.0.7.tar.gz/py_common_func_package-1.0.7/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+import platform
+
+# 判断系统，自动选择编译后缀
+ext = ".pyd" if platform.system() == "Windows" else ".so"
+
+setup(
+    name="py_common_func_package",
+    version="1.0.7",
+    authors=[{"name": "wangli", "email": "wl_926454@163.com"}],
+    description = "python test common function",
+    packages=find_packages(),  # 自动寻找并包括所有包
+    package_data={  # 确保混淆后的文件被包含
+        '': [
+            'obf/common_fun/__pycache__/*.pyc',
+            'obf/test_steps/__pycache__/*.pyc'
+        ],
+    },
+    include_package_data=True,  # 确保包含 package_data 中的文件
+    zip_safe=False,
+    install_requires=[  # 如果有依赖的其他包
+    ],
+)
