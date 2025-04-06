ef12d20f84ff29a3282d650c1ef228033bd0dcc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-06 04:03:24 +0000

    Add package rapidinstall/0.9.2/rapidinstall-0.9.2.tar.gz

---
+++ packages/rapidinstall/rapidinstall-0.9.2.tar.gz/rapidinstall-0.9.2/tests/test.py
@@ -0,0 +1,8 @@
+import ...src.rapidinstall as rapidinstall
+
+my_tasks = [
+	{'name': 'Pip', 'commands': 'pip install pip'},
+	{'name': 'Download MyFile', 'commands': "wget -c https://huggingface.co/WarriorMama777/OrangeMixs/resolve/main/VAEs/orangemix.vae.pt"}
+]
+
+rapidinstall.install(my_tasks, update_interval=5)
