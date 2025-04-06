7037d79c2dc58e1ea0f033a073960b02c4755d65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-06 03:17:14 +0000

    Add package mysql-checksum/0.6/mysql_checksum-0.6.tar.gz

---
+++ packages/mysql-checksum/mysql_checksum-0.6.tar.gz/mysql_checksum-0.6/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setup(
+    name='mysql_checksum',
+    author='demmonico',
+    version='0.6',
+    long_description=long_description,
+    long_description_type="text/markdown",
+
+    packages=find_packages(),
+
+    install_requires=[
+    ],
+
+    entry_points={
+        'console_scripts': [
+            'mysql-checksum = mysql_checksum:hello',
+        ],
+    },
+)
+        #'mysql-connector-python',
+        #'logging'
