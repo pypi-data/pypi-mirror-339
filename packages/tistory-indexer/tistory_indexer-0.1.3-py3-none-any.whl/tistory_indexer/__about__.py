cf8089d2133f86d2ff9b82f21561f19f3b3eeb8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-06 03:17:06 +0000

    Add package tistory-indexer/0.1.3/tistory_indexer-0.1.3-py3-none-any.whl

---
+++ packages/tistory-indexer/tistory_indexer-0.1.3-py3-none-any.whl/tistory_indexer/__about__.py
@@ -0,0 +1,7 @@
+__title__ = "tistory-indexer"
+__description__ = "Submit and check indexing status of Tistory blog posts via Google's Indexing API"
+__version__ = "0.1.3"
+__author__ = "OuOHoon"
+__author_email__ = "begreat@kakao.com"
+__license__ = "MIT"
+__url__ = "https://github.com/ouohoon/tistory-indexer"
