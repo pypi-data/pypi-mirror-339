bb815e4a386f0caa841349ed4487239f930b8a81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-06 14:03:38 +0000

    Add package exnd-curses/0.1.5/exnd_curses-0.1.5-py3-none-any.whl

---
+++ packages/exnd-curses/exnd_curses-0.1.5-py3-none-any.whl/exnd_curses/actions.py
@@ -0,0 +1,11 @@
+class Binding:
+    def __init__(self, code=0, action=None):
+        self.keyCode = code;
+        self.action = action;
+
+class Actions:
+    def addLetter():
+        pass;
+
+    def removeLetter():
+        pass;
