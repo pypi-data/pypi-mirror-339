94a75ce9afb7c3e461cd7e6dc5b3de62d84cebb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-06 04:03:44 +0000

    Add package maleo-core/0.2.96/maleo_core-0.2.96-py3-none-any.whl

---
+++ packages/maleo-core/maleo_core-0.2.96-py3-none-any.whl/maleo_core/clients/maleo_suite/maleo_access/http/services/__init__.py
@@ -0,0 +1,18 @@
+from .blood_type import MaleoAccessBloodTypeHTTPService
+from .gender import MaleoAccessGenderHTTPService
+from .organization_role import MaleoAccessOrganizationRoleHTTPService
+from .organization_type import MaleoAccessOrganizationTypeHTTPService
+from .organization import MaleoAccessOrganizationHTTPService
+from .system_role import MaleoAccessSystemRoleHTTPService
+from .user_type import MaleoAccessUserTypeHTTPService
+from .user import MaleoAccessUserHTTPService
+
+class MaleoAccessHTTPServices:
+    BloodType = MaleoAccessBloodTypeHTTPService
+    Gender = MaleoAccessGenderHTTPService
+    OrganizationRole = MaleoAccessOrganizationRoleHTTPService
+    OrganizationType = MaleoAccessOrganizationTypeHTTPService
+    Organization = MaleoAccessOrganizationHTTPService
+    SystemRole = MaleoAccessSystemRoleHTTPService
+    UserType = MaleoAccessUserTypeHTTPService
+    User = MaleoAccessUserHTTPService
