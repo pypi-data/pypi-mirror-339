14becd3f44e6ea5f5264be7c44527c2a3f08e671
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-06 03:17:19 +0000

    Add package at-client-data/0.2.0/at_client_data-0.2.0.tar.gz

---
+++ packages/at-client-data/at_client_data-0.2.0.tar.gz/at_client_data-0.2.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="at-client-data",
+    version="0.2.0",
+    description="AT Client and Schema Package for AT Data API",
+    author="AT Data",
+    author_email="info@atdata.com",
+    url="https://github.com/yourusername/at-client-data",
+    packages=find_packages(include=['at_client_data', 'at_client_data.*']),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+    python_requires=">=3.8",
+    install_requires=[
+        "requests>=2.25.0",
+        "pydantic>=1.8.0",
+    ],
+) 
