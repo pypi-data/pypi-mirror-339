6076c1e4d685a45a15a48d549a39f01f5f25f224
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-06 03:17:04 +0000

    Add package apirotater/0.4.9/apirotater-0.4.9.tar.gz

---
+++ packages/apirotater/apirotater-0.4.9.tar.gz/apirotater-0.4.9/apirotater/__init__.py
@@ -0,0 +1,3 @@
+from .core import key, hit, usage, get_all_keys, get_key_names, get_current_key_name, load_env_file, RateLimitExceeded
+
+__version__ = "0.4.9"  # Manuel .env dosya yolu belirleme özelliği eklendi
