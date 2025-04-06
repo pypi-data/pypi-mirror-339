43802afff5cbf8e0adb252cf7eba2c2ddc4aec18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-06 15:04:14 +0000

    Add package navconfig/1.7.13/navconfig-1.7.13-cp312-cp312-win_amd64.whl

---
+++ packages/navconfig/navconfig-1.7.13-cp312-cp312-win_amd64.whl/navconfig/loaders/__init__.py
@@ -0,0 +1,13 @@
+from importlib import import_module
+from .pyproject import pyProjectLoader
+
+
+def import_loader(loader: str = "file"):
+    classpath = f"navconfig.loaders.{loader}"
+    cls = f"{loader}Loader"
+    try:
+        module = import_module(classpath, package="loaders")
+        obj = getattr(module, cls)
+        return obj
+    except ImportError as err:
+        raise RuntimeError(f"Navconfig Error: Cannot load {cls}: {err}") from err
