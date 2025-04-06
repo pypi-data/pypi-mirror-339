0782a6d9ce3a4ef1f9e9d33584ff488294568572
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-06 04:04:00 +0000

    Add package maleo-core/0.2.85/maleo_core-0.2.85-py3-none-any.whl

---
+++ packages/maleo-core/maleo_core-0.2.85-py3-none-any.whl/maleo_core/models/maleo_suite/maleo_access/transfers/results/query/__init__.py
@@ -0,0 +1,36 @@
+# This file serves all MaleoAccess's query results
+
+from __future__ import annotations
+from .blood_type import MaleoAccessBloodTypeQueryResults
+from .gender import MaleoAccessGenderQueryResults
+from .organization_role import MaleoAccessOrganizationRoleQueryResults
+from .organization_type import MaleoAccessOrganizationTypeQueryResults
+from .organization import MaleoAccessOrganizationQueryResults
+from .system_role import MaleoAccessSystemRoleQueryResults
+from .user_profile import MaleoAccessUserProfileQueryResults
+from .user_type import MaleoAccessUserTypeQueryResults
+from .user import MaleoAccessUserQueryResults
+
+class MaleoAccessQueryResults:
+    BloodType = MaleoAccessBloodTypeQueryResults
+    Gender = MaleoAccessGenderQueryResults
+    OrganizationRole = MaleoAccessOrganizationRoleQueryResults
+    OrganizationType = MaleoAccessOrganizationTypeQueryResults
+    Organization = MaleoAccessOrganizationQueryResults
+    SystemRole = MaleoAccessSystemRoleQueryResults
+    UserProfile = MaleoAccessUserProfileQueryResults
+    UserType = MaleoAccessUserTypeQueryResults
+    User = MaleoAccessUserQueryResults
+
+__all__ = [
+    "MaleoAccessQueryResults",
+    "MaleoAccessBloodTypeQueryResults",
+    "MaleoAccessGenderQueryResults",
+    "MaleoAccessOrganizationRoleQueryResults",
+    "MaleoAccessOrganizationTypeQueryResults",
+    "MaleoAccessOrganizationQueryResults",
+    "MaleoAccessSystemRoleQueryResults",
+    "MaleoAccessUserProfileQueryResults",
+    "MaleoAccessUserTypeQueryResults",
+    "MaleoAccessUserQueryResults"
+]
