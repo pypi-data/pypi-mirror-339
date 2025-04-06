634d16a309b61527e807d06439d839204bdd65bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-06 15:04:37 +0000

    Add package exppandas/0.1.0/exppandas-0.1.0-py3-none-any.whl

---
+++ packages/exppandas/exppandas-0.1.0-py3-none-any.whl/exppandas/version.py
@@ -0,0 +1,9 @@
+"""
+Version information.
+"""
+
+__version__ = '0.1.0'
+__author__ = 'Sumedh Patil'
+__author_email__ = 'sumedh@aipresso.com'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2025 Sumedh Patil, Aipresso Limited, UK'
