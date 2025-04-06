537e3c65035167105f459ad83d19ff0d45f0bf2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-06 03:17:05 +0000

    Add package cfn-cur/0.1.1/cfn_cur-0.1.1.tar.gz

---
+++ packages/cfn-cur/cfn_cur-0.1.1.tar.gz/cfn_cur-0.1.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name='cfn-cur',
+    version='0.1.1',
+    description='A tool to generate CLI Command to continue update rollback of CloudFormation stacks',
+    author='S Murali Krishnan',
+    author_email="mrlikrsh@gmail.com",
+    url="https://github.com/mrlikl/continue-update-rollback",
+    packages=['continue_update_rollback'],
+    entry_points={
+        'console_scripts': [
+            'cfn-cur = continue_update_rollback.app:main'
+        ]
+    },
+    install_requires=[
+        'boto3',
+    ],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    python_requires='>=3.9',
+)
