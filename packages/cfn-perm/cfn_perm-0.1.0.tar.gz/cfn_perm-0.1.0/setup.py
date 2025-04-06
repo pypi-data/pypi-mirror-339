94d4b8e18c37de8a68b43698c10ffe1dcfcf3382
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-06 03:16:51 +0000

    Add package cfn-perm/0.1.0/cfn_perm-0.1.0.tar.gz

---
+++ packages/cfn-perm/cfn_perm-0.1.0.tar.gz/cfn_perm-0.1.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="cfn-perm",
+    version="0.1.0",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "boto3",
+        "pyyaml",
+    ],
+    entry_points={
+        "console_scripts": [
+            "cfn-perm=source.app:main",
+        ],
+    },
+    author="S Murali Krishnan",
+    author_email="mrlikrsh@gmail.com",
+    description="A tool to automatically generate IAM permissions from CloudFormation templates",
+    keywords="aws, cloudformation, iam, permissions",
+    url="https://github.com/mrlikl/cfn-perm.git",
+    python_requires=">=3.9",
+    classifiers=[
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3"
+    ],
+)
