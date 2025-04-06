1c685cb2c2b60f79f13da279b00d11009f7741ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-06 14:03:41 +0000

    Add package exnd-curses/0.1.9/exnd_curses-0.1.9-py3-none-any.whl

---
+++ packages/exnd-curses/exnd_curses-0.1.9-py3-none-any.whl/exnd_curses/read.py
@@ -0,0 +1,31 @@
+import sys, os;
+import window;
+
+def main():
+    file = "";
+
+    if len(sys.argv) == 2:
+        arg = sys.argv[1];
+        if (arg == "-h" or arg == "--help"):
+            print("fread 'relative path'\nfread -a 'absolute path'");
+        else:
+            try:
+                file = os.path.abspath(arg);       
+            except:
+                print("-h for help");
+                exit();
+    elif len(sys.argv) == 3:
+        arg = sys.argv[1];
+        if arg != '-a':
+            exit();
+        file = sys.argv[2];
+    else:
+        exit();
+
+    if (not os.path.isfile(file)): print("File not found!"); exit();
+
+    with open(file, 'r') as file:
+        win = window.Window();
+        win.changeLines(file.read());
+
+if __name__ == "__main__": main();
