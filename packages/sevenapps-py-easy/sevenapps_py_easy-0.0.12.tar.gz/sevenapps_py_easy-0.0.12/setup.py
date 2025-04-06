e72f46447d0b6e132593d43e601babe1e121a28d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-06 05:04:04 +0000

    Add package sevenapps-py-easy/0.0.12/sevenapps_py_easy-0.0.12.tar.gz

---
+++ packages/sevenapps-py-easy/sevenapps_py_easy-0.0.12.tar.gz/sevenapps_py_easy-0.0.12/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sevenapps-py-easy',
+    version='0.0.12',
+    license='MIT',
+    description="Paquete creado para optimizar mi trabajo con python unificando lo más necesitado",
+    author="SevenApps Studio",
+    author_email="sevenapps.studio@gmail.com",
+    packages=find_packages(),
+    url='https://github.com/juanjp1992/sevenapps-py-easy.git',
+    install_requires=[
+        'google-cloud-firestore',
+        'firebase-admin',
+        'selenium',
+        'gspread',
+        'scp',
+        'myjdapi',
+        'PlexAPI'
+    ]
+)
