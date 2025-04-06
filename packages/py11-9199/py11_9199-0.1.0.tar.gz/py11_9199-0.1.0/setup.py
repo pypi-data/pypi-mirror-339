4e5ec38801dd5dab69f9a63f01fb1365b0c7a9e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-06 15:04:22 +0000

    Add package py11-9199/0.1.0/py11_9199-0.1.0.tar.gz

---
+++ packages/py11-9199/py11_9199-0.1.0.tar.gz/py11_9199-0.1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='py11_9199',  # ✅ Use your own unique name
+    version='0.1.0',
+    packages=find_packages(),
+    description='A custom package with greetings and math operations',
+    author='notes',
+    author_email='mscsoftwaresystemsnotes@email.com',
+    license='MIT'
+)
+
