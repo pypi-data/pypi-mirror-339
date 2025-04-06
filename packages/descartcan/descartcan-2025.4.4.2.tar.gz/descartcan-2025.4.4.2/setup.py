025e96268356e6ed8dfc50a052d3912237969b8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-06 15:04:05 +0000

    Add package descartcan/2025.4.4.2/descartcan-2025.4.4.2.tar.gz

---
+++ packages/descartcan/descartcan-2025.4.4.2.tar.gz/descartcan-2025.4.4.2/setup.py
@@ -0,0 +1,35 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='descartcan',
+    version='2025.4.4.2',
+    description='A Python toolkit for advanced data processing and API interactions',
+    author='DescartCan',
+    author_email='louishwh@gmail.com',
+    url='',
+    #packages=['descartcan.connection'],
+    packages=setuptools.find_packages(),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    python_requires='>=3.10',
+    install_requires=[
+        'aiohttp>=3.11.10',
+        'aiofiles~=24.1.0',
+        'loguru==0.6.0',
+        'openai~=1.58.1',
+        'anthropic~=0.42.0',
+        'boto3~=1.34.79',
+        'httpx==0.25.2',
+        'tiktoken==0.8.0',
+        'pyyaml~=6.0.2',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
+
