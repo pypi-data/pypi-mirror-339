867a67691d736023d88287a8d3ea62bdd1c2e0a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-06 14:03:37 +0000

    Add package java-gradescope-autograder-helper/1.2.5/java_gradescope_autograder_helper-1.2.5-py3-none-any.whl

---
+++ packages/java-gradescope-autograder-helper/java_gradescope_autograder_helper-1.2.5-py3-none-any.whl/java_gradescope_autograder_helper/compiler.py
@@ -0,0 +1,29 @@
+from pathlib import Path
+from subprocess import run
+
+from .helpers import ConfigurationError
+
+
+def compile_java(entry_point_path: str, classpath: str | None) -> None:
+    """
+    Compiles all Java source files found recursively from the directory of
+    the given entry point.
+
+    Raises:
+        Exception: If the compilation process fails, an Exception is raised
+            with the corresponding error message from stderr.
+    """
+
+    cmd = ["javac"]
+    if classpath:
+        cmd.extend(["-cp", classpath])
+
+    # Recursively find all .java files
+    entry_point_dir = Path(entry_point_path).parent
+    java_files = list(entry_point_dir.rglob("*.java"))
+    cmd.extend([str(java_file) for java_file in java_files])
+
+    result = run(cmd, capture_output=True, text=True, cwd=entry_point_dir)
+
+    if result.returncode != 0:
+        raise ConfigurationError(f"Compilation failed:\n{result.stderr}")
