12c06df34a6d9dd278f2ec3562673ba82537b895
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-06 15:04:13 +0000

    Add package urban-transit-network-analysis/1.0.3/urban_transit_network_analysis-1.0.3.tar.gz

---
+++ packages/urban-transit-network-analysis/urban_transit_network_analysis-1.0.3.tar.gz/urban_transit_network_analysis-1.0.3/urban_transit_network_analysis/context/PrintGraphAnalisContext.py
@@ -0,0 +1,20 @@
+from urban_transit_network_analysis.enums.HeatMapMetrics import HeatMapMetrics
+from urban_transit_network_analysis.enums.HistogramMetrics import HistogramMetrics
+
+"""
+    Контекст для отрисовки графиков о метриках сетей
+"""
+
+
+class PrintGraphAnalisContext:
+    def __init__(
+            self,
+            heat_map_metrics_list: [HeatMapMetrics] = None,
+            histogram_map_metrics_list: [HistogramMetrics] = None,
+            mesh_size: int = 100
+    ):
+        self.heat_map_metrics_list = heat_map_metrics_list if heat_map_metrics_list is not None \
+            else list(HeatMapMetrics.__members__.values())
+        self.histogram_map_metrics_list = histogram_map_metrics_list if histogram_map_metrics_list is not None \
+            else list(HistogramMetrics.__members__.values())
+        self.mesh_size = mesh_size
