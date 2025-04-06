a49cb5845f4627ca9c969a2c47188ede001abec4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-06 05:03:58 +0000

    Add package pushinpay/0.2.0/pushinpay-0.2.0.tar.gz

---
+++ packages/pushinpay/pushinpay-0.2.0.tar.gz/pushinpay-0.2.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pushinpay",
+    version="0.2.0",
+    description="Wrapper para a API PushinPay",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Ricardo Santos",
+    author_email="ricardotenv@gmail.com",
+    url="https://github.com/ricardotenv/pushinpay",  # Substitua pelo URL do repositório no GitHub
+    packages=find_packages(),
+    install_requires=["requests"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    python_requires=">=3.6",
+)
