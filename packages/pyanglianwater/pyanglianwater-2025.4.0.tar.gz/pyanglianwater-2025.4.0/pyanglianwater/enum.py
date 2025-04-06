3460b83bda61cfae77e64ebf47dbbf7517c457fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-06 04:05:12 +0000

    Add package pyanglianwater/2025.4.0/pyanglianwater-2025.4.0.tar.gz

---
+++ packages/pyanglianwater/pyanglianwater-2025.4.0.tar.gz/pyanglianwater-2025.4.0/pyanglianwater/enum.py
@@ -0,0 +1,8 @@
+"""Anglian Water enums."""
+
+from enum import StrEnum
+
+class UsagesReadGranularity(StrEnum):
+    """Usages Read Granularity."""
+    DAILY = "20"
+    HOURLY = "10"
---
+++ packages/pyanglianwater/pyanglianwater-2025.4.0.tar.gz/pyanglianwater-2025.4.0/pyanglianwater/exceptions.py
@@ -0,0 +1,43 @@
+"""Exceptions for Anglian Water."""
+
+class LegacyAuthError(Exception):
+    """General authentication error for legacy authentication."""
+
+class AuthError(Exception):
+    """General authentication error."""
+
+class InvalidPasswordError(LegacyAuthError):
+    """E_LGN_006"""
+
+class InvalidUsernameError(LegacyAuthError):
+    """E_LGN_008"""
+
+class EndpointUnavailableError(LegacyAuthError):
+    """S_SMR_1058"""
+
+class UnknownEndpointError(Exception):
+    """Defines an unknown error."""
+
+class ExpiredAccessTokenError(LegacyAuthError):
+    """401 Unauthorized"""
+
+class ServiceUnavailableError(Exception):
+    """503 Service Unavailable."""
+
+class TariffNotAvailableError(Exception):
+    """Tariff information not available or set."""
+
+class InitialAuthError(AuthError):
+    """Error requesting auth configuration."""
+class SelfAssertedError(AuthError):
+    """Error performing login via username and password."""
+class ConfirmationRedirectError(AuthError):
+    """Error confirming login with redirect."""
+class TokenRequestError(AuthError):
+    """Error requesting a token from the token server."""
+
+API_RESPONSE_STATUS_CODE_MAPPING = {
+    "E_LGN_006": InvalidPasswordError,
+    "E_LGN_008": InvalidUsernameError,
+    "S_SMR_1058": EndpointUnavailableError
+}
