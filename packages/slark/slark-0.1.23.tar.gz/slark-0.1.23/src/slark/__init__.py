677c643ac73df9df89f39bb3943e18fae6cabebf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-06 04:03:54 +0000

    Add package slark/0.1.23/slark-0.1.23.tar.gz

---
+++ packages/slark/slark-0.1.23.tar.gz/slark-0.1.23/src/slark/__init__.py
@@ -0,0 +1,5 @@
+from slark.client.lark import AsyncLark
+from slark.client.sync_lark import Lark
+from slark.event.event_manager import EventManager
+
+__all__ = ["AsyncLark", "Lark", "EventManager"]
