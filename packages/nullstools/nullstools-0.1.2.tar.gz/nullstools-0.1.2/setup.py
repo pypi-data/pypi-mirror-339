5fb983d03176ccf7fe70b64b5304c31c576114a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-06 04:04:51 +0000

    Add package nullstools/0.1.2/nullstools-0.1.2.tar.gz

---
+++ packages/nullstools/nullstools-0.1.2.tar.gz/nullstools-0.1.2/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='nullstools',
+    version='0.1.2',
+    packages=find_packages(),
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='darkmean',
+    description=open('README.md').read(),
+    url='https://github.com/darkmean-dev/nullstools',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
