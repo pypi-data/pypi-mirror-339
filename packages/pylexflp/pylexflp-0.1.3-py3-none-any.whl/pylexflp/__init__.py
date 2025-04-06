a16bb03a5efdccc074e6007795e6409c869ad960
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-06 05:03:32 +0000

    Add package pylexflp/0.1.3/pylexflp-0.1.3-py3-none-any.whl

---
+++ packages/pylexflp/pylexflp-0.1.3-py3-none-any.whl/pylexflp/__init__.py
@@ -0,0 +1,10 @@
+from pylexflp.pylexflp import (FLP, 
+                               TFN, 
+                               TFN_Var, 
+                               flpMaximize, 
+                               flpMinimize,
+                               getSolver
+)
+
+__version__ = "0.1.3"
+__author__ = "Boris Pérez-Cañedo"
