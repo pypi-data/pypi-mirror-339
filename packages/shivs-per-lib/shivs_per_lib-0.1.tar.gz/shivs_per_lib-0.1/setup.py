c7877cd9a3a460e03137a3b8cdf1a41e3db93af5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-06 03:16:47 +0000

    Add package shivs-per-lib/0.1/shivs_per_lib-0.1.tar.gz

---
+++ packages/shivs-per-lib/shivs_per_lib-0.1.tar.gz/shivs_per_lib-0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='shivs_per_lib',
+    version='0.1',
+    packages=find_packages(),
+    description='A personal utility package by Shivsamb Harkare.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Shivsamb Mangalkumar Harkare',
+    author_email='shivsamb1984@gmail.com',
+    url='https://github.com/yourusername/shivs_per_lib',  # Replace if available
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    install_requires=[],  # Add dependencies here
+    python_requires='>=3.6',
+)
