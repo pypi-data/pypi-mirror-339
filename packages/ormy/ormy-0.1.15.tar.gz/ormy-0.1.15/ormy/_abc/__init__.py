3f9d23469127fee3cf3243488267344aa3f9becc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-07 14:04:37 +0000

    Add package ormy/0.1.15/ormy-0.1.15.tar.gz

---
+++ packages/ormy/ormy-0.1.15.tar.gz/ormy-0.1.15/ormy/_abc/__init__.py
@@ -0,0 +1,13 @@
+from .abstract import AbstractABC, AbstractMixinABC, SemiFrozenField
+from .config import ConfigABC
+from .typing import AbstractData
+
+# ----------------------- #
+
+__all__ = [
+    "ConfigABC",
+    "AbstractABC",
+    "AbstractMixinABC",
+    "SemiFrozenField",
+    "AbstractData",
+]
