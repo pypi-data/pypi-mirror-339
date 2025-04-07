7af98d0ee7bbe82fd35dc8209844fba40f160ba3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-07 16:04:56 +0000

    Add package akm/1.3.3/akm-1.3.3.tar.gz

---
+++ packages/akm/akm-1.3.3.tar.gz/akm-1.3.3/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='akm',
+    version='1.3.3',
+    description='- Gmail-Temp API By : t.me/goldenxpris .',
+    author='Your Name',
+    packages=['akm'],
+    install_requires=[
+        'requests',
+    ],
+)
