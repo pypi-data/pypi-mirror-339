c17504014ac0643587dc9d96122018aa59d4980c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-07 16:04:42 +0000

    Add package exasol-saas-api/1.1.0/exasol_saas_api-1.1.0-py3-none-any.whl

---
+++ packages/exasol-saas-api/exasol_saas_api-1.1.0-py3-none-any.whl/exasol/saas/client/openapi/models/connection_i_ps.py
@@ -0,0 +1,69 @@
+from typing import (
+    TYPE_CHECKING,
+    Any,
+    BinaryIO,
+    Optional,
+    TextIO,
+    TypeVar,
+    cast,
+)
+
+from attrs import define as _attrs_define
+from attrs import field as _attrs_field
+
+from ..types import (
+    UNSET,
+    Unset,
+)
+
+T = TypeVar("T", bound="ConnectionIPs")
+
+
+@_attrs_define
+class ConnectionIPs:
+    """ 
+        Attributes:
+            private (list[str]):
+            public (list[str]):
+     """
+
+    private: list[str]
+    public: list[str]
+
+
+    def to_dict(self) -> dict[str, Any]:
+        private = self.private
+
+
+
+        public = self.public
+
+
+
+
+        field_dict: dict[str, Any] = {}
+        field_dict.update({
+            "private": private,
+            "public": public,
+        })
+
+        return field_dict
+
+
+
+    @classmethod
+    def from_dict(cls: type[T], src_dict: dict[str, Any]) -> T:
+        d = src_dict.copy()
+        private = cast(list[str], d.pop("private"))
+
+
+        public = cast(list[str], d.pop("public"))
+
+
+        connection_i_ps = cls(
+            private=private,
+            public=public,
+        )
+
+        return connection_i_ps
+
