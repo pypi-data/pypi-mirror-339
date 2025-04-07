b7595e2d7449c49bfd9658cfbb3f6d85f655cef3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-07 16:04:51 +0000

    Add package flex-dtk/0.4.1/flex_dtk-0.4.1-py3-none-any.whl

---
+++ packages/flex-dtk/flex_dtk-0.4.1-py3-none-any.whl/flex_dtk/trading_db.py
@@ -0,0 +1,37 @@
+"""
+Functions for reading SQL queries from the trading db.
+
+Note that Azure authentication is not possible on the postgres-db, you will need
+to securely store username and password data using a package such as "keyring".
+"""
+
+import pandas as pd
+from sqlalchemy import create_engine
+
+
+def postgres_engine(server: str, database: str, username: str, password: str):
+    """
+    Return engine for postgres based on given settings
+    """
+    return create_engine(
+        f"postgresql://{username}:{password}@{server}:5432/{database}",
+    )
+
+
+def query(
+    sql: str,
+    server: str,
+    database: str,
+    username: str,
+    password: str,
+    **kwargs,
+) -> pd.DataFrame:
+    """
+    Run and return the results of a database query for the Postgres trading database
+    """
+    kwargs = kwargs or {"dtype_backend": "pyarrow"}
+    return pd.read_sql(
+        sql,
+        con=postgres_engine(server, database, username, password),
+        **kwargs,
+    )
