e5575de1b9bba33692f86944cc4d8255608f353a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-07 15:04:55 +0000

    Add package pathpilot/0.1.10/pathpilot-0.1.10.tar.gz

---
+++ packages/pathpilot/pathpilot-0.1.10.tar.gz/pathpilot-0.1.10/pathpilot/_csv.py
@@ -0,0 +1,31 @@
+import pandas as pd
+
+from ._file import FileBase
+from .decorators import purge_whitespace
+
+
+
+
+class CSVFile(FileBase):
+
+    #╭-------------------------------------------------------------------------╮
+    #| Initialize Instance                                                     |
+    #╰-------------------------------------------------------------------------╯
+
+    def __init__(self, f, **kwargs):
+        super().__init__(f, **kwargs)
+
+
+    #╭-------------------------------------------------------------------------╮
+    #| Instance Methods                                                        |
+    #╰-------------------------------------------------------------------------╯
+
+    @purge_whitespace
+    def read(self, **kwargs):
+        kwargs.setdefault('encoding', 'ISO-8859-1')
+        kwargs.setdefault('keep_default_na', False)
+        return pd.read_csv(self.path, **kwargs)
+
+
+    def _save(self, obj, **kwargs):
+        obj.to_csv(self.path, **kwargs)
