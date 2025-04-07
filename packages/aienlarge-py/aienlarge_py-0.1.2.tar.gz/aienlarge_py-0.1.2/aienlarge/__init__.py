519434955b54c9fcc7de7e5012793bc0e70677d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-07 03:18:04 +0000

    Add package aienlarge-py/0.1.2/aienlarge_py-0.1.2.tar.gz

---
+++ packages/aienlarge-py/aienlarge_py-0.1.2.tar.gz/aienlarge_py-0.1.2/aienlarge/__init__.py
@@ -0,0 +1,23 @@
+from .aienlarger import ImgLargerAPI
+from .aienlarger import (
+    ImgLargerError,
+    ImgLargerUploadError,
+    ImgLargerStatusError,
+    ImgLargerDownloadError,
+    ImgLargerInvalidProcessTypeError,
+    ImgLargerInvalidScaleRadioError,
+    ImgLargerAPIResponseError,
+    ImgLargerConfigFileError,
+)
+
+__all__ = [
+    "ImgLargerAPI",
+    "ImgLargerError",
+    "ImgLargerUploadError",
+    "ImgLargerStatusError",
+    "ImgLargerDownloadError",
+    "ImgLargerInvalidProcessTypeError",
+    "ImgLargerInvalidScaleRadioError",
+    "ImgLargerAPIResponseError",
+    "ImgLargerConfigFileError",
+]
