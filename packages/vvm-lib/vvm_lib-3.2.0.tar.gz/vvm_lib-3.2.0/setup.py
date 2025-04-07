e18ecf978e02e9d8c6320eba81433938d5a24186
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-07 16:04:39 +0000

    Add package vvm-lib/3.2.0/vvm_lib-3.2.0.tar.gz

---
+++ packages/vvm-lib/vvm_lib-3.2.0.tar.gz/vvm_lib-3.2.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(name='vvm_lib',
+      version='3.2.0',
+      description='my frequently used functions',
+      packages=[
+          'vvm_lib',
+          "vvm_lib.db",
+      ],
+      package_dir={
+          "vvm_lib": "vvm_lib",
+          "vvm_lib.db": "vvm_lib/db",
+      },
+      author_email='v.vazhinskiy@yandex.ru',
+      author="vvazhinskiy",
+      url="https://github.com/VazhikVM/vvm_lib",
+      zip_safe=False,
+      install_requires=[
+          "pandas", 'gspread', 'google-auth-oauthlib', 'google-auth', 'xlsxwriter'
+          , 'oauth2client', 'psycopg2-binary', 'mysql-connector-python',  "openpyxl",  "requests-ntlm"
+          , "clickhouse-connect", 'tzlocal', 'polars', 'hvac'
+      ],
+      )
