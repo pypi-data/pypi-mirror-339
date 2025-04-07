6b2fbbc9056695fa8e8ea1b282f17e04c2f97fde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-07 03:18:43 +0000

    Add package kmodels/0.1.1.0/kmodels-0.1.1.0.tar.gz

---
+++ packages/kmodels/kmodels-0.1.1.0.tar.gz/kmodels-0.1.1.0/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+setup(
+    author="kokaito-git",
+    name="kmodels",
+    version="0.1.1.0",
+    description="KModels",
+    author_email="kokaito.git@gmail.com",
+    url="https://github.com/kokaito-git/knotification",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: MacOS",
+        "Operating System :: POSIX :: Linux",
+        "License :: OSI Approved :: MIT License",
+    ],
+    install_requires=[
+        "pydantic",
+        "typeguard",
+        "kcolors"
+    ],
+    packages=find_packages(),
+    python_requires=">=3.13",
+    include_package_data=True,
+    # extras_require={
+    #     "Windows": ["WinToaster"],
+    #     "Linux": [],
+    # },
+    # packages=["knotification"],
+    # package_dir={"knotification": "knotification"},
+    # package_data={
+    #     "knotification": ["image/*", "sound/*"],
+    # },
+)
