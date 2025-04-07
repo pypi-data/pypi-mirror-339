a5aefbf25653e2c78947859d70b6c15eac0d1c9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-07 03:18:35 +0000

    Add package fastapi-cognito/2.9.0/fastapi_cognito-2.9.0-py3-none-any.whl

---
+++ packages/fastapi-cognito/fastapi_cognito-2.9.0-py3-none-any.whl/fastapi_cognito/models.py
@@ -0,0 +1,25 @@
+from typing import Union, List, Set, Optional, Tuple
+
+from pydantic import BaseModel, HttpUrl, Field
+
+
+class UserpoolModel(BaseModel):
+    region: str
+    userpool_id: str
+    app_client_id: Union[str, List[str], Set[str], Tuple[str]]
+    jwks_url: Optional[str] = Field(default=None)
+
+
+class CognitoToken(BaseModel):
+    origin_jti: Optional[str] = None
+    cognito_id: str = Field(alias="sub")
+    event_id: Optional[str] = None
+    token_use: str
+    scope: str
+    auth_time: int
+    iss: HttpUrl
+    exp: int
+    iat: int
+    jti: str
+    client_id: str
+    username: str
