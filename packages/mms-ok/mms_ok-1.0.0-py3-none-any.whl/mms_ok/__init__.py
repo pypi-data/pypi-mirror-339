b367b6eac574fb98dbbb60818b15b0daa76f6985
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-07 15:04:51 +0000

    Add package mms-ok/1.0.0/mms_ok-1.0.0-py3-none-any.whl

---
+++ packages/mms-ok/mms_ok-1.0.0-py3-none-any.whl/mms_ok/__init__.py
@@ -0,0 +1,13 @@
+from loguru import logger
+
+from .ok_setup import copy_frontpanel_files
+
+copy_frontpanel_files()
+
+try:
+    import ok
+except ImportError:
+    logger.critical("Please manually setup FrontPanel SDK!")
+    raise ImportError("Import ok failed")
+
+from .fpga import XEM7310, XEM7360
