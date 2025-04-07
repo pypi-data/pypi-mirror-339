51e5163e754602722df312bce673169a32476e38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-07 16:04:43 +0000

    Add package rqdatac/3.2.1/rqdatac-3.2.1-cp310-cp310-macosx_11_0_arm64.whl

---
+++ packages/rqdatac/rqdatac-3.2.1-cp310-cp310-macosx_11_0_arm64.whl/rqdatac/__init__.pyi
@@ -0,0 +1,157 @@
+import rqdatac.services.tmall
+import rqdatac.services.constant
+import rqdatac.services.shenwan
+import rqdatac.services.basic
+import rqdatac.services.calendar
+import rqdatac.services.get_price
+import rqdatac.services.concept
+import rqdatac.services.ksh_auction_info
+import rqdatac.services.factor
+import rqdatac.services.financial
+import rqdatac.services.future
+import rqdatac.services.index
+import rqdatac.services.extra
+import rqdatac.services.async_live_md_client
+import rqdatac.services.get_capital_flow
+import rqdatac.services.live_md_client
+import rqdatac.services.market_data
+import rqdatac.services.stock_status
+import rqdatac.services.consensus
+import rqdatac.client
+import rqdatac.services.live
+
+__version__: str = ...
+init = rqdatac.client.init
+reset = rqdatac.client.reset
+initialized = rqdatac.client.initialized
+concept_list = rqdatac.services.concept.concept_list
+concept = rqdatac.services.concept.concept
+concept_names = rqdatac.services.concept.concept_names
+get_concept_list = rqdatac.services.concept.get_concept_list
+get_concept = rqdatac.services.concept.get_concept
+get_stock_concept = rqdatac.services.concept.get_stock_concept
+shenwan_industry = rqdatac.services.shenwan.shenwan_industry
+shenwan_instrument_industry = rqdatac.services.shenwan.shenwan_instrument_industry
+zx_industry = rqdatac.services.shenwan.zx_industry
+zx_instrument_industry = rqdatac.services.shenwan.zx_instrument_industry
+get_industry = rqdatac.services.shenwan.get_industry
+get_industry_change = rqdatac.services.shenwan.get_industry_change
+get_instrument_industry = rqdatac.services.shenwan.get_instrument_industry
+get_industry_mapping = rqdatac.services.shenwan.get_industry_mapping
+industry_code = rqdatac.services.constant.IndustryCode
+IndustryCode = rqdatac.services.constant.IndustryCode
+sector_code = rqdatac.services.constant.SectorCode
+SectorCode = rqdatac.services.constant.SectorCode
+get_trading_dates = rqdatac.services.calendar.get_trading_dates
+get_next_trading_date = rqdatac.services.calendar.get_next_trading_date
+get_previous_trading_date = rqdatac.services.calendar.get_previous_trading_date
+get_latest_trading_date = rqdatac.services.calendar.get_latest_trading_date
+get_future_latest_trading_date = rqdatac.services.calendar.get_future_latest_trading_date
+trading_date_offset = rqdatac.services.calendar.trading_date_offset
+is_trading_date = rqdatac.services.calendar.is_trading_date
+has_night_trading = rqdatac.services.calendar.has_night_trading
+id_convert = rqdatac.services.basic.id_convert
+get_tick_size = rqdatac.services.basic.get_tick_size
+instruments = rqdatac.services.basic.instruments
+all_instruments = rqdatac.services.basic.all_instruments
+sector = rqdatac.services.basic.sector
+industry = rqdatac.services.basic.industry
+get_future_contracts = rqdatac.services.basic.get_future_contracts
+futures: str = ...
+jy_instrument_industry = rqdatac.services.basic.jy_instrument_industry
+econ: str = ...
+get_main_shareholder = rqdatac.services.basic.get_main_shareholder
+get_current_news = rqdatac.services.basic.get_current_news
+get_trading_hours = rqdatac.services.basic.get_trading_hours
+get_private_placement = rqdatac.services.basic.get_private_placement
+get_share_transformation = rqdatac.services.basic.get_share_transformation
+user: str = ...
+get_update_status = rqdatac.services.basic.get_update_status
+info = rqdatac.services.basic.info
+get_basic_info = rqdatac.services.basic.get_basic_info
+get_spot_benchmark_price = rqdatac.services.basic.get_spot_benchmark_price
+LiveMarketDataClient = rqdatac.services.live_md_client.LiveMarketDataClient
+AsyncLiveMarketDataClient = rqdatac.services.async_live_md_client.AsyncLiveMarketDataClient
+consensus: str = ...
+get_consensus_indicator = rqdatac.services.consensus.get_indicator
+get_consensus_price = rqdatac.services.consensus.get_price
+get_consensus_comp_indicators = rqdatac.services.consensus.get_comp_indicators
+all_consensus_industries = rqdatac.services.consensus.all_industries
+get_consensus_industry_rating = rqdatac.services.consensus.get_industry_rating
+get_consensus_market_estimate = rqdatac.services.consensus.get_market_estimate
+get_dominant_future = rqdatac.services.future.get_dominant_future
+future_commission_margin = rqdatac.services.future.future_commission_margin
+get_future_member_rank = rqdatac.services.future.get_future_member_rank
+current_stock_connect_quota = rqdatac.services.stock_status.current_stock_connect_quota
+get_stock_connect_quota = rqdatac.services.stock_status.get_stock_connect_quota
+is_st_stock = rqdatac.services.stock_status.is_st_stock
+_is_st_stock = rqdatac.services.stock_status._is_st_stock
+is_suspended = rqdatac.services.stock_status.is_suspended
+get_stock_connect = rqdatac.services.stock_status.get_stock_connect
+get_securities_margin = rqdatac.services.stock_status.get_securities_margin
+get_margin_stocks = rqdatac.services.stock_status.get_margin_stocks
+get_shares = rqdatac.services.stock_status.get_shares
+get_allotment = rqdatac.services.stock_status.get_allotment
+get_symbol_change_info = rqdatac.services.stock_status.get_symbol_change_info
+get_special_treatment_info = rqdatac.services.stock_status.get_special_treatment_info
+get_incentive_plan = rqdatac.services.stock_status.get_incentive_plan
+get_investor_ra = rqdatac.services.stock_status.get_investor_ra
+get_announcement = rqdatac.services.stock_status.get_announcement
+get_holder_number = rqdatac.services.stock_status.get_holder_number
+get_audit_opinion = rqdatac.services.stock_status.get_audit_opinion
+get_restricted_shares = rqdatac.services.stock_status.get_restricted_shares
+st_warning = rqdatac.services.stock_status.st_warning
+current_snapshot = rqdatac.services.live.current_snapshot
+get_ticks = rqdatac.services.live.get_ticks
+current_minute = rqdatac.services.live.current_minute
+get_live_ticks = rqdatac.services.live.get_live_ticks
+get_price = rqdatac.services.get_price.get_price
+get_stock_connect_holding_details = rqdatac.services.get_price.get_stock_connect_holding_details
+get_vwap = rqdatac.services.get_price.get_vwap
+convertible: str = ...
+current_freefloat_turnover = rqdatac.services.extra.current_freefloat_turnover
+get_live_minute_price_change_rate = rqdatac.services.extra.get_live_minute_price_change_rate
+get_all_factor_names = rqdatac.services.factor.get_all_factor_names
+get_factor = rqdatac.services.factor.get_factor
+get_factor_return = rqdatac.services.factor.get_factor_return
+get_live_factor_return = rqdatac.services.factor.get_live_factor_return
+get_factor_exposure = rqdatac.services.factor.get_factor_exposure
+get_style_factor_exposure = rqdatac.services.factor.get_style_factor_exposure
+get_descriptor_exposure = rqdatac.services.factor.get_descriptor_exposure
+get_stock_beta = rqdatac.services.factor.get_stock_beta
+get_factor_covariance = rqdatac.services.factor.get_factor_covariance
+get_specific_return = rqdatac.services.factor.get_specific_return
+get_specific_risk = rqdatac.services.factor.get_specific_risk
+get_index_factor_exposure = rqdatac.services.factor.get_index_factor_exposure
+get_pit_financials_ex = rqdatac.services.financial.get_pit_financials_ex
+current_performance = rqdatac.services.financial.current_performance
+performance_forecast = rqdatac.services.financial.performance_forecast
+get_capital_flow = rqdatac.services.get_capital_flow.get_capital_flow
+current_capital_flow_minute = rqdatac.services.get_capital_flow.current_capital_flow_minute
+get_open_auction_info = rqdatac.services.get_capital_flow.get_open_auction_info
+get_close_auction_info = rqdatac.services.get_capital_flow.get_close_auction_info
+index_components = rqdatac.services.index.index_components
+index_weights = rqdatac.services.index.index_weights
+index_indicator = rqdatac.services.index.index_indicator
+index_weights_ex = rqdatac.services.index.index_weights_ex
+get_ksh_auction_info = rqdatac.services.ksh_auction_info.get_ksh_auction_info
+get_auction_info = rqdatac.services.ksh_auction_info.get_auction_info
+get_split = rqdatac.services.market_data.get_split
+get_dividend = rqdatac.services.market_data.get_dividend
+get_dividend_info = rqdatac.services.market_data.get_dividend_info
+get_dividend_amount = rqdatac.services.market_data.get_dividend_amount
+get_ex_factor = rqdatac.services.market_data.get_ex_factor
+get_turnover_rate = rqdatac.services.market_data.get_turnover_rate
+get_price_change_rate = rqdatac.services.market_data.get_price_change_rate
+get_yield_curve = rqdatac.services.market_data.get_yield_curve
+get_block_trade = rqdatac.services.market_data.get_block_trade
+get_exchange_rate = rqdatac.services.market_data.get_exchange_rate
+get_temporary_code = rqdatac.services.market_data.get_temporary_code
+get_interbank_offered_rate = rqdatac.services.market_data.get_interbank_offered_rate
+get_abnormal_stocks = rqdatac.services.market_data.get_abnormal_stocks
+get_abnormal_stocks_detail = rqdatac.services.market_data.get_abnormal_stocks_detail
+get_buy_back = rqdatac.services.market_data.get_buy_back
+options: str = ...
+fenji: str = ...
+ecommerce = rqdatac.services.tmall.ecommerce
+xueqiu: str = ...
