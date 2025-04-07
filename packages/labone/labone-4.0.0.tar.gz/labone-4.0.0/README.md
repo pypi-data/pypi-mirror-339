cff5859098b3e89f8d976bfefbfe3ba3ca9de576
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-07 15:04:42 +0000

    Add package labone/4.0.0/labone-4.0.0.tar.gz

---
+++ packages/labone/labone-4.0.0.tar.gz/labone-4.0.0/README.md
@@ -0,0 +1,6 @@
+# LabOne Python API
+
+The `labone` package has been removed for the time being. 
+Please refer to [zhinst-toolkit](https://pypi.org/project/zhinst-toolkit/)
+for the latest LabOne Python API.
+
---
+++ packages/labone/labone-4.0.0.tar.gz/labone-4.0.0/src/labone/__init__.py
@@ -0,0 +1,11 @@
+"""Deprecated package for the Zurich Instruments LabOne software.
+
+The `labone` package has been removed for the time being.
+Please refer to [zhinst-toolkit](https://pypi.org/project/zhinst-toolkit/)
+for the latest LabOne Python API.
+"""
+
+msg = """The `labone` package has been removed for the time being.
+Please refer to [zhinst-toolkit](https://pypi.org/project/zhinst-toolkit/)
+for the latest LabOne Python API."""
+raise ImportError(msg)
