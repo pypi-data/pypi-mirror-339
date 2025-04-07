aaa7507a679cf5c37a95873d8118b6a29cbc8e06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-07 16:04:30 +0000

    Add package StatOmics/0.3/statomics-0.3-py3-none-any.whl

---
+++ packages/StatOmics/statomics-0.3-py3-none-any.whl/StatOmics/Preprocessing/Normalization_bySingleRef.R
@@ -0,0 +1,27 @@
+# Normalization_bySingleRef.R
+
+library(dplyr)
+
+# Normalization against a Single Reference
+reference_normalization <- function(reference, data_to_norm) {
+  # Initialize data frame
+  normalized_data <- data.frame(row.names = rownames(data_to_norm))
+  
+  character_cols <- data_to_norm %>% select(where(is.character))
+  
+  numeric_columns <- sapply(data_to_norm, is.numeric)
+  
+  for (col in which(numeric_columns)) {
+    col_name <- names(data_to_norm)[col]
+    
+    if(col_name %in% colnames(reference)) {
+      normalized_data[, col_name] <- data_to_norm[, col_name] / reference[, col_name]
+    } else {
+      warning(paste("Column", col_name, "not found in reference data"))
+    }
+  }
+  # Bind back gene and protein info
+  cbind(character_cols, normalized_data)
+  
+  return(normalized_data)
+}
