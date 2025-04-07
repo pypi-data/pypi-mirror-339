8f9a926c1a23582d0bb5b6119c29f90f2c5c6c5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-07 03:18:01 +0000

    Add package maleo-core/0.3.4/maleo_core-0.3.4.tar.gz

---
+++ packages/maleo-core/maleo_core-0.3.4.tar.gz/maleo_core-0.3.4/maleo_core/models/maleo_suite/maleo_access/transfers/results/query/user.py
@@ -0,0 +1,29 @@
+from __future__ import annotations
+from pydantic import Field
+from typing import Optional
+from maleo_core.models.base.transfers.results.services.query import BaseServiceQueryResults
+from maleo_core.models.maleo_suite.maleo_access.transfers.general.user import MaleoAccessUserGeneralTransfers
+from maleo_core.models.maleo_suite.maleo_access.transfers.general.user_profile import MaleoAccessUserProfileGeneralTransfers
+from maleo_core.models.maleo_suite.maleo_access.transfers.results.query.organization import MaleoAccessOrganizationQueryResults
+from maleo_core.models.maleo_suite.maleo_access.transfers.results.query.system_role import MaleoAccessSystemRoleQueryResults
+from maleo_core.models.maleo_suite.maleo_access.transfers.results.query.user_profile import MaleoAccessUserProfileQueryResults
+from maleo_core.models.maleo_suite.maleo_access.transfers.results.query.user_type import MaleoAccessUserTypeQueryResults
+
+class MaleoAccessUserQueryResults:
+    class UserSystemRole(MaleoAccessUserProfileGeneralTransfers.Base):
+        system_role:MaleoAccessSystemRoleQueryResults.Get = Field(..., description="System Role's data")
+
+    class Get(MaleoAccessUserGeneralTransfers.Base, BaseServiceQueryResults.Get):
+        user_type:MaleoAccessUserTypeQueryResults.Get = Field(..., description="User's type")
+        profile:Optional[MaleoAccessUserProfileQueryResults.Get] = Field(None, description="User's profile")
+        system_roles:list[MaleoAccessSystemRoleQueryResults.Get] = Field(..., description="User's system roles")
+        users_system_roles:list[MaleoAccessUserQueryResults.UserSystemRole] = Field(..., description="Users system roles")
+        organizations:list[MaleoAccessOrganizationQueryResults.Get] = Field(..., description="User's organizations")
+
+    Fail = BaseServiceQueryResults.Fail
+
+    class SingleData(BaseServiceQueryResults.SingleData):
+        data:Optional[MaleoAccessUserQueryResults.Get]
+
+    class MultipleData(BaseServiceQueryResults.MultipleData):
+        data:list[MaleoAccessUserQueryResults.Get]
