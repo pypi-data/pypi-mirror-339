1c31694cd3865f2bc24dcac62ecd0b9fe67df35e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-07 16:04:26 +0000

    Add package akm/1.3.2/akm-1.3.2.tar.gz

---
+++ packages/akm/akm-1.3.2.tar.gz/akm-1.3.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='akm',
+    version='1.3.2',
+    description='A library for generating and checking temporary Gmail emails.',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+    ],
+    entry_points={
+        'console_scripts': [
+            'gen-gmail=akm.akm:gen',
+            'check-gmail=akm.akm:check',
+        ],
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
