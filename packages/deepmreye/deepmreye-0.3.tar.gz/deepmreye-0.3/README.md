34072f04c7ca609e674cea2d5ac5fdb8a64fe30a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-07 16:04:36 +0000

    Add package deepmreye/0.3/deepmreye-0.3.tar.gz

---
+++ packages/deepmreye/deepmreye-0.3.tar.gz/deepmreye-0.3/README.md
@@ -0,0 +1,113 @@
+[![License: GPL v3](https://img.shields.io/badge/License-GPL%20v3-blue.svg)](http://www.gnu.org/licenses/gpl-3.0)
+![py38 status](https://img.shields.io/badge/python3.8-supported-green.svg)
+![Build Status](https://github.com/DeepMReye/DeepMReye/actions/workflows/main.yml/badge.svg)
+[![NatNeuro Paper](https://img.shields.io/badge/DOI-10.1038%2Fs41593--021--00947--w-blue)](https://doi.org/10.1038/s41593-021-00947-w)
+![Docker Pulls](https://img.shields.io/docker/pulls/deepmreye/deepmreye)
+![Logo](media/deepmreye_logo.png)
+
+# DeepMReye: magnetic resonance-based eye tracking using deep neural networks
+This [Jupyter Notebook](./notebooks/deepmreye_example_usage.ipynb) provides a step-by-step walkthrough of the code. It includes eyeball coregistration, voxel extraction, model training and test as well as basic performance measures. Alternatively, here is a [Colab Notebook](https://colab.research.google.com/drive/1kYVyierbKdNZ3RY4_pbACtdWEw7PKQuz?usp=sharing) that runs in the browser. DeepMReye also includes a user-friendly streamlit app (installation option 4): Drag and drop your fMRI data into the interface and download your gaze coordinates shortly after.
+
+This [Data Repository](https://osf.io/mrhk9/) includes exemplary data for model training and test, source data of all paper figures as well as pre-trained model weights.
+
+If you have questions, please check out our [Frequently Asked Questions](https://github.com/DeepMReye/DeepMReye/wiki/DeepMReye-%E2%80%90-FAQ) page. If you cannot find the answer to your question there, reach out to us!
+
+![deepMReye video](media/deepMReye_video.gif)
+
+## Installation
+
+### Option 1: Pip install
+
+#### Pip installation
+Install DeepMReye with a CPU/GPU version of [TensorFlow](https://www.tensorflow.org/install/) using the following command.
+```
+pip install deepmreye
+```
+
+#### Anaconda / Miniconda installation
+
+To encapsulate DeepMReye in a virtual environment install with the following commands:
+```
+conda create --name deepmreye python=3.9
+conda activate deepmreye
+pip install deepmreye
+```
+For GPU support, follow tensorflow [install](https://www.tensorflow.org/install/pip) instruction, e.g.:
+```
+conda install -c conda-forge cudatoolkit=11.2 cudnn=8.1.0
+```
+If installation of [ANTsPy](https://github.com/ANTsX/ANTsPy) fails try to manually install it via:
+```
+git clone https://github.com/ANTsX/ANTsPy
+cd ANTsPy
+pip install CMake
+python3 setup.py install
+```
+
+### Option 2: Colab
+
+We provide a [Colab Notebook](https://colab.research.google.com/drive/1kYVyierbKdNZ3RY4_pbACtdWEw7PKQuz?usp=sharing) showcasing model training and evaluation on a GPU provided by Google Colab. To use your own data, preprocess your data locally and upload only the extracted eyeball voxels. This saves space and avoids data privacy issues. See the [Jupyter Notebook](./notebooks/deepmreye_example_usage.ipynb) for the preprocessing and eyeball-extraction code.
+
+[![Model Training & Evaluation](https://colab.research.google.com/assets/colab-badge.svg)](https://colab.research.google.com/drive/1kYVyierbKdNZ3RY4_pbACtdWEw7PKQuz?usp=sharing)
+
+![Colab Walkthrough](media/colab_walkthrough.gif)
+
+### Option 3: Docker
+
+Pull the image from docker hub.
+
+```bash
+docker pull deepmreye/deepmreye
+```
+
+Use deepMReye in a docker container via jupyterlab:
+
+```bash
+mkdir -p $PWD/notebooks
+docker run -it --rm \
+    --publish 8888:8888 \
+    --volume $PWD/notebooks:/home/neuro/notebooks \
+    deepmreye/deepmreye:latest \
+        jupyter-lab --no-browser --ip 0.0.0.0
+```
+
+### Option 4: Streamlit app
+
+If you would like decode gaze coordinates in your data using a pretrained model, the easiest way is using our streamlit app.
+
+Running the following commands will open a browser window that allows you to upload your data (.nii or .nii.gz) and then download the corresponding gaze coordinates shortly after.
+Please read our [FAQ page](https://github.com/DeepMReye/DeepMReye/wiki/DeepMReye-%E2%80%90-FAQ) before using the pretrained models.
+
+```bash
+git clone https://github.com/DeepMReye/DeepMReye.git
+cd DeepMReye
+pip install .
+pip install streamlit
+streamlit run streamlit/streamlit.py
+```
+
+### Data formats
+The <u>**fMRI data**</u> should be organized in 4D NIFTI files (.nii), containing the realigned 3D images acquired over time. The pipeline then extracts the eyeball voxels automatically and saves them as Python Pickle files, which serve as model input. For model training, you additionally need <u>**training labels**</u>, a numpy array containing 10 gaze coordinates per functional volume. These gaze coordinates can either be camera-based eye-tracking labels or the coordinates of a fixation target, and many file formats can be easily read (e.g. .npy, .npz, .mat, .csv etc.).
+
+## Hardware requirements
+
+The GPU version of DeepMReye requires a NVIDIA GPU.
+
+## Software requirements
+The following python dependencies are being automatically installed when installing DeepMReye (specified in setup.cfg):
+```
+tensorflow-gpu (2.2.0)
+numpy (1.19.1)
+pandas (1.0.5)
+matplotlib (3.2.2)
+scipy (1.5.0)
+ipython (7.13.0)
+plotly (4.14.3)
+```
+Version in parentheses indicate the ones used for testing the framework. Its extensively tested on Linux 16.04 but should run on all OS (Windows, Mac, Linux) supporting a Python version >3.8 and pip. It is recommended to install the framework and dependencies in a virtual environment (e.g. conda).
+
+## BIDS app
+If you would like to run a pretrained version of DeepMReye on datasets that follow the Brain Imaging Data Structure (BIDS) format, check out [bidsMReye](https://pypi.org/project/bidsmreye/): a wrapper for DeepMReye for BIDS datasets (incl. [fMRIprep](https://fmriprep.org/en/stable/) outputs) developed by [Remi Gau](https://remi-gau.github.io/).
+
+## Correspondence
+If you have questions, comments or inquiries, please reach out to us: markus.frey1[at]gmail.com & m.nau[at]vu.nl
---
+++ packages/deepmreye/deepmreye-0.3.tar.gz/deepmreye-0.3/deepmreye/train.py
@@ -0,0 +1,196 @@
+from os.path import join
+
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+import tensorflow.keras.backend as K
+
+from deepmreye import architecture
+from deepmreye.util import data_generator, util
+
+
+def train_model(
+    dataset,
+    generators,
+    opts,
+    clear_graph=True,
+    save=False,
+    model_path="./",
+    workers=4,
+    use_multiprocessing=True,
+    models=None,
+    return_untrained=False,
+    verbose=0,
+):
+    """Train the model given a cross validation, \
+       hold out or leave one out generator, given model options.
+
+    Parameters
+    ----------
+    dataset : str
+        Description of dataset, used for saving dataset to file
+    generators : generator
+        Cross validation, Hold out or Leave one out generator, yielding X,y pairs
+    opts : dict
+        Model options for training
+    clear_graph : bool, optional
+        If computational graph should be reset before each run, by default True
+    save : bool, optional
+        If model weights should be saved to file, by default False
+    model_path : str, optional
+        Filepath to where model weights should be stored, by default './'
+    workers : int, optional
+        Number of workers used when using multiprocessing, by default 4
+    use_multiprocessing : bool, optional
+        If multiprocessing should be used, can speed up training by 10x if data loader is bottleneck, by default True
+    models : Keras Model instance, optional
+        Can be provided if already trained model should be used instead of training a new one, by default None
+    return_untrained : bool, optional
+        If true, returns untrained but compiled model, by default False
+    verbose : int, optional
+        Verbosity level, by default 0
+
+    Returns
+    -------
+    model : Keras Model
+        Full model instance, used for training uncertainty estimate
+    model_inference : Keras Model
+        Model instance used for inference, provides uncertainty estimate (unsupervised model)
+    """
+    # Clear session if needed
+    if clear_graph:
+        K.clear_session()
+    if use_multiprocessing:
+        tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
+    else:
+        workers = 1
+
+    # Unpack generators
+    (
+        training_generator,
+        testing_generator,
+        single_testing_generators,
+        single_testing_names,
+        single_training_generators,
+        single_training_names,
+        full_testing_list,
+        full_training_list,
+    ) = generators
+
+    # Test datagenerator and get representative X and y
+    ((X, y), _) = next(training_generator)
+    if verbose > 0:
+        print(f"Input shape {X.shape}, Output shape {y.shape}")
+        print(
+            f"Subjects in training set: {len(single_training_generators)}, "
+            f"Subjects in test set: {len(single_testing_generators)}"
+        )
+
+    # Learning rate scheduler
+    lr_sched = util.step_decay_schedule(initial_lr=opts["lr"], decay_factor=0.9, num_epochs=opts["epochs"])
+
+    # Get model
+    if models is None:
+        model, model_inference = architecture.create_standard_model(X.shape[1::], opts)
+    else:
+        model, model_inference = models
+    if return_untrained:
+        tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.INFO)
+        return (model, model_inference)
+
+    # Train model
+    if verbose > 1:
+        print(model.summary(line_length=200))
+    model.fit(
+        training_generator,
+        steps_per_epoch=opts["steps_per_epoch"],
+        epochs=opts["epochs"],
+        validation_data=testing_generator,
+        validation_steps=opts["validation_steps"],
+        callbacks=[lr_sched],
+        use_multiprocessing=use_multiprocessing,
+        workers=workers,
+    )
+
+    # Save model weights
+    if save:
+        model_inference.save_weights(join(model_path, f"modelinference_{dataset}.h5"))
+    if use_multiprocessing:
+        tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.INFO)
+
+    return (model, model_inference)
+
+
+def evaluate_model(dataset, model, generators, save=False, model_path="./", model_description="", verbose=0, **args):
+    """Evaluate model performance given model and generators \
+       used for training the model.
+
+       Evaluate only on test set.
+
+    Parameters
+    ----------
+    dataset : str
+        Description of dataset, used for saving dataset to file
+    model : Keras Model
+        Full model instance, used for training uncertainty estimate
+    generators : generator
+        Cross validation, Hold out or Leave one out generator, yielding X,y pairs
+    save : bool, optional
+        If true, save test set predictions to file, by default False
+    model_path : str, optional
+        Filepath to where model weights should be stored, by default './'
+    model_description : str, optional
+        Description of model used for saving the model evaluations, by default ''
+    verbose : int, optional
+        Verbosity level, by default 0
+
+    Returns
+    -------
+    evaluation: dict
+        Raw gaze coordinates, returned for each participant
+    scores: pandas DataFrame
+        Evaluation metrics for gaze coordinates (Pearson, R2-Score, Euclidean Error)
+    """
+    (
+        training_generator,
+        testing_generator,
+        single_testing_generators,
+        single_testing_names,
+        single_training_generators,
+        single_training_names,
+        full_testing_list,
+        full_training_list,
+    ) = generators
+    evaluation, scores = dict(), dict()
+    for idx, subj in enumerate(full_testing_list):
+        X, real_y = data_generator.get_all_subject_data(subj)
+        (pred_y, euc_pred) = model.predict(X, verbose=verbose - 2, batch_size=16)
+        evaluation[subj] = {"real_y": real_y, "pred_y": pred_y, "euc_pred": euc_pred}
+
+        # Quantify predictions
+        df_scores = util.get_model_scores(real_y, pred_y, euc_pred, **args)
+        scores[subj] = df_scores
+
+        # Print evaluation
+        if verbose > 0:
+            print(
+                f"{util.color.BOLD}{idx + 1} / {len(single_testing_names)} - "
+                f"Model Performance for {subj}{util.color.END}"
+            )
+            if verbose > 1:
+                pd.set_option("display.width", 120)
+                pd.options.display.float_format = "{:.3f}".format  # noqa
+                print(df_scores)
+            else:
+                print(
+                    f"Default: r={df_scores[('Pearson', 'Mean')]['Default']:.3f}, "
+                    f"subTR: r={df_scores[('Pearson', 'Mean')]['Default subTR']:.3f}, "
+                    f"Euclidean Error: {df_scores[('Eucl. Error', 'Mean')]['Default']:.3f}°"
+                )
+            print("\n")
+
+    # Save dict
+    if save:
+        np.save(join(model_path, f"results{model_description}_{dataset}.npy"), evaluation)
+
+    return (evaluation, scores)
