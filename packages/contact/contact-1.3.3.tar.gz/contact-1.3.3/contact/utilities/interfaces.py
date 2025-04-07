9acb5985144367baa28c6a05bd1b42f4cdb1ae2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-07 14:04:16 +0000

    Add package contact/1.3.3/contact-1.3.3.tar.gz

---
+++ packages/contact/contact-1.3.3.tar.gz/contact-1.3.3/contact/utilities/interfaces.py
@@ -0,0 +1,23 @@
+import logging
+import meshtastic.serial_interface, meshtastic.tcp_interface, meshtastic.ble_interface
+import contact.globals as globals
+
+def initialize_interface(args):
+    try:
+        if args.ble:
+            return meshtastic.ble_interface.BLEInterface(args.ble if args.ble != "any" else None)
+        elif args.host:
+            return meshtastic.tcp_interface.TCPInterface(args.host)
+        else:
+            try:
+                return meshtastic.serial_interface.SerialInterface(args.port)
+            except PermissionError as ex:
+                logging.error(f"You probably need to add yourself to the `dialout` group to use a serial connection. {ex}")
+            except Exception as ex:
+                logging.error(f"Unexpected error initializing interface: {ex}")
+            if globals.interface.devPath is None:
+                return meshtastic.tcp_interface.TCPInterface("meshtastic.local")
+    
+    except Exception as ex:
+        logging.critical(f"Fatal error initializing interface: {ex}")
+    
