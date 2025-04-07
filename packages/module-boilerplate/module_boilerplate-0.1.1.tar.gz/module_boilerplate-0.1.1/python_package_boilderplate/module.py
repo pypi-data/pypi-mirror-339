73ccb44bbe3c49c0db623e33ab292bc6db87fb01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-07 15:04:41 +0000

    Add package module-boilerplate/0.1.1/module_boilerplate-0.1.1.tar.gz

---
+++ packages/module-boilerplate/module_boilerplate-0.1.1.tar.gz/module_boilerplate-0.1.1/python_package_boilderplate/module.py
@@ -0,0 +1,3 @@
+def add(x, y):
+    """Add two numbers and return the sum."""
+    return x + y
---
+++ packages/module-boilerplate/module_boilerplate-0.1.1.tar.gz/module_boilerplate-0.1.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='module_boilerplate',
+    version='0.1.1', 
+    packages=find_packages(),
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Your Name',
+    author_email='your.email@example.com',
+    url='https://github.com/SoyVitouPro/python-modules-boilerplate',  # Optional
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
