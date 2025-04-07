568fc816ee1bc6fd341feb8b202ad4b3458706d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-07 03:18:43 +0000

    Add package wipo-gbd-transformation/1.1.72/wipo_gbd_transformation-1.1.72-py3-none-any.whl

---
+++ packages/wipo-gbd-transformation/wipo_gbd_transformation-1.1.72-py3-none-any.whl/gbdtransformation/brands/phtm/__init__.py
@@ -0,0 +1,8 @@
+# instruction to render the output to JSON format
+render = 'JSON'
+source = 'national'
+
+# PH/4/1981/00000115
+# PH/M/0001/01573483 
+appnum_mask = [ 'PH/\\d/\\d*/(\\d*)', 'PH/M/\\d*/(\\d*)' ]
+regnum_mask = [ 'PH/\\d/\\d*/(\\d*)', 'PH/M/\\d*/(\\d*)' ]
