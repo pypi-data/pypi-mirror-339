ba0ee7918ae021b7460193a014db3eb4618fecf1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-07 15:05:04 +0000

    Add package d3sdk/0.1.20/d3sdk-0.1.20-py3-none-any.whl

---
+++ packages/d3sdk/d3sdk-0.1.20-py3-none-any.whl/d3sdk/model/business_model_define.py
@@ -0,0 +1,56 @@
+from typing import List
+
+class DeviceMeasurementGroup:
+    def __init__(self, deviceCode, deviceName, measurementGroupCode, measurementGroupName, deviceTypeCode, schema):
+        self.deviceCode = deviceCode
+        self.deviceName = deviceName
+        self.measurementGroupCode = measurementGroupCode
+        self.measurementGroupName = measurementGroupName
+        self.deviceTypeCode = deviceTypeCode
+        self.schema = schema
+
+    def __repr__(self):
+        return f"DeviceMeasurementGroup(deviceCode={self.deviceCode}, deviceTypeCode={self.deviceTypeCode}, measurementGroupCode={self.measurementGroupCode}, schema={self.schema})"
+
+class InstanceMeasurement:
+    def __init__(self, deviceCode, deviceTypeCode, schema, schemaColumn, schemaColumnName, repoCode, repoName, 
+                 repoColumn, unit, lowerBound, upperBound, type, measurementGroupCode, measurementGroupName, measurement, measurementName):
+        self.deviceCode = deviceCode
+        self.deviceTypeCode = deviceTypeCode
+        self.schema = schema
+        self.schemaColumn = schemaColumn
+        self.schemaColumnName = schemaColumnName
+        self.schema = schema
+        self.repoCode = repoCode
+        self.repoName = repoName
+        self.repoColumn = repoColumn
+        self.unit = unit
+        self.lowerBound = lowerBound
+        self.upperBound = upperBound
+        self.type = type
+        self.measurement = measurement
+        self.measurementGroupCode = measurementGroupCode
+        self.measurementGroupName = measurementGroupName
+        self.measurementName = measurementName
+    def __repr__(self):
+        return f"InstanceMeasurement(deviceCode={self.deviceCode}, measurement={self.measurement}, repoCode={self.repoCode}, repoColumn={self.repoColumn})"
+    
+class AlarmInstancePointsConfig:
+    def __init__(self, deviceCode, deviceType, events, related):
+        self.deviceCode = deviceCode
+        self.deviceType = deviceType
+        self.events = events
+        self.related = related
+
+    def __repr__(self):
+        return f"AlarmInstancePointsConfig(deviceCode={self.deviceCode}, events={self.events}, related={self.related})"
+
+class AlarmInstancePointsStatistic:
+    def __init__(self, deviceCode, alarmCode, measurements:List[str]):
+        self.deviceCode = deviceCode
+        self.alarmCode = alarmCode
+        self.measurements = measurements
+
+    def __repr__(self):
+        return f"AlarmInstancePointsStatistic(deviceCode={self.deviceCode}, alarmCode={self.alarmCode}, measurements={self.measurements})"
+    
