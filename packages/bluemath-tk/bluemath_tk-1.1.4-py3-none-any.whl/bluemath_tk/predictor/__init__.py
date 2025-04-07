f22e02f013bbef3e90a029ae913c195b7c78ae44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-07 03:18:06 +0000

    Add package bluemath-tk/1.1.4/bluemath_tk-1.1.4-py3-none-any.whl

---
+++ packages/bluemath-tk/bluemath_tk-1.1.4-py3-none-any.whl/bluemath_tk/predictor/__init__.py
@@ -0,0 +1,15 @@
+"""
+Project: BlueMath_tk
+Sub-Module: predictor
+Author: GeoOcean Research Group, Universidad de Cantabria
+Creation Date: 19 March 2024
+License: MIT
+Repository: https://github.com/GeoOcean/BlueMath_tk.git
+Status: Under development (Working)
+"""
+
+# Import essential functions/classes to be available at the package level.
+from .xwt import XWT
+
+# Optionally, define the module's `__all__` variable to control what gets imported when using `from module import *`.
+__all__ = ["XWT"]
