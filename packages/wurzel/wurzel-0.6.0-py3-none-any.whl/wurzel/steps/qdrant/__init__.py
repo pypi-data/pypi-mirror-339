903a12e46b4c2d26222b1e5c73fb5073bcaa65c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-07 04:04:43 +0000

    Add package wurzel/0.6.0/wurzel-0.6.0-py3-none-any.whl

---
+++ packages/wurzel/wurzel-0.6.0-py3-none-any.whl/wurzel/steps/qdrant/__init__.py
@@ -0,0 +1,24 @@
+# SPDX-FileCopyrightText: 2025 Deutsche Telekom AG (opensource@telekom.de)
+#
+# SPDX-License-Identifier: Apache-2.0
+
+from typing import Type
+
+from ...step import TypedStep
+from .data import QdrantResult
+
+__all__ = []
+try:
+    _HAS_QDRANT = True
+    import qdrant_client as _  # noqa: F401
+except ImportError:
+    _HAS_QDRANT = False
+__all__ = ["QdrantResult", "STEPS"]
+STEPS: list[Type[TypedStep]] = []
+if _HAS_QDRANT:
+    from .step import QdrantConnectorStep  # noqa: F401
+    from .step_multi_vector import QdrantConnectorMultiVectorStep  # noqa: F401
+
+    __all__.extend(["QdrantConnectorStep", "QdrantConnectorMultiVectorStep"])
+else:
+    pass
