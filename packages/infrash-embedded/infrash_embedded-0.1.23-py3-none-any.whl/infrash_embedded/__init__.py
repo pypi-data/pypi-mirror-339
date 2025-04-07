4dbf39e3085a8c9390461294fbb338194cb28a50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-07 03:18:05 +0000

    Add package infrash-embedded/0.1.23/infrash_embedded-0.1.23-py3-none-any.whl

---
+++ packages/infrash-embedded/infrash_embedded-0.1.23-py3-none-any.whl/infrash_embedded/__init__.py
@@ -0,0 +1,11 @@
+# __init__.py
+# Version information
+
+# Import only basic package information here
+# Avoid importing components directly to prevent circular imports
+from infrash_embedded.infrash_embedded import infrash_embedded
+
+__version__ = "0.1.2"
+__version_tuple__ = (0, 1, 1)
+
+__all__ = ["infrash_embedded","infrash_embedded"]
