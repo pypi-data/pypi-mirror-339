fc77e02c6e2d8401ffd3205643ba86a008e87ae9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-07 05:04:18 +0000

    Add package utstd/0.1.1/utstd-0.1.1.tar.gz

---
+++ packages/utstd/utstd-0.1.1.tar.gz/utstd-0.1.1/tests/test_folders.py
@@ -0,0 +1,30 @@
+import pytest
+
+from utstd.folders import LabExFolder, AtFolder
+
+
+@pytest.fixture
+def lab01_ex02():
+    return LabExFolder(
+        course_code="36106",
+        lab="lab01",
+        exercise="ex02",
+    )
+
+@pytest.fixture
+def at1():
+    return AtFolder(
+        course_code="36106",
+        assignment="AT1",
+    )
+
+def test_default_initial_amount(lab01_ex02):
+    assert lab01_ex02.course_code == "36106"
+    assert lab01_ex02._lab == "lab01"
+    assert lab01_ex02._exercise == "ex02"
+    assert lab01_ex02.folder_path is not None
+
+def test_setting_initial_amount(at1):
+    assert at1.course_code == "36106"
+    assert at1._assignment == "AT1"
+    assert at1.folder_path is not None
