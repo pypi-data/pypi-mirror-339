02b274b4efda9502b6ceca369cc0d932af5170d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-07 14:04:33 +0000

    Add package phi-cloud-server/0.1.1a0/phi_cloud_server-0.1.1a0.tar.gz

---
+++ packages/phi-cloud-server/phi_cloud_server-0.1.1a0.tar.gz/phi_cloud_server-0.1.1a0/phi_cloud_server/__init__.py
@@ -0,0 +1,19 @@
+from phi_cloud_server.main import app
+from phi_cloud_server.utils import dev_mode
+
+# ---------------------- 启动 ----------------------
+
+RELOAD_DIR = "./phi_cloud_server"
+
+def main():
+    import uvicorn
+
+    # dev启用热重载
+    if dev_mode:
+        import jurigged
+
+        jurigged.watch(
+            pattern=RELOAD_DIR,
+        )
+
+    uvicorn.run(app, host="0.0.0.0", port=8000)
