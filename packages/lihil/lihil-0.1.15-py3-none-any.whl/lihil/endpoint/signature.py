8f06c22aea75a3c38d13b7d6144ccec65c057840
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-07 05:04:22 +0000

    Add package lihil/0.1.15/lihil-0.1.15-py3-none-any.whl

---
+++ packages/lihil/lihil-0.1.15-py3-none-any.whl/lihil/endpoint/signature.py
@@ -0,0 +1,206 @@
+from inspect import signature
+from typing import Any, Awaitable, Callable, Mapping
+
+from ididi import DependentNode, Graph
+from msgspec import DecodeError, Struct, ValidationError, field
+from starlette.requests import Request
+
+from lihil.config import AppConfig
+from lihil.endpoint.params import (
+    ParamParser,
+    PluginParam,
+    RequestBodyParam,
+    RequestParam,
+)
+from lihil.endpoint.returns import EndpointReturn, parse_returns
+from lihil.interface import MISSING, Base, IEncoder, Record, is_provided
+from lihil.problems import (
+    CustomDecodeErrorMessage,
+    CustomValidationError,
+    InvalidDataType,
+    InvalidJsonReceived,
+    MissingRequestParam,
+    ValidationProblem,
+)
+from lihil.utils.string import find_path_keys
+from lihil.vendor_types import FormData
+
+type ParamMap[T] = dict[str, T]
+type RequestParamMap = dict[str, RequestParam[Any]]
+
+
+def is_form_body(param_pair: tuple[str, RequestBodyParam[Any]] | None):
+    if not param_pair:
+        return False
+    _, param = param_pair
+    return param.content_type == "multipart/form-data" and param.type_ is not bytes
+
+
+class ParseResult(Record):
+    params: dict[str, Any]
+    errors: list[ValidationProblem]
+
+    callbacks: list[Callable[..., Awaitable[None]]] = field(default_factory=list)
+
+    def __getitem__(self, key: str):
+        return self.params[key]
+
+
+# TODO: we should rewrite this in cython, along with the request object
+class EndpointSignature[R](Base):
+    route_path: str
+
+    query_params: RequestParamMap
+    path_params: RequestParamMap
+    header_params: RequestParamMap
+    body_param: tuple[str, RequestBodyParam[Struct]] | None
+    dependencies: ParamMap[DependentNode]
+    plugins: ParamMap[PluginParam]
+
+    default_status: int
+    scoped: bool
+    form_body: bool
+
+    return_encoder: IEncoder[R]
+    return_params: dict[int, EndpointReturn[R]]
+
+    def override(self) -> None: ...
+
+    def prepare_params(
+        self,
+        req_path: Mapping[str, Any] | None = None,
+        req_query: Mapping[str, Any] | None = None,
+        req_header: Mapping[str, Any] | None = None,
+        body: bytes | FormData | None = None,
+    ) -> ParseResult:
+        verrors: list[Any] = []
+        params: dict[str, Any] = {}
+
+        zipped = (
+            (self.path_params, req_path),
+            (self.query_params, req_query),
+            (self.header_params, req_header),
+        )
+
+        for required, received in zipped:
+            if received is None:
+                continue
+
+            for name, param in required.items():
+                alias = param.alias if param.alias else param.name
+
+                if (val := received.get(alias, MISSING)) is not MISSING:
+                    val = received[alias]
+
+                    try:
+                        params[name] = param.decode(val)
+                    except ValidationError as mve:
+                        error = InvalidDataType(param.location, name, str(mve))
+                        verrors.append(error)
+                    except DecodeError:
+                        error = InvalidJsonReceived(param.location, name)
+                        verrors.append(error)
+                    except CustomValidationError as cve:
+                        error = CustomDecodeErrorMessage(
+                            param.location, name, cve.detail
+                        )
+                        verrors.append(error)
+
+                elif not param.required:
+                    params[name] = param.default
+                else:
+                    err = MissingRequestParam(param.location, alias)
+                    verrors.append(err)
+
+        if self.body_param and body is not None:
+            name, param = self.body_param
+            if body == b"":  # empty bytes body
+                if is_provided(param.default):
+                    body = param.default  # TODO: is_provided
+                else:
+                    err = MissingRequestParam("body", name)
+                    verrors.append(err)
+            elif isinstance(body, FormData) and len(body) == 0:  # empty form body
+                if is_provided(param.default):
+                    body = param.default
+                else:
+                    err = MissingRequestParam("body", name)
+                    verrors.append(err)
+            else:
+                try:
+                    params[name] = param.decode(body)
+                except ValidationError as mve:
+                    error = InvalidDataType("body", name, str(mve))
+                    verrors.append(error)
+                except DecodeError:
+                    error = InvalidJsonReceived("body", name)
+                    verrors.append(error)
+                except CustomValidationError as cve:
+                    error = CustomDecodeErrorMessage(param.location, name, cve.detail)
+                    verrors.append(error)
+
+        parsed_result = ParseResult(params, verrors)
+        return parsed_result
+
+    def parse_query(self, req: Request) -> ParseResult:
+        req_path = req.path_params if self.path_params else None
+        req_query = req.query_params if self.query_params else None
+        req_header = req.headers if self.header_params else None
+        params = self.prepare_params(req_path, req_query, req_header, None)
+        return params
+
+    async def parse_command(self, req: Request) -> ParseResult:
+        req_path = req.path_params if self.path_params else None
+        req_query = req.query_params if self.query_params else None
+        req_header = req.headers if self.header_params else None
+
+        if self.form_body:
+            body = await req.form()  # TODO: let user decide form configs
+            params = self.prepare_params(req_path, req_query, req_header, body)
+            params.callbacks.append(body.close)
+        else:
+            body = await req.body()
+            params = self.prepare_params(req_path, req_query, req_header, body)
+        return params
+
+    @classmethod
+    def from_function[FR](
+        cls,
+        graph: Graph,
+        route_path: str,
+        f: Callable[..., FR | Awaitable[FR]],
+        app_config: AppConfig | None = None,
+    ) -> "EndpointSignature[FR]":
+        path_keys = find_path_keys(route_path)
+        func_sig = signature(f)
+        func_params = tuple(func_sig.parameters.items())
+
+        parser = ParamParser(graph, path_keys, app_config=app_config)
+        params = parser.parse(func_params, path_keys)
+        return_params = parse_returns(func_sig.return_annotation, app_config=app_config)
+
+        default_status = next(iter(return_params))
+        default_encoder = return_params[default_status].encoder
+
+        scoped = any(
+            graph.should_be_scoped(node.dependent) for node in params.nodes.values()
+        )
+
+        body_param = params.get_body()
+        form_body: bool = is_form_body(body_param)
+
+        info = EndpointSignature(
+            route_path=route_path,
+            header_params=params.get_location("header"),
+            query_params=params.get_location("query"),
+            path_params=params.get_location("path"),
+            body_param=body_param,
+            plugins=params.plugins,
+            dependencies=params.nodes,
+            return_params=return_params,
+            default_status=default_status,
+            return_encoder=default_encoder,
+            scoped=scoped,
+            form_body=form_body,
+        )
+        return info
---
+++ packages/lihil/lihil-0.1.15-py3-none-any.whl/lihil/routing.py
@@ -0,0 +1,403 @@
+from functools import partial
+from types import MethodType
+from typing import Any, Callable, Pattern, Union, Unpack, cast, overload
+
+from ididi import Graph, INodeConfig
+from ididi.interfaces import IDependent
+
+from lihil.asgi import ASGIBase
+from lihil.config import SyncDeps
+from lihil.constant.resp import METHOD_NOT_ALLOWED_RESP
+from lihil.endpoint.endpoint import Endpoint, EndPointConfig, IEndPointConfig
+from lihil.interface import (
+    HTTP_METHODS,
+    ASGIApp,
+    Func,
+    IReceive,
+    IScope,
+    ISend,
+    MiddlewareFactory,
+    Protocol,
+)
+from lihil.oas.model import RouteConfig
+from lihil.plugins.bus import BusTerminal, Event, MessageRegistry
+from lihil.utils.string import (
+    build_path_regex,
+    generate_route_tag,
+    merge_path,
+    trim_path,
+)
+
+
+class EndpointFactory[R](Protocol):
+    def __call__(
+        self,
+        method: HTTP_METHODS,
+        path: str,
+        tag: str,
+        func: Callable[..., R],
+        busterm: BusTerminal,
+        graph: Graph,
+        epconfig: EndPointConfig,
+    ) -> Endpoint[R]: ...
+
+
+def endpoint_factory[R](
+    method: HTTP_METHODS,
+    path: str,
+    tag: str,
+    func: Callable[..., R],
+    busterm: BusTerminal,
+    graph: Graph,
+    epconfig: EndPointConfig,
+) -> Endpoint[R]:
+    endpoint = Endpoint(
+        method=method,
+        path=path,
+        tag=tag,
+        func=func,
+        busterm=busterm,
+        graph=graph,
+        config=epconfig,
+    )
+    return endpoint
+
+
+class Route(ASGIBase):
+    _flyweights: dict[str, "Route"] = {}
+
+    def __new__(cls, path: str = "", **_):
+        """
+        TODO?: we need something more sophisticated
+
+        Route("/users/{user_id}/orders/{order_id}")
+        we should know check if `Route(/users/{user_id}/orders)` is in _flyweight,
+        if parent exists, make new route a sub route of it.
+        if not, pass
+        """
+        p = trim_path(path)
+        if p_route := cls._flyweights.get(p):
+            return p_route
+        cls._flyweights[p] = route = super().__new__(cls)
+        return route
+
+    def __init__(  # type: ignore
+        self,
+        path: str = "",
+        *,
+        graph: Graph | None = None,
+        registry: MessageRegistry | None = None,
+        listeners: list[Callable[..., Any]] | None = None,
+        middlewares: list[MiddlewareFactory[Any]] | None = None,
+        route_config: RouteConfig | None = None,
+        endpoint_factory: EndpointFactory[Any] = endpoint_factory,
+    ):
+        super().__init__(middlewares)
+
+        self.path = trim_path(path)
+        self.path_regex: Pattern[str] | None = None
+        self.endpoints: dict[HTTP_METHODS, Endpoint[Any]] = {}
+        self.graph = graph or Graph(self_inject=False)
+        self.registry = registry or MessageRegistry(event_base=Event, graph=graph)
+        if listeners:
+            self.registry.register(*listeners)
+        self.busterm = BusTerminal(self.registry, graph=graph)
+        self.subroutes: list[Route] = []
+        self.call_stacks: dict[HTTP_METHODS, ASGIApp] = {}
+        self.config = route_config or RouteConfig()
+        self.endpoint_factory = endpoint_factory
+        self.tag = self.config.tag or generate_route_tag(self.path)
+
+    def __repr__(self):
+        endpoints_repr = "".join(
+            f", {method}: {endpoint.unwrapped_func}"
+            for method, endpoint in self.endpoints.items()
+        )
+        return f"{self.__class__.__name__}({self.path!r}{endpoints_repr})"
+
+    def __truediv__(self, path: str) -> "Route":
+        return self.sub(path)
+
+    async def __call__(self, scope: IScope, receive: IReceive, send: ISend) -> None:
+        http_method = scope["method"]
+
+        if (endpoint := self.call_stacks.get(http_method)) is None:
+            return await METHOD_NOT_ALLOWED_RESP(scope, receive, send)
+
+        await endpoint(scope, receive, send)
+
+    def is_direct_child_of(self, other_path: "Route | str") -> bool:
+        if isinstance(other_path, Route):
+            return self.is_direct_child_of(other_path.path)
+
+        if not self.path.startswith(other_path):
+            return False
+        rest = self.path.removeprefix(other_path)
+        return rest.count("/") < 2
+
+    def setup(self, **deps: Unpack[SyncDeps]):
+        if deps:
+            self.app_config = deps.get("app_config") or self.app_config
+            self.graph = deps.get("graph") or self.graph
+            self.busterm = deps.get("busterm") or self.busterm
+
+        for method, ep in self.endpoints.items():
+            ep.setup(**deps)
+            self.call_stacks[method] = self.chainup_middlewares(ep)
+
+    def get_endpoint(
+        self, method_func: HTTP_METHODS | Callable[..., Any]
+    ) -> Endpoint[Any]:
+        if isinstance(method_func, str):
+            methodname = cast(HTTP_METHODS, method_func.upper())
+            return self.endpoints[methodname]
+
+        for ep in self.endpoints.values():
+            if ep.unwrapped_func is method_func:
+                return ep
+        else:
+            raise KeyError(f"{method_func} is not in current route")
+
+    def sub(self, path: str) -> "Route":
+        sub_path = trim_path(path)
+        current_path = merge_path(self.path, sub_path)
+        sub = Route(path=current_path, graph=self.graph)
+        self.subroutes.append(sub)
+        return sub
+
+    def match(self, scope: IScope) -> IScope | None:
+        path = scope["path"]
+        if not self.path_regex or not (m := self.path_regex.match(path)):
+            return None
+        scope["path_params"] = m.groupdict()
+        return scope
+
+    def add_nodes[T](
+        self, *nodes: Union[IDependent[T], tuple[IDependent[T], INodeConfig]]
+    ) -> None:
+        self.graph.add_nodes(*nodes)
+
+    def redirect(self, method: MethodType, **path_params: str) -> None:
+        # owner = method.__self__
+        raise NotImplementedError
+
+    def add_endpoint[**P, R](
+        self,
+        *methods: HTTP_METHODS,
+        func: Func[P, R],
+        **iconfig: Unpack[IEndPointConfig],
+    ) -> Func[P, R]:
+        epconfig = EndPointConfig.from_unpack(**iconfig)
+        # TODO: use a end point factory that user can override
+        for method in methods:
+            endpoint = self.endpoint_factory(
+                method=method,
+                path=self.path,
+                tag=self.tag,
+                func=func,
+                busterm=self.busterm,
+                graph=self.graph,
+                epconfig=epconfig,
+            )
+            self.endpoints[method] = endpoint
+            if self.path_regex is not None:
+                return func
+            self.path_regex = build_path_regex(self.path)
+        return func
+
+    def factory[R](self, node: Callable[..., R], **node_config: Unpack[INodeConfig]):
+        return self.graph.node(node, **node_config)
+
+    def listen(self, listener: Callable[[Event, Any], None]) -> None:
+        self.registry.register(listener)
+
+    def has_listener(self, listener: Callable[..., Any]) -> bool:
+        event_metas = list(self.registry.event_mapping.values())
+
+        for metas in event_metas:
+            for meta in metas:
+                if meta.handler is listener:
+                    return True
+        return False
+
+    # ============ Http Methods ================
+
+    @overload
+    def get[**P, R](
+        self, **epconfig: Unpack[IEndPointConfig]
+    ) -> Callable[[Func[P, R]], Func[P, R]]: ...
+
+    @overload
+    def get[**P, R](self, func: Func[P, R]) -> Func[P, R]: ...
+
+    @overload
+    def get[**P, R](
+        self, func: Func[P, R] | None, **epconfig: Unpack[IEndPointConfig]
+    ) -> Func[P, R] | Callable[[Func[P, R]], Func[P, R]]: ...
+
+    def get[**P, R](
+        self, func: Func[P, R] | None = None, **epconfig: Unpack[IEndPointConfig]
+    ) -> Func[P, R] | Callable[[Func[P, R]], Func[P, R]]:
+        if func is None:
+            return cast(Func[P, R], partial(self.get, **epconfig))
+        return self.add_endpoint("GET", func=func, **epconfig)
+
+    @overload
+    def put[**P, R](
+        self, **epconfig: Unpack[IEndPointConfig]
+    ) -> Callable[[Func[P, R]], Func[P, R]]: ...
+
+    @overload
+    def put[**P, R](self, func: Func[P, R]) -> Func[P, R]: ...
+
+    @overload
+    def put[**P, R](
+        self, func: Func[P, R] | None, **epconfig: Unpack[IEndPointConfig]
+    ) -> Func[P, R]: ...
+
+    def put[**P, R](
+        self, func: Func[P, R] | None = None, **epconfig: Unpack[IEndPointConfig]
+    ) -> Func[P, R]:
+        if func is None:
+            return cast(Func[P, R], partial(self.put, **epconfig))
+        return self.add_endpoint("PUT", func=func, **epconfig)
+
+    @overload
+    def post[**P, R](
+        self, **epconfig: Unpack[IEndPointConfig]
+    ) -> Callable[[Func[P, R]], Func[P, R]]: ...
+
+    @overload
+    def post[**P, R](self, func: Func[P, R]) -> Func[P, R]: ...
+
+    @overload
+    def post[**P, R](
+        self, func: Func[P, R] | None, **epconfig: Unpack[IEndPointConfig]
+    ) -> Func[P, R]: ...
+
+    def post[**P, R](
+        self, func: Func[P, R] | None = None, **epconfig: Unpack[IEndPointConfig]
+    ) -> Func[P, R]:
+        if func is None:
+            return cast(Func[P, R], partial(self.post, **epconfig))
+        return self.add_endpoint("POST", func=func, **epconfig)
+
+    @overload
+    def delete[**P, R](
+        self, **epconfig: Unpack[IEndPointConfig]
+    ) -> Callable[[Func[P, R]], Func[P, R]]: ...
+
+    @overload
+    def delete[**P, R](self, func: Func[P, R]) -> Func[P, R]: ...
+
+    @overload
+    def delete[**P, R](
+        self, func: Func[P, R] | None, **epconfig: Unpack[IEndPointConfig]
+    ) -> Func[P, R]: ...
+
+    def delete[**P, R](
+        self, func: Func[P, R] | None = None, **epconfig: Unpack[IEndPointConfig]
+    ) -> Func[P, R]:
+        if func is None:
+            return cast(Func[P, R], partial(self.delete, **epconfig))
+        return self.add_endpoint("DELETE", func=func, **epconfig)
+
+    @overload
+    def patch[**P, R](
+        self, **epconfig: Unpack[IEndPointConfig]
+    ) -> Callable[[Func[P, R]], Func[P, R]]: ...
+
+    @overload
+    def patch[**P, R](self, func: Func[P, R]) -> Func[P, R]: ...
+
+    @overload
+    def patch[**P, R](
+        self, func: Func[P, R] | None, **epconfig: Unpack[IEndPointConfig]
+    ) -> Func[P, R]: ...
+
+    def patch[**P, R](
+        self, func: Func[P, R] | None = None, **epconfig: Unpack[IEndPointConfig]
+    ) -> Func[P, R]:
+        if func is None:
+            return cast(Func[P, R], partial(self.patch, **epconfig))
+        return self.add_endpoint("PATCH", func=func, **epconfig)
+
+    @overload
+    def head[**P, R](
+        self, **epconfig: Unpack[IEndPointConfig]
+    ) -> Callable[[Func[P, R]], Func[P, R]]: ...
+
+    @overload
+    def head[**P, R](self, func: Func[P, R]) -> Func[P, R]: ...
+
+    @overload
+    def head[**P, R](
+        self, func: Func[P, R] | None, **epconfig: Unpack[IEndPointConfig]
+    ) -> Func[P, R]: ...
+
+    def head[**P, R](
+        self, func: Func[P, R] | None = None, **epconfig: Unpack[IEndPointConfig]
+    ) -> Func[P, R]:
+        if func is None:
+            return cast(Func[P, R], partial(self.head, **epconfig))
+        return self.add_endpoint("HEAD", func=func, **epconfig)
+
+    @overload
+    def options[**P, R](
+        self, **epconfig: Unpack[IEndPointConfig]
+    ) -> Callable[[Func[P, R]], Func[P, R]]: ...
+
+    @overload
+    def options[**P, R](self, func: Func[P, R]) -> Func[P, R]: ...
+
+    @overload
+    def options[**P, R](
+        self, func: Func[P, R] | None, **epconfig: Unpack[IEndPointConfig]
+    ) -> Func[P, R]: ...
+
+    def options[**P, R](
+        self, func: Func[P, R] | None = None, **epconfig: Unpack[IEndPointConfig]
+    ) -> Func[P, R]:
+        if func is None:
+            return cast(Func[P, R], partial(self.options, **epconfig))
+        return self.add_endpoint("OPTIONS", func=func, **epconfig)
+
+    @overload
+    def trace[**P, R](
+        self, **epconfig: Unpack[IEndPointConfig]
+    ) -> Callable[[Func[P, R]], Func[P, R]]: ...
+
+    @overload
+    def trace[**P, R](self, func: Func[P, R]) -> Func[P, R]: ...
+
+    @overload
+    def trace[**P, R](
+        self, func: Func[P, R] | None, **epconfig: Unpack[IEndPointConfig]
+    ) -> Func[P, R]: ...
+
+    def trace[**P, R](
+        self, func: Func[P, R] | None = None, **epconfig: Unpack[IEndPointConfig]
+    ) -> Func[P, R]:
+        if func is None:
+            return cast(Func[P, R], partial(self.options, **epconfig))
+        return self.add_endpoint("TRACE", func=func, **epconfig)
+
+    @overload
+    def connect[**P, R](
+        self, **epconfig: Unpack[IEndPointConfig]
+    ) -> Callable[[Func[P, R]], Func[P, R]]: ...
+
+    @overload
+    def connect[**P, R](self, func: Func[P, R]) -> Func[P, R]: ...
+
+    @overload
+    def connect[**P, R](
+        self, func: Func[P, R] | None, **epconfig: Unpack[IEndPointConfig]
+    ) -> Func[P, R]: ...
+
+    def connect[**P, R](
+        self, func: Func[P, R] | None = None, **epconfig: Unpack[IEndPointConfig]
+    ) -> Func[P, R]:
+        if func is None:
+            return cast(Func[P, R], partial(self.options, **epconfig))
+        return self.add_endpoint("CONNECT", func=func, **epconfig)
