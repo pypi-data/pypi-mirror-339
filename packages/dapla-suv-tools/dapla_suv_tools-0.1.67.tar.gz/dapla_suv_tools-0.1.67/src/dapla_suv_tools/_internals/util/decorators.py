dc202f9a834f5788c3057caadb31750299fb6013
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-07 03:18:45 +0000

    Add package dapla-suv-tools/0.1.67/dapla_suv_tools-0.1.67.tar.gz

---
+++ packages/dapla-suv-tools/dapla_suv_tools-0.1.67.tar.gz/dapla_suv_tools-0.1.67/src/dapla_suv_tools/_internals/util/decorators.py
@@ -0,0 +1,22 @@
+from typing import Callable
+from functools import wraps
+
+from dapla_suv_tools._internals.util.operation_result import OperationResult
+
+
+def result_to_dict(func) -> Callable[..., dict]:
+    @wraps(func)
+    def inner(self, *args, **kwargs) -> dict:
+        try:
+            result = func(self, *args, **kwargs)
+            if isinstance(result, OperationResult):
+                return result.process(caller=self)
+            return result
+        except Exception as e:
+            if hasattr(self, "suppress_exceptions") and self.suppress_exceptions:
+                return {
+                    "result": "An unexpected error occurred",
+                    "error": str(e)
+                }
+            raise e
+    return inner
