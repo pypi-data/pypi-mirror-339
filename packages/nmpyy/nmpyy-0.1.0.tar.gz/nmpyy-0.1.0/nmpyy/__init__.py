8bfd70d28ca384f28a8288812a5756c2a385aa20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-07 15:04:56 +0000

    Add package nmpyy/0.1.0/nmpyy-0.1.0.tar.gz

---
+++ packages/nmpyy/nmpyy-0.1.0.tar.gz/nmpyy-0.1.0/nmpyy/__init__.py
@@ -0,0 +1,13 @@
+from .a1a import a1a
+from .a1b import a1b
+from .a2a import a2a
+from .a3a import a3a
+from .a4a import a4a
+from .a5a import a5a
+from .a6a import a6a
+from .a7a import a7a
+from .a8a import a8a
+from .a9a import a9a
+from .a10a import a10a
+
+
