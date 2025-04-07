0bb9184dd60dc0d3b65d25d1932b96a4c205b9cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-07 04:04:40 +0000

    Add package tgram/1.12.9/tgram-1.12.9-py3-none-any.whl

---
+++ packages/tgram/tgram-1.12.9-py3-none-any.whl/tgram/types/_transaction_partner_user.py
@@ -0,0 +1,80 @@
+import tgram
+from .type_ import Type_
+
+from typing import Optional, List
+
+
+class TransactionPartnerUser(Type_):
+    """
+    Describes a transaction with a user.
+
+    Telegram documentation: https://core.telegram.org/bots/api#transactionpartneruser
+
+    :param type: Type of the transaction partner, always “user”
+    :type type: :obj:`str`
+
+    :param user: Information about the user
+    :type user: :class:`User`
+
+    :return: Instance of the class
+    :rtype: :class:`TransactionPartnerUser`
+    """
+
+    def __init__(
+        self,
+        user: "tgram.types.User" = None,
+        affiliate: "tgram.types.AffiliateInfo" = None,
+        invoice_payload: "str" = None,
+        subscription_period: "int" = None,
+        paid_media: List["tgram.types.PaidMedia"] = None,
+        paid_media_payload: "str" = None,
+        gift: "tgram.types.Gift" = None,
+        me: "tgram.TgBot" = None,
+        json: "dict" = None,
+    ):
+        super().__init__(me=me, json=json)
+        self.type = "user"
+        self.user = user
+        self.affiliate = affiliate
+        self.invoice_payload = invoice_payload
+        self.subscription_period = subscription_period
+        self.paid_media = paid_media
+        self.paid_media_payload = paid_media_payload
+        self.gift = gift
+
+    @staticmethod
+    def _parse(
+        me: "tgram.TgBot" = None, d: dict = None, force: bool = None
+    ) -> Optional["tgram.types.TransactionPartnerUser"]:
+        return (
+            TransactionPartnerUser(
+                me=me,
+                json=d,
+                type=d.get("type"),
+                user=tgram.types.User._parse(me=me, d=d.get("user")),
+                affiliate=tgram.types.AffiliateInfo._parse(me=me, d=d.get("affiliate")),
+                invoice_payload=d.get("invoice_payload"),
+                subscription_period=d.get("subscription_period"),
+                paid_media=[
+                    (
+                        tgram.types.PaidMediaPreview._parse(me=me, d=i)
+                        if i["type"] == "preview"
+                        else tgram.types.PaidMediaPhoto._parse(me=me, d=i)
+                        if i["type"] == "photo"
+                        else tgram.types.PaidMediaVideo._parse(me=me, d=i)
+                    )
+                    for i in d.get("paid_media")
+                ]
+                if d.get("paid_media")
+                else None,
+                paid_media_payload=d.get("paid_media_payload"),
+                gift=tgram.types.Gift._parse(me=me, d=d.get("gift")),
+            )
+            if d and (force or me and __class__.__name__ not in me._custom_types)
+            else None
+            if not d
+            else Type_._custom_parse(
+                __class__._parse(me=me, d=d, force=True),
+                me._custom_types.get(__class__.__name__),
+            )
+        )
---
+++ packages/tgram/tgram-1.12.9-py3-none-any.whl/tgram/utils/types.py
@@ -0,0 +1,105 @@
+import tgram
+import os
+import re
+
+from typing import Optional, List, Union, Tuple
+from pathlib import Path
+from io import BytesIO
+
+from tgram import utils
+
+
+def message_origin_parse(
+    d: Optional[dict] = None, me: Optional["tgram.TgBot"] = None
+) -> Optional["tgram.types.MessageOrigin"]:
+    if d is None:
+        return None
+
+    origin_type = d["type"]
+
+    return (
+        tgram.types.MessageOriginUser._parse(me=me, d=d)
+        if origin_type == "user"
+        else tgram.types.MessageOriginHiddenUser._parse(me=me, d=d)
+        if origin_type == "hidden_user"
+        else tgram.types.MessageOriginChat._parse(me=me, d=d)
+        if origin_type == "chat"
+        else tgram.types.MessageOriginChannel._parse(me=me, d=d)
+    )
+
+
+def convert_input_media(
+    x: List[Union["tgram.types.InputMedia", "tgram.types.InputPaidMedia"]],
+):
+    files = {}
+    count = 1
+    for y in x:
+        if (
+            isinstance(y.media, (Path, str))
+            and os.path.isfile(y.media)
+            or isinstance(y.media, (bytes, BytesIO))
+        ):
+            files[f"file_{count}"] = utils.get_file_path(y.media)
+            y.media = f"attach://file_{count}"
+            count += 1
+
+            if hasattr(y, "thumbnail") and getattr(y, "thumbnail"):
+                if (
+                    isinstance(y.thumbnail, (Path, str)) and os.path.isfile(y.thumbnail)
+                ) or isinstance(y.thumbnail, (bytes, BytesIO)):
+                    files[f"file_{count}"] = utils.get_file_path(y.thumbnail)
+                    y.thumbnail = f"attach://file_{count}"
+                    count += 1
+
+            if hasattr(y, "cover") and getattr(y, "cover"):
+                if (
+                    isinstance(y.cover, (Path, str)) and os.path.isfile(y.cover)
+                ) or isinstance(y.cover, (bytes, BytesIO)):
+                    files[f"file_{count}"] = utils.get_file_path(y.cover)
+                    y.thumbnail = f"attach://file_{count}"
+                    count += 1
+
+    return x, files
+
+
+def reaction_type_parse(
+    bot: "tgram.TgBot",
+    x: Optional[Union[List[dict], dict]],
+) -> "tgram.types.ReactionType":
+    if x is None:
+        return None
+
+    x = x if isinstance(x, list) else [x]
+
+    return [
+        (
+            tgram.types.ReactionTypeCustomEmoji._parse(bot, i)
+            if i["type"] == "custom_emoji"
+            else tgram.types.ReactionTypeEmoji._parse(bot, i)
+            if i["type"] == "emoji"
+            else tgram.types.ReactionTypePaid._parse(bot, i)
+        )
+        for i in x
+    ]
+
+
+pattern = re.compile(
+    r"^(https?):\/\/" r"([a-zA-Z0-9.-]+)" r"(\.[a-zA-Z]{2,})" r"(\/[^\s]*)?$"
+)
+
+
+def convert_to_inline_keyboard_markup(v: List[List[Tuple]]):
+    return tgram.types.InlineKeyboardMarkup(
+        [
+            [
+                tgram.types.InlineKeyboardButton(
+                    x,
+                    callback_data=y if not re.match(pattern, y) else None,
+                    url=y if re.match(pattern, y) else None,
+                    user_id=y if isinstance(y, int) else None,
+                )
+                for x, y in z
+            ]
+            for z in v
+        ]
+    )
