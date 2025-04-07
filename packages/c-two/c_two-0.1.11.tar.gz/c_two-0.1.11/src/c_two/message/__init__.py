dffb06e7248125a1352cda06098bc9589f041928
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-07 15:05:34 +0000

    Add package c-two/0.1.11/c_two-0.1.11.tar.gz

---
+++ packages/c-two/c_two-0.1.11.tar.gz/c_two-0.1.11/src/c_two/message/__init__.py
@@ -0,0 +1,3 @@
+from .client import Client
+from .server import Server
+from .transferable import register_transferable, get_transferable, serialize_from_table, deserialize_to_table, deserialize_to_rows
