bc220636a3f111c472874f2597cf1aad0f55510d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-07 04:04:30 +0000

    Add package approvaltests/14.4.0/approvaltests-14.4.0-py3-none-any.whl

---
+++ packages/approvaltests/approvaltests-14.4.0-py3-none-any.whl/approvaltests/internals/logs/approved_file_log.py
@@ -0,0 +1,31 @@
+from pathlib import Path
+
+from approvaltests.internals.logs.log_commons import (
+    LogCommons,
+    APPROVAL_TESTS_TEMP_DIRECTORY,
+)
+
+
+class ApprovedFilesLog:
+    @staticmethod
+    def clear_log_file() -> None:
+        ApprovedFilesLog.get_approved_files_log().write_text("")
+        LogCommons.download_script_if_needed("detect_and_remove_abandoned")
+
+    @staticmethod
+    def get_approved_files_log() -> Path:
+        return ApprovedFilesLog.get_temp_directory() / ".approved_files.log"
+
+    @staticmethod
+    def get_temp_directory() -> Path:
+        APPROVAL_TESTS_TEMP_DIRECTORY.mkdir(parents=True, exist_ok=True)
+        APPROVAL_TESTS_TEMP_DIRECTORY.joinpath(".gitignore").write_text("*")
+        return APPROVAL_TESTS_TEMP_DIRECTORY
+
+    @staticmethod
+    def log(approved_file: str) -> None:
+        with ApprovedFilesLog.get_approved_files_log().open(mode="a") as file:
+            file.write(f"{approved_file}\n")
+
+
+ApprovedFilesLog.clear_log_file()
