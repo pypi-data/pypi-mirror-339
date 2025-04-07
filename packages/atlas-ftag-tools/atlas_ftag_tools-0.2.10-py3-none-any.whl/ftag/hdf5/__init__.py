22321951092046a63867baaa9a03dfc9bd364d49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-07 16:04:36 +0000

    Add package atlas-ftag-tools/0.2.10/atlas_ftag_tools-0.2.10-py3-none-any.whl

---
+++ packages/atlas-ftag-tools/atlas_ftag_tools-0.2.10-py3-none-any.whl/ftag/hdf5/__init__.py
@@ -0,0 +1,14 @@
+from __future__ import annotations
+
+from ftag.hdf5.h5reader import H5Reader
+from ftag.hdf5.h5utils import cast_dtype, get_dtype, join_structured_arrays, structured_from_dict
+from ftag.hdf5.h5writer import H5Writer
+
+__all__ = [
+    "H5Reader",
+    "H5Writer",
+    "cast_dtype",
+    "get_dtype",
+    "join_structured_arrays",
+    "structured_from_dict",
+]
