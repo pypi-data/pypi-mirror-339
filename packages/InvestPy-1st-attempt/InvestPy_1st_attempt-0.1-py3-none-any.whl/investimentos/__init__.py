8e46e193b80b0c1faee47757e094d4383ec3912e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-07 03:18:26 +0000

    Add package InvestPy-1st-attempt/0.1/InvestPy_1st_attempt-0.1-py3-none-any.whl

---
+++ packages/InvestPy-1st-attempt/InvestPy_1st_attempt-0.1-py3-none-any.whl/investimentos/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr  6 14:32:50 2025
+
+@author: josea
+"""
+
+from .investimentos import calcular_retorno_investimento
+from .investimentos import calcular_juros_compostos 
+from .investimentos import converter_taxa_anual_para_mensal 
+from .investimentos import alcular_cagr
+
+
+
