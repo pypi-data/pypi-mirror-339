562dd8daad1ccf3f8c3293dab0444428750f4a8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-07 14:03:56 +0000

    Add package jh-finance-api/0.1.18/jh_finance_api-0.1.18.tar.gz

---
+++ packages/jh-finance-api/jh_finance_api-0.1.18.tar.gz/jh_finance_api-0.1.18/src/jh_finance_api/__init__.py
@@ -0,0 +1,14 @@
+import sys; sys.dont_write_bytecode=True
+
+from . import index_us_dowjones
+from . import index_us_nasdaq100
+from . import index_us_sp500
+from . import info
+from . import market_history
+from . import financial_list
+from . import financial_raw
+from . import financial_ratios
+
+
+def test():
+    return { 'test':True }
