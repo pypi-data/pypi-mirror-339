41d84d4de004b836aa8f7f65084687797fce7a34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-07 04:04:08 +0000

    Add package grifftools/0.0.2/grifftools-0.0.2-py3-none-any.whl

---
+++ packages/grifftools/grifftools-0.0.2-py3-none-any.whl/grifftools/__init__.py
@@ -0,0 +1,3 @@
+from .lib import *
+from .func import *
+from .shell import *
---
+++ packages/grifftools/grifftools-0.0.2-py3-none-any.whl/grifftools/cli.py
@@ -0,0 +1,8 @@
+import fire
+
+from .lib import *
+from .func import *
+from .shell import *
+
+if __name__ == "__main__":
+    fire.Fire()
