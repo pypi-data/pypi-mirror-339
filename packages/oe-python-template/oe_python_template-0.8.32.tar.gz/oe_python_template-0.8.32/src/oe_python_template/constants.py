0e29fd7bc2b0b4c7cce8b282fe72541c0070235a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-07 04:04:29 +0000

    Add package oe-python-template/0.8.32/oe_python_template-0.8.32.tar.gz

---
+++ packages/oe-python-template/oe_python_template-0.8.32.tar.gz/oe_python_template-0.8.32/src/oe_python_template/constants.py
@@ -0,0 +1,10 @@
+"""Constants used throughout OE Python Template's codebase ."""
+
+import importlib.metadata
+import pathlib
+
+__project_name__ = __name__.split(".")[0]
+__project_path__ = str(pathlib.Path(__file__).parent.parent.parent)
+__version__ = importlib.metadata.version(__project_name__)
+
+LOREM_IPSUM = "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
