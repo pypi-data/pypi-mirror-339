62cb50c17d2cd85060a52fb8763aeeade7e70ca8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-07 16:04:34 +0000

    Add package mlchemy/0.0.1/mlchemy-0.0.1-py3-none-any.whl

---
+++ packages/mlchemy/mlchemy-0.0.1-py3-none-any.whl/mlchemy/preprocessing.py
@@ -0,0 +1,26 @@
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+# from config import PreprocessorConfig
+
+class Preprocessor:
+    # def __init__(self, config: PreprocessorConfig):
+    def __init__(self):
+
+        # self.config = config
+        self.encoders = {}
+
+    def fit_transform(self, df):
+        df = df.copy()
+        for col in df.select_dtypes(include="object").columns:
+            le = LabelEncoder()
+            df[col] = le.fit_transform(df[col])
+            self.encoders[col] = le
+        df.fillna(df.median(), inplace=True)
+        return df
+
+    def transform(self, df):
+        df = df.copy()
+        for col, le in self.encoders.items():
+            df[col] = le.transform(df[col])
+        df.fillna(df.median(), inplace=True)
+        return df
