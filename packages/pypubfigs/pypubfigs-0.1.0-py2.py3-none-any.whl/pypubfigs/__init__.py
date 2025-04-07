b26d7dbc1b487b28d8b04fb6bdaff8376455de00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-07 03:18:43 +0000

    Add package pypubfigs/0.1.0/pypubfigs-0.1.0-py2.py3-none-any.whl

---
+++ packages/pypubfigs/pypubfigs-0.1.0-py2.py3-none-any.whl/pypubfigs/__init__.py
@@ -0,0 +1,11 @@
+from .palettes import friendly_pal
+from .themes import (
+    theme_simple,
+    theme_black,
+    theme_red,
+    theme_blue,
+    theme_grey,
+    theme_big_simple,
+    theme_big_grid,
+    theme_grid,
+)
