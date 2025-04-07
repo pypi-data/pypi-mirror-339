1efb16e425c2666e67c88fd99ffea3dc8d255270
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-07 04:04:44 +0000

    Add package reusable-utils/0.1.0/reusable_utils-0.1.0.tar.gz

---
+++ packages/reusable-utils/reusable_utils-0.1.0.tar.gz/reusable_utils-0.1.0/src/reusable_utils/path_utils.py
@@ -0,0 +1,31 @@
+import sys
+import os
+
+def add_source_path_to_sys(source_name: str = 'Algotrading') -> None:
+    """
+    Adds the source path to the system path dynamically based on the provided source name.
+
+    This function determines the directory of the current notebook, splits the path into parts,
+    and constructs a relative path to the specified source folder. If the constructed path is
+    not already in `sys.path`, it appends it.
+
+    Args:
+        source_name (str): The name of the source folder to locate in the path. Defaults to 'Algotrading'.
+
+    Returns:
+        None
+    """
+    # Dynamically determine the notebook's directory
+    path = os.path.dirname(os.path.abspath('__file__'))
+    path_parts = path.split(os.sep)
+    i = path_parts.index(source_name)
+
+    for j in range(i + 1, len(path_parts)):
+        path_parts.remove(path_parts[j])
+    path_parts.insert(1, '\\')
+
+    # Join the parts to create a relative path
+    source_path = os.path.join(*path_parts)
+
+    if source_path not in sys.path:
+        sys.path.append(source_path)
