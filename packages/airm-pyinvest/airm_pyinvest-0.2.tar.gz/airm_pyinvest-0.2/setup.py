b8ac5ecd764fcdfdb687b6bcbdfb249852fdce47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-07 03:18:28 +0000

    Add package airm-pyinvest/0.2/airm_pyinvest-0.2.tar.gz

---
+++ packages/airm-pyinvest/airm_pyinvest-0.2.tar.gz/airm_pyinvest-0.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+   name='airm-pyinvest',
+   version='0.2',
+   packages=find_packages(),
+   install_requires=[],
+   author='Iury da Rocha Miguel',
+   author_email='iurymig.sht@gmail.com',
+   description='Uma biblioteca para cálculos de investimentos.',
+   url='',
+   classifiers=[
+       'Programming Language :: Python :: 3',
+       'License :: OSI Approved :: MIT License',
+       'Operating System :: OS Independent',
+   ],
+   python_requires='>=3.6',
+)
