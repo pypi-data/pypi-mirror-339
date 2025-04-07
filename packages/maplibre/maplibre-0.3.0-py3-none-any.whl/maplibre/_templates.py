e703496a6beb7c14c9f8bfd7f7d494ef5b69a67e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-07 03:18:26 +0000

    Add package maplibre/0.3.0/maplibre-0.3.0-py3-none-any.whl

---
+++ packages/maplibre/maplibre-0.3.0-py3-none-any.whl/maplibre/_templates.py
@@ -0,0 +1,29 @@
+html_template = """<!DOCTYPE html>
+<html lang="en">
+<meta charset="UTF-8">
+<title>{{ title|default('My MapLibre Map')}}</title>
+<meta name="viewport" content="width=device-width,initial-scale=1">
+<!--
+<script src="https://unpkg.com/maplibre-gl/dist/maplibre-gl.js"></script>
+<link rel="stylesheet" href="https://unpkg.com/maplibre-gl/dist/maplibre-gl.css"/>
+-->
+<script src="https://unpkg.com/maplibre-gl@3.6.2/dist/maplibre-gl.js"></script>
+<link rel="stylesheet" href="https://unpkg.com/maplibre-gl@3.6.2/dist/maplibre-gl.css"/>
+{% for header in headers|default([]) -%}
+{{ header }}
+{% endfor -%}
+<body>
+<div id="pymaplibregl" style="{{ style|default('height:600px;') }}"></div>
+<script>
+{{ js|safe }}
+</script>
+</body>
+</html>
+"""
+
+js_template = """// ...
+(() => {
+    var data = {{ data|safe }};
+    pymaplibregl(data);
+})();
+"""
