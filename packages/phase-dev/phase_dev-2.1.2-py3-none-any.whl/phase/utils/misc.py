6f8071ef00a581f21e9e5d1ff424040088e4afd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-07 03:18:13 +0000

    Add package phase-dev/2.1.2/phase_dev-2.1.2-py3-none-any.whl

---
+++ packages/phase-dev/phase_dev-2.1.2-py3-none-any.whl/phase/utils/misc.py
@@ -0,0 +1,130 @@
+import platform
+import getpass
+from .exceptions import EnvironmentNotFoundException
+from .const import __version__
+
+
+def phase_get_context(user_data, app_name=None, env_name=None, app_id=None):
+    """
+    Get the context (ID, name, and publicKey) for a specified application and environment or the default application and environment.
+
+    Parameters:
+    - user_data (dict): The user data from the API response.
+    - app_name (str, optional): The name (or partial name) of the desired application.
+    - env_name (str, optional): The name (or partial name) of the desired environment.
+    - app_id (str, optional): The explicit application ID to use. Takes precedence over app_name if both are provided.
+
+    Returns:
+    - tuple: A tuple containing the application's name, application's ID, environment's name, environment's ID, and publicKey.
+
+    Raises:
+    - ValueError: If no matching application or environment is found.
+    """
+    # 1. Set default environment name
+    default_env_name = "Development"
+    env_name = env_name or default_env_name
+
+    # 2. Match the application using app_id first, then fall back to app_name if app_id is not provided
+    try:
+        if app_id:  # app_id takes precedence
+            application = next((app for app in user_data["apps"] if app["id"] == app_id), None)
+            if not application:
+                raise ValueError(f"🔍 No application found with ID: '{app_id}'.")
+        elif app_name:  # only check app_name if app_id is not provided
+            matching_apps = [app for app in user_data["apps"] if app_name.lower() in app["name"].lower()]
+            if not matching_apps:
+                raise ValueError(f"🔍 No application found with the name '{app_name}'.")
+            # Sort matching applications by the length of their names, shorter names are likely to be more specific matches
+            matching_apps.sort(key=lambda app: len(app["name"]))
+            application = matching_apps[0]
+        else:
+            raise ValueError("🤔 No application context provided. Please provide either app_name or app_id.")
+
+        # 3. Attempt to match environment with the exact name or a name that contains the env_name string
+        environment = next((env for env in application["environment_keys"] if env_name.lower() in env["environment"]["name"].lower()), None)
+
+        if not environment:
+            raise EnvironmentNotFoundException(env_name)
+
+        # Return application name, application ID, environment name, environment ID, and public key
+        return (application["name"], application["id"], environment["environment"]["name"], environment["environment"]["id"], environment["identity_key"])
+    except StopIteration:
+        raise ValueError("🔍 Application or environment not found.")
+
+
+def normalize_tag(tag):
+    """
+    Normalize a tag by replacing underscores with spaces.
+
+    Args:
+        tag (str): The tag to normalize.
+
+    Returns:
+        str: The normalized tag.
+    """
+    return tag.replace('_', ' ').lower()
+
+
+def tag_matches(secret_tags, user_tag):
+    """
+    Check if the user-provided tag partially matches any of the secret tags.
+
+    Args:
+        secret_tags (list): The list of tags associated with a secret.
+        user_tag (str): The user-provided tag to match.
+
+    Returns:
+        bool: True if there's a partial match, False otherwise.
+    """
+    normalized_user_tag = normalize_tag(user_tag)
+    for tag in secret_tags:
+        normalized_secret_tag = normalize_tag(tag)
+        if normalized_user_tag in normalized_secret_tag:
+            return True
+    return False
+
+
+def get_user_agent():
+    """
+    Constructs a user agent string containing information about the CLI's version, 
+    the operating system, its version, its architecture, and the local username with machine name.
+    
+    Returns:
+        str: The constructed user agent string.
+    """
+
+    details = []
+    
+    # Get CLI version
+    try:
+        cli_version = f"phase-python-sdk/{__version__}"
+        details.append(cli_version)
+    except:
+        pass
+
+    # Get OS and version
+    try:
+        os_type = platform.system()  # e.g., Windows, Linux, Darwin (for macOS)
+        os_version = platform.release()
+        details.append(f"{os_type} {os_version}")
+    except:
+        pass
+
+    # Get architecture
+    try:
+        architecture = platform.machine()
+        details.append(architecture)
+    except:
+        pass
+
+    # Get username and hostname
+    try:
+        username = getpass.getuser()
+        hostname = platform.node()
+        user_host_string = f"{username}@{hostname}"
+        details.append(user_host_string)
+    except:
+        pass
+
+    user_agent_str = ' '.join(details)
+    return user_agent_str
