25f83b0a7991c76545c327756a5d5c3e51b0a92a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-07 04:04:04 +0000

    Add package anigamerpy/0.2.2/anigamerpy-0.2.2-py3-none-any.whl

---
+++ packages/anigamerpy/anigamerpy-0.2.2-py3-none-any.whl/anigamerpy/__init__.py
@@ -0,0 +1,3 @@
+from .anime import Anime
+
+__version__ = '0.2.2'
---
+++ packages/anigamerpy/anigamerpy-0.2.2-py3-none-any.whl/anigamerpy/response/__init__.py
@@ -0,0 +1,4 @@
+from .allanime import AllAnime, AllAnimeResponse
+from .newanime import NewAnime, NewAnimeResponse
+from .searchanime import Search, SearchResponse
+from .error import ErrorType
