ad054229ac186da1431d89b734632cb44e2e0cf9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-07 16:04:40 +0000

    Add package aas-pydantic/0.1.3/aas_pydantic-0.1.3-py3-none-any.whl

---
+++ packages/aas-pydantic/aas_pydantic-0.1.3-py3-none-any.whl/aas_pydantic/__init__.py
@@ -0,0 +1,20 @@
+from aas_pydantic.aas_model import AAS, Submodel, SubmodelElementCollection, Reference
+from aas_pydantic.convert_aas_instance import (
+    convert_object_store_to_pydantic_models,
+    convert_aas_to_pydantic_model_instance,
+    convert_submodel_to_model_instance,
+)
+from aas_pydantic.convert_aas_template import (
+    convert_object_store_to_pydantic_types,
+    convert_aas_to_pydantic_type,
+    convert_submodel_template_to_pydatic_type,
+)
+from aas_pydantic.convert_pydantic_model import (
+    convert_model_to_aas,
+    convert_model_to_submodel,
+)
+from aas_pydantic.convert_pydantic_type import (
+    convert_model_to_aas_template,
+    convert_model_to_submodel_template,
+    convert_model_instance_to_submodel_template,
+)
