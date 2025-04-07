8586cf40ec67b2a834a3bd399046ee096de87b0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-07 15:04:37 +0000

    Add package sonar-software-api/0.1.0/sonar_software_api-0.1.0.tar.gz

---
+++ packages/sonar-software-api/sonar_software_api-0.1.0.tar.gz/sonar_software_api-0.1.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="sonar_software_api",
+    version="0.1.0",
+    description="A simple GraphQL client for interacting with your Sonar Software instance",
+    author="JckHamm3r",
+    packages=find_packages(),
+    install_requires=[
+        "gql>=3.4.0",
+        "requests_toolbelt>=1.0.0"
+    ],
+    python_requires=">=3.7",
+)
