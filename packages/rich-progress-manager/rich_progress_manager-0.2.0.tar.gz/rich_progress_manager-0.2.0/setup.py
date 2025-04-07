884c9aec5cb5ddd0edbd9fce67bc4086a6ea3325
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-07 03:17:56 +0000

    Add package rich-progress-manager/0.2.0/rich_progress_manager-0.2.0.tar.gz

---
+++ packages/rich-progress-manager/rich_progress_manager-0.2.0.tar.gz/rich_progress_manager-0.2.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="rich-progress-manager",
+    version="0.2.0",
+    description="Thread-safe progress manager with rich logging",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Your Name",
+    author_email="your.email@example.com",
+    url="https://github.com/yourusername/rich-progress",
+    packages=find_packages(),
+    install_requires=[
+        "rich>=13.0.0",
+        "typing_extensions; python_version<'3.8'"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+    keywords="progress logging threading",
+)
