7957b7b6cf17da3577bd150bf17aaad3992542b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-07 04:04:34 +0000

    Add package pyrept/1.0.1/pyrept-1.0.1.tar.gz

---
+++ packages/pyrept/pyrept-1.0.1.tar.gz/pyrept-1.0.1/main.py
@@ -0,0 +1,9 @@
+# This is a sample Python script.
+
+def main_run(name):
+    print(f'Hi, {name}')
+
+
+# Run This Simple script for Python env Test
+if __name__ == '__main__':
+    main_run('pyrept')
