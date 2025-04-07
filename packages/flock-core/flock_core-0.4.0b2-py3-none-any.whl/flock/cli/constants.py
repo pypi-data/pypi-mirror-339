e4f5022f0c5945a8270ea25e2f08de15fc204a45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-07 14:04:14 +0000

    Add package flock-core/0.4.0b2/flock_core-0.4.0b2-py3-none-any.whl

---
+++ packages/flock-core/flock_core-0.4.0b2-py3-none-any.whl/flock/cli/constants.py
@@ -0,0 +1,25 @@
+"""Constants for the CLI module."""
+
+CLI_CREATE_AGENT = "Create an agent"
+CLI_CREATE_FLOCK = "Create a new Flock"
+CLI_LOAD_AGENT = "Load an agent"
+CLI_LOAD_FLOCK = "Load a *.flock file"
+CLI_THEME_BUILDER = "Theme builder"
+CLI_LOAD_EXAMPLE = "Load a example"
+CLI_SETTINGS = "Settings"
+CLI_NOTES = "'Hummingbird' release notes"
+CLI_START_WEB_SERVER = "Start web server"
+CLI_REGISTRY_MANAGEMENT = "Registry management"
+CLI_EXIT = "Exit"
+CLI_CHOICES = [
+    CLI_CREATE_AGENT,
+    CLI_CREATE_FLOCK,
+    CLI_LOAD_AGENT,
+    CLI_LOAD_FLOCK,
+    CLI_LOAD_EXAMPLE,
+    CLI_THEME_BUILDER,
+    CLI_REGISTRY_MANAGEMENT,
+    CLI_SETTINGS,
+    CLI_START_WEB_SERVER,
+    CLI_EXIT,
+]
