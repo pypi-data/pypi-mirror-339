d20ffcc91e6232e751e7955245b9e588ef773c16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-07 14:04:01 +0000

    Add package rayspatial/0.0.15/rayspatial-0.0.15-py3-none-any.whl

---
+++ packages/rayspatial/rayspatial-0.0.15-py3-none-any.whl/rayspatial/serve/core/actor/coreActor.py
@@ -0,0 +1,19 @@
+import ray
+from rayspatial.serve.core.actor.baseCore import BaseCore
+from datetime import datetime
+from ray.runtime_env import RuntimeEnv
+
+def general_execute_method(header, method_name, inputParams):
+    print(f"general_execute_method:{method_name}")
+    try:
+        ref = CoreActor().general_method_execute(header, method_name, inputParams)
+        return ref
+    except Exception as e:
+        raise e
+    
+class CoreActor(BaseCore):
+    def __init__(self, name=None):
+        self.name = name
+    def general_method_execute(self, header, org_method_name, inputParams):
+        method_name = org_method_name.replace("/", "_")
+        return getattr(self, method_name)(header, inputParams)
---
+++ packages/rayspatial/rayspatial-0.0.15-py3-none-any.whl/rayspatial/serve/serveApp.py
@@ -0,0 +1,13 @@
+from starlette.requests import Request
+from ray import serve
+import ray
+
+@serve.deployment(num_replicas='auto', ray_actor_options={"num_cpus": 0.2, "num_gpus": 0})
+class ServeApp:
+    async def __call__(self, request: Request):
+        import rayspatial
+        print(f"{rayspatial.__version__}")
+        requestJson = await request.json()
+        params = requestJson["params"]
+        header = requestJson["header"]
+        return rayspatial.serve.exe.ServeExecute.execute_serve(params, header)
