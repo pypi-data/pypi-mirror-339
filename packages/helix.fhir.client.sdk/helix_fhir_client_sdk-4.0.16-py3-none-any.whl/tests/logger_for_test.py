75b92059c1987e504bbbeeb870d60dda784cb215
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-07 15:05:20 +0000

    Add package helix.fhir.client.sdk/4.0.16/helix_fhir_client_sdk-4.0.16-py3-none-any.whl

---
+++ packages/helix.fhir.client.sdk/helix_fhir_client_sdk-4.0.16-py3-none-any.whl/tests/logger_for_test.py
@@ -0,0 +1,37 @@
+import logging
+from logging import Logger
+from typing import Any
+
+from helix_fhir_client_sdk.loggers.fhir_logger import FhirLogger
+
+
+class LoggerForTest(FhirLogger):
+    def __init__(self) -> None:
+        self.logger: Logger = Logger("FhirLogger")
+        handler = logging.StreamHandler()
+        handler.setFormatter(
+            logging.Formatter(
+                "%(asctime)s %(levelname)s %(module)s.%(funcName)s[%(lineno)d]: %(message)s",
+                datefmt="%Y-%m-%d %H:%M:%S",
+            )
+        )
+        self.logger.addHandler(handler)
+        self.logger.setLevel(logging.DEBUG)
+
+    def info(self, param: Any) -> None:
+        """
+        Handle messages at INFO level
+        """
+        self.logger.info(param)
+
+    def error(self, param: Any) -> None:
+        """
+        Handle messages at error level
+        """
+        self.logger.error(param)
+
+    def debug(self, param: Any) -> None:
+        """
+        Handle messages at debug level
+        """
+        self.logger.debug(param)
