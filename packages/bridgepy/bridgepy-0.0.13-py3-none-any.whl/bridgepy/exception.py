d2ea68488d85a2b71ba6303fdcaf9f3c8c2dd6f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-07 04:04:01 +0000

    Add package bridgepy/0.0.13/bridgepy-0.0.13-py3-none-any.whl

---
+++ packages/bridgepy/bridgepy-0.0.13-py3-none-any.whl/bridgepy/exception.py
@@ -0,0 +1,105 @@
+class BizException(Exception):
+    def __init__(self, code: int, msg: str):
+        self.code: int = code
+        self.msg: str = msg
+        super().__init__(self.msg)
+
+class GameAlready4Players(BizException):
+    def __init__(self):
+        super().__init__(10001, "Game already has 4 players!")
+
+class GamePlayerAlreadyAdded(BizException):
+    def __init__(self):
+        super().__init__(10002, "Game player already added!")
+
+class GamePlayerNotFound(BizException):
+    def __init__(self):
+        super().__init__(10003, "Game player not found!")
+
+class GamePartnerAlreadyChosenException(BizException):
+    def __init__(self):
+        super().__init__(10004, "Partner already chosen!")
+
+class GameNotReadyToDealYetException(BizException):
+    def __init__(self):
+        super().__init__(10005, "Game not ready to deal yet!")
+
+class GameAlreadyDealtException(BizException):
+    def __init__(self):
+        super().__init__(10006, "Game already dealt the cards!")
+
+class GameInvalidBidStateException(BizException):
+    def __init__(self):
+        super().__init__(10007, "Game invalid bid state!")
+
+class GameNotPlayerBidTurnException(BizException):
+    def __init__(self):
+        super().__init__(10008, "Game not player's turn to bid!")
+
+class GameAuctionNotFinishedException(BizException):
+    def __init__(self):
+        super().__init__(10009, "Game auction not finished yet!")
+
+class GameAuctionAlreadyFinishedException(BizException):
+    def __init__(self):
+        super().__init__(10010,  "Game auction already finished!")
+
+class GameInvalidBidException(BizException):
+    def __init__(self):
+        super().__init__(10011, "Game invalid bid!")
+
+class GameNotBidWinner(BizException):
+    def __init__(self):
+        super().__init__(10012, "Game not bid winner!")
+
+class GameNotReadyForTrickWinnerExcception(BizException):
+    def __init__(self):
+        super().__init__(10013, "Game not ready for trick winner!")
+
+class GameInvalidTrickStateException(BizException):
+    def __init__(self):
+        super().__init__(10014, "Game invalid trick state!")
+
+class GameAlreadyFinishedException(BizException):
+    def __init__(self):
+        super().__init__(10015, "Game already finished!")
+
+class GameNotPlayerTrickTurnException(BizException):
+    def __init__(self):
+        super().__init__(10016, "Game not player's turn to trick!")
+
+class GameInvalidPlayerTrickException(BizException):
+    def __init__(self):
+        super().__init__(10017, "Game player tricks with invalid card!")
+
+class PlayerInvalidHandException(BizException):
+    def __init__(self):
+        super().__init__(10018, "Player invalid hand!")
+
+class BridgeGameAlreadyCreatedException(BizException):
+    def __init__(self):
+        super().__init__(10019, "Bridge game already created!")
+
+class BridgeGameNotFoundException(BizException):
+    def __init__(self):
+        super().__init__(10020, "Bridge game not found!")
+
+class GamePartnerNotChosenYetException(BizException):
+    def __init__(self):
+        super().__init__(10021, "Partner not chosen yet!")
+
+class GameNotConcludedYetException(BizException):
+    def __init__(self):
+        super().__init__(10022, "Game not concluded yet!")
+
+class GamePlayerAlreadyVotedResetException(BizException):
+    def __init__(self):
+        super().__init__(10023, "Game player already voted reset!")
+
+class PlayerBidNotFoundException(BizException):
+    def __init__(self):
+        super().__init__(10024, "Player bid not found!")
+
+class GameBidWinnerNotFoundException(BizException):
+    def __init__(self):
+        super().__init__(10025, "Game bid winner not found!")
