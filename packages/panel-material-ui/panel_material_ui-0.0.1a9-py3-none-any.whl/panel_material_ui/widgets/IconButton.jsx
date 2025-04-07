388655dbd8aa4f3abb6c47524b73a3d41ca09707
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-07 14:03:54 +0000

    Add package panel-material-ui/0.0.1a9/panel_material_ui-0.0.1a9-py3-none-any.whl

---
+++ packages/panel-material-ui/panel_material_ui-0.0.1a9-py3-none-any.whl/panel_material_ui/widgets/IconButton.jsx
@@ -0,0 +1,34 @@
+import IconButton from "@mui/material/IconButton";
+
+export function render({model, el}) {
+  const [active_icon] = model.useState("active_icon")
+  const [color] = model.useState("button_type")
+  const [disabled] = model.useState("disabled")
+  const [edge] = model.useState("edge")
+  const [icon] = model.useState("icon")
+  const [size] = model.useState("size")
+  const [sx] = model.useState("sx")
+  const [toggle_duration] = model.useState("toggle_duration")
+  const [current_icon, setIcon] = React.useState(icon)
+
+  const handleClick = (e) => {
+    model.send_event("click", e)
+    if (active_icon) {
+      setIcon(active_icon)
+      setTimeout(() => setIcon(icon), toggle_duration)
+    }
+  }
+
+  return (
+    <IconButton
+      color={color}
+      disabled={disabled}
+      edge={edge}
+      size={size}
+      sx={sx}
+      onClick={handleClick}
+    >
+      <Icon style={{fontSize: size}}>{current_icon}</Icon>
+    </IconButton>
+  )
+}
