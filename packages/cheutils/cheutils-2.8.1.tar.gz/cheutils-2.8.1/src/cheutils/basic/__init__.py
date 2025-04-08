ef23f75c6f7f208fac7ec6f007d069157c4da807
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-08 03:15:51 +0000

    Add package cheutils/2.8.1/cheutils-2.8.1.tar.gz

---
+++ packages/cheutils/cheutils-2.8.1.tar.gz/cheutils-2.8.1/src/cheutils/basic/__init__.py
@@ -0,0 +1,7 @@
+from cheutils.basic.transformations import DropSelectedCols
+from cheutils.basic.transformations import DataPrep
+from cheutils.basic.transformations import TargetEncoderCats
+from cheutils.basic.transformations import ApplySelectiveFunction
+from cheutils.basic.transformations import BinarizerColumns
+from cheutils.basic.transformations import ClipOutliers
+from cheutils.basic.transformations import TransformSelectiveColumns
