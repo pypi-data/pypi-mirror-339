522e36953040c046ccabafbd7271035e57b652fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-08 17:04:53 +0000

    Add package dsp-tools/12.1.0.post15/dsp_tools-12.1.0.post15.tar.gz

---
+++ packages/dsp-tools/dsp_tools-12.1.0.post15.tar.gz/dsp_tools-12.1.0.post15/src/dsp_tools/xmllib/constants.py
@@ -0,0 +1,32 @@
+KNOWN_XML_TAGS = [  # defined at https://docs.dasch.swiss/latest/DSP-API/03-endpoints/api-v2/text/standard-standoff/
+    "a( [^>]+)?",  # <a> has attributes
+    "footnote( [^>]+)?",  # the footnote text is in the attributes
+    "p",
+    "em",
+    "strong",
+    "u",
+    "sub",
+    "sup",
+    "strike",
+    "h1",
+    "h2",
+    "h3",
+    "h4",
+    "h5",
+    "h6",
+    "ol",
+    "ul",
+    "li",
+    "tbody",
+    "table",
+    "tr",
+    "td",
+    "br",
+    "hr",
+    "pre",
+    "cite",
+    "blockquote",
+    "code",
+]
+XML_NAMESPACE_MAP = {None: "https://dasch.swiss/schema", "xsi": "http://www.w3.org/2001/XMLSchema-instance"}
+DASCH_SCHEMA = "{https://dasch.swiss/schema}"
