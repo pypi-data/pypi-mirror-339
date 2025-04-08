9b2eeabd6a364ee8e1ed237353d2c49902cbab2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-08 06:05:09 +0000

    Add package cellrank/2.0.7/cellrank-2.0.7.tar.gz

---
+++ packages/cellrank/cellrank-2.0.7.tar.gz/cellrank-2.0.7/CONTRIBUTING.rst
@@ -0,0 +1,3 @@
+Contributing guide
+==================
+Under construction.
---
+++ packages/cellrank/cellrank-2.0.7.tar.gz/cellrank-2.0.7/src/cellrank/__init__.py
@@ -0,0 +1,25 @@
+from importlib import metadata
+
+import numpy as np
+import scipy.sparse as sp
+
+from cellrank import datasets, estimators, kernels, logging, models, pl
+from cellrank._utils._lineage import Lineage
+from cellrank.settings import settings
+
+__all__ = ["datasets", "estimators", "kernels", "logging", "models", "pl", "Lineage", "settings"]
+
+try:
+    md = metadata.metadata(__name__)
+    __version__ = md.get("version", "")
+    __author__ = md.get("Author", "")
+    __maintainer__ = md.get("Maintainer-email", "")
+except ImportError:
+    md = None
+
+# pygam uses `.A`
+sp.spmatrix.A = property(lambda self: self.toarray())
+# pygam/utils.py:649: in b_spline_basis
+np.int = int  # noqa
+
+del metadata, md, sp, np
