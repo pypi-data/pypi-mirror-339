bf666c10ddcb77cdfc79b8744a2054006152046d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-08 17:04:54 +0000

    Add package telebom/6.1.6/telebom-6.1.6.tar.gz

---
+++ packages/telebom/telebom-6.1.6.tar.gz/telebom-6.1.6/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+import os
+
+long_description = ''
+with open(os.path.join(os.path.dirname(__file__), 'README.md'), 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='telebom',
+    version='6.1.6',
+    description='reaction-Telegram-Post By : t.me/goldenxpris.',
+    author='Golden',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+        'rich',
+        'colorama',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
