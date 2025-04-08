44f811996ffc150c529fd17a522dc277c566deed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-08 03:15:54 +0000

    Add package maleo-core/0.3.14/maleo_core-0.3.14-py3-none-any.whl

---
+++ packages/maleo-core/maleo_core-0.3.14-py3-none-any.whl/maleo_core/models/maleo_suite/maleo_access/transfers/parameters/service/user_system_role.py
@@ -0,0 +1,14 @@
+from __future__ import annotations
+from maleo_core.models.base.transfers.parameters.service import BaseServiceParameters
+from maleo_core.models.maleo_suite.maleo_access.transfers.parameters.general.user_system_role import MaleoAccessUserSystemRoleGeneralParameters
+
+class MaleoAccessUserSystemRoleServiceParameters:
+    class GetQuery(
+        MaleoAccessUserSystemRoleGeneralParameters.Get,
+        BaseServiceParameters.GetQuery
+    ): pass
+
+    class Get(
+        MaleoAccessUserSystemRoleGeneralParameters.Get,
+        BaseServiceParameters.Get
+    ): pass
