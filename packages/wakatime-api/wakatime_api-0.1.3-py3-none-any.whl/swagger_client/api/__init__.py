fe6198261e30c98d1fd6c394bd43d51cb5259855
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-08 14:09:09 +0000

    Add package wakatime-api/0.1.3/wakatime_api-0.1.3-py3-none-any.whl

---
+++ packages/wakatime-api/wakatime_api-0.1.3-py3-none-any.whl/swagger_client/api/__init__.py
@@ -0,0 +1,20 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from swagger_client.api.commits_api import CommitsApi
+from swagger_client.api.data_dumps_api import DataDumpsApi
+from swagger_client.api.durations_api import DurationsApi
+from swagger_client.api.external_durations_api import ExternalDurationsApi
+from swagger_client.api.goals_api import GoalsApi
+from swagger_client.api.heartbeats_api import HeartbeatsApi
+from swagger_client.api.insights_api import InsightsApi
+from swagger_client.api.leaderboards_api import LeaderboardsApi
+from swagger_client.api.meta_api import MetaApi
+from swagger_client.api.organizations_api import OrganizationsApi
+from swagger_client.api.projects_api import ProjectsApi
+from swagger_client.api.resources_api import ResourcesApi
+from swagger_client.api.stats_api import StatsApi
+from swagger_client.api.summaries_api import SummariesApi
+from swagger_client.api.users_api import UsersApi
