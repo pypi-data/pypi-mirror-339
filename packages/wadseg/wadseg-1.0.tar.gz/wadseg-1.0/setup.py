6a82da60658a2257cfa86cc57306c911b9eb3d26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-08 14:08:27 +0000

    Add package wadseg/1.0/wadseg-1.0.tar.gz

---
+++ packages/wadseg/wadseg-1.0.tar.gz/wadseg-1.0/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+setuptools.setup(
+    name='wadseg',  
+    version='1.0',   
+    author='Feiyu Qu',  
+    author_email='qufy66@gmail.com',  
+    description='This is a implementation of WADSeg',  
+    long_description=open('README.md').read(),  
+    long_description_content_type='text/markdown',  
+    packages=setuptools.find_packages(), 
+    classifiers=[                               
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    install_requires=['transformers', 'torch', 'matplotlib', 'matplotlib', 'numpy', 'seaborn', 'spacy' ],  
+    url='https://github.com/qufy6/wadseg',  
+    python_requires='>=3.6', 
+)
