302d7935ea540bf6b1ff4efe289fee79e61c2bf7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-08 03:15:40 +0000

    Add package konduktor-nightly/0.1.0.dev20250407104731/konduktor_nightly-0.1.0.dev20250407104731-py3-none-any.whl

---
+++ packages/konduktor-nightly/konduktor_nightly-0.1.0.dev20250407104731-py3-none-any.whl/konduktor/data/registry.py
@@ -0,0 +1,18 @@
+from konduktor.data.gcp import GcsCloudStorage
+
+# Maps bucket's URIs prefix(scheme) to its corresponding storage class
+
+_REGISTRY = {
+    'gs': GcsCloudStorage(),
+    # TODO(asaiacai): Add other cloud stores here
+    # 's3': S3CloudStorage(),
+    # 'r2': R2CloudStorage(),
+    # 'cos': IBMCosCloudStorage(),
+    # 'oci': OciCloudStorage(),
+    # # TODO: This is a hack, as Azure URL starts with https://, we should
+    # # refactor the registry to be able to take regex, so that Azure blob can
+    # # be identified with `https://(.*?)\.blob\.core\.windows\.net`
+    # 'https': AzureBlobCloudStorage()
+}
+
+_STORE_ENABLED_CLOUDS = list(_REGISTRY.keys())
