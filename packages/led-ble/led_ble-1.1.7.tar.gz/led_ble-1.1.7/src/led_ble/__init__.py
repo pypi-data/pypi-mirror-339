01fdf7ce0cd4a778515b5811b0860b31e10a2624
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-08 06:04:41 +0000

    Add package led-ble/1.1.7/led_ble-1.1.7.tar.gz

---
+++ packages/led-ble/led_ble-1.1.7.tar.gz/led_ble-1.1.7/src/led_ble/__init__.py
@@ -0,0 +1,17 @@
+from __future__ import annotations
+
+__version__ = "1.1.7"
+
+
+from bleak_retry_connector import get_device
+
+from .exceptions import CharacteristicMissingError
+from .led_ble import BLEAK_EXCEPTIONS, LEDBLE, LEDBLEState
+
+__all__ = [
+    "BLEAK_EXCEPTIONS",
+    "CharacteristicMissingError",
+    "LEDBLE",
+    "LEDBLEState",
+    "get_device",
+]
---
+++ packages/led-ble/led_ble-1.1.7.tar.gz/led_ble-1.1.7/src/led_ble/models.py
@@ -0,0 +1,15 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+
+
+@dataclass(frozen=True)
+class LEDBLEState:
+    power: bool = False
+    rgb: tuple[int, int, int] = (0, 0, 0)
+    w: int = 0
+    model_num: int = 0
+    preset_pattern: int = 0
+    mode: int = 0
+    speed: int = 0
+    version_num: int = 0
