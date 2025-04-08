0c54d4c930d5a2b9ab64faa484838af4ac5e62ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-08 05:04:59 +0000

    Add package dlt/1.9.1a0/dlt-1.9.1a0.tar.gz

---
+++ packages/dlt/dlt-1.9.1a0.tar.gz/dlt-1.9.1a0/dlt/destinations/impl/athena/configuration.py
@@ -0,0 +1,29 @@
+import dataclasses
+from typing import ClassVar, Final, List, Optional
+import warnings
+
+from dlt.common.configuration import configspec
+from dlt.common.destination.client import DestinationClientDwhWithStagingConfiguration
+from dlt.common.configuration.specs import AwsCredentials
+from dlt.common.warnings import Dlt100DeprecationWarning
+
+
+@configspec
+class AthenaClientConfiguration(DestinationClientDwhWithStagingConfiguration):
+    destination_type: Final[str] = dataclasses.field(default="athena", init=False, repr=False, compare=False)  # type: ignore[misc]
+    query_result_bucket: str = None
+    credentials: AwsCredentials = None
+    athena_work_group: Optional[str] = None
+    aws_data_catalog: Optional[str] = "awsdatacatalog"
+    force_iceberg: Optional[bool] = None
+    # possible placeholders: {dataset_name}, {table_name}, {location_tag}
+    table_location_layout: Optional[str] = "{dataset_name}/{table_name}"
+
+    __config_gen_annotations__: ClassVar[List[str]] = ["athena_work_group"]
+
+    def __str__(self) -> str:
+        """Return displayable destination location"""
+        if self.staging_config:
+            return f"{self.staging_config} on {self.aws_data_catalog}"
+        else:
+            return "[no staging set]"
---
+++ packages/dlt/dlt-1.9.1a0.tar.gz/dlt-1.9.1a0/dlt/destinations/impl/filesystem/sql_client.py
@@ -0,0 +1,189 @@
+from typing import Any, TYPE_CHECKING
+import os
+import re
+import duckdb
+
+from dlt.common import logger
+from dlt.common.destination.typing import PreparedTableSchema
+from dlt.common.storages.configuration import FileSystemCredentials
+
+from dlt.common.typing import TLoaderFileFormat
+from dlt.destinations.sql_client import raise_database_error
+from dlt.destinations.impl.duckdb.sql_client import WithTableScanners
+from dlt.destinations.impl.duckdb.factory import DuckDbCredentials
+
+from dlt.destinations.utils import is_compression_disabled
+
+SUPPORTED_PROTOCOLS = ["gs", "gcs", "s3", "file", "memory", "az", "abfss"]
+
+if TYPE_CHECKING:
+    from dlt.destinations.impl.filesystem.filesystem import FilesystemClient
+else:
+    FilesystemClient = Any
+
+
+class FilesystemSqlClient(WithTableScanners):
+    def __init__(
+        self,
+        remote_client: FilesystemClient,
+        dataset_name: str,
+        cache_db: DuckDbCredentials = None,
+        persist_secrets: bool = False,
+    ) -> None:
+        if remote_client.config.protocol not in SUPPORTED_PROTOCOLS:
+            raise NotImplementedError(
+                f"Protocol {remote_client.config.protocol} currently not supported for"
+                f" FilesystemSqlClient. Supported protocols are {SUPPORTED_PROTOCOLS}."
+            )
+        super().__init__(remote_client, dataset_name, cache_db, persist_secrets=persist_secrets)
+        self.remote_client: FilesystemClient = remote_client
+        self.is_abfss = self.remote_client.config.protocol == "abfss"
+        self.iceberg_initialized = False
+
+    def can_create_view(self, table_schema: PreparedTableSchema) -> bool:
+        if table_schema.get("table_format") in ("delta", "iceberg"):
+            return True
+        file_format = self.get_file_format(table_schema)
+        return file_format in ("jsonl", "parquet")
+
+    def get_file_format(self, table_schema: PreparedTableSchema) -> TLoaderFileFormat:
+        if table_schema["name"] in self.schema.dlt_table_names():
+            return "jsonl"
+        file_format = table_schema.get("file_format")
+        if not file_format or file_format == "preferred":
+            file_format = self.remote_client.capabilities.preferred_loader_file_format
+        return file_format
+
+    def create_secret(
+        self,
+        scope: str,
+        credentials: FileSystemCredentials,
+        secret_name: str = None,
+    ) -> bool:
+        protocol = self.remote_client.config.protocol
+        if protocol == "file":
+            return True
+        if not super().create_secret(scope, credentials, secret_name=secret_name):
+            # native google storage implementation is not supported..
+            if protocol in ["gs", "gcs"]:
+                logger.warn(
+                    "For gs/gcs access via duckdb please use the gs/gcs s3 compatibility layer if"
+                    " possible (not supported when using `iceberg` table format). Falling back to"
+                    " fsspec."
+                )
+                self._conn.register_filesystem(self.remote_client.fs_client)
+            # for memory we also need to register filesystem
+            elif protocol == "memory":
+                self._conn.register_filesystem(self.remote_client.fs_client)
+            elif protocol == "file":
+                # authentication for local filesystem not needed
+                pass
+            else:
+                raise ValueError(
+                    f"Cannot create secret or register filesystem for protocol {protocol}"
+                )
+        return True
+
+    def open_connection(self) -> duckdb.DuckDBPyConnection:
+        first_connection = self.credentials.never_borrowed
+        super().open_connection()
+
+        if first_connection:
+            # create single authentication for the whole client
+            self.create_secret(
+                self.remote_client.config.bucket_url, self.remote_client.config.credentials
+            )
+
+        return self._conn
+
+    def should_replace_view(self, view_name: str, table_schema: PreparedTableSchema) -> bool:
+        # we use alternative method to get snapshot on abfss and we need to replace
+        # the view each time to control the freshness (abfss cannot glob)
+        return self.is_abfss  # and table_format == "iceberg"
+
+    @raise_database_error
+    def create_view(self, view_name: str, table_schema: PreparedTableSchema) -> None:
+        # NOTE: data freshness
+        # iceberg - currently we glob the most recent snapshot (via built in duckdb mechanism) so data is fresh
+        #           (but not very efficient)
+        # delta - newest version is always read
+        # files - newest files
+        table_name = table_schema["name"]
+        table_format = table_schema.get("table_format")
+        protocol = self.remote_client.config.protocol
+        table_location = self.remote_client.get_open_table_location(table_format, table_name)
+
+        # discover whether compression is enabled
+        compression = "" if is_compression_disabled() else ", compression = 'gzip'"
+
+        # dlt tables are never compressed for now...
+        if table_name in self.remote_client.schema.dlt_table_names():
+            compression = ""
+
+        # create from statement
+        from_statement = ""
+        if table_format == "delta":
+            from_statement = f"delta_scan('{table_location}')"
+        elif table_format == "iceberg":
+            if not self.iceberg_initialized:
+                self._setup_iceberg(self._conn)
+                self.iceberg_initialized = True
+            if self.is_abfss:
+                # duckdb can't glob on abfss 🤯
+                from dlt.common.libs.pyiceberg import get_last_metadata_file
+
+                metadata_path = f"{table_location}/metadata"
+                last_metadata_file = get_last_metadata_file(
+                    metadata_path, self.remote_client.fs_client, self.remote_client.config
+                )
+                from_statement = f"iceberg_scan('{last_metadata_file}', skip_schema_inference=True)"
+            else:
+                # skip schema inference to make nested data types work
+                # https://github.com/duckdb/duckdb_iceberg/issues/47
+                from_statement = (
+                    f"iceberg_scan('{table_location}', version='?', allow_moved_paths = true,"
+                    " skip_schema_inference=true)"
+                )
+        else:
+            # get file format from schema
+            # NOTE: this does not support cases where table contains many different file formats
+            first_file_type = self.get_file_format(table_schema)
+
+            # build files string
+            supports_wildcard_notation = not self.is_abfss
+
+            resolved_files_string = f"'{table_location}/**/*.{first_file_type}'"
+            if not supports_wildcard_notation:
+                files = self.remote_client.list_table_files(table_name)
+                resolved_files_string = ",".join(map(lambda f: f"'{protocol}://{f}'", files))
+
+            if first_file_type == "parquet":
+                from_statement = f"read_parquet([{resolved_files_string}])"
+            elif first_file_type == "jsonl":
+                # build columns definition
+                type_mapper = self.capabilities.get_type_mapper()
+                columns = ",".join(
+                    map(
+                        lambda c: (
+                            f'{self.escape_column_name(c["name"])}:'
+                            f' "{type_mapper.to_destination_type(c, table_schema)}"'
+                        ),
+                        self.remote_client.schema.tables[table_name]["columns"].values(),
+                    )
+                )
+                from_statement = (
+                    f"read_json([{resolved_files_string}], columns = {{{columns}}}{compression})"
+                )
+            else:
+                raise NotImplementedError(
+                    f"Unknown filetype {first_file_type} for table {table_name}. Currently only"
+                    " jsonl and parquet files as well as delta and iceberg tables are"
+                    " supported."
+                )
+
+        # create table
+        view_name = self.make_qualified_table_name(view_name)
+        create_table_sql_base = (
+            f"CREATE OR REPLACE VIEW {view_name} AS SELECT * FROM {from_statement}"
+        )
+        self._conn.execute(create_table_sql_base)
