1613a48f024877c3c9192e2fe4e860145d9c23b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-08 06:05:34 +0000

    Add package lawyer/1.2.0/lawyer-1.2.0-py3-none-any.whl

---
+++ packages/lawyer/lawyer-1.2.0-py3-none-any.whl/lawyer/exceptions.pyi
@@ -0,0 +1,54 @@
+"""exceptions [`Module`].
+
+This module contains several exceptions related to argument parsing.
+
+----------------------------------------
+This module is under license as follows:
+----------------------------------------
+
+MIT License
+-----------
+
+Copyright (c) 2025 Soumyo Deep Gupta
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+---------------------------- END of LICENSE ----------------------------------
+"""
+
+class ParameterError(Exception):
+    """Parameter format error exception."""
+
+    __strerror__: str
+    __interror__: str
+    __boolerror__: str
+    __iterablerror__: str
+    __capturetyperror__: str
+
+
+class ArgumentError(Exception):
+    """Argument error exception."""
+
+class InstanceError(Exception):
+    """Instance is not of type `singlet` or `singlets`."""
+
+class ParseError(Exception):
+    """Parse error exception."""
+
+class ExistsError(Exception):
+    """Entity already exists."""
