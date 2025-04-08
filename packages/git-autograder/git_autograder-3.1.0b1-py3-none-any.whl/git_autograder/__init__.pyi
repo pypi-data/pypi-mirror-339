9ef03fdb9df5c189c4bb4de096fc2c5fd9e0e83b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-08 15:05:40 +0000

    Add package git-autograder/3.1.0b1/git_autograder-3.1.0b1-py3-none-any.whl

---
+++ packages/git-autograder/git_autograder-3.1.0b1-py3-none-any.whl/git_autograder/__init__.pyi
@@ -0,0 +1,14 @@
+from git_autograder.autograder import autograder
+from git_autograder.test_utils import setup_autograder, set_env
+from git_autograder.repo import GitAutograderRepo
+from git_autograder.status import GitAutograderStatus
+from git_autograder.output import GitAutograderOutput
+
+__all__ = [
+    "autograder",
+    "setup_autograder",
+    "set_env",
+    "GitAutograderRepo",
+    "GitAutograderStatus",
+    "GitAutograderOutput",
+]
