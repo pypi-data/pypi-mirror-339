92fadb7f4c5eb91d613a50f9db761679b8971366
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-08 05:04:45 +0000

    Add package bunnybase/0.1.7/bunnybase-0.1.7.tar.gz

---
+++ packages/bunnybase/bunnybase-0.1.7.tar.gz/bunnybase-0.1.7/bunnybase/datalist.py
@@ -0,0 +1,34 @@
+from .data import Data
+
+class DataList:
+    def __init__(self, datalist: list[Data]):
+        self._datalist = datalist
+        self._len = len(datalist)
+    def filter(self, *args, **kwargs):
+        elements = []
+        
+        for e in self._datalist:
+            if e.has_properties(**kwargs):
+                elements.append(e)
+            elif e.has_property_values(*args):
+                elements.append(e)
+        return DataList(elements)
+    def __repr__(self):
+        return repr(self._datalist)
+    def __getitem__(self, item: tuple[str]|str):
+        if isinstance(item, tuple):
+            return self.filter(*item)
+        elif isinstance(item, str):
+            return self.filter(*item)
+        else:
+            return self._datalist[item]
+    def __len__(self) -> int:
+        return self._len
+    def __iter__(self):
+        self.n = 0
+        return self
+    def __next__(self):
+        self.n += 1
+        if self.n == self._len:
+            raise StopIteration()
+        return self._datalist[self.n]
