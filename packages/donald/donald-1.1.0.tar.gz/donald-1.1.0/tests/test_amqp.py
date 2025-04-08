5535a21a9538f683c7ab371867ea96f959c8f897
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-08 03:15:51 +0000

    Add package donald/1.1.0/donald-1.1.0.tar.gz

---
+++ packages/donald/donald-1.1.0.tar.gz/donald-1.1.0/tests/test_amqp.py
@@ -0,0 +1,36 @@
+import pytest
+
+from .tasks import async_task, manager
+
+
+@pytest.fixture()
+async def setup():
+    manager.setup(backend="amqp")
+    assert manager._backend
+    assert manager._backend.params
+
+    async with manager:
+        w1 = manager.create_worker()
+        w2 = manager.create_worker()
+        w1.start()
+        w2.start()
+        yield manager
+        await w1.stop()
+        await w2.stop()
+
+
+async def test_submit_task(setup, sleep, checklog):
+    res = async_task.submit()
+    assert res
+    assert await res
+
+    await sleep()
+    assert checklog("Run async_task 42")
+
+    for n in range(3):
+        assert await async_task.submit(n)
+
+    await sleep(1e-1)
+    assert checklog("Run async_task 0")
+    assert checklog("Run async_task 1")
+    assert checklog("Run async_task 2")
