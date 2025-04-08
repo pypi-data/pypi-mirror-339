35904d0207582b50dd7a2b3d3b9f5ad7e6b90dfc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-08 04:04:38 +0000

    Add package hueytech-audit-logger/0.3.0/hueytech_audit_logger-0.3.0.tar.gz

---
+++ packages/hueytech-audit-logger/hueytech_audit_logger-0.3.0.tar.gz/hueytech_audit_logger-0.3.0/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+import io
+
+with io.open("README.md", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="hueytech-audit-logger",
+    version="0.3.0",
+    packages=find_packages(),
+    install_requires=[
+        "Django>=3.2",
+        "psycopg2-binary>=2.9.3",
+        "gunicorn>=20.1.0",
+        "boto3>=1.26.0",
+        "python-dotenv>=0.21.0",
+    ],
+    extras_require={
+        'async': ["celery>=5.2.0"],
+        'mongo': ["pymongo>=4.0.0", "dnspython>=2.0.0"],
+    },
+    author="payme-alok",
+    author_email="infra@paymeindia.in",
+    description="A Django middleware for logging requests and responses to PostgreSQL with dual logging capabilities",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/paymeinfra/hueytech_audit_logs",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+    python_requires=">=3.8",
+)
