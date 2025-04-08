e5e31c6eac1c2b6087b99564cc7479d82ea82f5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-08 07:04:20 +0000

    Add package argenta/0.4.8/argenta-0.4.8-py3-none-any.whl

---
+++ packages/argenta/argenta-0.4.8-py3-none-any.whl/argenta/app/defaults.py
@@ -0,0 +1,8 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class PredeterminedMessages:
+    USAGE = '[b dim]Usage[/b dim]: [i]<command> <[green]flags[/green]>[/i]'
+    HELP = '[b dim]Help[/b dim]: [i]<command>[/i] [b red]--help[/b red]'
+
