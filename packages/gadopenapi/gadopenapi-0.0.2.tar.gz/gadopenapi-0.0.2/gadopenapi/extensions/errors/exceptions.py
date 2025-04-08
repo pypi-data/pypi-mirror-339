06d34442a8011d01596237c74a1a0c09c40a2339
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-08 16:05:35 +0000

    Add package gadopenapi/0.0.2/gadopenapi-0.0.2.tar.gz

---
+++ packages/gadopenapi/gadopenapi-0.0.2.tar.gz/gadopenapi-0.0.2/gadopenapi/extensions/errors/exceptions.py
@@ -0,0 +1,14 @@
+from http import HTTPStatus
+
+
+class APIError(Exception):
+    status_code: int = HTTPStatus.IM_A_TEAPOT
+
+    def to_dict(self) -> dict:
+        return dict(
+            status_code=self.status_code,
+            detail=dict(type=self.__class__.__name__),
+        )
+
+
+__all__ = ["APIError"]
---
+++ packages/gadopenapi/gadopenapi-0.0.2.tar.gz/gadopenapi-0.0.2/gadopenapi/extensions/operationid/handlers.py
@@ -0,0 +1,30 @@
+from typing import Any
+
+from gadopenapi import const
+
+
+def use_route_as_operation_id(app: Any, openapi: dict, exclude: tuple[str] = ()) -> tuple[Any, dict]:
+    if not (routes := getattr(app, const.SPECIFICATION_ROUTES, [])):
+        return app, openapi
+
+    for route in routes:
+        if not (endpoint := getattr(route, const.SPECIFICATION_ENDPOINT, None)):
+            continue
+
+        if path := getattr(route, const.FASTAPI_ROUTE_PATH, None):
+            if path in exclude:
+                continue
+
+        if not (paths := openapi.get(const.SPECIFICATION_PATHS, {})):
+            continue
+
+        if not (path := paths.get(path, {})):
+            continue
+
+        for method in path:
+            path[method][const.SPECIFICATION_OPERATION_ID] = getattr(endpoint, "__name__")
+
+    return app, openapi
+
+
+__all__ = ["use_route_as_operation_id"]
