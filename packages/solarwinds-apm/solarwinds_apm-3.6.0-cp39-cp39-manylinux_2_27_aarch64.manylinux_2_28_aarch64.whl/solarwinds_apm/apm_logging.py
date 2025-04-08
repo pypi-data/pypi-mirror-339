7bcdc1d6273c4dc6627354cd2e97ec5a9ab70e39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-08 14:08:19 +0000

    Add package solarwinds-apm/3.6.0/solarwinds_apm-3.6.0-cp39-cp39-manylinux_2_27_aarch64.manylinux_2_28_aarch64.whl

---
+++ packages/solarwinds-apm/solarwinds_apm-3.6.0-cp39-cp39-manylinux_2_27_aarch64.manylinux_2_28_aarch64.whl/solarwinds_apm/apm_logging.py
@@ -0,0 +1,246 @@
+# © 2023 SolarWinds Worldwide, LLC. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at:http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+
+"""SolarWinds APM agent-internal logging module.
+
+The solarwinds_apm internal logging is making use of the built-in Python logging module. The solarwinds_apm package
+creates a logging handler reporting to stderr where all log messages are reported to. All logging from solarwinds_apm
+can be stopped completely by invoking the disable_logger() method.
+
+A log message has the following format:
+    '<date> <time> [ <logger_name> <logging_level> p#<process_id>.<thread_id>] <log_message>'
+
+The logging level of the solarwinds_apm logger can be configured with an integer in the range of -1 to 6, representing
+the following severities:
+    -1: logging disabled
+     0: fatal messages
+     1: errors
+     2: warnings
+     3: info
+     4: debug low
+     5: debug medium
+     6: debug high
+
+The log level should not be modified manually from outside the solarwinds_apm package.
+
+For package-internal purposes, the logging level can be configured by providing one of the above levels to the
+`set_sw_log_level`. The setLevel method should not be used to modify logging levels of individual module logger
+instances.
+
+The name of the main logger is `solarwinds_apm`. Each module should create its own child logger by simply calling
+logging.getLogger(__name__) which will return a child logger of the `solarwinds_apm` logger.
+"""
+
+import logging
+import os
+from logging.handlers import RotatingFileHandler
+
+
+class ApmLoggingType:
+    """Mapping of supported solarwinds_apm library log types"""
+
+    log_types = {
+        "STDERR": 0,
+        "FILE": 2,
+        "DISABLED": 4,
+    }
+
+    @classmethod
+    def default_type(cls):
+        """Returns integer representation of default log type"""
+        return cls.log_types["STDERR"]
+
+    @classmethod
+    def disabled_type(cls):
+        """Returns integer representation of disabled log type"""
+        return cls.log_types["DISABLED"]
+
+    @classmethod
+    def file_type(cls):
+        """Returns integer representation of to-file log type"""
+        return cls.log_types["FILE"]
+
+    @classmethod
+    def is_valid_log_type(cls, log_type):
+        """Returns True if the provided type is a valid interger representation of log type, False otherwise."""
+        try:
+            log_type = int(log_type)
+            return bool(log_type in list(cls.log_types.values()))
+        except (ValueError, TypeError):
+            return cls.default_type()
+
+
+class ApmLoggingLevel:
+    """Mapping class providing a conversion between solarwinds_apm library logging level and Python logging module
+    logging levels.
+    The solarwinds_apm package has six main log levels, which are defined in the debug_levels dictionary.
+    There is also a special 7th OBOE_DEBUG_DISABLE for disabling logging, as there is no "disabled" level in Python logging nor agent logging.
+    """
+
+    # maps string representation of solarwinds_apm.sw_logging levels to their integer counterpart
+    debug_levels = {
+        "OBOE_DEBUG_DISABLE": -1,
+        "OBOE_DEBUG_FATAL": 0,
+        "OBOE_DEBUG_ERROR": 1,
+        "OBOE_DEBUG_WARNING": 2,
+        "OBOE_DEBUG_INFO": 3,
+        "OBOE_DEBUG_LOW": 4,
+        "OBOE_DEBUG_MEDIUM": 5,
+        "OBOE_DEBUG_HIGH": 6,
+    }
+
+    # maps solarwinds_apm log levels to python logging module log levels
+    logging_map = {
+        debug_levels["OBOE_DEBUG_DISABLE"]: logging.CRITICAL,
+        debug_levels["OBOE_DEBUG_FATAL"]: logging.CRITICAL,
+        debug_levels["OBOE_DEBUG_ERROR"]: logging.ERROR,
+        debug_levels["OBOE_DEBUG_WARNING"]: logging.WARNING,
+        debug_levels["OBOE_DEBUG_INFO"]: logging.INFO,
+        debug_levels["OBOE_DEBUG_LOW"]: logging.DEBUG,
+        debug_levels["OBOE_DEBUG_MEDIUM"]: logging.DEBUG,
+        debug_levels["OBOE_DEBUG_HIGH"]: logging.DEBUG,
+    }
+
+    @classmethod
+    def default_level(cls):
+        """Returns integer representation of default debugging level"""
+        return cls.debug_levels["OBOE_DEBUG_WARNING"]
+
+    @classmethod
+    def is_valid_level(cls, level):
+        """Returns True if the provided level is a valid integer representation of a solarwinds_apm.sw_logging level,
+        False otherwise."""
+        try:
+            level = int(level)
+            return bool(level in list(cls.debug_levels.values()))
+        except (ValueError, TypeError):
+            return False
+
+
+def _create_stream_handler():
+    """Creates stream handler reporting to stderr."""
+    sh = logging.StreamHandler()  # get stream handler to custom logging prefix
+    fm = logging.Formatter(
+        "%(asctime)s [ %(name)s %(levelname)-8s p#%(process)d.%(thread)d] %(message)s"
+    )
+    sh.setFormatter(fm)
+    return sh
+
+
+_stream_handler = _create_stream_handler()
+
+
+def _get_logger():
+    """Creates the logger for agent-internal logging.
+    By default, the logging level of the created logger will be set to ApmLoggingLevel.default_level().
+    If the logging level of the agent-internal logger needs to be changed, this should happen through one of the
+    following options only:
+    (1) Through environment variable SW_APM_DEBUG_LEVEL
+        - When _get_logger is invoked, SW_APM_DEBUG_LEVEL is checked and the logging level will be set to the
+          value provided by the variable. If an invalid value has been provided, the logging level will not be changed.
+    (2) By invoking set_sw_log_level
+    """
+
+    # create base logger for solarwinds_apm package
+    _logger = logging.getLogger("solarwinds_apm")
+
+    # configure logging level of solarwinds_apm logger
+    log_level = ApmLoggingLevel.default_level()
+    # check if SW_APM_DEBUG_LEVEL has been set and configure newly created logger accordingly
+    envv_val = os.getenv("SW_APM_DEBUG_LEVEL", None)
+    if envv_val is not None:
+        if not ApmLoggingLevel.is_valid_level(envv_val):
+            _logger.warning(
+                "Misconfigured SW_APM_DEBUG_LEVEL ignored. Defaulted to debug_level %s",
+                log_level,
+            )
+            _logger.warning("")
+        else:
+            log_level = int(envv_val)
+
+    _logger.setLevel(ApmLoggingLevel.logging_map[log_level])
+
+    _logger.propagate = False
+    if log_level != ApmLoggingLevel.debug_levels["OBOE_DEBUG_DISABLE"]:
+        _logger.addHandler(_stream_handler)
+    else:
+        _logger.addHandler(logging.NullHandler())
+
+    return _logger
+
+
+logger = _get_logger()
+
+
+def disable_logger(disable=True):
+    """Disables all logging messages from the `solarwinds_apm` package when disable is True.
+    To restoring logging, set disable as False.
+    """
+    if disable:
+        logger.addHandler(logging.NullHandler())
+        logger.removeHandler(_stream_handler)
+        logger.propagate = False
+    else:
+        logger.addHandler(_stream_handler)
+        logger.removeHandler(logging.NullHandler())
+        logger.propagate = True
+
+
+def set_sw_log_type(log_type, log_filepath=""):
+    """Set the logging 'type' of the agent-internal logger.
+
+    This function expects the log_type to be one of the integer
+    representations of the levels defined in ApmLoggingType.log_types.
+    If an invalid type has been provided, the logging handler will not be
+    changed but a warning message will be emitted.
+
+    If log_filepath is provided and log_type is file_type, logger stream handler
+    is swapped with a rotating file handler.
+    """
+    if not ApmLoggingType.is_valid_log_type(log_type):
+        logger.warning(
+            "set_sw_log_type: Ignore attempt to set solarwinds_apm logger with invalid logging handler."
+        )
+        return
+
+    if log_type == ApmLoggingType.file_type() and log_filepath:
+        try:
+            # no rollover to match oboe logs
+            file_hander = RotatingFileHandler(
+                filename=log_filepath,
+                maxBytes=0,
+                backupCount=0,
+            )
+            file_formatter = logging.Formatter(
+                "%(asctime)s [ %(name)s %(levelname)-8s p#%(process)d.%(thread)d] %(message)s"
+            )
+            file_hander.setFormatter(file_formatter)
+            logger.propagate = False
+            logger.addHandler(file_hander)
+            # stop logging to stream
+            logger.removeHandler(_stream_handler)
+        except FileNotFoundError:
+            # path should be checked first by ApmConfig.update_log_filepath
+            logger.error(
+                "Could not write logs to file; please check configured SW_APM_LOG_FILEPATH."
+            )
+
+
+def set_sw_log_level(level):
+    """Set the logging level of the agent-internal logger to the provided level. This function expects the level
+    to be one of the integer representations of the levels defined in ApmLoggingLevel.debug_levels.
+    If an invalid level has been provided, the logging level will not be changed but a warning message will be emitted.
+
+    This function should not be used from outside the solarwinds_apm package to modify agent logging behaviour.
+    """
+    if ApmLoggingLevel.is_valid_level(level):
+        logger.setLevel(ApmLoggingLevel.logging_map[level])
+        if level == ApmLoggingLevel.debug_levels["OBOE_DEBUG_DISABLE"]:
+            disable_logger()
+    else:
+        logger.warning(
+            "set_sw_log_level: Ignore attempt to set solarwinds_apm logger to invalid logging level."
+        )
---
+++ packages/solarwinds-apm/solarwinds_apm-3.6.0-cp39-cp39-manylinux_2_27_aarch64.manylinux_2_28_aarch64.whl/solarwinds_apm/propagator.py
@@ -0,0 +1,187 @@
+# © 2023 SolarWinds Worldwide, LLC. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at:http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+
+# TODO: Remove when Python < 3.10 support dropped
+from __future__ import annotations
+
+import logging
+import typing
+from re import split
+
+from opentelemetry import trace
+from opentelemetry.baggage.propagation import _format_baggage
+from opentelemetry.context.context import Context
+from opentelemetry.propagators import textmap
+from opentelemetry.trace.span import TraceState
+from opentelemetry.util.re import _DELIMITER_PATTERN
+
+from solarwinds_apm.apm_constants import (
+    INTL_SWO_CURRENT_TRACE_ENTRY_SPAN_ID,
+    INTL_SWO_TRACESTATE_KEY,
+    INTL_SWO_X_OPTIONS_KEY,
+)
+from solarwinds_apm.traceoptions import XTraceOptions
+from solarwinds_apm.w3c_transformer import W3CTransformer
+
+logger = logging.getLogger(__name__)
+
+
+class SolarWindsPropagator(textmap.TextMapPropagator):
+    """Extracts and injects SolarWinds headers for trace propagation.
+    Must be used in composite with TraceContextTextMapPropagator.
+    """
+
+    _INVALID_SPAN_ID = 0x0000000000000000
+    _TRACESTATE_HEADER_NAME = "tracestate"
+    _BAGGAGE_HEADER_NAME = "baggage"
+    _XTRACEOPTIONS_HEADER_NAME = "x-trace-options"
+    _XTRACEOPTIONS_SIGNATURE_HEADER_NAME = "x-trace-options-signature"
+
+    def extract(
+        self,
+        carrier: textmap.CarrierT,
+        context: Context | None = None,
+        getter: textmap.Getter = textmap.default_getter,
+    ) -> Context:
+        """Extracts sw trace options and signature from carrier into OTel
+        Context. Note: tracestate is extracted by TraceContextTextMapPropagator
+        """
+        if context is None:
+            context = Context()
+
+        xtraceoptions_header = getter.get(
+            carrier, self._XTRACEOPTIONS_HEADER_NAME
+        ) or [""]
+        signature_header = getter.get(
+            carrier, self._XTRACEOPTIONS_SIGNATURE_HEADER_NAME
+        ) or [""]
+        xtraceoptions = XTraceOptions(
+            xtraceoptions_header[0],
+            signature_header[0],
+        )
+
+        context.update({INTL_SWO_X_OPTIONS_KEY: xtraceoptions})
+        return context
+
+    def inject(
+        self,
+        carrier: textmap.CarrierT,
+        context: Context | None = None,
+        setter: textmap.Setter = textmap.default_setter,
+    ) -> None:
+        """Injects valid sw tracestate from carrier into carrier for HTTP request, to get
+        tracestate injected by previous propagators. Excludes any xtraceoptions_response
+        if in tracestate."""
+        span = trace.get_current_span(context)
+        span_context = span.get_span_context()
+        sw_value = W3CTransformer.sw_from_context(span_context)
+        trace_state_header = carrier.get(self._TRACESTATE_HEADER_NAME, None)
+        baggage_header = carrier.get(self._BAGGAGE_HEADER_NAME, None)
+
+        # Prepare carrier with carrier's or new tracestate
+        trace_state = None
+        # Note: OTel Propagation API callers (OTel instrumentors) may
+        # inject header values as dictionary, not a string.
+        if isinstance(trace_state_header, dict):
+            trace_state_header = trace_state_header.get("StringValue")
+        if not trace_state_header:
+            # Only create new trace state if valid span_id
+            if span_context.span_id == self._INVALID_SPAN_ID:
+                return
+            logger.debug(
+                "Creating new trace state for injection with %s",
+                sw_value,
+            )
+            trace_state = TraceState([(INTL_SWO_TRACESTATE_KEY, sw_value)])
+        else:
+            trace_state = TraceState.from_header([trace_state_header])
+            # Check if trace_state already contains sw KV
+            if INTL_SWO_TRACESTATE_KEY in trace_state.keys():
+                # If so, modify current span_id and trace_flags, and move to beginning of list
+                logger.debug(
+                    "Updating trace state for injection with %s",
+                    sw_value,
+                )
+                trace_state = trace_state.update(
+                    INTL_SWO_TRACESTATE_KEY, sw_value
+                )
+
+            else:
+                # If not, add sw KV to beginning of list
+                logger.debug(
+                    "Adding KV to trace state for injection with %s",
+                    sw_value,
+                )
+                trace_state = trace_state.add(
+                    INTL_SWO_TRACESTATE_KEY, sw_value
+                )
+
+        # Remove any xtrace_options_response stored for ResponsePropagator
+        trace_state = W3CTransformer.remove_response_from_sw(trace_state)
+        setter.set(
+            carrier, self._TRACESTATE_HEADER_NAME, trace_state.to_header()
+        )
+
+        def _handle_baggage_header(baggage_header):
+            sanitized_baggage = self.remove_custom_naming_baggage_header(
+                baggage_header
+            )
+            if sanitized_baggage != "":
+                # Re-inject if sanitized baggage non-empty
+                setter.set(
+                    carrier, self._BAGGAGE_HEADER_NAME, sanitized_baggage
+                )
+            else:
+                # Or, remove baggage from carrier to prevent empty header
+                del carrier[self._BAGGAGE_HEADER_NAME]
+
+        # Remove any baggage stored for custom transaction naming
+        if baggage_header:
+            # Note: OTel Propagation API callers (OTel instrumentors) may
+            # inject header values as dictionary, not a string.
+            if isinstance(baggage_header, dict):
+                baggage_stringvalue = baggage_header.get("StringValue")
+                _handle_baggage_header(baggage_stringvalue)
+            elif isinstance(baggage_header, str):
+                _handle_baggage_header(baggage_header)
+            # Else: leave baggage header as is in carrier
+
+    def remove_custom_naming_baggage_header(
+        self,
+        baggage_header: str,
+    ) -> str:
+        """Removes values used for custom naming from baggage header created
+        by upstream W3CBaggagePropagator propagator, if present"""
+        baggage_entries: list[str] = split(_DELIMITER_PATTERN, baggage_header)
+        baggage_kvs = {}
+        for entry in baggage_entries:
+            try:
+                e_name, e_value = entry.split("=", 1)
+            except Exception:  # pylint: disable=broad-except
+                logger.warning(
+                    "Baggage list-member `%s` doesn't match the format; "
+                    "skipping injection",
+                    entry,
+                )
+                continue
+
+            # empty key/val
+            if not e_name or not e_value:
+                continue
+            if e_name != INTL_SWO_CURRENT_TRACE_ENTRY_SPAN_ID:
+                baggage_kvs[e_name] = e_value
+
+        # Otel Python API method to nicely join items into header str
+        return _format_baggage(baggage_kvs)
+
+    # Note: this inherits deprecated `typing` use by OTel,
+    #       for compatibility with Python3.8 else TypeError
+    @property
+    def fields(
+        self,
+    ) -> typing.Set[str]:  # pylint: disable=deprecated-typing-alias
+        """Returns a set with the fields set in `inject`"""
+        return {self._TRACESTATE_HEADER_NAME, self._BAGGAGE_HEADER_NAME}
