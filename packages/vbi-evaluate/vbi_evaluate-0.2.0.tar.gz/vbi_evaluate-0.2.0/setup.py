94c8bbdd87590de69fc5c531cb0e0004cd54b36e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-08 15:06:30 +0000

    Add package vbi-evaluate/0.2.0/vbi_evaluate-0.2.0.tar.gz

---
+++ packages/vbi-evaluate/vbi_evaluate-0.2.0.tar.gz/vbi_evaluate-0.2.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="vbi-evaluate",  # tên sẽ hiển thị trên PyPI
+    version="0.2.0",
+    author="LeeMinHoon",
+    author_email="your_email@example.com",
+    description="A package for evaluating and fact-checking documents.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourusername/vbi-evaluate",  # nếu có
+    packages=find_packages(),  # tự động tìm package (như "package/")
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+)
