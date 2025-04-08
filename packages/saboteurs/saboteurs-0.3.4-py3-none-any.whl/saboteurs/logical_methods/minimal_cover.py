b747125916832487869011d068ec1f1e9d1dfbe2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-08 05:04:21 +0000

    Add package saboteurs/0.3.4/saboteurs-0.3.4-py3-none-any.whl

---
+++ packages/saboteurs/saboteurs-0.3.4-py3-none-any.whl/saboteurs/logical_methods/minimal_cover.py
@@ -0,0 +1,81 @@
+"""Provides a generic and slightly-smarter minimal cover algorithm."""
+
+
+def minimal_cover(
+    elements_set, subsets, max_subsets=None, heuristic="default", selected=(), depth=0
+):
+    """
+    Generic method to find minimal subset covers.
+
+    Parameters
+    ----------
+    elements_set : set
+        The set of all elements to cover.
+    subsets : list
+        A list of (name, subset).
+    max_subsets : int
+        Maximal number of subsets allowed.
+    heuristic : function
+        A function ``((name, subset), selected) => value`` where ``name`` is the
+        name of a subset, ``subset`` is what remains of the subset at this stage,
+        ``selected`` is a list of already-selected subset names.
+    selected : list, optional
+        (Recursion parameter, do not use.) Already-selected elements.
+    depth : int, optional
+        (Recursion parameter, do not use.) Depth of the recursion.
+
+    Returns
+    -------
+    None or list
+        None if no solution is found, otherwise a collection of [(name, subset)...].
+    """
+
+    if len(elements_set) == 0:
+        return []
+    if max_subsets == 0:
+        return None
+
+    if depth == 0:
+        full_set = set().union(*[subset for name, subset in subsets])
+        if full_set != elements_set:
+            return None
+
+    subsets = [(n, s) for (n, s) in subsets if len(s)]
+
+    def sorting_heuristic(named_subset):
+        name, subset = named_subset
+        if heuristic == "default":
+            return len(subset)
+        else:
+            return heuristic(named_subset, selected)
+
+    ordered_subsets = sorted(subsets, key=sorting_heuristic)
+
+    while len(ordered_subsets):
+        if max_subsets is not None:
+            critical_subset_length = len(elements_set) / max_subsets
+            max_len = max(len(s) for name, s in ordered_subsets)
+            if max_len < critical_subset_length:
+                return None
+        name, subset = ordered_subsets.pop()
+        new_elements_set = elements_set.difference(subset)
+        new_subsets = [
+            (name_, sub.difference(subset)) for (name_, sub) in ordered_subsets
+        ]
+        new_max_subsets = None if (max_subsets is None) else max_subsets - 1
+        result = minimal_cover(
+            new_elements_set,
+            new_subsets,
+            heuristic=heuristic,
+            selected=list(selected) + [subset],
+            max_subsets=new_max_subsets,
+            depth=depth + 1,
+        )
+        if result is not None:
+            return result + [name]
+        ordered_subsets = [
+            subset_
+            for (subset_, (new_name, new_subset)) in zip(ordered_subsets, new_subsets)
+            if len(new_subset) != 0
+        ]
+    return None
---
+++ packages/saboteurs/saboteurs-0.3.4-py3-none-any.whl/tests/test_logical_methods.py
@@ -0,0 +1,43 @@
+import os
+from saboteurs import (
+    find_logical_saboteurs,
+    generate_combinatorial_groups,
+    design_test_batch,
+    csv_to_groups_data,
+    generate_batch_report,
+)
+
+
+def test_find_logical_saboteurs():
+    groups = {
+        1: ["A", "C", "D"],
+        2: ["B", "C", "E"],
+        3: ["A", "B", "D"],
+        4: ["D", "F", "G"],
+    }
+    result = find_logical_saboteurs(groups, failed_groups=[2, 4])
+    assert result["saboteurs"] == ["E"]
+    assert sorted(result["suspicious"]) == ["F", "G"]
+
+
+def test_find_logical_saboteurs_from_csv():
+    csv_path = os.path.join("tests", "data", "logical.csv")
+    groups, failed_groups = csv_to_groups_data(csv_path)
+    result = find_logical_saboteurs(groups, failed_groups=failed_groups)
+    assert result["saboteurs"] == ["E"]
+    assert sorted(result["suspicious"]) == ["F", "G"]
+
+
+def test_design_test_batch():
+    elements_per_position = {
+        "Position_1": ["A", "B", "C"],
+        "Position_2": ["D", "E", "F", "G"],
+        "Position_3": ["H", "I", "J", "K"],
+        "Position_4": ["L", "M", "N"],
+    }
+    possible_groups = generate_combinatorial_groups(elements_per_position)
+    selected_groups, error = design_test_batch(possible_groups, max_saboteurs=2)
+    assert error is None
+    assert len(selected_groups) == 15
+    raw_data = generate_batch_report(selected_groups)
+    assert len(raw_data) > 2000
