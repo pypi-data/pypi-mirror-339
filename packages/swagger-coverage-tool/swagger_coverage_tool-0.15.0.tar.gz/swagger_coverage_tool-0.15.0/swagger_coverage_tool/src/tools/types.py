b466fabd5fd81f95cdc9b19bfab13fcbf0b764a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-08 06:04:27 +0000

    Add package swagger-coverage-tool/0.15.0/swagger_coverage_tool-0.15.0.tar.gz

---
+++ packages/swagger-coverage-tool/swagger_coverage_tool-0.15.0.tar.gz/swagger_coverage_tool-0.15.0/swagger_coverage_tool/src/tools/types.py
@@ -0,0 +1,9 @@
+from typing import NewType
+
+StatusCode = NewType('StatusCode', int)
+EndpointName = NewType('EndpointName', str)
+
+ServiceKey = NewType('ServiceKey', str)
+ServiceName = NewType('ServiceName', str)
+
+CoveragePercent = NewType('CoveragePercent', float)
