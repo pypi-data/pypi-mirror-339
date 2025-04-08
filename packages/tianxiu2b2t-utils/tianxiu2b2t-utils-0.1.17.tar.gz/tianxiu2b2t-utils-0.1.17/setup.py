0c9605234deae6ba4c52d0d1bf5115d86bdc5ac2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-08 15:05:36 +0000

    Add package tianxiu2b2t-utils/0.1.17/tianxiu2b2t-utils-0.1.17.tar.gz

---
+++ packages/tianxiu2b2t-utils/tianxiu2b2t-utils-0.1.17.tar.gz/tianxiu2b2t-utils-0.1.17/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='tianxiu2b2t-utils',
+    version='0.1.17',
+    description='A collection of utilities for the tianxiu2b2t packages',
+    url='https://github.com/tianxiu2b2t/py-tianxiu2b2t',
+    author='tianxiu2b2t',
+    author_email='administrator@ttb-network.top',
+    license='MIT',
+    packages=[
+        'tianxiu2b2t',
+        'tianxiu2b2t/anyio',
+        'tianxiu2b2t/anyio/streams',
+    ],
+    requires=[
+        'anyio',
+    ]
+)
