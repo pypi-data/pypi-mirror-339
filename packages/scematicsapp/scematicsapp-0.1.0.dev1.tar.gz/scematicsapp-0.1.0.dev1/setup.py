3e66d6a5e8ed702b9e402a20b6bb9461187f4210
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-08 03:16:13 +0000

    Add package scematicsapp/0.1.0.dev1/scematicsapp-0.1.0.dev1.tar.gz

---
+++ packages/scematicsapp/scematicsapp-0.1.0.dev1.tar.gz/scematicsapp-0.1.0.dev1/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="scematicsapp",
+    version=" 0.1.0.dev1",
+    packages=find_packages(),
+    install_requires=[
+        "typer",
+        "requests",
+        "rich",
+        "pillow",
+        "opencv-python",
+        "pathlib",
+    ],
+    entry_points={
+        'console_scripts': [
+            'scematicsapp=scematicsapp.cli:main',
+        ],
+    },
+    author="Karthick M",
+    author_email="karthickeyan@scematics.xyz",
+    description="A CLI tool for managing file uploads",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/scematicsapp",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
