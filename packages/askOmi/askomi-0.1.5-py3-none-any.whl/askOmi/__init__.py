e63a88556bb860bd37c5c0cecb4125eb6cc9342b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-08 04:04:41 +0000

    Add package askOmi/0.1.5/askomi-0.1.5-py3-none-any.whl

---
+++ packages/askOmi/askomi-0.1.5-py3-none-any.whl/askOmi/__init__.py
@@ -0,0 +1,18 @@
+import os
+from google import genai
+
+
+def askOmi(error):
+
+    api_key = "AIzaSyATDPGbokzoJaBm9CU56GbvJT-1rCd75ls"
+    if not api_key:
+        raise ValueError("GENAI_API_KEY environment variable not set!")
+
+    client = genai.Client(api_key=api_key)
+
+    response = client.models.generate_content(
+        model="gemini-2.0-flash",
+        contents=error
+    )
+
+    print(response.text)
