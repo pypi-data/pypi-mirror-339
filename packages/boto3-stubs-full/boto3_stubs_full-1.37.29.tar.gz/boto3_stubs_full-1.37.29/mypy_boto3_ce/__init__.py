cecaa412d302fe99a0316f8b3012c3eb2452dcd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-08 14:09:07 +0000

    Add package boto3-stubs-full/1.37.29/boto3_stubs_full-1.37.29.tar.gz

---
+++ packages/boto3-stubs-full/boto3_stubs_full-1.37.29.tar.gz/boto3_stubs_full-1.37.29/mypy_boto3_ce/__init__.py
@@ -0,0 +1,27 @@
+"""
+Main interface for ce service.
+
+[Documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_ce/)
+
+Copyright 2025 Vlad Emelianov
+
+Usage::
+
+    ```python
+    from boto3.session import Session
+    from mypy_boto3_ce import (
+        Client,
+        CostExplorerClient,
+    )
+
+    session = Session()
+    client: CostExplorerClient = session.client("ce")
+    ```
+"""
+
+from .client import CostExplorerClient
+
+Client = CostExplorerClient
+
+
+__all__ = ("Client", "CostExplorerClient")
