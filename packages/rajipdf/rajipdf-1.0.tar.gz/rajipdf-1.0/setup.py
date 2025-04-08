8437a804f49a56224f4a3f77ea541613c9767aef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-08 06:05:25 +0000

    Add package rajipdf/1.0/rajipdf-1.0.tar.gz

---
+++ packages/rajipdf/rajipdf-1.0.tar.gz/rajipdf-1.0/setup.py
@@ -0,0 +1,10 @@
+import setuptools  # type: ignore
+from pathlib import Path
+
+setuptools.setup(
+    name="rajipdf",
+    version="1.0",
+    # Call the function to get the string
+    long_description=Path("README.md").read_text(),
+    packages=setuptools.find_packages(exclude=["tests", "data"])
+)
