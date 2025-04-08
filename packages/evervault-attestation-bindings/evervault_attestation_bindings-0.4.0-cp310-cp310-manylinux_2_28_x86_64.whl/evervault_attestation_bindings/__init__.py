7f971cca7be03312a47e579ac3cc2a5d37f0cffa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-08 04:04:24 +0000

    Add package evervault-attestation-bindings/0.4.0/evervault_attestation_bindings-0.4.0-cp310-cp310-manylinux_2_28_x86_64.whl

---
+++ packages/evervault-attestation-bindings/evervault_attestation_bindings-0.4.0-cp310-cp310-manylinux_2_28_x86_64.whl/evervault_attestation_bindings/__init__.py
@@ -0,0 +1,5 @@
+from .evervault_attestation_bindings import *
+
+__doc__ = evervault_attestation_bindings.__doc__
+if hasattr(evervault_attestation_bindings, "__all__"):
+    __all__ = evervault_attestation_bindings.__all__
