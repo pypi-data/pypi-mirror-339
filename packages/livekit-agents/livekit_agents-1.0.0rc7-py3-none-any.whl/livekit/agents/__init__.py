e90f61aaf8e35497d6d2afa5b38229feff329f71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-08 04:04:41 +0000

    Add package livekit-agents/1.0.0rc7/livekit_agents-1.0.0rc7-py3-none-any.whl

---
+++ packages/livekit-agents/livekit_agents-1.0.0rc7-py3-none-any.whl/livekit/agents/__init__.py
@@ -0,0 +1,106 @@
+# Copyright 2023 LiveKit, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from . import cli, ipc, llm, metrics, stt, tokenize, tts, utils, vad  # noqa: F401, I001
+from ._exceptions import (
+    APIConnectionError,
+    APIError,
+    APIStatusError,
+    APITimeoutError,
+    AssignmentTimeoutError,
+)
+from .job import AutoSubscribe, JobContext, JobExecutorType, JobProcess, JobRequest, get_job_context
+from .llm.chat_context import (
+    ChatContent,
+    ChatContext,
+    ChatItem,
+    ChatMessage,
+    ChatRole,
+    FunctionCall,
+    FunctionCallOutput,
+)
+from .llm.tool_context import FunctionTool, function_tool
+from .plugin import Plugin
+from .types import (
+    DEFAULT_API_CONNECT_OPTIONS,
+    NOT_GIVEN,
+    AgentState,
+    APIConnectOptions,
+    NotGiven,
+    NotGivenOr,
+)
+from .version import __version__
+from .voice import Agent, AgentEvent, AgentSession, ModelSettings, RunContext, io
+from .voice.background_audio import AudioConfig, BackgroundAudioPlayer, BuiltinAudioClip
+from .voice.room_io import RoomInputOptions, RoomIO, RoomOutputOptions
+from .worker import Worker, WorkerOptions, WorkerPermissions, WorkerType, SimulateJobInfo
+
+__all__ = [
+    "__version__",
+    "Worker",
+    "WorkerOptions",
+    "WorkerType",
+    "WorkerPermissions",
+    "JobProcess",
+    "JobContext",
+    "JobRequest",
+    "get_job_context",
+    "JobExecutorType",
+    "AutoSubscribe",
+    "AgentState",
+    "FunctionTool",
+    "function_tool",
+    "ChatContext",
+    "ChatItem",
+    "RoomIO",
+    "RoomInputOptions",
+    "RoomOutputOptions",
+    "ChatMessage",
+    "ChatRole",
+    "ChatContent",
+    "io",
+    "FunctionCall",
+    "FunctionCallOutput",
+    "RunContext",
+    "Plugin",
+    "AgentSession",
+    "AgentEvent",
+    "ModelSettings",
+    "Agent",
+    "cli",
+    "AssignmentTimeoutError",
+    "APIConnectionError",
+    "APIError",
+    "APIStatusError",
+    "APITimeoutError",
+    "APIConnectOptions",
+    "AgentState",
+    "NotGiven",
+    "NOT_GIVEN",
+    "NotGivenOr",
+    "DEFAULT_API_CONNECT_OPTIONS",
+    "BackgroundAudioPlayer",
+    "BuiltinAudioClip",
+    "AudioConfig",
+    "SimulateJobInfo",
+]
+
+# Cleanup docs of unexported modules
+_module = dir()
+NOT_IN_ALL = [m for m in _module if m not in __all__]
+
+__pdoc__ = {}
+
+for n in NOT_IN_ALL:
+    __pdoc__[n] = False
---
+++ packages/livekit-agents/livekit_agents-1.0.0rc7-py3-none-any.whl/livekit/agents/metrics/base.py
@@ -0,0 +1,129 @@
+from __future__ import annotations
+
+from typing import Literal, Union
+
+from pydantic import BaseModel
+
+
+class Error(BaseModel):
+    error: str
+
+
+class LLMMetrics(BaseModel):
+    type: Literal["llm_metrics"] = "llm_metrics"
+    label: str
+    request_id: str
+    timestamp: float
+    duration: float
+    ttft: float
+    cancelled: bool
+    completion_tokens: int
+    prompt_tokens: int
+    total_tokens: int
+    tokens_per_second: float
+    speech_id: str | None = None
+    error: Error | None = None
+
+
+class STTMetrics(BaseModel):
+    type: Literal["stt_metrics"] = "stt_metrics"
+    label: str
+    request_id: str
+    timestamp: float
+    duration: float
+    audio_duration: float
+    streamed: bool
+    speech_id: str | None = None
+    error: Error | None = None
+
+
+class TTSMetrics(BaseModel):
+    type: Literal["tts_metrics"] = "tts_metrics"
+    label: str
+    request_id: str
+    timestamp: float
+    ttfb: float
+    duration: float
+    audio_duration: float
+    cancelled: bool
+    characters_count: int
+    streamed: bool
+    speech_id: str | None = None
+    error: Error | None = None
+
+
+class VADMetrics(BaseModel):
+    type: Literal["vad_metrics"] = "vad_metrics"
+    label: str
+    timestamp: float
+    idle_time: float
+    inference_duration_total: float
+    inference_count: int
+    speech_id: str | None = None
+    error: Error | None = None
+
+
+class EOUMetrics(BaseModel):
+    type: Literal["eou_metrics"] = "eou_metrics"
+    timestamp: float
+    end_of_utterance_delay: float
+    """Amount of time between the end of speech from VAD and the decision to end the user's turn."""
+
+    transcription_delay: float
+    """Time taken to obtain the transcript after the end of the user's speech."""
+
+    speech_id: str | None = None
+    error: Error | None = None
+
+
+AgentMetrics = Union[
+    STTMetrics,
+    LLMMetrics,
+    TTSMetrics,
+    VADMetrics,
+    EOUMetrics,
+]
+
+# @dataclass
+# class MultimodalLLMError(Error):
+#     type: str | None
+#     reason: str | None = None
+#     code: str | None = None
+#     message: str | None = None
+
+
+# @dataclass
+# class MultimodalLLMMetrics(LLMMetrics):
+#     @dataclass
+#     class CachedTokenDetails:
+#         text_tokens: int
+#         audio_tokens: int
+
+#     @dataclass
+#     class InputTokenDetails:
+#         cached_tokens: int
+#         text_tokens: int
+#         audio_tokens: int
+#         cached_tokens_details: MultimodalLLMMetrics.CachedTokenDetails
+
+#     @dataclass
+#     class OutputTokenDetails:
+#         text_tokens: int
+#         audio_tokens: int
+
+#     input_token_details: InputTokenDetails
+#     output_token_details: OutputTokenDetails
+
+
+# AgentMetrics = Union[
+#     STTMetrics,
+#     LLMMetrics,
+#     TTSMetrics,
+#     VADMetrics,
+#     PipelineSTTMetrics,
+#     PipelineEOUMetrics,
+#     PipelineLLMMetrics,
+#     PipelineTTSMetrics,
+#     PipelineVADMetrics,
+#     # MultimodalLLMMetrics,
+# ]
