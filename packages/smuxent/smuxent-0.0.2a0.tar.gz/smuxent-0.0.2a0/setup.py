c4ed4f70c66224da72760629261025a327434f76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-08 14:09:09 +0000

    Add package smuxent/0.0.2a0/smuxent-0.0.2a0.tar.gz

---
+++ packages/smuxent/smuxent-0.0.2a0.tar.gz/smuxent-0.0.2a0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="smuxent",
+    version="0.0.2a0",
+    description="Native threading for Python using C++ and pybind11",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    author="Patryk Wrzesniewski",
+    license="MIT",
+    packages=find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3.12",
+        "Operating System :: Microsoft :: Windows",
+        "License :: OSI Approved :: MIT License",
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    python_requires=">=3.12,<3.13",
+)
