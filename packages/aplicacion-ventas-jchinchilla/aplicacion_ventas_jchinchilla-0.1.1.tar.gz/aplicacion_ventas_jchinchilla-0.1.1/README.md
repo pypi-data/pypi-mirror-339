887cb4359fa9b178999fb8d8541719b7aeb2eb29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-08 14:08:49 +0000

    Add package aplicacion-ventas-jchinchilla/0.1.1/aplicacion_ventas_jchinchilla-0.1.1.tar.gz

---
+++ packages/aplicacion-ventas-jchinchilla/aplicacion_ventas_jchinchilla-0.1.1.tar.gz/aplicacion_ventas_jchinchilla-0.1.1/README.md
@@ -0,0 +1,10 @@
+# Aplicación de ventas
+
+Este paquete proporciona funcionalidades para gestionar ventas , incluyendo cálculo de precios, impuestos y descuentos.
+
+## Instalación
+
+Puedes instalar el paquete usando:
+
+```bash 
+pip install .
---
+++ packages/aplicacion-ventas-jchinchilla/aplicacion_ventas_jchinchilla-0.1.1.tar.gz/aplicacion_ventas_jchinchilla-0.1.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup,find_packages
+
+setup(
+    name='aplicacion_ventas_jchinchilla',
+    version='0.1.1',
+    author='Jorge Chinchilla Gómez',
+    author_email='geochinchilla@gmail.com',
+    description='Paquete para gestionar ventas , precios , impuestos y descuentos',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/curso_python_camara/gestor/aplicacionventas',
+    packages=find_packages(),
+    install_requires=[],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.7'
+),
