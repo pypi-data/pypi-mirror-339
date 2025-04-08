a66c78a73ceceac8093702cd794ef42aa01ccfd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-08 05:04:23 +0000

    Add package p1-auth/0.2.3/p1_auth-0.2.3.tar.gz

---
+++ packages/p1-auth/p1_auth-0.2.3.tar.gz/p1_auth-0.2.3/p1_auth/middleware.py
@@ -0,0 +1,18 @@
+from django.conf import settings
+from django.contrib import auth
+from django.utils.deprecation import MiddlewareMixin
+
+
+class AuthenticateSessionMiddleware(MiddlewareMixin):
+
+    def process_request(self, request):
+        if (hasattr(request, 'user') and request.user.is_authenticated) or \
+                (request.path_info and not request.path_info.lower(
+                ).removeprefix(getattr(settings, "FORCE_SCRIPT_NAME", ""
+                                       ).lower()).startswith("/admin")):
+            return
+        user = auth.authenticate(request)
+        if hasattr(user, 'backend'):
+            request._cached_user = user
+            auth.login(
+                request, user, user.backend)
