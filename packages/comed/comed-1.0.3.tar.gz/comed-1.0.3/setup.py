ae8ec8a7337f3cba3c2b95d214c0121f76e3f016
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-08 15:06:05 +0000

    Add package comed/1.0.3/comed-1.0.3.tar.gz

---
+++ packages/comed/comed-1.0.3.tar.gz/comed-1.0.3/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="comed",
+    version="1.0.3",
+    packages=find_packages(),
+    install_requires=[
+        "pandas>=2.2.3",
+        "numpy>=2.2.3",
+        "biopython>=1.85",
+        "tqdm>=4.67.1",
+        "openai>=1.65.1",
+        "requests>=2.32.3",
+    ],
+    author="Studentiz",
+    author_email="studentiz@live.com",
+    description="CoMed: A framework for analyzing co-medication risks using Chain-of-Thought reasoning.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/studentiz/comed",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.12.9",
+)
