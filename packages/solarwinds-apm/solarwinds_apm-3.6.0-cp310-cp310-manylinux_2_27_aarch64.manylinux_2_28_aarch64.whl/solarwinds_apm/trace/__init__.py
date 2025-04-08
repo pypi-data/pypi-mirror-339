4075e95453a839c7a826107ee5d92b8b2beed1cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-08 14:08:16 +0000

    Add package solarwinds-apm/3.6.0/solarwinds_apm-3.6.0-cp310-cp310-manylinux_2_27_aarch64.manylinux_2_28_aarch64.whl

---
+++ packages/solarwinds-apm/solarwinds_apm-3.6.0-cp310-cp310-manylinux_2_27_aarch64.manylinux_2_28_aarch64.whl/solarwinds_apm/trace/__init__.py
@@ -0,0 +1,13 @@
+from .inbound_metrics_processor import SolarWindsInboundMetricsSpanProcessor
+from .otlp_metrics_processor import SolarWindsOTLPMetricsSpanProcessor
+from .serviceentry_processor import ServiceEntryIdSpanProcessor
+from .txnname_calculator_processor import TxnNameCalculatorProcessor
+from .txnname_cleanup_processor import TxnNameCleanupProcessor
+
+__all__ = [
+    "ServiceEntryIdSpanProcessor",
+    "SolarWindsInboundMetricsSpanProcessor",
+    "SolarWindsOTLPMetricsSpanProcessor",
+    "TxnNameCalculatorProcessor",
+    "TxnNameCleanupProcessor",
+]
