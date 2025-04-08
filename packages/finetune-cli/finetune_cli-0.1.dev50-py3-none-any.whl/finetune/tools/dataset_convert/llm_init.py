85a6727b6ddc69912e32d108457649b4a1c24d5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-08 15:06:11 +0000

    Add package finetune-cli/0.1.dev50/finetune_cli-0.1.dev50-py3-none-any.whl

---
+++ packages/finetune-cli/finetune_cli-0.1.dev50-py3-none-any.whl/finetune/tools/dataset_convert/llm_init.py
@@ -0,0 +1,16 @@
+from kink import di
+from langchain_openai import ChatOpenAI
+
+from finetune.tools.dataset_convert.config import Master
+
+
+class global_llm:
+    def __enter__(self):
+        di['llm'] = ChatOpenAI(
+            model=Master.get("default_model"),
+            base_url=Master.get("openai_api_endpoint"),
+            api_key=Master.get("openai_api_key"),
+        )
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        di['llm'] = None
