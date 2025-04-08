45c8e6aef67bd4943fb2e2ef2ef86a676856a918
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-08 04:04:14 +0000

    Add package rdapy/2.0.1/rdapy-2.0.1-py3-none-any.whl

---
+++ packages/rdapy/rdapy-2.0.1-py3-none-any.whl/rdapy/graph/connected.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+"""
+CONTIGUOUS - Is a district in a plan fully connected?
+"""
+
+from typing import Any
+
+from .constants import *
+
+
+def is_connected(ids: list[Any], graph: dict[str, list[str]]) -> bool:
+    """Is a district fully connected?
+    i.e., w/o regard to the virtual state boundary "shapes".
+
+    Kenshi's iterative implementation of the recursive algorithm
+
+    ids - the list of ids for the geographies
+    graph - the connectedness (adjacency) of the geos
+    """
+    visited: set[Any] = set()
+
+    all_geos: set[Any] = set(ids)
+    all_geos.discard(OUT_OF_STATE)
+
+    start: str = next(iter(all_geos))
+    assert start != OUT_OF_STATE
+
+    to_process: list[Any] = [start]
+    while to_process:
+        node: Any = to_process.pop()
+        visited.add(node)
+        neighbors: list[Any] = list(graph[node])
+        if OUT_OF_STATE in neighbors:
+            neighbors.remove(OUT_OF_STATE)
+        neighbors_to_visit: list[Any] = [
+            n for n in neighbors if n in all_geos and n not in visited
+        ]
+        to_process.extend(neighbors_to_visit)
+
+    return len(visited) == len(all_geos)
+
+
+### END ###
---
+++ packages/rdapy/rdapy-2.0.1-py3-none-any.whl/rdapy/partisan/__init__.py
@@ -0,0 +1,8 @@
+# rdapy/partisan/__init__.py
+
+from .partisan import *
+from .bias import *
+from .responsiveness import *
+from .method import *
+
+name = "partisan"
