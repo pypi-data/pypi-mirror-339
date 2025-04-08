0d06bc74d41f56d6587e05c3f2dfc9f2a7d74b10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-08 03:16:04 +0000

    Add package omc3-gui/1.0.0/omc3_gui-1.0.0.tar.gz

---
+++ packages/omc3-gui/omc3_gui-1.0.0.tar.gz/omc3_gui-1.0.0/omc3_gui/__init__.py
@@ -0,0 +1,17 @@
+"""
+The omc3-gui library
+~~~~~~~~~~~~~~~~~~~~
+
+omc3-gui provides graphical user interfaces for the ``omc3`` package for the optics measurements and corrections group (OMC) at CERN.
+
+:copyright: pyLHC/OMC-Team working group.
+"""
+
+__title__ = "omc3-gui"
+__description__ = "QT Graphical User Interface wrapper of the ``omc3`` package"
+__url__ = "https://github.com/pylhc/omc3_gui"
+__version__ = "1.0.0"
+__author__ = "pylhc"
+__author_email__ = "pylhc@github.com"
+
+__all__ = [__version__]
