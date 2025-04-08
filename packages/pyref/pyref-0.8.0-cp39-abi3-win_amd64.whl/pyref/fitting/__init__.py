5125e9b6389137399cb80a2320d063e8bd2d317b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-08 15:05:28 +0000

    Add package pyref/0.8.0/pyref-0.8.0-cp39-abi3-win_amd64.whl

---
+++ packages/pyref/pyref-0.8.0-cp39-abi3-win_amd64.whl/pyref/fitting/__init__.py
@@ -0,0 +1,35 @@
+"""Fitting module for pyref."""
+
+from refnx.analysis import GlobalObjective, Objective, Transform
+from refnx.dataset import ReflectDataset
+
+from pyref.fitting.fitters import (
+    AnisotropyObjective,
+    LogpExtra,
+    demove,
+    gmove,
+)
+from pyref.fitting.fitters import (
+    Fitter as CurveFitter,
+)
+from pyref.fitting.reflectivity import ReflectModel, XrayReflectDataset
+from pyref.fitting.refnx_converters import to_reflect_dataset
+from pyref.fitting.structure import SLD, MaterialSLD, UniTensorSLD
+
+__all__ = [
+    "SLD",
+    "AnisotropyObjective",
+    "CurveFitter",
+    "GlobalObjective",
+    "LogpExtra",
+    "MaterialSLD",
+    "Objective",
+    "ReflectDataset",
+    "ReflectModel",
+    "Transform",
+    "UniTensorSLD",
+    "XrayReflectDataset",
+    "demove",
+    "gmove",
+    "to_reflect_dataset",
+]
