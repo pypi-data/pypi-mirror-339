facf6623222d049495bedf21eea654b200d11ced
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-08 15:05:34 +0000

    Add package maleo-core/0.3.25/maleo_core-0.3.25-py3-none-any.whl

---
+++ packages/maleo-core/maleo_core-0.3.25-py3-none-any.whl/maleo_core/models/maleo_suite/maleo_access/transfers/parameters/general/user_organization_role.py
@@ -0,0 +1,32 @@
+from __future__ import annotations
+from enum import StrEnum
+from pydantic import BaseModel, Field
+from typing import Optional
+from maleo_core.models.base.transfers.parameters.general import BaseGeneralParameters
+from maleo_core.models.maleo_suite.maleo_access.transfers.general.user_organization_role import MaleoAccessUserOrganizationRoleGeneralTransfers
+
+class MaleoAccessUserOrganizationRoleGeneralParameters:
+    class ExpandableFields(StrEnum):
+        USER_ORGANIZATION = "user_organization"
+        ORGANIZATION_ROLE = "organization_role"
+
+    class Expand(BaseModel):
+        expand:list[MaleoAccessUserOrganizationRoleGeneralParameters.ExpandableFields] = Field([], description="Expanded field(s)")
+
+    class BaseGet(BaseModel):
+        user_ids:Optional[list[int]] = Field(None, description="Users id's")
+        organization_ids:Optional[list[int]] = Field(None, description="Organizations id's")
+        organization_role_ids:Optional[list[int]] = Field(None, description="Organization roles id's")
+
+    class Get(Expand, BaseGet): pass
+
+    class BaseGetMultiple(BaseGeneralParameters.Statuses):
+        organization_role_ids:Optional[list[int]] = Field(None, description="Organization roles id's")
+
+    class GetMultiple(Expand, BaseGetMultiple): pass
+
+    class GetSingle(Expand, BaseGeneralParameters.Statuses): pass
+
+    class Create(Expand, MaleoAccessUserOrganizationRoleGeneralTransfers.Base): pass
+
+    class StatusUpdate(Expand, BaseGeneralParameters.StatusUpdate): pass
