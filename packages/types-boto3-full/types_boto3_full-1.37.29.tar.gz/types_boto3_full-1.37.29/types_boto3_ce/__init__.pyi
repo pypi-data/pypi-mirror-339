69216687c06cd8fbc367fd545b29e07924c4ccb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-08 14:08:55 +0000

    Add package types-boto3-full/1.37.29/types_boto3_full-1.37.29.tar.gz

---
+++ packages/types-boto3-full/types_boto3_full-1.37.29.tar.gz/types_boto3_full-1.37.29/types_boto3_ce/__init__.pyi
@@ -0,0 +1,26 @@
+"""
+Main interface for ce service.
+
+[Documentation](https://youtype.github.io/types_boto3_docs/types_boto3_ce/)
+
+Copyright 2025 Vlad Emelianov
+
+Usage::
+
+    ```python
+    from boto3.session import Session
+    from types_boto3_ce import (
+        Client,
+        CostExplorerClient,
+    )
+
+    session = Session()
+    client: CostExplorerClient = session.client("ce")
+    ```
+"""
+
+from .client import CostExplorerClient
+
+Client = CostExplorerClient
+
+__all__ = ("Client", "CostExplorerClient")
