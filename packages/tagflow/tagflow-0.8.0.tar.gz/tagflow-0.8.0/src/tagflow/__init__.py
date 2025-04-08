8d16470f308fd3d1f4a20b218d0a6db3d925e8ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-08 14:08:56 +0000

    Add package tagflow/0.8.0/tagflow-0.8.0.tar.gz

---
+++ packages/tagflow/tagflow-0.8.0.tar.gz/tagflow-0.8.0/src/tagflow/__init__.py
@@ -0,0 +1,34 @@
+from .tagflow import (
+    tag,
+    html,
+    text,
+    attr,
+    classes,
+    dataset,
+    clear,
+    document,
+    spawn,
+    transition,
+    DocumentMiddleware,
+    TagResponse,
+    XMLResponse,
+    Live,
+)
+
+
+__all__ = [
+    "tag",
+    "html",
+    "text",
+    "attr",
+    "classes",
+    "dataset",
+    "clear",
+    "document",
+    "spawn",
+    "transition",
+    "DocumentMiddleware",
+    "TagResponse",
+    "XMLResponse",
+    "Live",
+]
