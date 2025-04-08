fd5b8cabf198c76b2eeb3889c16e9df917e87c7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-08 05:04:54 +0000

    Add package speculative-decoding-metrics/0.1.5/speculative_decoding_metrics-0.1.5.tar.gz

---
+++ packages/speculative-decoding-metrics/speculative_decoding_metrics-0.1.5.tar.gz/speculative_decoding_metrics-0.1.5/src/speculative_decoding_metrics/main.py
@@ -0,0 +1,27 @@
+from speculative_decoding_metrics.evaluator import SpeculativeDecoderEvaluator
+from speculative_decoding_metrics.plotter import ResultPlotter
+
+def run_evaluation(
+    base_model="phi-3-mini-4k-instruct",
+    prompt="Write a story about Einstein.",
+    main_quant="8bit",
+    draft_quant="4bit",
+    num_draft_tokens_list=None,
+    plot=True,
+):
+    if num_draft_tokens_list is None:
+        num_draft_tokens_list = [0, 1, 2, 3, 4, 5]
+
+    evaluator = SpeculativeDecoderEvaluator(
+        base_model=base_model,
+        prompt_text=prompt,
+        main_quant=main_quant,
+        draft_quant=draft_quant
+    )
+    results = evaluator.evaluate(num_draft_tokens_list)
+
+    if plot:
+        plotter = ResultPlotter(num_draft_tokens_list, results)
+        plotter.plot_all()
+
+    return results
