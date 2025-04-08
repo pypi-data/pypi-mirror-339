00c11af6f7e0bfc5386ab3d1b4fddf7011437b6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-08 07:04:20 +0000

    Add package wetro/0.1.21/wetro-0.1.21.tar.gz

---
+++ packages/wetro/wetro-0.1.21.tar.gz/wetro-0.1.21/tests/test.py
@@ -0,0 +1,82 @@
+from wetro import WetroRAG, WetroTools, Wetrocloud
+import os
+
+def main():
+    pass
+    # Test RAG operations via SDK
+    print("=== Testing RAG Operations via SDK ===")
+    wetro_client = Wetrocloud(api_key="wtc-sk-8f956b955c76a1049aa87b11e5f589104baf7e73")
+    # wetro_client.collection.get_or_create_collection_id("test_collection_sdk")
+    # collection_resp = wetro_client.collection.create_collection("test__sdk040425")
+    # print("SDK Create Collection Response: %s", collection_resp.model_dump())
+    # all_collection_resp = wetro_client.collection.get_collection_list()
+    # print("SDK Get All Collection Response: %s", all_collection_resp.model_dump())
+    
+    insert_resp = wetro_client.collection.insert_resource(resource="https://medium.com/@wetrocloud/are-image-models-getting-out-of-hand-068b13090556", type="web", collection_id="test_collection_sdk")
+    # print("SDK Insert Response: %s", insert_resp.model_dump())
+    print(os.path.abspath("test-resources/1.pdf"))
+   
+    # insert_resp = wetro_client.collection.insert_resource(resource="./test-resources/1.pdf", type="file", collection_id="test__sdk040425")
+    # print("SDK Insert Response: %s", insert_resp.model_dump())
+    
+    # Test basic query
+    query_resp = wetro_client.collection.query_collection(request_query="What is the collection about?", collection_id="test__sdk040425")
+    print("SDK Query Response: %s", query_resp.model_dump())
+    
+    # Test streaming query
+    # print("SDK Streaming Query Responses:")
+    # stream_resp = wetro_client.collection.query_collection(request_query="Streaming test", stream=True, collection_id="test_collection_sdk")
+    # for chunk in stream_resp:
+    #     print(chunk.model_dump())
+    
+    # # Test structured query
+    # structured_query = wetro_client.collection.query_collection(
+    #     request_query="What is this about?",
+    #     json_schema={"title": "string", "summary": "string"},
+    #     json_schema_rules=["Summaries the collection"],
+    #     collection_id="test_collection_sdk"
+    # )
+    # print("SDK Structured Query Response: %s", structured_query.model_dump())
+    
+    # chat_history= [{"role": "user", "content": "What is this all about?"}]
+    # chat_resp = wetro_client.collection.chat(message="What is this all about?", chat_history=chat_history, collection_id="test_collection_sdk")
+    # print("SDK Chat Response: %s", chat_resp.model_dump())
+
+    # delete_resource_resp = wetro_client.collection.delete_resource(insert_resp.resource_id, collection_id="test_collection_sdk")
+    # print("SDK Delete Resource Response: %s", delete_resource_resp.model_dump())
+    
+    # delete_resp = wetro_client.collection.delete_collection(collection_id="test_collection_sdk")
+    # print("SDK Delete Response: %s", delete_resp.model_dump())
+    
+    # # Test Tools operations via SDK
+    # print("=== Testing Tools Operations via SDK ===")
+
+    # categorize_resp = wetro_client.categorize(
+    #     resource="Match review: Example vs. Test.",
+    #     type="text",
+    #     json_schema={"label": "string"},
+    #     categories=["sports", "entertainment"],
+    #     prompt="Categorize the text to see which category it best fits"
+    # )
+    # print("SDK Categorize Response: %s", categorize_resp.model_dump())
+    
+    # generate_resp = wetro_client.generate_text(
+    #     messages=[{"role": "user", "content": "What is a large language model?"}],
+    #     model="gpt-4"
+    # )
+    # print("SDK Generate Text Response: %s", generate_resp.model_dump())
+    
+    # ocr_resp = wetro_client.image_to_text(
+    #     image_url="https://images.squarespace-cdn.com/content/v1/54822a56e4b0b30bd821480c/45ed8ecf-0bb2-4e34-8fcf-624db47c43c8/Golden+Retrievers+dans+pet+care.jpeg",
+    #     request_query="What animal is in this image?"
+    # )
+    # print("SDK Image to Text Response: %s", ocr_resp.model_dump())
+    
+    # extract_resp = wetro_client.extract(
+    #     website="https://medium.com/@wetrocloud/are-image-models-getting-out-of-hand-068b13090556",
+    #     json_schema={"title" : "<string>", "models" : "<string>"}
+    # )
+    # print("SDK Extract Data Response: %s", extract_resp.model_dump())
+
+if __name__ == "__main__":
+    main()
---
+++ packages/wetro/wetro-0.1.21.tar.gz/wetro-0.1.21/wetro/custom_response.py
@@ -0,0 +1,69 @@
+from typing import Union, Dict, Any,Optional,List
+from pydantic import BaseModel
+from wetro.custom_types import CollectionID,ResourceID, JSONSchema, CollectionItem
+
+class CreateCollectionResponse(BaseModel):
+    collection_id: CollectionID
+    success: bool
+
+class GetCollectionResponse(BaseModel):
+    success: bool
+    found: bool
+    collection_id: Optional[str] = None
+
+class ListCollectionResponse(BaseModel):
+    count: Optional[int] = None
+    next: Optional[str] = None
+    previous: Optional[str] = None
+    results: List[CollectionItem]
+
+class QueryResponse(BaseModel):
+    response: Union[str, JSONSchema]
+    tokens: int
+    success: bool
+
+class ChatResponse(BaseModel):
+    response: Union[str, JSONSchema]
+    tokens: int
+    success: bool
+
+class InsertResponse(BaseModel):
+    resource_id : ResourceID
+    success: bool
+    tokens: Optional[int] = None
+
+class RemoveResponse(BaseModel):
+    success: bool
+
+class DeleteCollectionResponse(BaseModel):
+    message: str
+    success: bool
+
+class CategorizeResponse(BaseModel):
+    response: JSONSchema
+    tokens: int
+    success: bool
+
+class GenerateTextResponse(BaseModel):
+    response: Union[str, JSONSchema]
+    tokens: int
+    success: bool
+
+class ImageToTextResponse(BaseModel):
+    response: str
+    tokens: int
+    success: bool
+
+class ExtractDataResponse(BaseModel):
+    response: JSONSchema
+    tokens: int
+    success: bool
+
+
+class WetrocloudAPIResponse:
+    def __init__(self, data: Dict[str, Any]):
+        for key, value in data.items():
+            setattr(self, key, value)
+
+    def to_dict(self) -> Dict[str, Any]:
+        return self.__dict__
