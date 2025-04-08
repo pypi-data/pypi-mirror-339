8b365118afd2cdc75a52bfee45faf68bb42327a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-08 04:04:19 +0000

    Add package maleo-core/0.3.18/maleo_core-0.3.18-py3-none-any.whl

---
+++ packages/maleo-core/maleo_core-0.3.18-py3-none-any.whl/maleo_core/models/maleo_suite/maleo_access/transfers/parameters/service/__init__.py
@@ -0,0 +1,39 @@
+# This file serves all MaleoAccess's Service Parameters
+
+from __future__ import annotations
+from .blood_type import MaleoAccessBloodTypeServiceParameters
+from .gender import MaleoAccessGenderServiceParameters
+from .organization_role import MaleoAccessOrganizationRoleServiceParameters
+from .organization_type import MaleoAccessOrganizationTypeServiceParameters
+from .organization import MaleoAccessOrganizationServiceParameters
+from .system_role import MaleoAccessSystemRoleServiceParameters
+from .user_profile import MaleoAccessUserProfileServiceParameters
+from .user_system_role import MaleoAccessUserSystemRoleServiceParameters
+from .user_type import MaleoAccessUserTypeServiceParameters
+from .user import MaleoAccessUserServiceParameters
+
+class MaleoAccessServiceParameters:
+    BloodType = MaleoAccessBloodTypeServiceParameters
+    Gender = MaleoAccessGenderServiceParameters
+    OrganizationRole = MaleoAccessOrganizationRoleServiceParameters
+    OrganizationType = MaleoAccessOrganizationTypeServiceParameters
+    Organization = MaleoAccessOrganizationServiceParameters
+    SystemRole = MaleoAccessSystemRoleServiceParameters
+    UserProfile = MaleoAccessUserProfileServiceParameters
+    UserSystemRole = MaleoAccessUserSystemRoleServiceParameters
+    UserType = MaleoAccessUserTypeServiceParameters
+    User = MaleoAccessUserServiceParameters
+
+__all__ = [
+    "MaleoAccessServiceParameters",
+    "MaleoAccessBloodTypeServiceParameters",
+    "MaleoAccessGenderServiceParameters",
+    "MaleoAccessOrganizationRoleServiceParameters",
+    "MaleoAccessOrganizationTypeServiceParameters",
+    "MaleoAccessOrganizationServiceParameters",
+    "MaleoAccessSystemRoleServiceParameters",
+    "MaleoAccessUserProfileServiceParameters",
+    "MaleoAccessUserSystemRoleServiceParameters",
+    "MaleoAccessUserTypeServiceParameters",
+    "MaleoAccessUserServiceParameters"
+]
