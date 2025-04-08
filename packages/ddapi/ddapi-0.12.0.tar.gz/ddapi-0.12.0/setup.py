70e5b190567e3aba741443388d5906c2a919a4b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-08 04:04:52 +0000

    Add package ddapi/0.12.0/ddapi-0.12.0.tar.gz

---
+++ packages/ddapi/ddapi-0.12.0.tar.gz/ddapi-0.12.0/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ddapi",
+    version="0.12.0",
+    author="ByFox",
+    description="DDnet api",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ByFox213/ddapi",
+    license="MIT",
+    project_urls={
+        "Github": "https://github.com/ByFox213/ddapi"
+    },
+    package_dir={"": "ddapi"},
+    packages=setuptools.find_packages(where="ddapi"),
+    python_requires=">=3.8"
+)
