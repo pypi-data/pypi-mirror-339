5192117b3780ff4c48b86866b690fcb99ab7db1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-08 04:04:19 +0000

    Add package forcolate/0.1.12/forcolate-0.1.12.tar.gz

---
+++ packages/forcolate/forcolate-0.1.12.tar.gz/forcolate-0.1.12/forcolate/__init__.py
@@ -0,0 +1,8 @@
+# forcolate/__init__.py
+
+from .outlook_search import search_outlook_emails
+from .folder_search import search_folder
+from .document_to_text import convert_documents_to_markdown, convert_folders_to_markdown
+from .tool_selector import select_tool
+from .bullet_point_workflow import run_bullet_point_workflow
+from .query_url_extractor import extract_query_and_folder
