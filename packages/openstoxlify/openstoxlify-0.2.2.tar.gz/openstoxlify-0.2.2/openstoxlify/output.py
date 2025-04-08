62dcecaec71411b4bf559f0eafa2b4d53778c7d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-08 05:04:49 +0000

    Add package openstoxlify/0.2.2/openstoxlify-0.2.2.tar.gz

---
+++ packages/openstoxlify/openstoxlify-0.2.2.tar.gz/openstoxlify-0.2.2/openstoxlify/output.py
@@ -0,0 +1,34 @@
+import json
+from .plotter import PLOT_DATA
+from .strategy import STRATEGY_DATA
+from .models import PlotType
+
+
+def output():
+    """Generate the final output as JSON."""
+    result = {}
+
+    result["histogram"] = [
+        {"label": plot["label"], "data": [item for item in plot["data"]]}
+        for plot in PLOT_DATA.get(PlotType.HISTOGRAM, [])
+    ]
+
+    result["line"] = [
+        {"label": plot["label"], "data": [item for item in plot["data"]]}
+        for plot in PLOT_DATA.get(PlotType.LINE, [])
+    ]
+
+    result["area"] = [
+        {"label": plot["label"], "data": [item for item in plot["data"]]}
+        for plot in PLOT_DATA.get(PlotType.AREA, [])
+    ]
+
+    result["strategy"] = [
+        {
+            "label": entry["label"],
+            "data": [action for action in entry["data"]],
+        }
+        for entry in STRATEGY_DATA.get("strategy", [])
+    ]
+
+    print(json.dumps(result))
