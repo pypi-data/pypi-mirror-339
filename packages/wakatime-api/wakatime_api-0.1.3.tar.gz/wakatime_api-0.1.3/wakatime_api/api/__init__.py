da8fe0b29df1a2f19af7eb683109f8df55443c0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-08 14:09:09 +0000

    Add package wakatime-api/0.1.3/wakatime_api-0.1.3.tar.gz

---
+++ packages/wakatime-api/wakatime_api-0.1.3.tar.gz/wakatime_api-0.1.3/wakatime_api/api/__init__.py
@@ -0,0 +1,20 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from wakatime_api.api.commits_api import CommitsApi
+from wakatime_api.api.data_dumps_api import DataDumpsApi
+from wakatime_api.api.durations_api import DurationsApi
+from wakatime_api.api.external_durations_api import ExternalDurationsApi
+from wakatime_api.api.goals_api import GoalsApi
+from wakatime_api.api.heartbeats_api import HeartbeatsApi
+from wakatime_api.api.insights_api import InsightsApi
+from wakatime_api.api.leaderboards_api import LeaderboardsApi
+from wakatime_api.api.meta_api import MetaApi
+from wakatime_api.api.organizations_api import OrganizationsApi
+from wakatime_api.api.projects_api import ProjectsApi
+from wakatime_api.api.resources_api import ResourcesApi
+from wakatime_api.api.stats_api import StatsApi
+from wakatime_api.api.summaries_api import SummariesApi
+from wakatime_api.api.users_api import UsersApi
