dd1b05bab40172ecf7c04d1870bc65064b9564bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-08 05:04:43 +0000

    Add package genius-client-sdk/4.0.2/genius_client_sdk-4.0.2.tar.gz

---
+++ packages/genius-client-sdk/genius_client_sdk-4.0.2.tar.gz/genius_client_sdk-4.0.2/tests/conftest.py
@@ -0,0 +1,7 @@
+import os
+
+
+SDK_MEMORY_LIMIT = (
+    os.getenv("SDK_MEMORY_LIMIT") or 1024 * 14
+)  # 14 GB (current limit on remote agent)
+SDK_REQUEST_TIMEOUT = os.getenv("SDK_REQUEST_TIMEOUT") or 20 * 60  # seconds
