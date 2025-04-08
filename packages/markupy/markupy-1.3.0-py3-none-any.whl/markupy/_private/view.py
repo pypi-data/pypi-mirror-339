d3a850584edd8d459d8bd63cab0d20ca23fbaeb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-08 15:06:20 +0000

    Add package markupy/1.3.0/markupy-1.3.0-py3-none-any.whl

---
+++ packages/markupy/markupy-1.3.0-py3-none-any.whl/markupy/_private/view.py
@@ -0,0 +1,27 @@
+from collections.abc import Iterable, Iterator
+from typing import final
+
+
+class View(Iterable[str]):
+    __slots__ = ()
+
+    def __init__(self) -> None:
+        super().__init__()
+
+    def __iter__(self) -> Iterator[str]:
+        yield from ()
+
+    @final
+    def __str__(self) -> str:
+        return "".join(self)
+
+    def __repr__(self) -> str:
+        return "<markupy.View>"
+
+    # Allow starlette Response.render to directly render this element without
+    # explicitly casting to str:
+    # https://github.com/encode/starlette/blob/5ed55c441126687106109a3f5e051176f88cd3e6/starlette/responses.py#L44-L49
+    def encode(self, encoding: str = "utf-8", errors: str = "strict") -> bytes:
+        return str(self).encode(encoding, errors)
+
+    __html__ = __str__
