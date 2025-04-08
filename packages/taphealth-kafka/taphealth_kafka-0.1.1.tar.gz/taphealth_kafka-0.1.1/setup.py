b7c11d719bad8640d72830af1b293f25b83306d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-08 03:15:48 +0000

    Add package taphealth-kafka/0.1.1/taphealth_kafka-0.1.1.tar.gz

---
+++ packages/taphealth-kafka/taphealth_kafka-0.1.1.tar.gz/taphealth_kafka-0.1.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="taphealth-kafka",
+    version="0.1.1",
+    packages=find_packages(),
+    install_requires=[
+        "kafka-python>=2.0.0",
+    ],
+    author="Afif",
+    author_email="afif@tap.health",
+    description="Kafka utilities for TapHealth Python services",
+    keywords="kafka, taphealth",
+    python_requires=">=3.8",
+)
