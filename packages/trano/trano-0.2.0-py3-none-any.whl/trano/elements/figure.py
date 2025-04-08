7837cdb291089ea6a31e12ff26d7e2aa4e90f4b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-08 06:05:35 +0000

    Add package trano/0.2.0/trano-0.2.0-py3-none-any.whl

---
+++ packages/trano/trano-0.2.0-py3-none-any.whl/trano/elements/figure.py
@@ -0,0 +1,30 @@
+from typing import TYPE_CHECKING
+
+import jinja2.exceptions
+from jinja2 import Environment
+from pydantic import BaseModel, Field
+
+from trano.elements.types import Axis
+
+if TYPE_CHECKING:
+    from trano.elements.base import BaseElement
+
+
+class Figure(BaseModel):
+    right_axis: Axis = Field(default=Axis(lines=[], label=""))
+    left_axis: Axis = Field(default=Axis(lines=[], label=""))
+
+    def render_key(self, element: "BaseElement") -> "Figure":
+        environment = Environment(autoescape=True)
+        for axis in self.right_axis.lines + self.left_axis.lines:
+            template = environment.from_string(axis.template)
+            try:
+                axis.key = template.render(element=element)
+            except jinja2.exceptions.UndefinedError:
+                continue
+
+        return self
+
+
+class NamedFigure(Figure):
+    name: str
