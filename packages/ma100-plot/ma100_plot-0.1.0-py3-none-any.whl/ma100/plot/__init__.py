5051099669cdaca163f8d9a319069e71d513571e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-08 15:05:34 +0000

    Add package ma100-plot/0.1.0/ma100_plot-0.1.0-py3-none-any.whl

---
+++ packages/ma100-plot/ma100_plot-0.1.0-py3-none-any.whl/ma100/plot/__init__.py
@@ -0,0 +1,6 @@
+# -*- encoding：utf-8 -*-
+"""
+@File :__init__.py.py
+@Time :2025-4-8  12:45
+@Author：AI Lab Morgan
+"""
---
+++ packages/ma100-plot/ma100_plot-0.1.0-py3-none-any.whl/ma100/plot/bi.py
@@ -0,0 +1,11 @@
+# -*- encoding：utf-8 -*-
+"""
+@File :bi.py
+@Time :2025-4-8  12:45
+@Author：AI Lab Morgan
+"""
+
+
+class BI(object):
+    def __init__(self):
+        print("BI")
