510736fc954440cc07a7cb7c5458af734f1ffd85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-08 04:04:39 +0000

    Add package instmodel/0.1.1/instmodel-0.1.1-py3-none-any.whl

---
+++ packages/instmodel/instmodel-0.1.1-py3-none-any.whl/instmodel/__init__.py
@@ -0,0 +1,8 @@
+# MIT License
+# Copyright (c) 2025 João Ferreira
+# See LICENSE file for details.
+
+from . import embeddings
+from . import instruction_model
+from . import training_utils
+from . import model
