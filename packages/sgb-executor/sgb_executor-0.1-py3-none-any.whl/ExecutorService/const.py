7d110c2359c26332147f8ba6531c4ef1c06f02ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-08 14:08:51 +0000

    Add package sgb-executor/0.1/sgb_executor-0.1-py3-none-any.whl

---
+++ packages/sgb-executor/sgb_executor-0.1-py3-none-any.whl/ExecutorService/const.py
@@ -0,0 +1,25 @@
+import isgb
+
+from sgb.collections import strtuple
+from sgb.collections.service import ServiceDescription
+
+from sgb import A
+
+NAME: str = "Executor"
+
+VERSION: str = "0.01"
+
+HOST = A.CT_H.EXECUTOR
+
+PACKAGES: strtuple = None
+
+SD: ServiceDescription = ServiceDescription(
+    name=NAME,
+    description="Executor",
+    host_changeable=True,
+    packages=PACKAGES,
+    commands=(),
+    version=VERSION,
+    use_standalone=True,
+    standalone_name="executor",
+)
