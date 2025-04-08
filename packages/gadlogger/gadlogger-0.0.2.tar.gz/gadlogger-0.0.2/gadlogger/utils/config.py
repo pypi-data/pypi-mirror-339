475c095f51dadc2449c9502b9787039492612e1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-08 05:04:09 +0000

    Add package gadlogger/0.0.2/gadlogger-0.0.2.tar.gz

---
+++ packages/gadlogger/gadlogger-0.0.2.tar.gz/gadlogger-0.0.2/gadlogger/utils/config.py
@@ -0,0 +1,51 @@
+import logging.config
+import sys
+
+from gadlogger import formatters
+from gadlogger import models
+from gadlogger.utils import modules
+
+
+def setup(*loggers: models.Logger) -> None:
+    config = {
+        "version": 1,
+        "disable_existing_loggers": False,
+        "formatters": {},
+        "handlers": {},
+        "loggers": {},
+        "root": {},
+    }
+
+    for logger in loggers:
+        formattername, formatter = "plain", formatters.PlainFormatter
+
+        if logger.module:
+            formattername = modules.define(logger.module)
+            if formattername == "json":
+                formatter = formatters.JSONFormatter
+
+        if formattername not in config["formatters"]:
+            config["formatters"][formattername] = {"()": formatter, **logger.kwargs}
+
+        config["handlers"][logger.id] = {
+            "class": "logging.StreamHandler",
+            "stream": logger.stream or sys.stdout,
+            "formatter": formattername,
+        }
+
+        if logger.name == "root":
+            config["root"] = {
+                "handlers": [logger.id],
+                "level": logger.level,
+            }
+        else:
+            config["loggers"][logger.name] = {
+                "handlers": [logger.id],
+                "level": logger.level,
+                "propagate": False,
+            }
+
+    logging.config.dictConfig(config)
+
+
+__all__ = ["setup"]
