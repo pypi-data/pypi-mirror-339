4d8a4bb6d9189c67de008451e03e6bce2edae8b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-08 17:04:55 +0000

    Add package poetry-source-env-zp/2.0.8/poetry_source_env_zp-2.0.8-py3-none-any.whl

---
+++ packages/poetry-source-env-zp/poetry_source_env_zp-2.0.8-py3-none-any.whl/poetry_source_env_zp/plugin.py
@@ -0,0 +1,58 @@
+import os
+import re
+from os.path import expandvars
+
+from cleo.io.io import IO
+from poetry.plugins.plugin import Plugin
+from poetry.poetry import Poetry
+from poetry.repositories.legacy_repository import LegacyRepository
+from poetry.repositories.repository_pool import Priority
+from pydantic_settings import BaseSettings
+
+
+class PSPConfig(BaseSettings):
+    prefix: str = "POETRY_REPOSITORIES_"
+    env: bool = True
+    toml: bool = True
+
+
+class PoetrySourcePlugin(Plugin):
+    def activate(self, poetry: Poetry, io: IO = None) -> None:
+        config: PSPConfig = PSPConfig()
+
+        if config.env:
+            repositories = {}
+            pattern = re.compile(rf"{config.prefix}(?P<name>[A-Z_]+)_URL")
+
+            for env_key in os.environ.keys():
+                match = pattern.match(env_key)
+                if match:
+                    repositories[match.group("name").lower().replace("_", "-")] = {
+                        "env_name": match.group("name"),
+                        "url": os.environ[env_key],
+                    }
+
+            for name, repository in repositories.items():
+                repo = LegacyRepository(name, repository["url"])
+
+                priority_name = os.getenv(
+                    f"{config.prefix}{repository['env_name']}_PRIORITY", "primary"
+                )
+
+                priorities = {
+                    "primary": Priority.PRIMARY,
+                    "supplemental": Priority.SUPPLEMENTAL,
+                    "explicit": Priority.EXPLICIT,
+                }
+
+                priority = priorities.get(priority_name.casefold(), Priority.PRIMARY)
+
+                poetry.pool.add_repository(repo, priority=priority)
+
+        if config.toml:
+            for repository in poetry.get_sources():
+                poetry.pool.remove_repository(repository.name)
+                repo = LegacyRepository(
+                    expandvars(repository.name), expandvars(repository.url)
+                )
+                poetry.pool.add_repository(repo, priority=repository.priority)
