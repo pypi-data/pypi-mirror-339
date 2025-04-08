6359ab217a75e50e6ec37b5e9cabf3975c8827ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-08 07:04:21 +0000

    Add package httpdbg/1.2.1/httpdbg-1.2.1-py3-none-any.whl

---
+++ packages/httpdbg/httpdbg-1.2.1-py3-none-any.whl/httpdbg/hooks/utils.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+import inspect
+
+from httpdbg.log import logger
+
+
+def getcallargs(original_method, *args, **kwargs):
+    while hasattr(
+        original_method, "__httpdbg__"
+    ):  # to retrieve the original method in case of nested hooks
+        original_method = original_method.__httpdbg__
+
+    try:
+        callargs = (
+            inspect.signature(original_method).bind_partial(*args, **kwargs).arguments
+        )
+    except Exception as ex:
+        logger().debug(f"getcallargs - exception {str(ex)}")
+        # TypeError('too many positional arguments') may occur when using pytest (seems related to teardown_method)
+        callargs = {}
+        i = 0
+        for arg in args:
+            i += 1
+            callargs[f"_positional_argument{i}"] = arg
+
+    callargs.update(kwargs)
+    logger().info(f"getcallargs {original_method} - {[arg for arg in callargs]}")
+    return callargs
+
+
+def decorate(records, method, hook):
+    ori = method
+    method = hook(records, method)
+    method.__httpdbg__ = ori
+    return method
+
+
+def undecorate(method):
+    return method.__httpdbg__
