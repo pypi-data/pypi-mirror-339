45ce8818d1d4bf8639345fea7ae2466a2cb4690a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-08 03:15:54 +0000

    Add package swagger-coverage-tool/0.9.0/swagger_coverage_tool-0.9.0-py3-none-any.whl

---
+++ packages/swagger-coverage-tool/swagger_coverage_tool-0.9.0-py3-none-any.whl/swagger_coverage_tool/src/coverage/builder.py
@@ -0,0 +1,95 @@
+from typing import Self
+
+from swagger_coverage_tool.config import ServiceConfig, Settings
+from swagger_coverage_tool.src.coverage.models import (
+    ServiceCoverage,
+    ServiceEndpointCoverage,
+    ServiceEndpointStatusCodeCoverage
+)
+from swagger_coverage_tool.src.history.core import SwaggerServiceCoverageHistory
+from swagger_coverage_tool.src.history.models import CoverageHistoryState, ServiceCoverageHistory
+from swagger_coverage_tool.src.libs.models import EndpointCoverageList
+from swagger_coverage_tool.src.swagger.models import SwaggerNormalized, SwaggerNormalizedStatusCode
+from swagger_coverage_tool.src.tools.types import ServiceKey
+
+
+class SwaggerServiceCoverageBuilder:
+    def __init__(
+            self,
+            service: ServiceKey,
+            swagger: SwaggerNormalized,
+            service_history: SwaggerServiceCoverageHistory,
+            endpoint_coverage_list: EndpointCoverageList
+    ):
+        self.service = service
+        self.swagger = swagger
+        self.service_history = service_history
+        self.endpoint_coverage_list = endpoint_coverage_list
+
+    @classmethod
+    def from_service_factory(
+            cls,
+            service: ServiceConfig,
+            swagger: SwaggerNormalized,
+            settings: Settings,
+            history_state: CoverageHistoryState,
+            endpoint_coverage_list: EndpointCoverageList,
+    ) -> Self:
+        return SwaggerServiceCoverageBuilder(
+            service=service.key,
+            swagger=swagger,
+            service_history=SwaggerServiceCoverageHistory(
+                history=history_state.services.get(service.key, ServiceCoverageHistory()),
+                settings=settings,
+            ),
+            endpoint_coverage_list=endpoint_coverage_list,
+        )
+
+    @classmethod
+    def build_status_code_coverage(
+            cls,
+            status_code: SwaggerNormalizedStatusCode,
+            coverage_list: EndpointCoverageList,
+    ) -> ServiceEndpointStatusCodeCoverage:
+        status_code_coverage = coverage_list.filter(status_code=status_code.value)
+
+        return ServiceEndpointStatusCodeCoverage(
+            value=status_code.value,
+            covered=status_code_coverage.is_covered,
+            total_cases=len(status_code_coverage.root),
+            description=status_code.description
+        )
+
+    def build(self) -> ServiceCoverage:
+        result: ServiceCoverage = ServiceCoverage(endpoints=[])
+
+        for endpoint in self.swagger.endpoints:
+            coverage_list = self.endpoint_coverage_list.filter(
+                name=endpoint.name,
+                method=endpoint.method,
+                service=self.service
+            )
+
+            service_endpoint_coverage = ServiceEndpointCoverage(
+                name=endpoint.name,
+                method=endpoint.method,
+                summary=endpoint.summary,
+                covered=coverage_list.is_covered,
+                total_cases=len(coverage_list.root),
+                status_codes=[
+                    self.build_status_code_coverage(status_code=status_code, coverage_list=coverage_list)
+                    for status_code in endpoint.status_codes
+                ],
+                total_coverage_history=[]
+            )
+            service_endpoint_coverage.total_coverage_history = self.service_history.get_endpoint_total_coverage_history(
+                name=endpoint.name,
+                method=endpoint.method,
+                total_coverage=service_endpoint_coverage.total_coverage
+            )
+
+            result.endpoints.append(service_endpoint_coverage)
+
+        result.total_coverage_history = self.service_history.get_total_coverage_history(result.total_coverage)
+
+        return result
