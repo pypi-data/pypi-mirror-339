36987eac66cefcd8b5af6b856b0422858c3f2899
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-08 15:05:36 +0000

    Add package ma100/0.1.0/ma100-0.1.0-py3-none-any.whl

---
+++ packages/ma100/ma100-0.1.0-py3-none-any.whl/ma100/__init__.py
@@ -0,0 +1,13 @@
+# -*- encoding：utf-8 -*-
+"""
+@File :__init__.py.py
+@Time :2025-4-8  13:09
+@Author：AI Lab Morgan
+"""
+import ma100_odbc as odbc
+import ma100_plot as plot
+
+__All__=[
+    'odbc',
+    'plot'
+]
