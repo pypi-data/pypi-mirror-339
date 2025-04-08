6834192bbd016bea41c1db2d586f43ff8cb8e1e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-08 05:04:27 +0000

    Add package fnet-expression/0.1.26/fnet_expression-0.1.26.tar.gz

---
+++ packages/fnet-expression/fnet_expression-0.1.26.tar.gz/fnet_expression-0.1.26/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="fnet_expression",
+    version="0.1.26",
+    packages=["fnet_expression","fnet_expression/lib","fnet_expression/cli"],
+    package_dir={"": "."},
+    entry_points={
+        "console_scripts": [
+            "fnet_expression=fnet_expression.cli:main",
+        ],
+    },
+    python_requires=">=3.12",
+    install_requires=[
+      
+    ],
+    # Optional metadata, to be added later:
+    # author="justai.pro",  # Your name (optional)
+    # author_email="devops@justai.pro",  # Your email address (optional)
+    # description="A Python dependency analyzer for import parsing and metadata extraction.",
+    # long_description=open("README.md").read(),
+    # long_description_content_type="text/markdown",
+    # url="https://gitlab.com/fnetai/py-import-parser",
+    # classifiers=[
+    #     "Programming Language :: Python :: 3",
+    #     "License :: OSI Approved :: MIT License",
+    #     # "Operating System :: OS Independent",
+    # ],
+    # include_package_data=True,  # Include non-Python files (e.g., YAML files) (optional)
+)
