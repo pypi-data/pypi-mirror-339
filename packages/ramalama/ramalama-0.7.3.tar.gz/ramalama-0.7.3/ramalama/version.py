a631f70aa19f44fe1e707385f8b13b5dd5dce811
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-08 06:04:25 +0000

    Add package ramalama/0.7.3/ramalama-0.7.3.tar.gz

---
+++ packages/ramalama/ramalama-0.7.3.tar.gz/ramalama-0.7.3/ramalama/version.py
@@ -0,0 +1,16 @@
+import importlib.metadata
+
+"""Version of RamaLamaPy."""
+
+
+def version():
+    try:
+        return importlib.metadata.version("ramalama")
+    except importlib.metadata.PackageNotFoundError:
+        return "0"
+
+    return "0"
+
+
+def print_version(args):
+    print("ramalama version %s" % version())
