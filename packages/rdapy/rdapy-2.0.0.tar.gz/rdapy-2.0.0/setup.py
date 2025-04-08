08480d40f2e21a872460e12fb446a8a5f381b509
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-08 04:04:38 +0000

    Add package rdapy/2.0.0/rdapy-2.0.0.tar.gz

---
+++ packages/rdapy/rdapy-2.0.0.tar.gz/rdapy-2.0.0/setup.py
@@ -0,0 +1,59 @@
+from setuptools import setup, find_packages
+
+"""
+https://python-packaging.readthedocs.io/en/latest/minimal.html
+https://packaging.python.org/en/latest/specifications/declaring-project-metadata/
+
+python setup.py register
+python setup.py register sdist upload
+
+https://packaging.python.org/en/latest/tutorials/packaging-projects/
+
+Once:
+
+$ python3 -m pip install --upgrade build
+$ python3 -m pip install --upgrade twine
+
+Each iteration:
+
+$ python3 -m build
+$ python3 -m twine upload --repository pypi dist/*
+
+"""
+
+setup(
+    name="rdapy",
+    version="2.0.0",
+    description="Redistricting analytics in Python",
+    url="https://github.com/dra2020/rdapy",
+    author="alecramsay",
+    author_email="alec@davesredistricting.org",
+    license="MIT",
+    packages=[
+        "rdapy",
+        "rdapy.compactness",
+        "rdapy.compactness.pypoly",
+        "rdapy.equal",
+        "rdapy.graph",
+        "rdapy.minority",
+        "rdapy.partisan",
+        "rdapy.splitting",
+        "rdapy.rate",
+        "rdapy.utils",
+        "rdapy.score",
+    ],
+    install_requires=[
+        "Fiona",
+        "geographiclib",
+        "geopandas",
+        "libpysal",
+        "nptyping",
+        "numpy",
+        "pandas",
+        "pyproj",
+        "pytest",
+        "scipy",
+        "shapely",
+    ],
+    zip_safe=False,
+)
