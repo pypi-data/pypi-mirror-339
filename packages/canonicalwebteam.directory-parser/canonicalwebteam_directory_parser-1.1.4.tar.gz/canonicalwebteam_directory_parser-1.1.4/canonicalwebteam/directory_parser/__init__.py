9b6bad8de7199b1ef0fa45a36528c307952ddef5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-08 15:06:09 +0000

    Add package canonicalwebteam.directory-parser/1.1.4/canonicalwebteam_directory_parser-1.1.4.tar.gz

---
+++ packages/canonicalwebteam.directory-parser/canonicalwebteam_directory_parser-1.1.4.tar.gz/canonicalwebteam_directory_parser-1.1.4/canonicalwebteam/directory_parser/__init__.py
@@ -0,0 +1,4 @@
+from canonicalwebteam.directory_parser.app import (  # noqa
+    scan_directory,  # noqa
+    generate_sitemap,  # noqa
+)
---
+++ packages/canonicalwebteam.directory-parser/canonicalwebteam_directory_parser-1.1.4.tar.gz/canonicalwebteam_directory_parser-1.1.4/setup.py
@@ -0,0 +1,28 @@
+#! /usr/bin/env python3
+
+from setuptools import setup, find_packages
+
+setup(
+    name="canonicalwebteam.directory-parser",
+    version="1.1.4",
+    author="Canonical webteam",
+    author_email="webteam@canonical.com",
+    url="https://github.com/canonical/canonicalwebteam.directory-parser",
+    description=(
+        "Flask extension to parse websites and extract structured data to "
+        "build sitemaps."
+    ),
+    packages=find_packages(),
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    install_requires=[
+        "Flask>=1.0.2",
+        "beautifulsoup4",
+        "humanize",
+        "lxml",
+        "requests",
+        "python-dateutil",
+        "validators",
+        "python-slugify",
+    ],
+)
