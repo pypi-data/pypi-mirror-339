d09849f6a018df0f414a6231168f553c8cfced01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-08 06:05:01 +0000

    Add package cleanpad/0.1.1/cleanpad-0.1.1.tar.gz

---
+++ packages/cleanpad/cleanpad-0.1.1.tar.gz/cleanpad-0.1.1/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="cleanpad",
+    version="0.1.1",
+    author="Omdeep Borkar",
+    author_email="omdeeborkar@gmail.com",
+    description="Text cleaner and formatter for handling messy copy-pasted content",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourusername/cleanpad",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Topic :: Text Processing :: General",
+    ],
+    python_requires=">=3.6",
+    keywords="text cleaning formatting normalization",
+    project_urls={
+        "Bug Reports": "https://github.com/yourusername/cleanpad/issues",
+        "Source": "https://github.com/yourusername/cleanpad",
+    },
+)
