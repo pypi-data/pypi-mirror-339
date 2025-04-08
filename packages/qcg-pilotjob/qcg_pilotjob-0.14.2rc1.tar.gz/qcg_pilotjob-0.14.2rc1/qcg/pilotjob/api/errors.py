aac095173435fa16d594cdc825ab8dada2a37996
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-08 16:05:16 +0000

    Add package qcg-pilotjob/0.14.2rc1/qcg_pilotjob-0.14.2rc1.tar.gz

---
+++ packages/qcg-pilotjob/qcg_pilotjob-0.14.2rc1.tar.gz/qcg_pilotjob-0.14.2rc1/qcg/pilotjob/api/errors.py
@@ -0,0 +1,34 @@
+class QCGPJMAError(Exception):
+    pass
+
+
+class InternalError(QCGPJMAError):
+    pass
+
+
+class InvalidJobDescriptionError(QCGPJMAError):
+    pass
+
+
+class JobNotDefinedError(QCGPJMAError):
+    pass
+
+
+class ConnectionError(QCGPJMAError):
+    pass
+
+
+class WrongArgumentsError(QCGPJMAError):
+    pass
+
+
+class FileError(QCGPJMAError):
+    pass
+
+
+class ServiceError(QCGPJMAError):
+    pass
+
+
+class TimeoutElapsed(Exception):
+    pass
