80e4b45f1937e61333560e43f05d6d073ec22999
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-08 14:09:02 +0000

    Add package moerl/0.0.0/moerl-0.0.0.tar.gz

---
+++ packages/moerl/moerl-0.0.0.tar.gz/moerl-0.0.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="moerl",
+    version="0.0.0",
+    author="slowfast ai",
+    description="This is an empty package to reserve the name on PyPI.",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "License :: OSI Approved :: Apache Software License",
+    ],
+)
