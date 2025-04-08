2bed1e40e58bb9c1a78ac8f71d5f8123d3344193
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-08 15:05:29 +0000

    Add package newton-physics/0.1.1a1/newton_physics-0.1.1a1-py3-none-any.whl

---
+++ packages/newton-physics/newton_physics-0.1.1a1-py3-none-any.whl/newton/__init__.py
@@ -0,0 +1,82 @@
+# SPDX-FileCopyrightText: Copyright (c) 2025 The Newton Developers
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from . import solvers
+from .core.model import (
+    GEO_BOX,
+    GEO_CAPSULE,
+    GEO_CONE,
+    GEO_CYLINDER,
+    GEO_MESH,
+    GEO_NONE,
+    GEO_PLANE,
+    GEO_SDF,
+    GEO_SPHERE,
+    JOINT_BALL,
+    JOINT_COMPOUND,
+    JOINT_D6,
+    JOINT_DISTANCE,
+    JOINT_FIXED,
+    JOINT_FREE,
+    JOINT_MODE_FORCE,
+    JOINT_MODE_TARGET_POSITION,
+    JOINT_MODE_TARGET_VELOCITY,
+    JOINT_PRISMATIC,
+    JOINT_REVOLUTE,
+    JOINT_UNIVERSAL,
+    SDF,
+    Control,
+    JointAxis,
+    Mesh,
+    Model,
+    ModelBuilder,
+    ModelShapeGeometry,
+    ModelShapeMaterials,
+    State,
+)
+
+__all__ = [
+    "GEO_BOX",
+    "GEO_CAPSULE",
+    "GEO_CONE",
+    "GEO_CYLINDER",
+    "GEO_MESH",
+    "GEO_NONE",
+    "GEO_PLANE",
+    "GEO_SDF",
+    "GEO_SPHERE",
+    "JOINT_BALL",
+    "JOINT_COMPOUND",
+    "JOINT_D6",
+    "JOINT_DISTANCE",
+    "JOINT_FIXED",
+    "JOINT_FREE",
+    "JOINT_MODE_FORCE",
+    "JOINT_MODE_TARGET_POSITION",
+    "JOINT_MODE_TARGET_VELOCITY",
+    "JOINT_PRISMATIC",
+    "JOINT_REVOLUTE",
+    "JOINT_UNIVERSAL",
+    "SDF",
+    "Control",
+    "JointAxis",
+    "Mesh",
+    "Model",
+    "ModelBuilder",
+    "ModelShapeGeometry",
+    "ModelShapeMaterials",
+    "State",
+    "solvers",
+]
