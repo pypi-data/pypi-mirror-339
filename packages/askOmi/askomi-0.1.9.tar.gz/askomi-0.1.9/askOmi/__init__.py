17756cda84e4fd7f130988a2a61b51f9a1140810
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-08 05:04:47 +0000

    Add package askOmi/0.1.9/askomi-0.1.9.tar.gz

---
+++ packages/askOmi/askomi-0.1.9.tar.gz/askomi-0.1.9/askOmi/__init__.py
@@ -0,0 +1,45 @@
+import os
+import subprocess
+
+try:
+    from google import genai
+except ImportError:
+    subprocess.check_call(["pip", "install", "google-genai"])
+    from google import genai
+
+def askOmi(error):
+
+    api_key = "AIzaSyATDPGbokzoJaBm9CU56GbvJT-1rCd75ls"
+    
+    client = genai.Client(api_key=api_key)
+
+    response = client.models.generate_content(
+        model="gemini-2.0-flash",
+        contents=error
+    )
+    return response.text
+
+def html(error):
+    answer = askOmi(error)
+
+    html_content = f"""
+<!DOCTYPE html>
+<html>
+<head>
+    <title>css</title>
+</head>
+<body>
+    <br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+    <br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+    <br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+    <br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+    <br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+    <p>{answer}</p>
+</body>
+</html>
+"""
+
+    with open("output.html", "w") as f:
+        f.write(html_content)
+
+    
