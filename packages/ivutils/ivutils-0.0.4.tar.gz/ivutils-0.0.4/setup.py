957c7d00b26229afb469ace6a420e1b1bc358c3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-08 14:08:55 +0000

    Add package ivutils/0.0.4/ivutils-0.0.4.tar.gz

---
+++ packages/ivutils/ivutils-0.0.4.tar.gz/ivutils-0.0.4/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open("README.md") as fp:
+    desc_text = fp.read()
+
+setup(
+    name="ivutils",
+    version="0.0.4",
+    license="MIT license",
+    long_description=desc_text,
+    long_description_content_type= "text/markdown",
+    packages=find_packages(),
+    install_requires=[
+        "opencv-python~=4.10.0.84",
+        "numpy~=2.1.3",
+        "pillow~=11.0.0"
+    ],
+    entry_points={
+        'console_scripts': [
+            'ivutils = ivutils.cli:run',
+        ],
+    },
+)
