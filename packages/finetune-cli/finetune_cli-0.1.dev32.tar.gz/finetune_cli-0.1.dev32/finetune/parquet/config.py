6aefd964ee7b1e57ef59473e8774a482d00fba77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-08 15:06:20 +0000

    Add package finetune-cli/0.1.dev32/finetune_cli-0.1.dev32.tar.gz

---
+++ packages/finetune-cli/finetune_cli-0.1.dev32.tar.gz/finetune_cli-0.1.dev32/finetune/parquet/config.py
@@ -0,0 +1,9 @@
+from finetune.parquet.agentManager import AgentModel
+
+Master = {
+    "openai_api_key": "sk-gnZfIx337omYX3bd30B1C95a0f1047Ee86CaD8925177AbEa",
+    "openai_api_endpoint": "http://100.64.0.2:8000/v1",
+    "default_model": AgentModel.QwQ_32B,
+    "prefix": "",
+}
+
