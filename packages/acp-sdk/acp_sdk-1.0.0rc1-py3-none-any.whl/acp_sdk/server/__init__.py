0cbb1b5d77ef11ed84080aa4689b7796ebc31058
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-08 04:04:39 +0000

    Add package acp-sdk/1.0.0rc1/acp_sdk-1.0.0rc1-py3-none-any.whl

---
+++ packages/acp-sdk/acp_sdk-1.0.0rc1-py3-none-any.whl/acp_sdk/server/__init__.py
@@ -0,0 +1,2 @@
+from acp_sdk.server.server import create_app
+from acp_sdk.server.agent import Agent
---
+++ packages/acp-sdk/acp_sdk-1.0.0rc1-py3-none-any.whl/acp_sdk/server/bundle.py
@@ -0,0 +1,133 @@
+import asyncio
+import logging
+
+from opentelemetry import trace
+from pydantic import ValidationError
+
+from acp_sdk.server.agent import Agent
+from acp_sdk.models import (
+    ACPError,
+    AnyModel,
+    Await,
+    AwaitEvent,
+    CancelledEvent,
+    CompletedEvent,
+    CreatedEvent,
+    FailedEvent,
+    GenericEvent,
+    InProgressEvent,
+    Message,
+    MessageEvent,
+    Run,
+    AwaitResume,
+    RunEvent,
+    RunStatus,
+)
+from acp_sdk.server.context import Context
+
+logger = logging.getLogger("uvicorn.error")
+
+
+class RunBundle:
+    def __init__(self, *, agent: Agent, run: Run, task: asyncio.Task | None = None):
+        self.agent = agent
+        self.run = run
+        self.task = task
+
+        self.stream_queue: asyncio.Queue[RunEvent] = asyncio.Queue()
+        self.composed_message = Message()
+
+        self.await_queue: asyncio.Queue[AwaitResume] = asyncio.Queue(maxsize=1)
+        self.await_or_terminate_event = asyncio.Event()
+
+    async def stream(self):
+        try:
+            while True:
+                event = await self.stream_queue.get()
+                yield event
+                self.stream_queue.task_done()
+        except asyncio.QueueShutDown:
+            pass
+
+    async def emit(self, event: RunEvent):
+        await self.stream_queue.put(event)
+
+    async def await_(self) -> AwaitResume:
+        self.stream_queue.shutdown()
+        self.await_queue.empty()
+        self.await_or_terminate_event.set()
+        self.await_or_terminate_event.clear()
+        resume = await self.await_queue.get()
+        self.await_queue.task_done()
+        return resume
+
+    async def resume(self, resume: AwaitResume):
+        self.stream_queue = asyncio.Queue()
+        await self.await_queue.put(resume)
+
+    async def join(self):
+        await self.await_or_terminate_event.wait()
+
+    async def execute(self, input: Message):
+        with trace.get_tracer(__name__).start_as_current_span("execute"):
+            run_logger = logging.LoggerAdapter(logger, {"run_id": self.run.run_id})
+
+            await self.emit(CreatedEvent(run=self.run))
+            try:
+                self.run.session_id = await self.agent.session(self.run.session_id)
+                run_logger.info("Session loaded")
+
+                generator = self.agent.run(
+                    input=input, context=Context(session_id=self.run.session_id)
+                )
+                run_logger.info("Run started")
+
+                self.run.status = RunStatus.IN_PROGRESS
+                await self.emit(InProgressEvent(run=self.run))
+
+                await_resume = None
+                while True:
+                    next = await generator.asend(await_resume)
+                    if isinstance(next, Message):
+                        self.composed_message += next
+                        await self.emit(MessageEvent(message=next))
+                    elif isinstance(next, Await):
+                        self.run.await_ = next
+                        self.run.status = RunStatus.AWAITING
+                        await self.emit(
+                            AwaitEvent.model_validate(
+                                {
+                                    "run_id": self.run.run_id,
+                                    "type": "await",
+                                    "await": next,
+                                }
+                            )
+                        )
+                        run_logger.info("Run awaited")
+                        await_resume = await self.await_()
+                        self.run.status = RunStatus.IN_PROGRESS
+                        await self.emit(InProgressEvent(run=self.run))
+                        run_logger.info("Run resumed")
+                    else:
+                        try:
+                            generic = AnyModel.model_validate(next)
+                            await self.emit(GenericEvent(generic=generic))
+                        except ValidationError:
+                            raise TypeError("Invalid yield")
+            except StopAsyncIteration:
+                self.run.output = self.composed_message
+                self.run.status = RunStatus.COMPLETED
+                await self.emit(CompletedEvent(run=self.run))
+                run_logger.info("Run completed")
+            except asyncio.CancelledError:
+                self.run.status = RunStatus.CANCELLED
+                await self.emit(CancelledEvent(run=self.run))
+                run_logger.info("Run cancelled")
+            except Exception as e:
+                self.run.error = ACPError(code="unspecified", message=str(e))
+                self.run.status = RunStatus.FAILED
+                await self.emit(FailedEvent(run=self.run))
+                run_logger.exception("Run failed")
+            finally:
+                self.await_or_terminate_event.set()
+                self.stream_queue.shutdown()
