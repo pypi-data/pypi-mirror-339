a743aefa4c2d86a5b43616559fa434697ddca189
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-08 16:05:10 +0000

    Add package pelican-nlp/0.1.1/pelican_nlp-0.1.1.tar.gz

---
+++ packages/pelican-nlp/pelican_nlp-0.1.1.tar.gz/pelican_nlp-0.1.1/pelican_nlp/__init__.py
@@ -0,0 +1,9 @@
+# Version and metadata
+__version__ = "0.1.1"
+__author__ = "Yves Pauli"
+
+try:
+    from .main import Pelican
+except ImportError as e:
+    print(f"Warning: Could not import Pelican class: {e}")
+
