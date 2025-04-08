97f01c1c2cc96f7f4df19163f92011f345753d61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-08 08:05:10 +0000

    Add package science-synapse/1.0.2/science_synapse-1.0.2.tar.gz

---
+++ packages/science-synapse/science_synapse-1.0.2.tar.gz/science_synapse-1.0.2/synapse/cli/__main__.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+import argparse
+import logging
+from importlib import metadata
+from synapse.cli import discover, rpc, streaming, offline_plot, files
+from rich.logging import RichHandler
+
+
+def main():
+    logging.basicConfig(level=logging.INFO, handlers=[RichHandler()])
+
+    parser = argparse.ArgumentParser(
+        description="Synapse Device Manager",
+        formatter_class=lambda prog: argparse.HelpFormatter(prog, width=124),
+    )
+
+    parser.add_argument(
+        "--version",
+        action="version",
+        version="synapsectl %s" % metadata.version("science-synapse"),
+    )
+    parser.add_argument(
+        "--verbose",
+        "-v",
+        action="store_true",
+        default=False,
+        help="Enable verbose output",
+    )
+    parser.add_argument(
+        "--uri", metavar="-u", type=str, default=None, help="Device control plane URI"
+    )
+
+    subparsers = parser.add_subparsers(title="Commands")
+
+    discover.add_commands(subparsers)
+    rpc.add_commands(subparsers)
+    streaming.add_commands(subparsers)
+    offline_plot.add_commands(subparsers)
+    files.add_commands(subparsers)
+    args = parser.parse_args()
+
+    if hasattr(args, "func"):
+        args.func(args)
+    else:
+        parser.print_help()
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/science-synapse/science_synapse-1.0.2.tar.gz/science_synapse-1.0.2/synapse/client/__init__.py
@@ -0,0 +1,15 @@
+from synapse.client.node import Node
+from synapse.client.config import Config
+from synapse.client.device import Device
+
+from synapse.client.channel import Channel
+from synapse.client.signal_config import SignalConfig, ElectrodeConfig, PixelConfig
+
+from synapse.client.nodes.stream_in import StreamIn
+from synapse.client.nodes.stream_out import StreamOut
+from synapse.client.nodes.optical_stimulation import OpticalStimulation
+from synapse.client.nodes.electrical_stimulation import ElectricalStimulation
+from synapse.client.nodes.broadband_source import BroadbandSource
+from synapse.client.nodes.spike_source import SpikeSource
+from synapse.client.nodes.spike_detect import SpikeDetect
+from synapse.client.nodes.spectral_filter import SpectralFilter
