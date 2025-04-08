61bb5a3c62f24213e1e435a1fbbaf720b0db1655
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-08 03:15:57 +0000

    Add package data-syncmaster/0.2.1/data_syncmaster-0.2.1.tar.gz

---
+++ packages/data-syncmaster/data_syncmaster-0.2.1.tar.gz/data_syncmaster-0.2.1/syncmaster/exceptions/group.py
@@ -0,0 +1,31 @@
+# SPDX-FileCopyrightText: 2023-2024 MTS PJSC
+# SPDX-License-Identifier: Apache-2.0
+from syncmaster.exceptions.base import SyncmasterError
+
+
+class GroupNameAlreadyExistsError(SyncmasterError):
+    pass
+
+
+class GroupAdminNotFoundError(SyncmasterError):
+    pass
+
+
+class GroupAlreadyExistsError(SyncmasterError):
+    pass
+
+
+class AlreadyIsGroupMemberError(SyncmasterError):
+    pass
+
+
+class AlreadyIsNotGroupMemberError(SyncmasterError):
+    pass
+
+
+class AlreadyIsGroupOwnerError(SyncmasterError):
+    pass
+
+
+class GroupNotFoundError(SyncmasterError):
+    pass
