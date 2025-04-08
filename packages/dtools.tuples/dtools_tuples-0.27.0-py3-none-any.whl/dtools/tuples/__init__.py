a194ca8c4f4987268c9cf1b12349d32f0eda165e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-08 14:08:46 +0000

    Add package dtools.tuples/0.27.0/dtools_tuples-0.27.0-py3-none-any.whl

---
+++ packages/dtools.tuples/dtools_tuples-0.27.0-py3-none-any.whl/dtools/tuples/__init__.py
@@ -0,0 +1,30 @@
+# Copyright 2023-2024 Geoffrey R. Scheller
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+### Developer Tools - Tuple-like data structures
+
+#### Modules
+
+##### *module* dtools.tuples.ftuples
+
+  - *class* dtools.tuples.ftuple.FTuple
+    - an immutable tuple-like data structure with functional interface
+
+"""
+
+__version__ = '0.27.0'
+__author__ = 'Geoffrey R. Scheller'
+__copyright__ = 'Copyright (c) 2023-2025 Geoffrey R. Scheller'
+__license__ = 'Apache License 2.0'
