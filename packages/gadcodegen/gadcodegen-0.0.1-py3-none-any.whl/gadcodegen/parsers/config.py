ecc81dba05a2740e61c0f6d250529466178f8df4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-08 04:04:39 +0000

    Add package gadcodegen/0.0.1/gadcodegen-0.0.1-py3-none-any.whl

---
+++ packages/gadcodegen/gadcodegen-0.0.1-py3-none-any.whl/gadcodegen/parsers/config.py
@@ -0,0 +1,30 @@
+from pathlib import Path
+
+from gadcodegen import const
+from gadcodegen.os import HTTP
+from gadcodegen.os import File
+from gadcodegen.utils import temp
+from gadcodegen.utils import urls
+
+
+def getconfig(file: str) -> tuple[Path, bool]:
+    if urls.checkurl(file):
+        return temp.getfile(HTTP.download(file), extension=const.EXTENSION_TOML), True
+    else:
+        return Path(file), False
+
+
+def getcontent(workdir: Path, content: str) -> str:
+    if content.startswith(const.SYNTAX_FILES_CONTENT_FILE):
+        path = Path(content[len(const.SYNTAX_FILES_CONTENT_FILE) :].strip())
+
+        if not path.is_absolute():
+            path = workdir / path
+
+        if path.exists() and path.is_file():
+            return File.read(path)
+
+    elif urls.checkurl(content):
+        return HTTP.download(content)
+
+    return content
---
+++ packages/gadcodegen/gadcodegen-0.0.1-py3-none-any.whl/gadcodegen/utils/paths.py
@@ -0,0 +1,37 @@
+import os
+from pathlib import Path
+
+from gadcodegen import const
+
+
+def current() -> Path:
+    return Path.cwd()
+
+
+def define(workdir: str | None = None) -> Path:
+    if not workdir:
+        return current()
+    elif workdir.startswith(const.SYMBOL_FORWARD_SLASH):
+        return Path(workdir)
+    else:
+        return current() / workdir
+
+
+def tree(workdir: Path | str) -> tuple[set[Path], set[Path]]:
+    if isinstance(workdir, str):
+        workdir = define(workdir)
+
+    folders, files = set(), set()
+
+    for path, _, filenames in os.walk(workdir):
+        path = Path(path)
+
+        if path.name.startswith(const.SYMBOL_DOT):
+            continue
+
+        folders.add(path)
+
+        for filename in filenames:
+            files.add(path / filename)
+
+    return folders, files
