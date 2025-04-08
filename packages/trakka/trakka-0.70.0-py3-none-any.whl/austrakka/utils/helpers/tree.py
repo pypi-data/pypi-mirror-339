9513881e92109adcd2f7fb10a94d3f75d2724fe0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-08 06:05:05 +0000

    Add package trakka/0.70.0/trakka-0.70.0-py3-none-any.whl

---
+++ packages/trakka/trakka-0.70.0-py3-none-any.whl/austrakka/utils/helpers/tree.py
@@ -0,0 +1,6 @@
+from austrakka.utils.api import api_get
+from austrakka.utils.paths import ANALYSIS_PATH
+
+
+def get_tree_by_abbrev(abbrev: str):
+    return api_get(path=f"{ANALYSIS_PATH}/abbrev/{abbrev}")['data']
