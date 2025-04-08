18882d731b58682ad78c16972e358289c8809140
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-08 14:09:09 +0000

    Add package tgraphx/0.0.1/tgraphx-0.0.1.tar.gz

---
+++ packages/tgraphx/tgraphx-0.0.1.tar.gz/tgraphx-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="tgraphx",
+    version="0.0.1",
+    description="Early placeholder for TGraphX PyPI reservation",
+    author="Arash Sajjadi",
+    author_email="arash.sajjadi@usask.ca",
+    packages=["tgraphx"],
+    python_requires=">=3.8",
+)
