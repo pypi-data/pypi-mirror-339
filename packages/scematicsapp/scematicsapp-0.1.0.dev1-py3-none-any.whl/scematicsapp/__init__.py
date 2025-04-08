f0c86eb90044554476b12df47832844ead2a5564
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-08 03:16:13 +0000

    Add package scematicsapp/0.1.0.dev1/scematicsapp-0.1.0.dev1-py3-none-any.whl

---
+++ packages/scematicsapp/scematicsapp-0.1.0.dev1-py3-none-any.whl/scematicsapp/__init__.py
@@ -0,0 +1,7 @@
+
+"""ScematicsApp module for file upload CLI tools."""
+
+__version__ = ' 0.1.0.dev1'
+
+# You can import key functions to make them available at the package level
+from .cli import main
