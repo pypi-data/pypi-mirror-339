8747a3dc7a39aad21f2d25271948eb89a47dc2f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-09 16:05:23 +0000

    Add package zrb/1.5.5/zrb-1.5.5.tar.gz

---
+++ packages/zrb/zrb-1.5.5.tar.gz/zrb-1.5.5/src/zrb/builtin/llm/tool/api.py
@@ -0,0 +1,31 @@
+import json
+from typing import Annotated, Literal
+
+
+def get_current_location() -> (
+    Annotated[str, "JSON string representing latitude and longitude"]
+):  # noqa
+    """Get the user's current location. This function take no argument."""
+    import requests
+
+    return json.dumps(requests.get("http://ip-api.com/json?fields=lat,lon").json())
+
+
+def get_current_weather(
+    latitude: float,
+    longitude: float,
+    temperature_unit: Literal["celsius", "fahrenheit"],
+) -> str:
+    """Get the current weather in a given location."""
+    import requests
+
+    resp = requests.get(
+        "https://api.open-meteo.com/v1/forecast",
+        params={
+            "latitude": latitude,
+            "longitude": longitude,
+            "temperature_unit": temperature_unit,
+            "current_weather": True,
+        },
+    )
+    return json.dumps(resp.json())
