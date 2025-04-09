512f05123e94f580877b583336ca79b7a5294019
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-09 14:05:57 +0000

    Add package lp-plotter/0.1.1/lp_plotter-0.1.1.tar.gz

---
+++ packages/lp-plotter/lp_plotter-0.1.1.tar.gz/lp_plotter-0.1.1/README.md
@@ -0,0 +1,8 @@
+# LP Plotter
+
+Ferramenta para visualizar graficamente modelos de programação linear com duas variáveis.
+
+## Instalação
+
+```bash
+pip install -e .
---
+++ packages/lp-plotter/lp_plotter-0.1.1.tar.gz/lp_plotter-0.1.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="lp_plotter",
+    version="0.1.1",
+    description="Visualização gráfica de modelos de programação linear com duas variáveis",
+    author="Pedro Eckel",
+    packages=find_packages(),
+    install_requires=[
+        "matplotlib",
+        "numpy",
+        "sympy",
+    ],
+    python_requires=">=3.7",
+)
