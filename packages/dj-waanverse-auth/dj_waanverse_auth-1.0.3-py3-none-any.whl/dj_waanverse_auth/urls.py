eb4743bf75e5460dc1b54c1bfef11bb6a3371a86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-09 05:05:22 +0000

    Add package dj-waanverse-auth/1.0.3/dj_waanverse_auth-1.0.3-py3-none-any.whl

---
+++ packages/dj-waanverse-auth/dj_waanverse_auth-1.0.3-py3-none-any.whl/dj_waanverse_auth/urls.py
@@ -0,0 +1,17 @@
+from django.urls import include, path
+
+from dj_waanverse_auth.routes import (
+    authorization_urls,
+    login_urls,
+    mfa_urls,
+    password_urls,
+    signup_urls,
+)
+
+urlpatterns = [
+    path("login/", include(login_urls)),
+    path("", include(authorization_urls)),
+    path("mfa/", include(mfa_urls)),
+    path("signup/", include(signup_urls)),
+    path("password/", include(password_urls)),
+]
