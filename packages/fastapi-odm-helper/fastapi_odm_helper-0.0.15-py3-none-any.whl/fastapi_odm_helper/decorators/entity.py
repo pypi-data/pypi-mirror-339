68495ade1d17335db2ca25a58ee5b997c350d7e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-09 17:05:44 +0000

    Add package fastapi-odm-helper/0.0.15/fastapi_odm_helper-0.0.15-py3-none-any.whl

---
+++ packages/fastapi-odm-helper/fastapi_odm_helper-0.0.15-py3-none-any.whl/fastapi_odm_helper/decorators/entity.py
@@ -0,0 +1,31 @@
+import re
+from typing import Callable, Type, TypeVar
+
+T = TypeVar('T')
+
+
+EXCEPTION_NAMES = {'category'}
+
+
+def underscoring_entity_name(entity_name: str):
+    return re.sub(r'(?<=[a-z])(?=[A-Z])|(?<=[A-Z])(?=[A-Z][a-z])', '_', entity_name)
+
+def standardize_entity_name(table_name: str) -> str:
+    transformed_name = underscoring_entity_name(table_name[: -len("Entity")]).lower()
+
+    is_exception = any(name.lower() in transformed_name for name in EXCEPTION_NAMES)
+
+    if is_exception:
+        return transformed_name
+
+    return f'{transformed_name}s'
+
+def Entity() -> Callable[[Type[T]], Type[T]]:
+    def decorator(cls: Type[T]) -> Type[T]:
+        class Settings:
+            name = standardize_entity_name(cls.__name__)
+
+        setattr(cls, "Settings", Settings)
+        return cls
+
+    return decorator
