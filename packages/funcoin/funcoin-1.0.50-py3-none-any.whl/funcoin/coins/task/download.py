62f21d0e340140fa99fa06f9e0ec252b3c6c003b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-09 17:05:40 +0000

    Add package funcoin/1.0.50/funcoin-1.0.50-py3-none-any.whl

---
+++ packages/funcoin/funcoin-1.0.50-py3-none-any.whl/funcoin/coins/task/download.py
@@ -0,0 +1,33 @@
+import ccxt
+from fundrive import OSSDrive
+from funsecret import read_secret
+from funtable import DriveTable
+
+from funcoin.coins.table.load import LoadTask
+
+
+def download_daily(days=800, *arge, **kwargs):
+    days = int(days)
+    exchange = ccxt.binance(  # noqa: F821
+        {
+            "apiKey": read_secret("coin", "binance", "api_key"),
+            "secret": read_secret("coin", "binance", "secret_key"),
+        }
+    )
+
+    drive = OSSDrive()
+
+    drive.login(
+        access_key=read_secret("fundrive", "oss", "farfarfun", "access_key"),
+        access_secret=read_secret("fundrive", "oss", "farfarfun", "access_secret"),
+        endpoint=read_secret("fundrive", "oss", "farfarfun", "endpoint"),
+        bucket_name="farfarfun",
+    )
+
+    table = DriveTable(table_fid="funcoin/binance_kline_daily_1m/", drive=drive)
+    table.update_partition_meta()
+    task = LoadTask(table=table, exchange=exchange)
+    task.run(days=days)
+
+
+# download_daily(days=3)
