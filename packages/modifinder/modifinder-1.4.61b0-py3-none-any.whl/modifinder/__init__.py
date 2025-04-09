e456e0c561403608111cd0a8eb642a8c624a51db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-09 07:04:59 +0000

    Add package modifinder/1.4.61b0/modifinder-1.4.61b0-py3-none-any.whl

---
+++ packages/modifinder/modifinder-1.4.61b0-py3-none-any.whl/modifinder/__init__.py
@@ -0,0 +1,20 @@
+"""
+ModiFinder
+==========
+
+ModiFinder is a Python package for the identification of modifications in mass spectrometry data.
+"""
+
+from modifinder import convert
+from modifinder.convert import *
+
+from modifinder.exceptions import *
+
+from modifinder import classes
+from modifinder.classes import *
+
+from modifinder import utilities
+from modifinder.utilities import *
+
+from modifinder import engines
+from modifinder.engines import *
