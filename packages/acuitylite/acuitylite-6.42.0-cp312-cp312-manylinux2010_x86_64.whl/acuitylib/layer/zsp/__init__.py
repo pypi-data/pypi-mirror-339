e53f69f39b6c180a5a22a5e306199ca7e169c2e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-09 14:06:38 +0000

    Add package acuitylite/6.42.0/acuitylite-6.42.0-cp312-cp312-manylinux2010_x86_64.whl

---
+++ packages/acuitylite/acuitylite-6.42.0-cp312-cp312-manylinux2010_x86_64.whl/acuitylib/layer/zsp/__init__.py
@@ -0,0 +1,12 @@
+
+# zsp layers
+from acuitylib.layer.zsp.zsp_convolution import ZSPConvolution
+from acuitylib.layer.zsp.zsp_depthwise_convolution import ZSPDepthwiseConvolution
+from acuitylib.layer.zsp.zsp_fullconnect import ZSPFullConnect
+from acuitylib.layer.zsp.zsp_poinwise_convolution import ZSPPointWise_Convolution
+from acuitylib.layer.zsp.zsp_pool import ZSPPool
+from acuitylib.layer.zsp.zsp_quantize import ZSPQuantize
+from acuitylib.layer.zsp.zsp_reshapelayer import ZSPReshape
+from acuitylib.layer.zsp.zsp_softmax import ZSPSoftMax
+from acuitylib.layer.zsp.zsp_svdf import ZSPSVDF
+from acuitylib.layer.zsp.zsp_add import ZSPAdd
