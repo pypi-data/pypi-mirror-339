04af6190f01f13282ef7d92a7d27266b47e4a70b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-09 17:05:33 +0000

    Add package code2/5.2.2/code2-5.2.2.tar.gz

---
+++ packages/code2/code2-5.2.2.tar.gz/code2-5.2.2/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup
+
+
+setup(
+    name='code2',
+    version='5.2.2',
+    setup_requires='setupmeta',
+    packages=['code2'],
+    install_requires=[
+        'cli2',
+        'tree-sitter',
+        'tree-sitter-language-pack',
+        'peewee',
+        'prompt2',
+    ],
+    author='James Pic',
+    author_email='jamespic@gmail.com',
+    url='https://yourlabs.io/oss/cli2',
+    include_package_data=True,
+    license='MIT',
+    keywords='cli',
+    python_requires='>=3.6',
+    entry_points={
+        'console_scripts': [
+            'code2 = code2.cli:cli.entry_point',
+        ],
+        'code2_workflow': [
+            'cmd = code2.workflows.cmd:CmdWorkflow',
+            #'analyze = code2.workflows.analyze:AnalyzeWorkflow',
+            #'edit = code2.workflows.edit:EditWorkflow',
+            #'ask = code2.plugins.workflow.ask:AskWorkflow',
+            #'hack = code2.plugins.workflow.hack:HackWorkflow',
+            #'ask = code2.plugins.workflow.ask:AskWorkflow',
+            #'create = code2.plugins.workflow.create:CreateWorkflow',
+        ],
+        'prompt2_paths': [
+            'code2 = code2.prompt2:paths',
+        ],
+    },
+)
