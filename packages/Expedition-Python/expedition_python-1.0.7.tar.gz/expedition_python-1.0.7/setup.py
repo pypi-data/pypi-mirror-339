c0c47898b98992964c073d2fe022d49e9971bbd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-09 06:05:42 +0000

    Add package Expedition-Python/1.0.7/expedition_python-1.0.7.tar.gz

---
+++ packages/Expedition-Python/expedition_python-1.0.7.tar.gz/expedition_python-1.0.7/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Expedition-Python',
+    version='1.0.7',
+    author='Tom Cheney',
+    author_email='tom@ttcmarine.com',
+    description='Python wrapper for the Expedition DLL',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/TTCMarine/Expedition-Python',
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.12',
+    ],
+)
