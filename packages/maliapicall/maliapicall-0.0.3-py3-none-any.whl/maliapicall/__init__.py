39ccdbdac09f8e2f2b64d9cc0bb7a7762c5c0a3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-09 16:05:06 +0000

    Add package maliapicall/0.0.3/maliapicall-0.0.3-py3-none-any.whl

---
+++ packages/maliapicall/maliapicall-0.0.3-py3-none-any.whl/maliapicall/__init__.py
@@ -0,0 +1,10 @@
+from . import muji  
+from . import sk  
+from . import wms  
+
+
+__all__ = [
+    'muji',
+    'sk',
+    'wms',
+]
---
+++ packages/maliapicall/maliapicall-0.0.3-py3-none-any.whl/maliapicall/sk.py
@@ -0,0 +1,24 @@
+import requests
+
+
+def get_token(auth_url: str, user_name: str, user_password: str) -> str:
+    """
+    Get the access token through login authentication.
+    """    
+    proxies = {"http": None, "https": None}
+    params = {'username': user_name, 'password': user_password}
+    headers = {
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36",
+        "Authorization":'Basic bWFsbGVlLXNhYXMtdWFjOm1hbGlTb2FDbGllbnRTZWNyZXQ=',
+        'Connection': 'keep-alive',
+        'Content-Length':'0',
+        'Accept':'application/json, text/plain, */*',
+        'Accept-Encoding':'gzip, deflate, br',
+        'accept-language':'zh-CN,zh;q=0.9,en;q=0.8'
+        }
+    query = requests.post(auth_url, params = params, headers = headers, proxies=proxies)
+
+    if query.json()['code'] == 200:
+        return query.json()['result']['access_token']
+    else:
+        return False
