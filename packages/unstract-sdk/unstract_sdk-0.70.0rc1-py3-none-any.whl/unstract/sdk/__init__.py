2b4b33feb265615f7fbef4f9fba1e1d403b5dc7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-09 17:05:50 +0000

    Add package unstract-sdk/0.70.0rc1/unstract_sdk-0.70.0rc1-py3-none-any.whl

---
+++ packages/unstract-sdk/unstract_sdk-0.70.0rc1-py3-none-any.whl/unstract/sdk/__init__.py
@@ -0,0 +1,6 @@
+__version__ = "v0.70.0rc1"
+
+
+def get_sdk_version():
+    """Returns the SDK version."""
+    return __version__
---
+++ packages/unstract-sdk/unstract_sdk-0.70.0rc1-py3-none-any.whl/unstract/sdk/adapters/x2text/llm_whisperer_v2/src/dto.py
@@ -0,0 +1,22 @@
+from dataclasses import dataclass
+from typing import Optional, Union, List
+
+
+@dataclass
+class WhispererRequestParams:
+    """DTO for LLM Whisperer API request parameters.
+
+    Args:
+        tag (Optional[Union[str, List[str]]]): Tag value. Can be initialized with List[str] or str.
+             Will be converted to str or None after initialization.
+        enable_highlight (bool): Whether to enable highlighting. Defaults to False.
+    """
+
+    # TODO: Extend this DTO to include all Whisperer API parameters
+    tag: Optional[Union[str, List[str]]] = None
+    enable_highlight: bool = False
+
+    def __post_init__(self) -> None:
+        # TODO: Allow list of tags once it's supported in LLMW v2
+        if isinstance(self.tag, list):
+            self.tag = self.tag[0] if self.tag else None
