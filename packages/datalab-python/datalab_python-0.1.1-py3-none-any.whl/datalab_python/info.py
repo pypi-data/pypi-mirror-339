ab05a52d34107e26fe4de81fcdfa75e055f4d5c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-09 03:17:54 +0000

    Add package datalab-python/0.1.1/datalab_python-0.1.1-py3-none-any.whl

---
+++ packages/datalab-python/datalab_python-0.1.1-py3-none-any.whl/datalab_python/info.py
@@ -0,0 +1,11 @@
+def info():
+    message = """
+
+Hello from the DataLab Team!
+
+This package is a placeholder for the DataLab package
+More information TBA.
+
+You can find more information about as at: https://bigdatalab.ai
+"""
+    print(message)
