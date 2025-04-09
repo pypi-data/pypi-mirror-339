ddff87d0a73365cbedcc0867af33c35919acc997
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-09 07:05:08 +0000

    Add package lidapy/0.2.0/lidapy-0.2.0.tar.gz

---
+++ packages/lidapy/lidapy-0.2.0.tar.gz/lidapy-0.2.0/src/AttentionCodelets/AttentionCodelet.py
@@ -0,0 +1,12 @@
+from Framework.Tasks.Codelet import Codelet
+
+
+class AttentionCodelet(Codelet):
+    def __init__(self):
+        super().__init__()
+
+    def getModuleContent(self):
+        pass
+
+    def notify(self, module):
+        pass
---
+++ packages/lidapy/lidapy-0.2.0.tar.gz/lidapy-0.2.0/src/GlobalWorkspace/GlobalWorkSpace.py
@@ -0,0 +1,30 @@
+from Module.Initialization.ModuleInterface import Module
+
+
+class GlobalWorkspace(Module):
+    def __init__(self):
+        super().__init__()
+
+    def addCoalition(self, coalition):
+        pass
+
+    def addBroadcastTrigger(self, trigger):
+        pass
+
+    def getBroadcastSentCount(self):
+        pass
+
+    def getTickAtLastBroadcast(self):
+        pass
+
+    def setCoalitionDecayStrategy(self, decay_strategy):
+        pass
+
+    def getCoalitionDecayStrategy(self):
+        pass
+
+    def __getstate__(self):
+        pass
+
+    def notify(self, module):
+        pass
