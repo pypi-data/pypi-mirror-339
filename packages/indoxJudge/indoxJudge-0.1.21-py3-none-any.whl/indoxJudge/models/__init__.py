19c5741af1bf8a6158a0f84e24a409bdff79e9c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-09 03:17:55 +0000

    Add package indoxJudge/0.1.21/indoxJudge-0.1.21-py3-none-any.whl

---
+++ packages/indoxJudge/indoxJudge-0.1.21-py3-none-any.whl/indoxJudge/models/__init__.py
@@ -0,0 +1,10 @@
+from .openai import OpenAi
+from .googleai import GoogleAi
+from .nerd_token import NerdTokenApi
+from .huggingface import HuggingFaceModel
+from .mistral import Mistral
+from .azure_openai import AzureOpenAi
+from .anthropic import Anthropic
+from .indoxrouter import IndoxRouter
+
+# from .ollama import Ollama
