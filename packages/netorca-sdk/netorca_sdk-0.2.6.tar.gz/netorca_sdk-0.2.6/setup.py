78d25037e9dd97bbb5bf39dedae3322a17f66c65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-09 04:06:14 +0000

    Add package netorca-sdk/0.2.6/netorca_sdk-0.2.6.tar.gz

---
+++ packages/netorca-sdk/netorca_sdk-0.2.6.tar.gz/netorca_sdk-0.2.6/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+setup(
+    name="netorca-sdk",
+    packages=["netorca_sdk"],
+    version="0.2.6",
+    license="MIT",
+
+    description="A package for interacting with the NetOrca API",
+    long_description=open("README.rst").read(),
+    long_description_content_type="text/x-rst",
+    author="Scott Rowlandson",
+    author_email="scott@netautomate.org",
+    url="https://gitlab.com/netorca_public/netorca_sdk/",
+    keywords=["netorca", "orchestration", "netautomate"],
+    install_requires=[
+        "beautifultable",
+        "ruamel.yaml",
+        "requests",
+        "gitpython",
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+    python_requires=">=3.8"
+)
