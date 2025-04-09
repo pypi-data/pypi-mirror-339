4587cae52af00e1452a966b4c14b781c6921169a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-09 18:05:30 +0000

    Add package compression-cache/0.0.15/compression_cache-0.0.15.tar.gz

---
+++ packages/compression-cache/compression_cache-0.0.15.tar.gz/compression_cache-0.0.15/setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+from setuptools import find_packages, setup
+
+
+PROJECT_URL = "https://github.com/AMarsel2551/compression-cache"
+
+
+def read_readme():
+    with open(os.path.join(os.path.dirname(__file__), 'README.md'), encoding='utf-8') as f:
+        return f.read()
+
+
+def read_requirements():
+    install_requires = []
+    with open(os.path.join(os.path.dirname(__file__), 'requirements.txt'), encoding='utf-8') as f:
+        install_requires = f.read().splitlines()
+    print(f"install_requires: {install_requires}")
+    return install_requires
+
+
+setup(
+    name="compression-cache",
+    version="0.0.15",
+    packages=find_packages(),
+    description='Python function caching with compression',
+    long_description=read_readme(),
+    long_description_content_type="text/markdown",
+    install_requires=read_requirements(),
+    author_email="m.adbullinn@gmail.com",
+    zip_safe=False,
+    url=PROJECT_URL,
+    project_urls={
+        "Source Code": PROJECT_URL + ".git",
+        "Documentation": PROJECT_URL + "/wiki",
+    },
+)
