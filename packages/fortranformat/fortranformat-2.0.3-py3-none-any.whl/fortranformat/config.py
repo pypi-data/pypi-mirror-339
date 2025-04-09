2bfeea337fc082ed062566468e62b6ce1f2e8695
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-09 14:05:34 +0000

    Add package fortranformat/2.0.3/fortranformat-2.0.3-py3-none-any.whl

---
+++ packages/fortranformat/fortranformat-2.0.3-py3-none-any.whl/fortranformat/config.py
@@ -0,0 +1,57 @@
+# Should all edit descriptor values be returned even if they were not
+# written to?
+RET_WRITTEN_VARS_ONLY = False
+# Should 'None' values be returned when no record is available to read
+# from or the FORTRAN 'default'?
+RET_UNWRITTEN_VARS_NONE = True
+# The order in which edit descriptors are tried by default when G edit
+# descriptor encountered on input
+G_INPUT_TRIAL_EDS = ['F', 'L', 'A']
+# Contrary to specification, many compilers allow zero width edit
+# descriptors
+ALLOW_ZERO_WIDTH_EDS = True
+# Set the characters that separate the records
+RECORD_SEPARATOR = '\n'
+# Set this to emulate FORTRAN overflow behaviour in Z and I edit descriptors. None means no overflow, 2**31 for 32bit signed int
+PROC_MAXINT = 2**31
+# Processor dependent default for including leading plus or not
+PROC_INCL_PLUS = False
+# Option to allow signed binary, octal and hex on input (not a FORTRAN feature)
+PROC_ALLOW_NEG_BOZ = False
+# Processor dependent padding character
+PROC_PAD_CHAR = ' '
+# Interpret blanks or just a negative as a zero, as in ifort behaviour
+PROC_NEG_AS_ZERO = True
+# Show a sign for zero?
+PROC_SIGN_ZERO = False
+PROC_MIN_FIELD_WIDTH = 46
+PROC_DECIMAL_CHAR = '.'
+G0_NO_BLANKS = False
+PROC_NO_LEADING_BLANK = False
+# The default value if BN, BZ edit descriptors are not specified
+PROC_BLANKS_AS_ZEROS = False
+
+
+def reset():
+    global RET_WRITTEN_VARS_ONLY, RET_UNWRITTEN_VARS_NONE, PROC_INCL_PLUS, \
+        PROC_ALLOW_NEG_BOZ, PROC_PAD_CHAR, PROC_NEG_AS_ZERO, PROC_SIGN_ZERO, \
+        PROC_MIN_FIELD_WIDTH, PROC_DECIMAL_CHAR, G0_NO_BLANKS, \
+        PROC_NO_LEADING_BLANK, PROC_BLANKS_AS_ZEROS, PROC_MAXINT, G_INPUT_TRIAL_EDS, \
+        ALLOW_ZERO_WIDTH_EDS, RECORD_SEPARATOR
+    G_INPUT_TRIAL_EDS = ['F', 'L', 'A']
+
+    PROC_MAXINT = 2**31
+    RET_WRITTEN_VARS_ONLY = False
+    RET_UNWRITTEN_VARS_NONE = True
+    PROC_INCL_PLUS = False
+    PROC_ALLOW_NEG_BOZ = False
+    PROC_PAD_CHAR = ' '
+    PROC_NEG_AS_ZERO = True
+    PROC_SIGN_ZERO = False
+    PROC_MIN_FIELD_WIDTH = 46
+    PROC_DECIMAL_CHAR = '.'
+    G0_NO_BLANKS = False
+    PROC_NO_LEADING_BLANK = False
+    PROC_BLANKS_AS_ZEROS = False
+    ALLOW_ZERO_WIDTH_EDS = True
+    RECORD_SEPARATOR = '\n'
