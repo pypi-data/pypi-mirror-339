3a2e2ffed3aff4dbafa196d09c5a2085bc37dcb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-09 04:05:44 +0000

    Add package mibiscreen/0.4.0/mibiscreen-0.4.0-py3-none-any.whl

---
+++ packages/mibiscreen/mibiscreen-0.4.0-py3-none-any.whl/mibiscreen/__init__.py
@@ -0,0 +1,8 @@
+"""Documentation about mibiscreen."""
+import logging
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+__author__ = "Alraune Zech"
+__email__ = "a.zech@uu.nl"
+__version__ = "0.4.0"
