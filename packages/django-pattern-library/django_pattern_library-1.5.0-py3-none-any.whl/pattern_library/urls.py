e013879a3dc2b9f39c9ffc17cee940e96a3e629c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-09 05:06:46 +0000

    Add package django-pattern-library/1.5.0/django_pattern_library-1.5.0-py3-none-any.whl

---
+++ packages/django-pattern-library/django_pattern_library-1.5.0-py3-none-any.whl/pattern_library/urls.py
@@ -0,0 +1,24 @@
+from django.urls import path, re_path
+
+from pattern_library import get_pattern_template_suffix, views
+
+app_name = "pattern_library"
+urlpatterns = [
+    # UI
+    re_path(r"^$", views.IndexView.as_view(), name="index"),
+    re_path(
+        r"^pattern/(?P<pattern_template_name>[\w./\-\\]+%s)$"
+        % (get_pattern_template_suffix()),
+        views.IndexView.as_view(),
+        name="display_pattern",
+    ),
+    # iframe rendering
+    re_path(
+        r"^render-pattern/(?P<pattern_template_name>[\w./\-\\]+%s)$"
+        % (get_pattern_template_suffix()),
+        views.RenderPatternView.as_view(),
+        name="render_pattern",
+    ),
+    # API rendering
+    path("api/v1/render-pattern", views.render_pattern_api, name="render_pattern_api"),
+]
