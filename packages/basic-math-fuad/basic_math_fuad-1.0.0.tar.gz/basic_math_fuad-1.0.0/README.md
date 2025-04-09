9e37d39e51260cd0f4b91481ed6d180e029bb976
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-09 17:05:31 +0000

    Add package basic-math-fuad/1.0.0/basic_math_fuad-1.0.0.tar.gz

---
+++ packages/basic-math-fuad/basic_math_fuad-1.0.0.tar.gz/basic_math_fuad-1.0.0/README.md
@@ -0,0 +1,19 @@
+# Basic Math Library
+
+This is a custom Python module for basic and advanced math operations.
+
+## Features:
+- Basic operations (add, subtract, multiply, divide)
+- Power and roots (sqrt, cbrt, power)
+- Factorial
+- Average calculation
+
+## Installation:
+To install the library, use pip:
+
+```bash
+pip install basic-math-fuad
+```
+
+## License:
+MIT License
---
+++ packages/basic-math-fuad/basic_math_fuad-1.0.0.tar.gz/basic_math_fuad-1.0.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="basic-math-fuad",
+    version="1.0.0",
+    packages=find_packages(),
+    description="A powerful basic math library by Fuad",
+    author="Mubtasim Fuad",
+    author_email="mubtasimfad64@gmail.com",
+    url="https://github.com/yourusername/basic-math-fuad",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
