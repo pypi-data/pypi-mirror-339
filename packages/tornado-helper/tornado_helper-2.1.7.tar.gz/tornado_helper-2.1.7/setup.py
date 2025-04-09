b32613435a7ccba343ed7f72ea5782bd6a95bc7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-09 14:06:27 +0000

    Add package tornado-helper/2.1.7/tornado_helper-2.1.7.tar.gz

---
+++ packages/tornado-helper/tornado_helper-2.1.7.tar.gz/tornado_helper-2.1.7/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tornado_helper",
+    version="2.1.7",
+    packages=find_packages(),
+    install_requires=[
+        "requests",
+        "b2sdk",
+        "python-dotenv",
+        "aria2p",
+        "tqdm",
+        "boto3",
+        "pandas",
+        "pyproj",
+        "netcdf4",
+        "xarray",
+        "folium"
+    ],
+    author="August Meyers",
+    description="A helper package for uploading and downloading Tornado training data",
+    url="https://github.com/meyersa/tornado_helper",
+)
