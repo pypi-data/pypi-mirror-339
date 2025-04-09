ea64f4b4794c196477d0d4b6c3cf401555ecf328
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-09 04:05:12 +0000

    Add package parallel-brain/2.0.2/parallel-brain-2.0.2.tar.gz

---
+++ packages/parallel-brain/parallel-brain-2.0.2.tar.gz/parallel-brain-2.0.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='parallel-brain',
+    version='2.0.2',
+    author='Darshan U P',
+    author_email='darshandileep4@gmail.com',
+    description='ParallelBrain ANN with Adaptive Inhibition and Sparse Connectivity',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/yourusername/parallel-brain',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
