b5d1760b0f9c2a2b801457931dbe9177818c3480
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-09 04:05:14 +0000

    Add package meteofetch/0.4/meteofetch-0.4.tar.gz

---
+++ packages/meteofetch/meteofetch-0.4.tar.gz/meteofetch-0.4/meteofetch/__init__.py
@@ -0,0 +1,25 @@
+from ._arome import (
+    Arome001,
+    Arome0025,
+    AromeOutreMerAntilles,
+    AromeOutreMerGuyane,
+    AromeOutreMerIndien,
+    AromeOutreMerNouvelleCaledonie,
+    AromeOutreMerPolynesie,
+)
+from ._arpege import Arpege01, Arpege025
+from ._misc import set_grib_defs, set_test_mode
+
+__all__ = [
+    "Arome001",
+    "Arome0025",
+    "AromeOutreMerAntilles",
+    "AromeOutreMerGuyane",
+    "AromeOutreMerIndien",
+    "AromeOutreMerNouvelleCaledonie",
+    "AromeOutreMerPolynesie",
+    "Arpege01",
+    "Arpege025",
+    "set_grib_defs",
+    "set_test_mode",
+]
