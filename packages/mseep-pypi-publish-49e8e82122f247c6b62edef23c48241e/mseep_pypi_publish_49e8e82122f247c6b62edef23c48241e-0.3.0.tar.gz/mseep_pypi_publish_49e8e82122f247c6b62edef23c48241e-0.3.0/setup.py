e71d5f9b0b4a27786c06db1024986e2f3989fe73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-09 14:06:08 +0000

    Add package mseep-pypi-publish-49e8e82122f247c6b62edef23c48241e/0.3.0/mseep_pypi_publish_49e8e82122f247c6b62edef23c48241e-0.3.0.tar.gz

---
+++ packages/mseep-pypi-publish-49e8e82122f247c6b62edef23c48241e/mseep_pypi_publish_49e8e82122f247c6b62edef23c48241e-0.3.0.tar.gz/mseep_pypi_publish_49e8e82122f247c6b62edef23c48241e-0.3.0/setup.py
@@ -0,0 +1,20 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="mseep-pypi_publish_49e8e82122f247c6b62edef23c48241e",
+    version="0.1.0",
+    description="A package republished under mseep",
+    author="mseep",
+    author_email="mseep@example.com",
+    url="https://github.com/mseep",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=[],
+    keywords=["mseep"] + [],
+)
