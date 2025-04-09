03a0218aac71df12378922b6112a94f31d6d46bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-09 18:05:53 +0000

    Add package dj-waanverse-auth/1.0.4/dj_waanverse_auth-1.0.4-py3-none-any.whl

---
+++ packages/dj-waanverse-auth/dj_waanverse_auth-1.0.4-py3-none-any.whl/dj_waanverse_auth/utils/email_utils.py
@@ -0,0 +1,25 @@
+from dj_waanverse_auth import settings as auth_config
+from dj_waanverse_auth.services.email_service import EmailService
+from dj_waanverse_auth.utils.security_utils import (
+    get_device,
+    get_ip_address,
+    get_location_from_ip,
+)
+
+
+def send_login_email(request, user):
+    if user.email_address:
+        email_manager = EmailService(request=request)
+        template_name = "emails/login_alert.html"
+        ip_address = get_ip_address(request)
+        context = {
+            "ip_address": ip_address,
+            "location": get_location_from_ip(ip_address),
+            "device": get_device(request),
+        }
+        email_manager.send_email(
+            subject=auth_config.login_alert_email_subject,
+            template_name=template_name,
+            recipient=user.email_address,
+            context=context,
+        )
