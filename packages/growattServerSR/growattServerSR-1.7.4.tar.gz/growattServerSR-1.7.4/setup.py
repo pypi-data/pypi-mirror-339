41c1035fca9c513c6f26135c8e3288cc11b2748a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-09 07:05:29 +0000

    Add package growattServerSR/1.7.4/growattServerSR-1.7.4.tar.gz

---
+++ packages/growattServerSR/growattServerSR-1.7.4.tar.gz/growattServerSR-1.7.4/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+import os
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="growattServerSR",
+    version="1.7.4",
+    author="Simon Rankine",
+    author_email="simon@srankine.uk",
+    description="A package to talk to growatt server - forked from the original by @indykoning",
+    license="MIT",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/simonrankine/PyPi_GrowattServer",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "requests",
+    ],
+)
