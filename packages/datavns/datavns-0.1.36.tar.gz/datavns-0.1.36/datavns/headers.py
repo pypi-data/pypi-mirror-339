5e9bc6b49939d6fb9d2e47feccf6fb0bd3eaa340
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-09 16:05:24 +0000

    Add package datavns/0.1.36/datavns-0.1.36.tar.gz

---
+++ packages/datavns/datavns-0.1.36.tar.gz/datavns-0.1.36/datavns/headers.py
@@ -0,0 +1,40 @@
+
+
+class headers_list:
+    cafef_header =  {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36'
+    }
+
+
+
+    fireant_header = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36',
+        'Origin':'https://fireant.vn',
+        'Referer':'https://fireant.vn/',
+        'Authorization':'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6IkdYdExONzViZlZQakdvNERWdjV4QkRITHpnSSIsImtpZCI6IkdYdExONzViZlZQakdvNERWdjV4QkRITHpnSSJ9.eyJpc3MiOiJodHRwczovL2FjY291bnRzLmZpcmVhbnQudm4iLCJhdWQiOiJodHRwczovL2FjY291bnRzLmZpcmVhbnQudm4vcmVzb3VyY2VzIiwiZXhwIjoxODg5NjIyNTMwLCJuYmYiOjE1ODk2MjI1MzAsImNsaWVudF9pZCI6ImZpcmVhbnQudHJhZGVzdGF0aW9uIiwic2NvcGUiOlsiYWNhZGVteS1yZWFkIiwiYWNhZGVteS13cml0ZSIsImFjY291bnRzLXJlYWQiLCJhY2NvdW50cy13cml0ZSIsImJsb2ctcmVhZCIsImNvbXBhbmllcy1yZWFkIiwiZmluYW5jZS1yZWFkIiwiaW5kaXZpZHVhbHMtcmVhZCIsImludmVzdG9wZWRpYS1yZWFkIiwib3JkZXJzLXJlYWQiLCJvcmRlcnMtd3JpdGUiLCJwb3N0cy1yZWFkIiwicG9zdHMtd3JpdGUiLCJzZWFyY2giLCJzeW1ib2xzLXJlYWQiLCJ1c2VyLWRhdGEtcmVhZCIsInVzZXItZGF0YS13cml0ZSIsInVzZXJzLXJlYWQiXSwianRpIjoiMjYxYTZhYWQ2MTQ5Njk1ZmJiYzcwODM5MjM0Njc1NWQifQ.dA5-HVzWv-BRfEiAd24uNBiBxASO-PAyWeWESovZm_hj4aXMAZA1-bWNZeXt88dqogo18AwpDQ-h6gefLPdZSFrG5umC1dVWaeYvUnGm62g4XS29fj6p01dhKNNqrsu5KrhnhdnKYVv9VdmbmqDfWR8wDgglk5cJFqalzq6dJWJInFQEPmUs9BW_Zs8tQDn-i5r4tYq2U8vCdqptXoM7YgPllXaPVDeccC9QNu2Xlp9WUvoROzoQXg25lFub1IYkTrM66gJ6t9fJRZToewCt495WNEOQFa_rwLCZ1QwzvL0iYkONHS_jZ0BOhBCdW9dWSawD6iF1SIQaFROvMDH1rg'
+    }
+
+
+    fmarket_headers = {
+        "Authorization": "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJidWl0cnVvbmdhbmFkQGdtYWlsLmNvbSIsImF1ZGllbmNlIjoid2ViIiwiY3JlYXRlZCI6MTcxNzg1MTQyODE5NCwidXNlcnR5cGUiOiJJTlZFU1RPUiIsImV4cCI6MTcxNzg1ODYyOCwic3ViZm9hY2NvdW50IjpudWxsfQ.4KpCJ_gPoUBM9CFKMhcuM4oOLToztRoErE5h0u9ymAzdtrWSNTK-JE_yovEo7mc7Q675tpePk7u_SxcVQeuHSQ",
+        "Content-Type": "application/json",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36 Edg/125.0.0.0"
+    }
+
+    cbonds_headers = {
+        "CP-TOKEN": "CfDJ8PdG7etAbaxFiQmMzHBRJ_2sVlLjgLxmoDfJYkf4DjW5bvnDJ0jm-UsdJfuS_hLAbVTiQZ6psIzlBXVnVRyOdnpSEnchf4U3itTQaXkg_SWahIllAkNVKSTpv8oCL2MHaaU5QdRL5O4XLxBstm040Mw",
+        "Cookie": "CP-TOKEN-COOKIE=CfDJ8PdG7etAbaxFiQmMzHBRJ_28XYX8SzsfoJ67GJY4CpOXn0po4J_zwHwacvfZGnL6YMsmSjVkPXSeTwdIvfm4leybvQZlFf_PArnsx2G_XyFPV2-xh29T4u6MvCspQOkBPe96tjg9GSN9O7JX4AoO5sQ",
+    }
+
+    yahoo_headers = {
+    "User-Agent": ("Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+            "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36 Edg/134.0.0.0")
+    }
+
+    ssi_headers = {
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36 Edg/134.0.0.0",
+        "x-fiin-key": "KEY",
+        "x-fiin-seed": "SEED",
+        "x-fiin-user-id": "ID",
+        "x-fiin-user-token": "68,147,4,12,109,76,113,14,200,156,164,206,18,70,53,104,218,243,181,101,193,24,133,16,34,35,101,8,187,0,222,65,161,143,68,56,127,156,14,165,107,50,161,163,61,118,229,30,60,38,22,181,137,173,137,74,237,136,117,242,187,49,80,158,68,162,243,158,84,147,240,96,50,128,16,102,214,228,248,40,248,101,161,202,248,50,225,208,254,13,88,14,18,223,149,178,219,199,214,6,89,140,215,240,206,255,77,243,247,6,187,189,91,68,14,129,220,152,28,77,91,211,240,151,4,76,5,76,147,110,79,26,118,38,16,171,37,70,209,187,87,139,255,215,46,80,185,84,103,45,133,98,8,14,48,11,31,164,141,147,248,217,213,156,96,69,39,59,152,182,195,101,160,74,173,189,21,73,28,83,176,182,13,117,125,106,35,125,50,49,51,113,21,99,5,165,20,127,104,248,149,9,139,234,166,190,87,153,53,255,20,30,73,45,197,221,194,47,5,132,102,48,53,114,133,108,176,98,85,164,138,6,81,32,83,5,188,71,6,251,98,22,92,35,121,26,28,190,226,82,122,20,23,117,126,180"
+    }
