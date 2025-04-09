87834662e9562e882ec3a30cef1860af23e85ecb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-09 16:05:14 +0000

    Add package det3d/0.0.4/det3d-0.0.4.tar.gz

---
+++ packages/det3d/det3d-0.0.4.tar.gz/det3d-0.0.4/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+with open("readme.md", "r") as readme_file:
+    readme = readme_file.read()
+
+requirements = [
+    "torch==1.8.0",
+    'numpy==1.19.2',
+    ] 
+
+setup(
+    name="det3d",
+    version="0.0.4",
+    author="Tao Xiang",
+    author_email="xiang.tao@outlook.de",
+    description="A package of 3D object detection models",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/leoxiang66/3D-Detection",
+    packages=find_packages(),
+    install_requires=requirements,
+    classifiers=[
+  "Programming Language :: Python :: 3",
+  "License :: OSI Approved :: MIT License",
+    ],
+)
