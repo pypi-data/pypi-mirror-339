669bd278307a20a7ce8b8952466d38fa2b2c6b04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-09 07:05:08 +0000

    Add package science-museum-mcp/0.1.26/science_museum_mcp-0.1.26-py3-none-any.whl

---
+++ packages/science-museum-mcp/science_museum_mcp-0.1.26-py3-none-any.whl/science_museum_mcp/constants.py
@@ -0,0 +1,8 @@
+LOGGER_NAME = "ScienceMuseumMCPServer"
+SEARCH_ALL_PATH = "search"
+SEARCH_OBJECTS_PATH = "search/objects"
+SEARCH_PEOPLE_PATH = "search/people"
+SEARCH_DOCUMENTS_PATH = "search/documents"
+
+DEFAULT_LIMIT = 100
+DEFAULT_OFFSET = 0
