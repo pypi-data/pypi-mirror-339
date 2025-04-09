f78033bb069a84d0cc71a084753c779527de1c80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-09 16:05:57 +0000

    Add package pyenumerable/1.1.3/pyenumerable-1.1.3.tar.gz

---
+++ packages/pyenumerable/pyenumerable-1.1.3.tar.gz/pyenumerable-1.1.3/pyenumerable/__init__.py
@@ -0,0 +1,14 @@
+"""
+Implementation of .NET's IEnumerable interface in python W/ support for generics.
+"""  # noqa: E501
+
+from pyenumerable.constructors import pp_enumerable
+from pyenumerable.implementations import PurePythonEnumerable
+from pyenumerable.protocol import Enumerable
+
+__all__ = ["Enumerable", "PurePythonEnumerable", "pp_enumerable"]
+__author__ = "AmirHossein Ahmadi"
+__license__ = "WTFPL"
+__version__ = "1.1.2"
+__maintainer__ = "AmirHossein Ahmadi"
+__email__ = "amirthehossein@gmail.com"
