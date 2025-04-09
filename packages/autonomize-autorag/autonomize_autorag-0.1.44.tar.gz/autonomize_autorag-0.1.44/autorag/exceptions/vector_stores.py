e1f21a7c417fa2a54335f078c189118ae7bbaf42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-09 03:17:40 +0000

    Add package autonomize-autorag/0.1.44/autonomize_autorag-0.1.44.tar.gz

---
+++ packages/autonomize-autorag/autonomize_autorag-0.1.44.tar.gz/autonomize_autorag-0.1.44/autorag/exceptions/vector_stores.py
@@ -0,0 +1,17 @@
+"""Exceptions for vector stores."""
+
+
+class VectorStoreException(Exception):
+    """Base exception for vector store errors."""
+
+
+class VectorStoreCollectionNotFoundException(VectorStoreException):
+    """Exception raised when the collection name is not found in the vector store."""
+
+
+class VectorStoreCollectionAlreadyExistsException(VectorStoreException):
+    """Exception raised when the collection name already exists in the vector store."""
+
+
+class VectorStoreDataMismatchException(VectorStoreException):
+    """Exception raised when the data in the vector store does not match the expected data."""
