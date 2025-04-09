c2f1e578d421396430709d86962543b827a3c0bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-09 03:17:39 +0000

    Add package python-datamodel/0.10.17/python_datamodel-0.10.17-cp310-cp310-musllinux_1_2_x86_64.whl

---
+++ packages/python-datamodel/python_datamodel-0.10.17-cp310-cp310-musllinux_1_2_x86_64.whl/datamodel/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""DataModels.
+
+DataModel is a reimplementation of dataclasses with true inheritance and composition.
+"""
+from datamodel.fields import Field, Column, fields
+from .models import Model
+from .base import BaseModel
+from .exceptions import ValidationError
+from .version import (
+    __title__, __description__, __version__, __author__, __author_email__
+)
+
+__all__ = ('fields', 'Field', 'Column', 'Model', 'BaseModel', 'ValidationError', )
