70cfd3f1910375b34c6555577e131c1eccbbae72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-09 05:05:55 +0000

    Add package gitbase/0.5.8/gitbase-0.5.8.tar.gz

---
+++ packages/gitbase/gitbase-0.5.8.tar.gz/gitbase-0.5.8/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="gitbase",
+    version="0.5.8",
+    author="Taireru LLC",
+    author_email="tairerullc@gmail.com",
+    description="A GitHub-based database system ('GitBase') offering offline backups and optional encryption.",
+    long_description=open("README.md", "r", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/TaireruLLC/gitbase",
+    packages=find_packages(),
+    install_requires=[
+        "requests",
+        "cryptography",
+        "altcolor>=0.0.5",
+        "moviepy",
+        "fancyutil>=0.0.4",
+        "numpy",
+        "opencv-python",
+        "pyaudio",
+        "wave"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    license="MIT",
+)
