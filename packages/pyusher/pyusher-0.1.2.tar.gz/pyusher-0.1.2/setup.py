adfd88363f1fd958ef4179ac59e04b1045628b7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-09 16:05:53 +0000

    Add package pyusher/0.1.2/pyusher-0.1.2.tar.gz

---
+++ packages/pyusher/pyusher-0.1.2.tar.gz/pyusher-0.1.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pyusher",
+    version="0.1.2",
+    packages=find_packages(),  # This will include the package directory named agent_server
+    install_requires=[
+        "fastapi",
+        "structlog",
+        "uvicorn"
+    ]
+)
