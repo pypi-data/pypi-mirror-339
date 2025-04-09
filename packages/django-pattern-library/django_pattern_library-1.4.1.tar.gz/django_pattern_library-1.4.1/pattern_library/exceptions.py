f131080a95511a783603f060c2be312fcfd59d58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-09 05:05:06 +0000

    Add package django-pattern-library/1.4.1/django_pattern_library-1.4.1.tar.gz

---
+++ packages/django-pattern-library/django_pattern_library-1.4.1.tar.gz/django_pattern_library-1.4.1/pattern_library/exceptions.py
@@ -0,0 +1,10 @@
+class PatternLibraryException(Exception):
+    pass
+
+
+class TemplateIsNotPattern(PatternLibraryException):
+    pass
+
+
+class PatternLibraryEmpty(PatternLibraryException):
+    pass
