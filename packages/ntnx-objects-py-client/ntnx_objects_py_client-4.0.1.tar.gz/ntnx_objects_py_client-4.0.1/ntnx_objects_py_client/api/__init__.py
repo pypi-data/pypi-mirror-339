47366d8050c3857e19ee0d86787a11bfe0f40317
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-09 06:06:05 +0000

    Add package ntnx-objects-py-client/4.0.1/ntnx_objects_py_client-4.0.1.tar.gz

---
+++ packages/ntnx-objects-py-client/ntnx_objects_py_client-4.0.1.tar.gz/ntnx_objects_py_client-4.0.1/ntnx_objects_py_client/api/__init__.py
@@ -0,0 +1,8 @@
+
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from ntnx_objects_py_client.api.object_stores_api import ObjectStoresApi
+from ntnx_objects_py_client.api.stats_api import StatsApi
