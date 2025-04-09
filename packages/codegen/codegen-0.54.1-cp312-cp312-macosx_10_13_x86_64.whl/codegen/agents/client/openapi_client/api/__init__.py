18c46f8b5a6fec7872eab8af94163fdd29858935
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-09 07:05:33 +0000

    Add package codegen/0.54.1/codegen-0.54.1-cp312-cp312-macosx_10_13_x86_64.whl

---
+++ packages/codegen/codegen-0.54.1-cp312-cp312-macosx_10_13_x86_64.whl/codegen/agents/client/openapi_client/api/__init__.py
@@ -0,0 +1,6 @@
+# flake8: noqa
+
+# import apis into api package
+from codegen.agents.client.openapi_client.api.agents_api import AgentsApi
+from codegen.agents.client.openapi_client.api.organizations_api import OrganizationsApi
+from codegen.agents.client.openapi_client.api.users_api import UsersApi
