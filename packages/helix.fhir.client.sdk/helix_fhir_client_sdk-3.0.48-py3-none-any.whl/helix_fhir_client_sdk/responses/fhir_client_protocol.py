e5fdb2bc0225274fb2ece83678e48f0306411669
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-09 18:05:42 +0000

    Add package helix.fhir.client.sdk/3.0.48/helix_fhir_client_sdk-3.0.48-py3-none-any.whl

---
+++ packages/helix.fhir.client.sdk/helix_fhir_client_sdk-3.0.48-py3-none-any.whl/helix_fhir_client_sdk/responses/fhir_client_protocol.py
@@ -0,0 +1,188 @@
+import uuid
+from datetime import datetime
+from logging import Logger
+from threading import Lock
+from typing import (
+    Protocol,
+    Optional,
+    Dict,
+    Any,
+    List,
+    Union,
+    AsyncGenerator,
+    runtime_checkable,
+)
+
+from aiohttp import ClientSession
+from requests.adapters import BaseAdapter
+
+from helix_fhir_client_sdk.filters.base_filter import BaseFilter
+from helix_fhir_client_sdk.filters.sort_field import SortField
+from helix_fhir_client_sdk.function_types import (
+    RefreshTokenFunction,
+    HandleStreamingChunkFunction,
+    TraceRequestFunction,
+)
+from helix_fhir_client_sdk.loggers.fhir_logger import FhirLogger
+from helix_fhir_client_sdk.responses.fhir_get_response import FhirGetResponse
+from helix_fhir_client_sdk.structures.get_access_token_result import (
+    GetAccessTokenResult,
+)
+from helix_fhir_client_sdk.utilities.retryable_aiohttp_client import (
+    RetryableAioHttpClient,
+)
+from helix_fhir_client_sdk.utilities.retryable_aiohttp_response import (
+    RetryableAioHttpResponse,
+)
+from helix_fhir_client_sdk.well_known_configuration import (
+    WellKnownConfigurationCacheEntry,
+)
+
+
+@runtime_checkable
+class FhirClientProtocol(Protocol):
+    _action: Optional[str]
+    _action_payload: Optional[Dict[str, Any]]
+    _resource: Optional[str]
+    _id: Optional[Union[List[str], str]]
+    _url: Optional[str]
+    _additional_parameters: Optional[List[str]]
+    _filter_by_resource: Optional[str]
+    _filter_parameter: Optional[str]
+    _include_only_properties: Optional[List[str]]
+    _page_number: Optional[int]
+    _page_size: Optional[int]
+    _last_updated_after: Optional[datetime]
+    _last_updated_before: Optional[datetime]
+    _sort_fields: Optional[List[SortField]]
+    _auth_server_url: Optional[str]
+    _auth_wellknown_url: Optional[str]
+    _auth_scopes: Optional[List[str]]
+    _login_token: Optional[str]
+    _client_id: Optional[str]
+    _access_token: Optional[str]
+    _access_token_expiry_date: Optional[datetime]
+    _logger: Optional[FhirLogger]
+    _internal_logger: Logger
+    _adapter: Optional[BaseAdapter]
+    _limit: Optional[int]
+    _validation_server_url: Optional[str]
+    _separate_bundle_resources: bool
+    _obj_id: Optional[str]
+    _include_total: bool
+    _filters: List[BaseFilter]
+    _expand_fhir_bundle: bool
+
+    _stop_processing: bool = False
+    _last_page: Optional[int]
+
+    _use_data_streaming: bool = False
+    _send_data_as_chunked: bool = False
+    _last_page_lock: Lock
+
+    _use_post_for_search: bool = False
+
+    _accept: str
+    _content_type: str
+    _additional_request_headers: Dict[str, str]
+    _accept_encoding: str
+
+    _maximum_time_to_retry_on_429: int
+
+    _extra_context_to_return: Optional[Dict[str, Any]]
+
+    _retry_count: int
+    _exclude_status_codes_from_retry: Optional[List[int]]
+
+    _uuid: uuid.UUID
+    _log_level: Optional[str]
+    # default to built-in function to refresh token
+    _refresh_token_function: RefreshTokenFunction
+    _trace_request_function: Optional[TraceRequestFunction]
+    _chunk_size: int
+    _time_to_live_in_secs_for_cache: int
+    _well_known_configuration_cache_lock: Lock
+
+    _well_known_configuration_cache: Dict[str, WellKnownConfigurationCacheEntry]
+
+    _compress: bool
+
+    _throw_exception_on_error: bool
+
+    _log_all_response_urls: bool
+
+    async def get_access_token_async(self) -> GetAccessTokenResult: ...
+
+    async def _send_fhir_request_async(
+        self,
+        *,
+        client: RetryableAioHttpClient,
+        full_url: str,
+        headers: Dict[str, str],
+        payload: Dict[str, Any] | None,
+    ) -> RetryableAioHttpResponse: ...
+
+    def create_http_session(self) -> ClientSession: ...
+
+    async def _get_with_session_async(
+        self,
+        *,
+        page_number: Optional[int],
+        ids: Optional[List[str]],
+        id_above: Optional[str],
+        fn_handle_streaming_chunk: Optional[HandleStreamingChunkFunction],
+        additional_parameters: Optional[List[str]],
+        resource_type: Optional[str],
+    ) -> AsyncGenerator[FhirGetResponse, None]: ...
+
+    def separate_bundle_resources(self, separate_bundle_resources: bool):  # type: ignore[no-untyped-def]
+        ...
+
+    def resource(self, resource: str):  # type: ignore[no-untyped-def]
+        ...
+
+    def set_access_token(self, value: str | None) -> "FhirClientProtocol": ...
+
+    def set_access_token_expiry_date(
+        self, value: datetime | None
+    ) -> "FhirClientProtocol": ...
+
+    def include_only_properties(
+        self, include_only_properties: List[str] | None
+    ) -> "FhirClientProtocol": ...
+
+    def page_size(self, page_size: int) -> "FhirClientProtocol": ...
+
+    def filter(self, filter_: List[BaseFilter]) -> "FhirClientProtocol": ...
+
+    def clone(self) -> "FhirClientProtocol": ...
+
+    def last_page(self, last_page: int) -> "FhirClientProtocol": ...
+
+    def page_number(self, page_number: int) -> "FhirClientProtocol": ...
+
+    def id_(self, id_: Union[List[str], str] | None) -> "FhirClientProtocol": ...
+
+    def additional_parameters(
+        self, additional_parameters: List[str]
+    ) -> "FhirClientProtocol": ...
+
+    def action_payload(
+        self, action_payload: Dict[str, Any]
+    ) -> "FhirClientProtocol": ...
+
+    def action(self, action: str) -> "FhirClientProtocol": ...
+
+    async def build_url(
+        self,
+        *,
+        additional_parameters: Optional[List[str]],
+        id_above: Optional[str],
+        ids: Optional[List[str]],
+        page_number: Optional[int],
+        resource_type: Optional[str],
+    ) -> str: ...
+
+    def throw_exception_on_error(
+        self, throw_exception_on_error: bool
+    ) -> "FhirClientProtocol": ...
---
+++ packages/helix.fhir.client.sdk/helix_fhir_client_sdk-3.0.48-py3-none-any.whl/tests/async/simulated_graph/test_fhir_simulated_graph_separate_resources_async.py
@@ -0,0 +1,261 @@
+import json
+from pathlib import Path
+from typing import Dict, Any, Optional
+
+from mockserver_client.mockserver_client import (
+    MockServerFriendlyClient,
+    mock_request,
+    mock_response,
+    times,
+)
+
+from helix_fhir_client_sdk.fhir_client import FhirClient
+
+from helix_fhir_client_sdk.responses.fhir_get_response import FhirGetResponse
+
+
+async def test_fhir_simulated_graph_async() -> None:
+    print("")
+    data_dir: Path = Path(__file__).parent.joinpath("./")
+    graph_json: Dict[str, Any]
+    with open(data_dir.joinpath("graphs").joinpath("aetna.json"), "r") as file:
+        contents = file.read()
+        graph_json = json.loads(contents)
+
+    test_name = "test_fhir_simulated_graph_async"
+
+    mock_server_url = "http://mock-server:1080"
+    mock_client: MockServerFriendlyClient = MockServerFriendlyClient(
+        base_url=mock_server_url
+    )
+
+    relative_url: str = test_name
+    absolute_url: str = mock_server_url + "/" + test_name
+
+    mock_client.clear(f"/{test_name}/*.*")
+    mock_client.reset()
+
+    response_text = {
+        "resourceType": "Patient",
+        "id": "1",
+        "generalPractitioner": [{"reference": "Practitioner/5"}],
+        "managingOrganization": {"reference": "Organization/6"},
+    }
+
+    mock_client.expect(
+        request=mock_request(path=f"/{relative_url}/Patient/1", method="GET"),
+        response=mock_response(body=response_text),
+        timing=times(1),
+    )
+
+    response_text = {"resourceType": "Practitioner", "id": "5"}
+    mock_client.expect(
+        request=mock_request(path=f"/{relative_url}/Practitioner/5", method="GET"),
+        response=mock_response(body=response_text),
+        timing=times(1),
+    )
+
+    response_text = {"resourceType": "Organization", "id": "6"}
+    mock_client.expect(
+        request=mock_request(path=f"/{relative_url}/Organization/6", method="GET"),
+        response=mock_response(body=response_text),
+        timing=times(1),
+    )
+
+    response_text = {
+        "entry": [
+            {
+                "resource": {
+                    "resourceType": "Coverage",
+                    "id": "7",
+                    "payor": [{"reference": "Organization/CoveragePayor"}],
+                }
+            }
+        ]
+    }
+    mock_client.expect(
+        request=mock_request(
+            path=f"/{relative_url}/Coverage",
+            querystring={"patient": "1"},
+            method="GET",
+        ),
+        response=mock_response(body=response_text),
+        timing=times(1),
+    )
+
+    response_text = {"resourceType": "Organization", "id": "CoveragePayor"}
+    mock_client.expect(
+        request=mock_request(
+            path=f"/{relative_url}/Organization/CoveragePayor", method="GET"
+        ),
+        response=mock_response(body=response_text),
+        timing=times(1),
+    )
+
+    response_text = {
+        "entry": [{"resource": {"resourceType": "Observation", "id": "8"}}]
+    }
+    mock_client.expect(
+        request=mock_request(
+            path=f"/{relative_url}/Observation",
+            querystring={
+                "patient": "1",
+                "category": "vital-signs,social-history,laboratory",
+            },
+            method="GET",
+        ),
+        response=mock_response(body=response_text),
+        timing=times(1),
+    )
+
+    fhir_client = FhirClient()
+
+    fhir_client = fhir_client.log_level("DEBUG")
+
+    fhir_client = fhir_client.url(absolute_url).resource("Patient")
+
+    fhir_client.extra_context_to_return({"service_slug": "medstar"})
+
+    auth_access_token = "my_access_token"
+    if auth_access_token:
+        fhir_client = fhir_client.set_access_token(auth_access_token)
+
+    response: Optional[FhirGetResponse] = await FhirGetResponse.from_async_generator(
+        fhir_client.simulate_graph_streaming_async(
+            id_="1",
+            graph_json=graph_json,
+            contained=False,
+            separate_bundle_resources=True,
+        )
+    )
+    assert response is not None
+    print(response.responses)
+
+    expected_json: Dict[str, Any] = {
+        "Patient": [
+            {
+                "resourceType": "Patient",
+                "id": "1",
+                "generalPractitioner": [{"reference": "Practitioner/5"}],
+                "managingOrganization": {"reference": "Organization/6"},
+            }
+        ],
+        "Practitioner": [{"resourceType": "Practitioner", "id": "5"}],
+        "Organization": [
+            {"resourceType": "Organization", "id": "6"},
+            {"resourceType": "Organization", "id": "CoveragePayor"},
+        ],
+        "Coverage": [
+            {
+                "resourceType": "Coverage",
+                "id": "7",
+                "payor": [{"reference": "Organization/CoveragePayor"}],
+            }
+        ],
+        "OperationOutcome": [
+            {
+                "resourceType": "OperationOutcome",
+                "issue": [
+                    {
+                        "severity": "error",
+                        "code": "not-found",
+                        "details": {
+                            "coding": [
+                                {
+                                    "system": "https://www.icanbwell.com/url",
+                                    "code": "http://mock-server:1080/test_fhir_simulated_graph_async/ExplanationOfBenefit?patient=1",
+                                },
+                                {
+                                    "system": "https://www.icanbwell.com/resourceType",
+                                    "code": "ExplanationOfBenefit",
+                                },
+                                {
+                                    "system": "https://www.icanbwell.com/statuscode",
+                                    "code": 404,
+                                },
+                                {
+                                    "system": "https://www.icanbwell.com/accessToken",
+                                    "code": "my_access_token",
+                                },
+                            ]
+                        },
+                        "diagnostics": '{"url": "http://mock-server:1080/test_fhir_simulated_graph_async/ExplanationOfBenefit?patient=1", "error": "NotFound", "status": 404, "extra_context_to_return": {"service_slug": "medstar"}, "accessToken": "my_access_token", "requestId": null, "resourceType": "ExplanationOfBenefit", "id": null}',
+                    }
+                ],
+            },
+            {
+                "resourceType": "OperationOutcome",
+                "issue": [
+                    {
+                        "severity": "error",
+                        "code": "not-found",
+                        "details": {
+                            "coding": [
+                                {
+                                    "system": "https://www.icanbwell.com/url",
+                                    "code": "http://mock-server:1080/test_fhir_simulated_graph_async/MedicationDispense?patient=1",
+                                },
+                                {
+                                    "system": "https://www.icanbwell.com/resourceType",
+                                    "code": "MedicationDispense",
+                                },
+                                {
+                                    "system": "https://www.icanbwell.com/statuscode",
+                                    "code": 404,
+                                },
+                                {
+                                    "system": "https://www.icanbwell.com/accessToken",
+                                    "code": "my_access_token",
+                                },
+                            ]
+                        },
+                        "diagnostics": '{"url": "http://mock-server:1080/test_fhir_simulated_graph_async/MedicationDispense?patient=1", "error": "NotFound", "status": 404, "extra_context_to_return": {"service_slug": "medstar"}, "accessToken": "my_access_token", "requestId": null, "resourceType": "MedicationDispense", "id": null}',
+                    }
+                ],
+            },
+            {
+                "resourceType": "OperationOutcome",
+                "issue": [
+                    {
+                        "severity": "error",
+                        "code": "not-found",
+                        "details": {
+                            "coding": [
+                                {
+                                    "system": "https://www.icanbwell.com/url",
+                                    "code": "http://mock-server:1080/test_fhir_simulated_graph_async/MedicationRequest?patient=1",
+                                },
+                                {
+                                    "system": "https://www.icanbwell.com/resourceType",
+                                    "code": "MedicationRequest",
+                                },
+                                {
+                                    "system": "https://www.icanbwell.com/statuscode",
+                                    "code": 404,
+                                },
+                                {
+                                    "system": "https://www.icanbwell.com/accessToken",
+                                    "code": "my_access_token",
+                                },
+                            ]
+                        },
+                        "diagnostics": '{"url": "http://mock-server:1080/test_fhir_simulated_graph_async/MedicationRequest?patient=1", "error": "NotFound", "status": 404, "extra_context_to_return": {"service_slug": "medstar"}, "accessToken": "my_access_token", "requestId": null, "resourceType": "MedicationRequest", "id": null}',
+                    }
+                ],
+            },
+        ],
+        "Observation": [{"resourceType": "Observation", "id": "8"}],
+    }
+
+    result = json.loads(response.responses)
+    result = dict(sorted(result.items()))
+    expected_json = dict(sorted(expected_json.items()))
+    result["OperationOutcome"] = sorted(
+        result["OperationOutcome"],
+        key=lambda x: x["issue"][0]["details"]["coding"][0]["code"],
+    )
+    expected_json["OperationOutcome"] = sorted(
+        expected_json["OperationOutcome"],
+        key=lambda x: x["issue"][0]["details"]["coding"][0]["code"],
+    )
+    assert result == expected_json
