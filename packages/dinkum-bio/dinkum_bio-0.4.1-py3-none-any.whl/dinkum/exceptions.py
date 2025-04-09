95aaffcbc5ab03a4da2084aa23121fb562073a67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-09 05:06:23 +0000

    Add package dinkum-bio/0.4.1/dinkum_bio-0.4.1-py3-none-any.whl

---
+++ packages/dinkum-bio/dinkum_bio-0.4.1-py3-none-any.whl/dinkum/exceptions.py
@@ -0,0 +1,20 @@
+class DinkumException(Exception):
+    pass
+
+class DinkumObservationFailed(DinkumException):
+    pass
+
+class DinkumInvalidGene(DinkumException):
+    pass
+
+class DinkumInvalidTissue(DinkumException):
+    pass
+
+class DinkumNoSuchGene(DinkumException):
+    pass
+
+class DinkumInvalidActivationFunction(DinkumException):
+    pass
+
+class DinkumInvalidActivationResult(DinkumException):
+    pass
