9e6a74528073804ea76802d47c124ba42dd46551
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-09 16:05:46 +0000

    Add package jcompoundmapper-pywrapper/0.0.3/jcompoundmapper_pywrapper-0.0.3.tar.gz

---
+++ packages/jcompoundmapper-pywrapper/jcompoundmapper_pywrapper-0.0.3.tar.gz/jcompoundmapper_pywrapper-0.0.3/src/jCompoundMapper_pywrapper/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8
+
+"""Wrapper for jCompoundMapper molecular fingerprints"""
+
+from .jcompoundmapper import (JCompoundMapper, Fingerprint, Fingerprints_2D, Fingerprints_3D,
+                              AtomType, FpParamType, DEFAULT_FP_PARAMETERS) # Advanced tools
+
+__version__ = "0.0.3"
