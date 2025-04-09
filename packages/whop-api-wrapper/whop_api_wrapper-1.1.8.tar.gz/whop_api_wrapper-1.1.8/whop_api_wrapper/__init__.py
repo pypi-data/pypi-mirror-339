4db17a6d47ae393473c0e5321ca78303f31f2618
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-09 16:05:54 +0000

    Add package whop-api-wrapper/1.1.8/whop_api_wrapper-1.1.8.tar.gz

---
+++ packages/whop-api-wrapper/whop_api_wrapper-1.1.8.tar.gz/whop_api_wrapper-1.1.8/whop_api_wrapper/__init__.py
@@ -0,0 +1,4 @@
+# Importing modules within the package
+from .Client import Client
+from .Objects import Product, Plan, Membership, Company, Payment, CheckoutSession, Customer, PromoCode
+from .Endpoints import Endpoints
