feb52fe2e1d4cca50252f2fbdae4e1a5d972d862
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-09 03:17:46 +0000

    Add package explicit-sql/0.0.5/explicit_sql-0.0.5-py3-none-any.whl

---
+++ packages/explicit-sql/explicit_sql-0.0.5-py3-none-any.whl/explicit_sql/__init__.py
@@ -0,0 +1,23 @@
+from explicit_sql.explicit_sql import (
+    ExplicitTable,
+    TableConfig,
+    TableDefinition,
+    create_table_sql,
+    delete_sql_by_id,
+    delete_where,
+    insert_sql,
+    select_sql,
+    update_sql_by_id,
+)
+
+__all__ = [
+    "ExplicitTable",
+    "TableConfig",
+    "TableDefinition",
+    "create_table_sql",
+    "delete_sql_by_id",
+    "delete_where",
+    "insert_sql",
+    "select_sql",
+    "update_sql_by_id",
+]
