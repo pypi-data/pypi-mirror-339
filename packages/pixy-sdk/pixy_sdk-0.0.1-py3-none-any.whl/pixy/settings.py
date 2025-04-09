7cb9c13500c98550ba6013eec034cd1739399a31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-09 06:05:51 +0000

    Add package pixy-sdk/0.0.1/pixy_sdk-0.0.1-py3-none-any.whl

---
+++ packages/pixy-sdk/pixy_sdk-0.0.1-py3-none-any.whl/pixy/settings.py
@@ -0,0 +1,20 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+from core.schemas import ImageGenProperties, VideoGenProperties, SubtitleGenProperties
+
+
+class Settings:
+
+	url_mapping = {
+		'api_key_verification' : os.getenv('PIXY_API_KEY_VERIFICATION_ENDPOINT'),
+		'image' : os.getenv('PIXY_IMAGINE_ENDPOINT'),
+		'video' : os.getenv('PIXY_VIDEOGEN_ENDPOINT'),
+		'subtitle' : os.getenv('PIXY_SUBTITLE_ENDPOINT'),
+	}
+	
+	properties_mapping = {
+		'image': ImageGenProperties,
+		'video': VideoGenProperties,
+		'subtitle': SubtitleGenProperties
+	}
