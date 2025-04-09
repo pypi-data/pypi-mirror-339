755555f81df77b0eacc21b847d77e43d61a8d8da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-09 05:05:30 +0000

    Add package python-lingualens/1.0.1/python_lingualens-1.0.1-py3-none-any.whl

---
+++ packages/python-lingualens/python_lingualens-1.0.1-py3-none-any.whl/lingualens/__init__.py
@@ -0,0 +1,24 @@
+"""
+LinguaLens - LLM-Powered Content Evaluation
+A flexible Python framework for evaluating content quality using Large Language Models (LLMs).
+"""
+
+__version__ = "1.0.1"
+
+from .evaluator.evaluator import Evaluator
+from .models.llm_manager import LLMManager, BaseLLMClient
+from .config.config_manager import config_manager
+from .utils.metrics import MetricsCalculator
+from .utils.llm_parser import LLMResponseParser
+from .evaluator.task_manager import TaskManager 
+
+__all__ = [
+    'Evaluator',
+    'LLMManager',
+    'BaseLLMClient',
+    'ConfigManager',
+    'config_manager',
+    'MetricsCalculator',
+    'LLMResponseParser',
+    'TaskManager'
+]
