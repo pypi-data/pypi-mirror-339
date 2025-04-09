41495fb9dc966a03a5c30dcad56368fabb1d31d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-09 16:05:49 +0000

    Add package cratonml/0.1.1.24/cratonml-0.1.1.24.tar.gz

---
+++ packages/cratonml/cratonml-0.1.1.24.tar.gz/cratonml-0.1.1.24/cratonml/calculate/Calculate.py
@@ -0,0 +1,41 @@
+import numpy as np
+from abc import ABC, abstractmethod
+
+
+class Calculate(ABC):
+    @staticmethod
+    @abstractmethod
+    def calculate(values):
+        pass
+
+
+class Average(Calculate):
+    @staticmethod
+    def calculate(values):
+        return np.mean(values)
+
+
+class Min(Calculate):
+    @staticmethod
+    def calculate(values):
+        return np.min(values)
+
+
+class Max(Calculate):
+    @staticmethod
+    def calculate(values):
+        return np.max(values)
+
+
+class Median(Calculate):
+    @staticmethod
+    def calculate(values):
+        sorted_values = sorted(values)
+        idx = int(len(sorted_values) // 2)
+        return sorted_values[idx]
+
+
+class Top(Calculate):
+    @staticmethod
+    def calculate(values):
+        return values[0]
