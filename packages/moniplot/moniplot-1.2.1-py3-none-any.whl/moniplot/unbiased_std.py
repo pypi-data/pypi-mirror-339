3d97d2f593918f7e9529651e4a63cce1bfa78764
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-09 05:05:57 +0000

    Add package moniplot/1.2.1/moniplot-1.2.1-py3-none-any.whl

---
+++ packages/moniplot/moniplot-1.2.1-py3-none-any.whl/moniplot/unbiased_std.py
@@ -0,0 +1,40 @@
+#
+# https://github.com/rmvanhees/moniplot.git
+#
+# Copyright (c) 2019-2025 SRON
+#    All rights reserved.
+#
+# License:  GPLv3
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+"""Calculate the unbiased estimator for the standard deviation."""
+
+from __future__ import annotations
+
+__all__ = ["unbiased_std"]
+
+from math import pi, sqrt
+
+import numpy as np
+
+# pylint: disable=no-member
+import scipy.special as sc
+
+
+def unbiased_std(data: np.ndarray) -> np.ndarray:
+    """Return the unbiased estimator for the standard deviation."""
+    nval = data.shape[0]
+    unbias = sqrt((nval - 1) / (2 * pi)) * sc.beta((nval - 1) / 2, 1 / 2)
+
+    return unbias * np.std(data, ddof=1, axis=0)
