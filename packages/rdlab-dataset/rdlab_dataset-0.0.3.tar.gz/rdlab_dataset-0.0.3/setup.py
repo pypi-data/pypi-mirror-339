98e887823c991c3140d55dda6c3ca6ec7e63017d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-09 05:06:42 +0000

    Add package rdlab-dataset/0.0.3/rdlab_dataset-0.0.3.tar.gz

---
+++ packages/rdlab-dataset/rdlab_dataset-0.0.3.tar.gz/rdlab_dataset-0.0.3/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='rdlab_dataset',
+    version='0.0.3', 
+    packages=find_packages(),
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='soyvitou',
+    author_email='soyvitoupro@gmail.com',
+    url='https://github.com/SoyVitouPro/rdlab-dataset',  # Optional
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
