48d5ea734de59cf5a85fcb1c00b4c7cf4c14cd79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-09 05:06:41 +0000

    Add package pytrnsys-process/0.0.24/pytrnsys_process-0.0.24.tar.gz

---
+++ packages/pytrnsys-process/pytrnsys_process-0.0.24.tar.gz/pytrnsys_process-0.0.24/pytrnsys_process/plot/__init__.py
@@ -0,0 +1,18 @@
+from pytrnsys_process.plot.plot_wrappers import (
+    line_plot,
+    bar_chart,
+    stacked_bar_chart,
+    histogram,
+    energy_balance,
+    scatter_plot,
+)
+
+# pylint: disable=duplicate-code
+__all__ = [
+    "line_plot",
+    "bar_chart",
+    "stacked_bar_chart",
+    "histogram",
+    "energy_balance",
+    "scatter_plot",
+]
