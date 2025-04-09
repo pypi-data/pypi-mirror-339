2d8a5026cb95d12096a70e208d40dc50fbd4f2a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-09 07:05:04 +0000

    Add package fathomnet/1.8.1/fathomnet-1.8.1.tar.gz

---
+++ packages/fathomnet/fathomnet-1.8.1.tar.gz/fathomnet-1.8.1/src/fathomnet/api/stats.py
@@ -0,0 +1,14 @@
+# stats.py (fathomnet-py)
+from typing import List
+
+from fathomnet.api import EndpointManager
+
+
+class Stats(EndpointManager):
+    PATH = "stats"
+
+
+def most_popular_searches() -> List[str]:
+    """Get a list of the most popular searches."""
+    res_json = Stats.get("list/popular/searches")
+    return res_json
