c979e779c77c1a5cb6d58959fecfc0ecac7012b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-09 16:05:24 +0000

    Add package py-tes/1.1.1/py_tes-1.1.1-py3-none-any.whl

---
+++ packages/py-tes/py_tes-1.1.1-py3-none-any.whl/tes/__init__.py
@@ -0,0 +1,39 @@
+from tes.client import HTTPClient
+from tes.utils import unmarshal
+from tes.models import (
+    Input,
+    Output,
+    Resources,
+    Executor,
+    Task,
+    ExecutorLog,
+    TaskLog,
+    OutputFileLog,
+    CreateTaskResponse,
+    GetTaskRequest,
+    ListTasksRequest,
+    ListTasksResponse,
+    ServiceInfoRequest,
+    ServiceInfo
+)
+
+__all__ = [
+    "HTTPClient",
+    "unmarshal",
+    "Input",
+    "Output",
+    "Resources",
+    "Executor",
+    "Task",
+    "ExecutorLog",
+    "TaskLog",
+    "OutputFileLog",
+    "CreateTaskResponse",
+    "GetTaskRequest",
+    "ListTasksRequest",
+    "ListTasksResponse",
+    "ServiceInfoRequest",
+    "ServiceInfo"
+]
+
+__version__ = "1.1.1"
