7925488758e4a4fcd79db4db68a83e1e8906b4a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-09 04:05:28 +0000

    Add package VocalTractLab/0.5.7/vocaltractlab-0.5.7.tar.gz

---
+++ packages/VocalTractLab/vocaltractlab-0.5.7.tar.gz/vocaltractlab-0.5.7/setup.py
@@ -0,0 +1,61 @@
+
+
+
+from setuptools import setup
+from setuptools import find_packages
+
+
+
+install_requires = [
+    'numpy',
+    'target-approximation>=0.0.4',
+    'tools-mp==0.2.0',
+    'torch',
+    'torchaudio',
+    'vocaltractlab-cython==0.0.16',
+    ]
+
+
+
+#CLASSIFIERS = """\
+#Development Status :: 3 - Alpha
+#Intended Audience :: Science/Research
+#Intended Audience :: Developers
+#License :: OSI Approved :: GNU General Public License v3 (GPLv3)
+#Programming Language :: C++
+#Programming Language :: Python
+#Programming Language :: Python :: 3
+#Programming Language :: Python :: 3.8
+#Programming Language :: Python :: 3.9
+#Programming Language :: Python :: Implementation :: CPython
+#Topic :: Software Development
+#Topic :: Scientific/Engineering
+#Typing :: Typed
+#Operating System :: Microsoft :: Windows
+#Operating System :: POSIX
+#Operating System :: Unix
+#"""
+
+setup_args = dict(
+    name='VocalTractLab',
+    version='0.5.7',
+    description='High-performance articulatory speech synthesis in Python',
+    long_description_content_type='text/markdown',
+    long_description=open('README.md').read(),
+    url='https://github.com/paul-krug/VocalTractLab-Python',
+    author='Paul Krug',
+    license='GPL-3.0',
+    #classifiers = [_f for _f in CLASSIFIERS.split('\n') if _f],
+    keywords=[
+        'text-to-speech',
+        'speech synthesis',
+        'articulatory synthesis',
+        'vocal tract',
+        'speech production',
+        'vocoder',
+        ],
+    packages=find_packages(include=["vocaltractlab", "vocaltractlab.*"]),
+    install_requires=install_requires
+)
+
+setup(**setup_args)
