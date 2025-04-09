37c9d8cea325025cbce3065da4e4b0557649ce8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-09 06:05:45 +0000

    Add package finch-api/1.20.1/finch_api-1.20.1-py3-none-any.whl

---
+++ packages/finch-api/finch_api-1.20.1-py3-none-any.whl/finch/types/hris/benefits/__init__.py
@@ -0,0 +1,13 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from .individual_benefit import IndividualBenefit as IndividualBenefit
+from .enrolled_individual import EnrolledIndividual as EnrolledIndividual
+from .unenrolled_individual import UnenrolledIndividual as UnenrolledIndividual
+from .individual_enroll_many_params import IndividualEnrollManyParams as IndividualEnrollManyParams
+from .individual_unenroll_many_params import IndividualUnenrollManyParams as IndividualUnenrollManyParams
+from .individual_enrolled_ids_response import IndividualEnrolledIDsResponse as IndividualEnrolledIDsResponse
+from .individual_retrieve_many_benefits_params import (
+    IndividualRetrieveManyBenefitsParams as IndividualRetrieveManyBenefitsParams,
+)
