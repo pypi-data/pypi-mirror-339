bf9536db4208a853f33448e6de14f11b116d23b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-09 07:05:10 +0000

    Add package staygreen/0.3.2/staygreen-0.3.2-py3-none-any.whl

---
+++ packages/staygreen/staygreen-0.3.2-py3-none-any.whl/staygreen/green.py
@@ -0,0 +1,18 @@
+import pyautogui, time
+
+
+def main():
+    count = 0
+    print("Starting")
+    while True:
+        pyautogui.moveRel(10, 0)
+        time.sleep(1)
+        pyautogui.moveRel(-10, 0)
+        pyautogui.click()
+        count += 1
+        # print(f"tick {count}")
+        time.sleep(59)
+
+
+if __name__ == "__main__":
+    main()
