5ade07c3b24cf00d8c39279b784026eceb311c98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-09 17:05:52 +0000

    Add package compression-cache/0.0.2/compression_cache-0.0.2.tar.gz

---
+++ packages/compression-cache/compression_cache-0.0.2.tar.gz/compression_cache-0.0.2/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import find_packages, setup
+
+setup(
+    name="compression-cache",
+    version="0.0.2",
+    packages=find_packages(),
+    description='Python function caching with compression',
+    install_requires=["zstandard==0.23.0"],
+    author_email="m.adbullinn@gmail.com",
+    zip_safe=False,
+    url="https://github.com/AMarsel2551/cache.git"
+)
