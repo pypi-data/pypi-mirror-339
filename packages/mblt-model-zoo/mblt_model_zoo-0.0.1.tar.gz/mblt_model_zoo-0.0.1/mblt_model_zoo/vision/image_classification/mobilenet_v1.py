ef38e9e4251488a78f3cdc6558dc9291e03f2380
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-09 17:06:04 +0000

    Add package mblt-model-zoo/0.0.1/mblt_model_zoo-0.0.1.tar.gz

---
+++ packages/mblt-model-zoo/mblt_model_zoo-0.0.1.tar.gz/mblt_model_zoo-0.0.1/mblt_model_zoo/vision/image_classification/mobilenet_v1.py
@@ -0,0 +1,42 @@
+from ..utils.types import ModelInfo, ModelInfoSet
+from ..wrapper import MBLT_Engine
+
+
+class MobileNet_V1_Set(ModelInfoSet):
+    IMAGENET1K_V1 = ModelInfo(
+        model_cfg={
+            "url": "https://dl.mobilint.com/model/image_classification/mobilenet_v1.mxq",
+        },
+        pre_cfg={
+            "Reader": {
+                "style": "pil",
+            },
+            "Resize": {
+                "size": 256,
+                "interpolation": "bilinear",
+            },
+            "CenterCrop": {
+                "size": [224, 224],
+            },
+            "Normalize": {"style": "torch"},
+            "SetOrder": {"shape": "CHW"},
+        },
+        post_cfg={
+            "task": "image_classification",
+        },
+    )
+    DEFAULT = IMAGENET1K_V1  # Default model
+
+
+class MobileNet_V1(MBLT_Engine):
+    def __init__(self, local_model: str = None, model_type: str = "DEFAULT"):
+        assert (
+            model_type in MobileNet_V1_Set.__dict__.keys()
+        ), f"Model type {model_type} not found. Available types: {MobileNet_V1_Set.__dict__.keys()}"
+        model_cfg = MobileNet_V1_Set.__dict__[model_type].value.model_cfg
+        if local_model is not None:
+            model_cfg["url"] = local_model
+
+        pre_cfg = MobileNet_V1_Set.__dict__[model_type].value.pre_cfg
+        post_cfg = MobileNet_V1_Set.__dict__[model_type].value.post_cfg
+        super().__init__(model_cfg, pre_cfg, post_cfg)
