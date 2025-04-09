a911ceae1a02aae611cbc29dd993a80471dc07d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-09 18:05:48 +0000

    Add package cursofiap-package-gothcoder/1.0.0/cursofiap_package_gothcoder-1.0.0.tar.gz

---
+++ packages/cursofiap-package-gothcoder/cursofiap_package_gothcoder-1.0.0.tar.gz/cursofiap_package_gothcoder-1.0.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='cursofiap-package-gothcoder',
+    version='1.0.0',
+    packages=find_packages(),
+    description='Descricao da sua lib cursofiap',
+    author='Victor Santos',
+    author_email='vlsoexecutivo@gmail.com',
+    url='https://github.com/goth-coder/cursofiap',  
+    license='MIT',  
+    long_description=long_description,
+    long_description_content_type='text/markdown'
+)
