03466c5bf1894bd66dd5adad361292c3b6b7c2be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-09 06:05:45 +0000

    Add package qexec/0.1.8/qexec-0.1.8.tar.gz

---
+++ packages/qexec/qexec-0.1.8.tar.gz/qexec-0.1.8/qexec/__init__.py
@@ -0,0 +1,21 @@
+from .main import (
+    config,
+    regexec,
+    llm,
+    pne,
+    Provider,
+    red
+)
+
+__all__ = [
+    
+    # Core qexec functions
+    'config',
+    'register_exec',
+    'llm',
+    'pne',
+    
+    # Classes
+    'Provider',
+    'red'
+] 
