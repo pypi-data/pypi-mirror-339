7866c718bada05083d7c0bc3b8d61f62071119c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-09 05:05:20 +0000

    Add package agi-med-protos/3.2.11/agi_med_protos-3.2.11-py3-none-any.whl

---
+++ packages/agi-med-protos/agi_med_protos-3.2.11-py3-none-any.whl/agi_med_protos/ocr_client.py
@@ -0,0 +1,30 @@
+from .abstract_client import AbstractClient
+from .DigitalAssistantOCR_pb2 import (
+    OCRType,
+    DigitalAssistantOCRRequest,
+    DigitalAssistantOCRResponse,
+)
+from .DigitalAssistantOCR_pb2_grpc import DigitalAssistantOCRStub
+
+
+class OCRClient(AbstractClient):
+    def __init__(self, address) -> None:
+        super().__init__(address)
+        self._stub = DigitalAssistantOCRStub(self._channel)
+
+    def __call__(
+        self,
+        ocr_type_: OCRType,
+        resource: bytes = None,
+        resource_id: str = None,
+        request_id: str = "",
+    ) -> str:
+        if resource is None and resource_id is None:
+            raise ValueError("Argument `resource` or `resource_id` should be passed!")
+        request = DigitalAssistantOCRRequest(
+            ResourceId=resource_id,
+            OCRType=ocr_type_,
+            RequestId=request_id
+        )
+        response: DigitalAssistantOCRResponse = self._stub.GetTextResponse(request)
+        return response.Text
