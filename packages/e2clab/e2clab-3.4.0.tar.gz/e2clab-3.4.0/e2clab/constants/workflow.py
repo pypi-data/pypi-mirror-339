0c8f8fac7dbb21331250cd54b064962e127af00a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-09 18:05:28 +0000

    Add package e2clab/3.4.0/e2clab-3.4.0.tar.gz

---
+++ packages/e2clab/e2clab-3.4.0.tar.gz/e2clab-3.4.0/e2clab/constants/workflow.py
@@ -0,0 +1,29 @@
+"""Workflow file constants"""
+
+TARGET = "hosts"
+DEPENDS_ON = "depends_on"
+SERV_SELECT = "service_selector"
+GROUPING = "grouping"
+PREFIX = "prefix"
+SELF_PREFIX = "_self"
+
+WORKFLOW_GROUPING_LIST = [
+    "round_robin",
+    "asarray",
+    "aggregate",
+    # used internally by e2clab, not meant to use for the user
+    "address_match",
+]
+
+GROUPING_LIST_USER = [
+    "round_robin",
+    "asarray",
+    "aggregate",
+    "address_match",
+]
+
+DEFAULT_GROUPING = "round_robin"
+
+ANSIBLE_TASKS = "tasks"
+
+WORKFLOW_DEVICE_TASK = ["copy", "shell", "fetch"]
