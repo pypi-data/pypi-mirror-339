012e187c02f4f083402ff10bfa6c09160f6322ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-09 03:18:16 +0000

    Add package llm-manager/0.2.0/llm_manager-0.2.0-cp311-cp311-macosx_11_0_arm64.whl

---
+++ packages/llm-manager/llm_manager-0.2.0-cp311-cp311-macosx_11_0_arm64.whl/llm_manager/message.pyi
@@ -0,0 +1,18 @@
+from enum import StrEnum
+from pydantic import BaseModel, Field
+from typing import Any
+
+class MessageRole(StrEnum):
+    system = "system"
+    user = "user"
+    assistant = "assistant"
+    tool = "tool"
+
+class Message(BaseModel):
+    role: MessageRole
+    contents: list[str | list[dict[str, Any]]] = Field(default_factory=list)
+    tool_call_id: str = ""
+    tool_calls: list[dict[str, Any]] = Field(default_factory=list)
+    @property
+    def latest(self) -> dict[str, Any]: ...
+    def __repr__(self) -> str: ...
