cc7c01c1df97a490fb9ec087569cd3d7aa08c978
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-09 07:05:02 +0000

    Add package fast-pauli/0.0.33rc1/fast_pauli-0.0.33rc1-cp312-cp312-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/fast-pauli/fast_pauli-0.0.33rc1-cp312-cp312-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/fast_pauli/cpp/include/__type_traits.hpp
@@ -0,0 +1,57 @@
+/**
+ * This code is part of Fast Pauli.
+ *
+ * (C) Copyright Qognitive Inc 2024.
+ *
+ * This code is licensed under the BSD 2-Clause License. You may
+ * obtain a copy of this license in the LICENSE.txt file in the root directory
+ * of this source tree.
+ *
+ * Any modifications or derivative works of this code must retain this
+ * copyright notice, and modified files need to carry a notice indicating
+ * that they have been altered from the originals.
+ */
+
+#ifndef __FP_TYPE_TRAITS_HPP
+#define __FP_TYPE_TRAITS_HPP
+
+#include <complex>
+#include <concepts>
+#include <execution>
+#include <type_traits>
+
+namespace fast_pauli
+{
+
+template <typename T> struct is_complex : std::false_type
+{
+};
+
+template <std::floating_point T> struct is_complex<std::complex<T>> : std::true_type
+{
+};
+
+/**
+ * @brief Execution policy concept
+ *
+ * @tparam T
+ */
+template <typename T>
+concept execution_policy = std::is_execution_policy_v<std::remove_cvref_t<T>>;
+
+/**
+ * @brief Parallel execution policy helpers
+ *
+ * Modelled after libcxx implementation
+ * https://github.com/llvm/llvm-project/blob/6bed79b3f00d3e2c273bc36ed350f802d76607b3/libcxx/include/execution#L96-L118
+ *
+ * @tparam class
+ */
+
+template <typename T>
+inline constexpr bool is_parallel_execution_policy_v =
+    std::is_same_v<std::execution::parallel_policy, std::remove_cvref_t<T>>;
+
+} // namespace fast_pauli
+
+#endif
