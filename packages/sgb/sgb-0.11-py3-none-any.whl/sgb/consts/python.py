9c5af2b4138ea45f0db498fe4f842d46c238a514
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-09 16:05:26 +0000

    Add package sgb/0.11/sgb-0.11-py3-none-any.whl

---
+++ packages/sgb/sgb-0.11-py3-none-any.whl/sgb/consts/python.py
@@ -0,0 +1,16 @@
+from sgb.consts.file import FILE
+
+class PYTHON:
+        EXECUTOR_ALIAS: str = "py"
+        EXECUTOR: str = "python"
+        EXECUTOR3: str = "python3"
+        PIP: str = "pip"
+        SEARCH_PATTERN: str = "\\Python\\Python"
+        PACKAGE_EXTENSION: str = "whl"
+
+        class COMMAND:
+            VERSION: str = "--version"
+            INSTALL: str = "install"
+            UNINSTALL: str = "uninstall"
+            SHOW: str = "show"
+            FLAG: str = "-c"
