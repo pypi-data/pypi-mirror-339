34f327878ada4674df2277806241dddd5fe22700
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-09 14:05:30 +0000

    Add package ripley/0.1.0b0/ripley-0.1.0b0-py3-none-any.whl

---
+++ packages/ripley/ripley-0.1.0b0-py3-none-any.whl/ripley/clickhouse_models/disk.py
@@ -0,0 +1,21 @@
+from dataclasses import dataclass
+
+from .._base_model import BaseModel
+
+
+@dataclass
+class ClickhouseDiskModel(BaseModel):
+    name: str
+    path: str
+    cache_path: str
+    type: str
+
+    free_space: int
+    total_space: int
+    unreserved_space: int
+    keep_free_space: int
+    is_encrypted: int
+    is_read_only: int
+    is_write_once: int
+    is_remote: int
+    is_broken: int
