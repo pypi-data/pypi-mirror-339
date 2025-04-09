e8de0f6dbfb2e44bda6e4f682e111162cb6fd635
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-09 04:06:03 +0000

    Add package crypticorn/2.1.2/crypticorn-2.1.2.tar.gz

---
+++ packages/crypticorn/crypticorn-2.1.2.tar.gz/crypticorn-2.1.2/crypticorn/common/auth.py
@@ -0,0 +1,42 @@
+from typing import Optional, Callable, Any, Annotated
+from typing_extensions import Doc
+from enum import Enum
+
+from fastapi import params
+from fastapi.security import APIKeyHeader, HTTPBearer
+
+from crypticorn.common import Scope
+
+
+http_bearer = HTTPBearer(
+    bearerFormat="JWT",
+    auto_error=False,
+    description="The JWT to use for authentication.",
+)
+
+apikey_header = APIKeyHeader(
+    name="X-API-Key",
+    auto_error=False,
+    description="The API key to use for authentication.",
+)
+
+
+def Security(  # noqa: N802
+    dependency: Annotated[
+        Callable[..., Any], Doc("A dependable callable (like a function).")
+    ],
+    scopes: Annotated[
+        list[Scope],
+        Doc("API scopes required for the *path operation*."),
+    ] = [],
+    use_cache: Annotated[
+        bool, Doc("Whether to cache the dependency during a single request.")
+    ] = True,
+) -> Any:
+    """Security Dependency. Extends the fastapi.Security class to support enum scopes instead of strings."""
+
+    scopes_str = [s.value if isinstance(s, Enum) else s for s in scopes] if scopes else None
+
+    return params.Security(
+        dependency=dependency, scopes=scopes_str, use_cache=use_cache
+    )
