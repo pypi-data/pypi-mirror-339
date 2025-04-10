58a760650a12179bf02bbd7f30c0dffe84f0d132
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-10 03:18:34 +0000

    Add package mdformat_admon/2.1.0/mdformat_admon-2.1.0.tar.gz

---
+++ packages/mdformat_admon/mdformat_admon-2.1.0.tar.gz/mdformat_admon-2.1.0/mdformat_admon/__init__.py
@@ -0,0 +1,9 @@
+"""An mdformat plugin for admonitions."""
+
+__version__ = "2.1.0"
+
+# FYI see source code for available interfaces:
+#   https://github.com/executablebooks/mdformat/blob/5d9b573ce33bae219087984dd148894c774f41d4/src/mdformat/plugins.py
+from .plugin import RENDERERS, update_mdit
+
+__all__ = ("RENDERERS", "update_mdit")
