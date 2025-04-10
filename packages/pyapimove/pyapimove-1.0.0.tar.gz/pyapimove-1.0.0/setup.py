68bb8fe701fbb0768780185d6eef88e6c77b5c2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-10 14:06:46 +0000

    Add package pyapimove/1.0.0/pyapimove-1.0.0.tar.gz

---
+++ packages/pyapimove/pyapimove-1.0.0.tar.gz/pyapimove-1.0.0/setup.py
@@ -0,0 +1,18 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name='pyapimove',
+    version='1.0.0',
+    packages=find_packages(),
+    description='auto API',
+    long_description='import API',
+    long_description_content_type='text/markdown',
+    author='Open',
+    author_email='sjsbcaksgg.s.ksv@gmail.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
