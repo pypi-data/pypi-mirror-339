919f622de10c69dab5fba2cd146b8764f4ede136
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-10 03:18:44 +0000

    Add package Thermalift/1.0.2/thermalift-1.0.2.tar.gz

---
+++ packages/Thermalift/thermalift-1.0.2.tar.gz/thermalift-1.0.2/README.md
@@ -0,0 +1,15 @@
+# Thermalift
+
+**Calculator for eliminating the thermal lift effect from production data in geothermal wells.**
+
+Thermalift was developed as part of the [GeoModel](https://geomodel.pl/en/) project.
+
+The user's guide can be found in the docs directory or [here](https://github.com/maciejmiecznik/thermalift/blob/main/docs/Thermalift_1.0_user_guide.pdf)
+
+
+The output from Thermalift calculator can be a figure similar to the below one, showing how thermal lift influences the observed water table level or water table drawdown.
+
+![Thermalft output figure](https://github.com/maciejmiecznik/thermalift/blob/main/examples/Case%20ST/Test%20well%20raw%20and%20corrected%20pumping%20data_thermal_lift.png?raw=true)
+
+
+Thermalift is a part of project "Optimal management of low-temperature geothermal reservoirs – Polish-Icelandic cooperation on reservoir modelling" (acronym GeoModel), financed under the Fund for Bilateral Relations through the [European Economic Area Financial Mechanism (EEA FM) and the Norwegian Financial Mechanism (NFM)](https://eeagrants.org/) 2014-2021 under the Environment, Energy and Climate Change Programme.
---
+++ packages/Thermalift/thermalift-1.0.2.tar.gz/thermalift-1.0.2/src/brine_density.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+"""
+Validity: brine_density(t, s) in (kg/m3 ); 0 < t < 180 deg. C; 0 < S < 0.16 kg/kg
+Accuracy: ±0.1 %
+Reference: H. Sun, R. Feistel, M. Koch and A. Markoe, New equations for density,
+entropy, heat capacity, and potential temperature of a saline thermal fluid,
+Deep-Sea Research, I 55 (2008), 1304–1310.
+"""
+
+# correlation parameters
+a1 = 9.999e2
+a2 = 2.034e-2
+a3 = -6.162e-3
+a4 = 2.261e-5
+a5 = -4.657e-8
+b1 = 8.020e2
+b2 = -2.001
+b3 = 1.677e-2
+b4 = -3.060e-5
+b5 = -1.613e-5
+
+
+def brine_density(t, ss):
+    # Below function calculates brine density as a function of temperature [deg. C]
+    # and salinity, expressed in kg/kg
+    if ss < 0.35:
+        rho_brine = (
+            a1
+            + a2 * t
+            + a3 * t**2
+            + a4 * t**3
+            + a5 * t**4
+            + b1 * ss
+            + b2 * ss * t
+        )
+        +b3 * ss * t**2 + b4 * ss * t**3 + b5 * ss**2 * t**2
+    else:
+        # Below function calculates brine density as a function of temperature [deg. C]
+        # and salinity, expressed in g/dm3
+        s = ss / 1050
+        for i in range(1, 10):
+            rho_brine = (
+                a1
+                + a2 * t
+                + a3 * t**2
+                + a4 * t**3
+                + a5 * t**4
+                + b1 * s
+                + b2 * s * t
+            )
+            +b3 * s * t**2 + b4 * s * t**3 + b5 * s**2 * t**2
+            s = ss / rho_brine
+    return rho_brine
