d073ccdef448669de841d1e1081285b77006d7cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-10 03:18:38 +0000

    Add package st-screenwidth-detector/0.1.3/st_screenwidth_detector-0.1.3-py3-none-any.whl

---
+++ packages/st-screenwidth-detector/st_screenwidth_detector-0.1.3-py3-none-any.whl/st_screenwidth_detector/frontend/main.js
@@ -0,0 +1,32 @@
+import { Streamlit } from "streamlit-component-lib";
+
+// hide the component
+parent.document.querySelector(".st-key-screenwidth-detector").style.display =
+  "none";
+
+// Debounce function to delay execution
+function debounce(func, wait) {
+  let timeout;
+  return function (...args) {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => func.apply(this, args), wait);
+  };
+}
+
+// Add event listener to watch for screen width changes
+parent.addEventListener(
+  "resize",
+  debounce(() => {
+    const screenWidth = parent.innerWidth;
+    Streamlit.setComponentValue(screenWidth);
+  }, 200)
+);
+
+// Set initial component value to current screen width
+function onRender() {
+  const screenWidth = parent.innerWidth;
+  Streamlit.setComponentValue(screenWidth);
+}
+
+Streamlit.events.addEventListener(Streamlit.RENDER_EVENT, onRender);
+Streamlit.setComponentReady();
