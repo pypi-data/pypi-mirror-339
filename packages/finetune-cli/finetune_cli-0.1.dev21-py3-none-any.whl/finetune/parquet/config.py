eb4b7b1192349a19ca62fb81ec561a6d9dd42674
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-10 04:05:48 +0000

    Add package finetune-cli/0.1.dev21/finetune_cli-0.1.dev21-py3-none-any.whl

---
+++ packages/finetune-cli/finetune_cli-0.1.dev21-py3-none-any.whl/finetune/parquet/config.py
@@ -0,0 +1,9 @@
+from finetune.parquet.agentManager import AgentModel
+from kink import di
+
+Master = {
+    "openai_api_key": di['openai_api_key'],
+    "openai_api_endpoint": di['openai_api_endpoint'],
+    "default_model": di['default_model'],
+    "prefix": "",
+}
