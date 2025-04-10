8d9ad5be294b6d7983d8a7cc54425b5628ec9a44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-10 05:07:18 +0000

    Add package django-compat-patcher/0.13/django_compat_patcher-0.13-py3-none-any.whl

---
+++ packages/django-compat-patcher/django_compat_patcher-0.13-py3-none-any.whl/django_compat_patcher/fixers/__init__.py
@@ -0,0 +1,21 @@
+from __future__ import absolute_import, print_function, unicode_literals
+
+from . import (
+    django1_6,
+    django1_7,
+    django1_8,
+    django1_9,
+    django1_10,
+    django1_11,
+    django2_0,
+    django2_1,
+    django2_2,
+    django3_0,
+    django3_1,
+    django3_2,
+    django4_0,
+    django4_1,
+    # No fix for Django4.2 and Django5.0 so far
+    django5_1
+    # TODO - add and import new sets of fixers for more recent Django versions
+)
