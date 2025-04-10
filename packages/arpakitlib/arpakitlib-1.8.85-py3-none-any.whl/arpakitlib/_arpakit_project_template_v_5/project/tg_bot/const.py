dddf917002c95034fed42ead3474014619bf7a94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-10 05:05:25 +0000

    Add package arpakitlib/1.8.85/arpakitlib-1.8.85-py3-none-any.whl

---
+++ packages/arpakitlib/arpakitlib-1.8.85-py3-none-any.whl/arpakitlib/_arpakit_project_template_v_5/project/tg_bot/const.py
@@ -0,0 +1,40 @@
+from arpakitlib.ar_enumeration_util import Enumeration
+
+
+class GeneralTgBotCommands(Enumeration):
+    start = "start"
+    about = "about"
+    author = "author"
+    support = "support"
+    healthcheck = "healthcheck"
+    hello_world = "hello_world"
+
+
+class AdminTgBotCommands(Enumeration):
+    arpakitlib_project_template_info = "arpakitlib_project_template_info"
+    init_sqlalchemy_db = "init_sqlalchemy_db"
+    reinit_sqlalchemy_db = "reinit_sqlalchemy_db"
+    drop_sqlalchemy_db = "drop_sqlalchemy_db"
+    set_all_tg_bot_commands = "set_all_tg_bot_commands"
+    raise_fake_error = "raise_fake_error"
+    me = "me"
+    log_file = "log_file"
+    clear_log_file = "clear_log_file"
+    kb_with_old_cd = "kb_with_old_cd"
+    kb_with_not_modified = "kb_with_not_modified"
+    kb_with_raise_error = "kb_with_raise_error"
+    kb_with_remove_message = "kb_with_remove_message"
+
+
+def __example():
+    print("ClientCommandsTgBot:")
+    for v in GeneralTgBotCommands.values_list():
+        print(f"- {v}")
+    print()
+    print("AdminCommandsTgBot:")
+    for v in AdminTgBotCommands.values_list():
+        print(f"- {v}")
+
+
+if __name__ == '__main__':
+    __example()
