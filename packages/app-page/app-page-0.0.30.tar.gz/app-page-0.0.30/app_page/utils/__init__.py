7c669d2aa644fb5b7ecb062ae3637a8186367bf6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-10 03:19:04 +0000

    Add package app-page/0.0.30/app-page-0.0.30.tar.gz

---
+++ packages/app-page/app-page-0.0.30.tar.gz/app-page-0.0.30/app_page/utils/__init__.py
@@ -0,0 +1,23 @@
+# app_page/utils/__init__.py
+from .common import assetsPath, layout_clear, timestamp, escape_xml, unescape_xml
+from .load_ui import loadUI, setupUiFromSetting
+from .set_style import setAppStyle, setWidgetStyle, setWidgetStyleById, s2t
+from .cut_image import cut_image_in, cut_image_out
+from .select_image import select_image
+
+__all__ = [
+  'assetsPath',
+  'layout_clear',
+  'timestamp',
+  'loadUI',
+  'setupUiFromSetting',
+  'setAppStyle',
+  'setWidgetStyle',
+  'setWidgetStyleById',
+  'cut_image_in',
+  'cut_image_out',
+  'select_image',
+  'escape_xml',
+  'unescape_xml',
+  's2t',
+]
