68c7dbd61a5490ed2924e4f29d0184567a36d97b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-10 07:07:43 +0000

    Add package enzymetk/0.0.1/enzymetk-0.0.1-py3-none-any.whl

---
+++ packages/enzymetk/enzymetk-0.0.1-py3-none-any.whl/enzymetk/__init__.py
@@ -0,0 +1,56 @@
+###############################################################################
+#                                                                             #
+#    This program is free software: you can redistribute it and/or modify     #
+#    it under the terms of the GNU General Public License as published by     #
+#    the Free Software Foundation, either version 3 of the License, or        #
+#    (at your option) any later version.                                      #
+#                                                                             #
+#    This program is distributed in the hope that it will be useful,          #
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of           #
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            #
+#    GNU General Public License for more details.                             #
+#                                                                             #
+#    You should have received a copy of the GNU General Public License        #
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.     #
+#                                                                             #
+###############################################################################
+
+"""
+Author: Ariane Mora
+Date: March 2025
+"""
+__title__ = 'enzymetk'
+__description__ = 'Toolkit for enzymes and what not'
+__url__ = 'https://github.com/arianemora/enzyme-tk/'
+__version__ = '0.0.1'
+__author__ = 'Ariane Mora'
+__author_email__ = 'ariane.n.mora@gmail.com'
+__license__ = 'GPL3'
+
+# from enzymetk.step import *
+# from enzymetk.generate_msa_step import ClustalOmega
+# from enzymetk.annotateEC_CLEAN_step import CLEAN
+# from enzymetk.annotateEC_proteinfer_step import ProteInfer
+# from enzymetk.dock_chai_step import Chai
+# from enzymetk.dock_vina_step import Vina
+# from enzymetk.embedchem_chemberta_step import ChemBERT
+# from enzymetk.embedchem_rxnfp_step import RxnFP
+# from enzymetk.embedchem_selformer_step import SelFormer
+# from enzymetk.embedchem_unimol_step import UniMol
+# from enzymetk.embedprotein_esm_step import EmbedESM
+# from enzymetk.generate_tree_step import FastTree
+# from enzymetk.inpaint_ligandMPNN_step import LigandMPNN
+# from enzymetk.metagenomics_porechop_trim_reads_step import PoreChop
+# from enzymetk.metagenomics_prokka_annotate_genes import Prokka
+# #from enzymetk.predict_activity_step import 
+# from enzymetk.predict_catalyticsite_step import ActiveSitePred
+# from enzymetk.sequence_search_blast import BLAST
+# from enzymetk.similarity_foldseek_step import FoldSeek
+# from enzymetk.similarity_mmseqs_step import MMseqs
+# from enzymetk.similarity_reaction_step import ReactionDist
+# from enzymetk.similarity_substrate_step import SubstrateDist
+
+
+
+
+
