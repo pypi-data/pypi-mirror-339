7b6fab1b14d98e7a9454cf50e23d44a2ce7f97be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-10 04:06:20 +0000

    Add package educommon/3.13.2/educommon-3.13.2.tar.gz

---
+++ packages/educommon/educommon-3.13.2.tar.gz/educommon-3.13.2/src/educommon/audit_log/app_meta.py
@@ -0,0 +1,19 @@
+from educommon import (
+    ioc,
+)
+from educommon.audit_log.actions import (
+    AuditLogPack,
+)
+from educommon.audit_log.error_log.actions import (
+    PostgreSQLErrorPack,
+)
+
+
+def register_actions():
+    """Регистрация паков и экшенов."""
+    ioc.get('main_controller').packs.extend(
+        (
+            AuditLogPack(),
+            PostgreSQLErrorPack(),
+        )
+    )
