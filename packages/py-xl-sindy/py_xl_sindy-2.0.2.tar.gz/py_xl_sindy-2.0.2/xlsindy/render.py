cabe9a2906803e2ba53ae6bc9bcc6b76a551f1be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-10 14:06:12 +0000

    Add package py-xl-sindy/2.0.2/py_xl_sindy-2.0.2.tar.gz

---
+++ packages/py-xl-sindy/py_xl_sindy-2.0.2.tar.gz/py_xl_sindy-2.0.2/xlsindy/render.py
@@ -0,0 +1,111 @@
+"""
+
+This module contain some render function for the basics experiment
+
+"""
+
+import sys
+import matplotlib.animation as animation
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def animate_single_pendulum(
+    length: float, angle_array: np.ndarray, time_array: np.ndarray
+):
+    """
+    Animates a single pendulum based on its length, angle data, and time steps.
+
+    Parameters:
+        length (float): Length of the pendulum.
+        angle_array (ndarray): Array of angular positions over time.
+        time_array (ndarray): Array of time steps corresponding to angles.
+    """
+    x_coords = length * np.sin(angle_array[:, 0])
+    y_coords = -length * np.cos(angle_array[:, 0])
+
+    fig = plt.figure(figsize=(5, 4))
+    ax = fig.add_subplot(
+        autoscale_on=False, xlim=(-length, length), ylim=(-length, length)
+    )
+    ax.set_aspect("equal")
+    ax.grid()
+
+    (trace_line,) = ax.plot([], [], ".-", lw=1, ms=2)
+    (pendulum_line,) = ax.plot([], [], "o-", lw=2)
+    time_template = "time = %.1fs"
+    time_text = ax.text(0.05, 0.9, "", transform=ax.transAxes)
+
+    def update_frame(i):
+        current_x = [0, x_coords[i]]
+        current_y = [0, y_coords[i]]
+
+        trace_x = x_coords[:i]
+        trace_y = y_coords[:i]
+
+        pendulum_line.set_data(current_x, current_y)
+        trace_line.set_data(trace_x, trace_y)
+        time_text.set_text(time_template % time_array[i])
+        return pendulum_line, trace_line, time_text
+
+    ani = animation.FuncAnimation(
+        fig, update_frame, len(angle_array), interval=40, blit=True
+    )
+    plt.show()
+
+
+def animate_double_pendulum(
+    length1: float,
+    length2: float,
+    angle_array: np.ndarray,
+    time_array: np.ndarray,
+    fig=None,
+):
+    """
+    Animates a double pendulum based on its segment lengths, angles, and time steps.
+
+    Parameters:
+        length1 (float): Length of the first segment.
+        length2 (float): Length of the second segment.
+        angle_array (ndarray): Array of angular positions of both segments over time.
+        time_array (ndarray): Array of time steps corresponding to angles.
+    """
+    total_length = length1 + length2
+
+    x1 = length1 * np.sin(angle_array[:, 0])
+    y1 = -length1 * np.cos(angle_array[:, 0])
+
+    x2 = length2 * np.sin(angle_array[:, 2]) + x1
+    y2 = -length2 * np.cos(angle_array[:, 2]) + y1
+    if fig == None:
+        fig = plt.figure(figsize=(5, 4))
+
+    ax = fig.add_subplot(
+        autoscale_on=False,
+        xlim=(-total_length, total_length),
+        ylim=(-total_length, total_length),
+    )
+    ax.set_aspect("equal")
+    ax.grid()
+
+    (trace_line,) = ax.plot([], [], ".-", lw=1, ms=2)
+    (pendulum_line,) = ax.plot([], [], "o-", lw=2)
+    time_template = "time = %.1fs"
+    time_text = ax.text(0.05, 0.9, "", transform=ax.transAxes)
+
+    def update_frame(i):
+        current_x = [0, x1[i], x2[i]]
+        current_y = [0, y1[i], y2[i]]
+
+        trace_x = x2[:i]
+        trace_y = y2[:i]
+
+        pendulum_line.set_data(current_x, current_y)
+        trace_line.set_data(trace_x, trace_y)
+        time_text.set_text(time_template % time_array[i])
+        return pendulum_line, trace_line, time_text
+
+    ani = animation.FuncAnimation(
+        fig, update_frame, len(angle_array), interval=40, blit=True
+    )
+    plt.show()
