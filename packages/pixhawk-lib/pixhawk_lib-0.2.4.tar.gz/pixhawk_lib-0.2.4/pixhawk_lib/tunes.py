502d36d12d4f60a8aa36c96c776b033aab226551
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-10 04:06:41 +0000

    Add package pixhawk-lib/0.2.4/pixhawk_lib-0.2.4.tar.gz

---
+++ packages/pixhawk-lib/pixhawk_lib-0.2.4.tar.gz/pixhawk_lib-0.2.4/pixhawk_lib/tunes.py
@@ -0,0 +1,5 @@
+# pixhawk_lib/tunes.py
+HAPPY_BIRTHDAY = "L16 C C D C F E L16 C C D C G F L16 C C C A F E L16 D D B F G F" # Happy birthday song
+DANGER = "L4 E E E E E E E E E E"  # Rapid beeps for danger
+ALL_SAFE = "L4 C G C"  # Simple ascending notes
+EXPLODE = "L8 C C C C C C C C"  # Fast descending notes
