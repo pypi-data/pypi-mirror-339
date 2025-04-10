d6247202b2c263b300824fa2465dd04499d8de1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-10 06:07:00 +0000

    Add package beautified/0.1.0/beautified-0.1.0.tar.gz

---
+++ packages/beautified/beautified-0.1.0.tar.gz/beautified-0.1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="beautified",
+    version="0.1.0",
+    description="A package for CLI text stylization.",
+    author="Chase Galloway",
+    author_email="chase.h.galloway21@gmail.com",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "Topic :: Terminals",
+    ],
+    python_requires=">=3.6",
+)
