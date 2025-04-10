5be7bbd75d33799b09f25c7ee31d6e45b1b09ec2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-10 03:18:38 +0000

    Add package inspirems/1.5.2/inspirems-1.5.2-py3-none-any.whl

---
+++ packages/inspirems/inspirems-1.5.2-py3-none-any.whl/inspire/input/mgf.py
@@ -0,0 +1,101 @@
+""" Functions for reading in scans results in mgf format.
+"""
+import re
+
+import numpy as np
+import pandas as pd
+from pyteomics import mgf
+
+from inspire.constants import (
+    CHARGE_KEY,
+    INTENSITIES_KEY,
+    MZS_KEY,
+    RT_KEY,
+    SCAN_KEY,
+    SOURCE_KEY,
+)
+
+def process_mgf_file(
+        mgf_filename,
+        scan_ids,
+        scan_file_format,
+        source_list,
+        combined_source_file=False,
+        with_charge=False,
+        with_retention_time=False,
+    ):
+    """ Function to process an mgf file to find matches with scan IDs.
+
+    Parameters
+    ----------
+    mgf_filename : str
+        The mgf file from which we are reading.
+    scan_ids : set of int
+        A set of the scan IDs we require.
+    scan_file_format : str
+        The format of the file used.
+    source_list : list of str
+        A list of source names.
+
+    Returns
+    -------
+    scans_df : pd.DataFrame
+        A DataFrame of scan results.
+    """
+    matched_scan_ids = []
+    matched_intensities = []
+    matched_mzs = []
+    sources = []
+    filename = mgf_filename.split('/')[-1]
+    if with_charge:
+        charge_list = []
+    if with_retention_time:
+        rt_list = []
+
+    with mgf.read(mgf_filename) as reader:
+        for spectrum in reader:
+            if scan_file_format is None or scan_file_format == 'distiller':
+                if 'scans' in spectrum['params']:
+                    scan_id = int(spectrum['params']['scans'])
+                else:
+                    regex_match = re.match(
+                        r'(\d+)(.*?)',
+                        spectrum['params']['title'].split('scan=')[-1]
+                    )
+                    scan_id = int(regex_match.group(1))
+                if combined_source_file:
+                    source = spectrum['params']['title'].split('File:"')[-1].split('.raw"')[0]
+                else:
+                    source = filename[:-4]
+            else:
+                scan_id = int(spectrum['params']['title'].split(' Scan ')[-1].split(' (rt')[0])
+                source = source_list[
+                    int(spectrum['params']['title'].split(' from file [')[-1].strip(']'))
+                ]
+
+            if scan_ids is None or scan_id in scan_ids:
+                sources.append(source)
+                matched_scan_ids.append(scan_id)
+                matched_intensities.append(np.array(list(spectrum['intensity array'])))
+                matched_mzs.append(np.array(list(spectrum['m/z array'])))
+                if with_charge:
+                    charge_list.append(int(spectrum['params']['charge'][0]))
+                if with_retention_time:
+                    rt_list.append(float(spectrum['params']['rtinseconds']))
+
+    mgf_df = pd.DataFrame(
+        {
+            SOURCE_KEY: pd.Series(sources),
+            SCAN_KEY: pd.Series(matched_scan_ids),
+            INTENSITIES_KEY: pd.Series(matched_intensities),
+            MZS_KEY: pd.Series(matched_mzs)
+        }
+    )
+    if with_charge:
+        mgf_df[CHARGE_KEY] = pd.Series(charge_list)
+    if with_retention_time:
+        mgf_df[RT_KEY] = pd.Series(rt_list)
+
+    mgf_df = mgf_df.drop_duplicates(subset=[SOURCE_KEY, SCAN_KEY])
+
+    return mgf_df
---
+++ packages/inspirems/inspirems-1.5.2-py3-none-any.whl/inspire/input/mzml.py
@@ -0,0 +1,87 @@
+""" Functions for loading experimental spectra from mzml files.
+"""
+import os
+
+import numpy as np
+import pandas as pd
+from pyteomics import mzml
+
+from inspire.constants import (
+    CHARGE_KEY,
+    INTENSITIES_KEY,
+    MZS_KEY,
+    RT_KEY,
+    SCAN_KEY,
+    SOURCE_KEY,
+)
+
+
+def process_mzml_file(mzml_filename, scan_ids, with_charge=False, with_retention_time=False):
+    """ Function to process an MzML file to find matches with scan IDs.
+
+    Parameters
+    ----------
+    mzml_filename : str
+        The mzml file from which we are reading.
+    scan_ids : list of int
+        A list of the scan IDs we require.
+
+    Returns
+    -------
+    scans_df : pd.DataFrame
+        A DataFrame of scan results.
+    """
+    ion_list = []
+    intensities_list = []
+    scan_id_list = []
+    mzml_filenames = []
+    filename = mzml_filename.split('/')[-1]
+
+    if os.path.exists(mzml_filename.replace('.mzML', '_calibrated.mzML')):
+        input_name = mzml_filename.replace('.mzML', '_calibrated.mzML')
+    elif os.path.exists(mzml_filename):
+        input_name = mzml_filename
+    else:
+        input_name = mzml_filename.replace('.mzML', '_uncalibrated.mzML')
+
+    if with_charge:
+        charge_list = []
+
+    if with_retention_time:
+        rt_list = []
+
+    with mzml.read(input_name) as reader:
+        for spectrum in reader:
+            scan_id = int(spectrum['id'].split('scan=')[1])
+            if scan_ids is None or scan_id in scan_ids:
+                mzml_filenames.append(filename)
+                scan_id_list.append(scan_id)
+                intensities_list.append(np.array(list(spectrum['intensity array'])))
+                ion_list.append(np.array(list(spectrum['m/z array'])))
+
+                if with_charge:
+                    charge_list.append(
+                        int(spectrum['precursorList']['precursor'][0]['selectedIonList'][
+                            'selectedIon'
+                        ][0]['charge state'])
+                    )
+
+                if with_retention_time:
+                    rt_list.append(float(spectrum['scanList']['scan'][0]['scan start time']))
+
+    scans_df =  pd.DataFrame(
+        {
+            SOURCE_KEY: pd.Series(mzml_filenames),
+            SCAN_KEY: pd.Series(scan_id_list),
+            INTENSITIES_KEY: pd.Series(intensities_list),
+            MZS_KEY: pd.Series(ion_list)
+        }
+    )
+    if with_charge:
+        scans_df[CHARGE_KEY] = pd.Series(charge_list)
+    if with_retention_time:
+        scans_df[RT_KEY] = pd.Series(rt_list)
+
+    scans_df = scans_df.drop_duplicates(subset=[SOURCE_KEY, SCAN_KEY])
+
+    return scans_df
