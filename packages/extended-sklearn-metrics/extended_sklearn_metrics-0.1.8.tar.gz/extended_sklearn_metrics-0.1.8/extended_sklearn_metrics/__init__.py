80c48e0ae479e603d6c68f561fe732969c8d421f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-10 03:18:39 +0000

    Add package extended-sklearn-metrics/0.1.8/extended_sklearn_metrics-0.1.8.tar.gz

---
+++ packages/extended-sklearn-metrics/extended_sklearn_metrics-0.1.8.tar.gz/extended_sklearn_metrics-0.1.8/extended_sklearn_metrics/__init__.py
@@ -0,0 +1,9 @@
+"""
+extended-sklearn-metrics - A Python package for enhanced scikit-learn model evaluation metrics
+"""
+
+__version__ = "0.1.8"
+
+from .model_evaluation import evaluate_model_with_cross_validation
+
+__all__ = ['evaluate_model_with_cross_validation'] 
