4d148b13fb4152903f1efebabdeb0073cb00c38f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-10 06:06:21 +0000

    Add package maze-dataset/1.3.2/maze_dataset-1.3.2-py3-none-any.whl

---
+++ packages/maze-dataset/maze_dataset-1.3.2-py3-none-any.whl/maze_dataset/generation/__init__.py
@@ -0,0 +1,23 @@
+"""generation functions have signature `(grid_shape: Coord, **kwargs) -> LatticeMaze` and are methods in `LatticeMazeGenerators`
+
+`DEFAULT_GENERATORS` is a list of generator name, generator kwargs pairs used in tests and demos
+"""
+
+from maze_dataset.generation.generators import (
+	GENERATORS_MAP,
+	LatticeMazeGenerators,
+	get_maze_with_solution,
+	numpy_rng,
+)
+
+__all__ = [
+	# submodules
+	"default_generators",
+	"generators",
+	"seed",
+	# imports
+	"LatticeMazeGenerators",
+	"GENERATORS_MAP",
+	"get_maze_with_solution",
+	"numpy_rng",
+]
