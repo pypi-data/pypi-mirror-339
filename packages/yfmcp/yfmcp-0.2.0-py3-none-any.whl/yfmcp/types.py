da09be844f20b333e81752d443e934d7829e8b93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-10 05:06:38 +0000

    Add package yfmcp/0.2.0/yfmcp-0.2.0-py3-none-any.whl

---
+++ packages/yfmcp/yfmcp-0.2.0-py3-none-any.whl/yfmcp/types.py
@@ -0,0 +1,22 @@
+from typing import Literal
+
+TopType = Literal[
+    "top_etfs",
+    "top_mutual_funds",
+    "top_companies",
+    "top_growth_companies",
+    "top_performing_companies",
+]
+Sector = Literal[
+    "basic-materials",
+    "communication-services",
+    "consumer-cyclical",
+    "consumer-defensive",
+    "energy",
+    "financial-services",
+    "healthcare",
+    "industrials",
+    "real-estate",
+    "technology",
+    "utilities",
+]
