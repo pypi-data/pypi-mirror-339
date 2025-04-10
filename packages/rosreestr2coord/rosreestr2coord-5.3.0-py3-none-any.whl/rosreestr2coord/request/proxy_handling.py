62b1627d16dbefa9215b80a48ec4f4aa2a911a2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-10 03:18:48 +0000

    Add package rosreestr2coord/5.3.0/rosreestr2coord-5.3.0-py3-none-any.whl

---
+++ packages/rosreestr2coord/rosreestr2coord-5.3.0-py3-none-any.whl/rosreestr2coord/request/proxy_handling.py
@@ -0,0 +1,69 @@
+import os
+import re
+import time
+import urllib.error
+import urllib.parse
+import urllib.request
+from urllib.parse import urlparse
+
+from .helpers import USER_AGENT
+
+
+class ProxyHandling:
+    def __init__(self, path="proxy.txt", user_agent=USER_AGENT):
+        self.path = path
+        self.user_agent = user_agent
+
+    def update_proxies(self):
+        # Getting proxy list from address if file is old
+        proxies = self._load_proxies_from_file()
+        if not proxies:
+            self._download_proxies()
+        try:
+            older = 3600 * 6  # 6h
+            diff = time.time() - os.path.getmtime(self.path)
+            if diff > older:
+                self._download_proxies()
+        except OSError:
+            self._download_proxies()
+
+    def dump_proxies(self, proxies):
+        with open(self.path, "w") as outfile:
+            for proxy in proxies:
+                outfile.write(proxy)
+
+    def load_proxies(self):
+        if not os.path.exists(self.path):
+            with open(self.path, "w"):
+                pass
+        self.update_proxies()
+        return self._load_proxies_from_file()
+
+    def get_proxies(self):
+        return self._load_proxies_from_file()
+
+    def _download_proxies(self):
+        found = self._free_proxies()
+        self.dump_proxies(found[:20])  # 20 top proxies
+
+    def _free_proxies(self, url="https://free-proxy-list.net/"):
+        # Downloading without proxy
+        opener = urllib.request.build_opener(urllib.request.ProxyHandler())
+        urllib.request.install_opener(opener)
+        request = urllib.request.Request(url)
+        request.add_header("user-agent", self.user_agent)
+        parsed_uri = urlparse(url)
+        host = "{uri.scheme}://{uri.netloc}/".format(uri=parsed_uri)
+        request.add_header("referer", host)
+        f = urllib.request.urlopen(request)
+        pattern = r"\d*\.\d*\.\d*\.\d*\</td><td>\d*"
+        s = f.read().decode("utf-8")
+        found = [i.replace("</td><td>", ":") + "\n" for i in re.findall(pattern, s)]
+        return found
+
+    def _load_proxies_from_file(self):
+        try:
+            with open(self.path) as outfile:
+                return outfile.readlines()
+        except Exception:
+            return []
---
+++ packages/rosreestr2coord/rosreestr2coord-5.3.0-py3-none-any.whl/rosreestr2coord/request/urlib_adapter.py
@@ -0,0 +1,56 @@
+import json
+import ssl
+import urllib.error
+import urllib.request
+from typing import Dict, Optional, Union
+from urllib.request import Request, urlopen
+
+from .base_adapter import RequestAdapter
+from .exceptions import HTTPBadRequestException, HTTPForbiddenException
+
+
+class UrllibAdapter(RequestAdapter):
+    def _make_request(
+        self,
+        url: str,
+        proxy: Optional[str],
+        timeout: int,
+        headers: dict,
+        method: str = "GET",
+        body: Optional[Union[Dict, bytes]] = None,
+    ) -> bytes:
+        """Performs an HTTP request using urllib with optional proxy settings."""
+        if proxy:
+            proxy_handler = urllib.request.ProxyHandler({"http": proxy, "https": proxy})
+            opener = urllib.request.build_opener(proxy_handler)
+            urllib.request.install_opener(opener)
+
+        if body and isinstance(body, dict):
+            body = json.dumps(body).encode("utf-8")
+
+        request = Request(url, data=body, headers=headers, method=method)
+        context = ssl._create_unverified_context()
+        context.set_ciphers("ALL:@SECLEVEL=1")
+
+        try:
+            with urlopen(request, context=context, timeout=timeout) as response:
+                encoding = response.headers.get_content_charset() or "utf-8"
+                data = response.read().decode(encoding)
+
+                return json.loads(data)
+
+        except urllib.error.HTTPError as e:
+            if e.code == 403:
+                raise HTTPForbiddenException(f"HTTP 403 Forbidden: {e.reason}") from e
+            elif e.code == 400:
+                raise HTTPBadRequestException("HTTP 400 Bad Request") from e
+        except Exception:
+            raise
+
+    def get_specific_http_error(self):
+        """Returns the urllib HTTPError class."""
+        return urllib.error.HTTPError
+
+    def is_specific_error(self, er: Exception) -> bool:
+        """Checks if the HTTP error code is 400."""
+        return hasattr(er, "code") and er.code == 400
