7ef38d1e077ffe91e44151a9c375a487c1c4db86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-10 03:19:01 +0000

    Add package arpakitlib/1.8.84/arpakitlib-1.8.84.tar.gz

---
+++ packages/arpakitlib/arpakitlib-1.8.84.tar.gz/arpakitlib-1.8.84/arpakitlib/_arpakit_project_template_v_5/project/tg_bot/router/admin/kb_with_raise_error.py
@@ -0,0 +1,48 @@
+import aiogram.filters
+from aiogram.types import InlineKeyboardButton
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+
+from arpakitlib.ar_aiogram_util import as_tg_command
+from project.tg_bot.blank.admin import get_cached_admin_tg_bot_blank
+from project.tg_bot.callback.common import BaseCD
+from project.tg_bot.const import AdminTgBotCommands
+from project.tg_bot.filter_.is_private_chat import IsPrivateChatTgBotFilter
+from project.tg_bot.filter_.user_roles_has_admin import UserRolesHasAdminTgBotFilter
+from project.tg_bot.middleware.common import MiddlewareDataTgBot
+
+tg_bot_router = aiogram.Router()
+
+
+class _CD(BaseCD, prefix=AdminTgBotCommands.kb_with_raise_error):
+    pass
+
+
+@tg_bot_router.message(
+    IsPrivateChatTgBotFilter(),
+    UserRolesHasAdminTgBotFilter(),
+    aiogram.filters.Command(AdminTgBotCommands.kb_with_raise_error)
+)
+@as_tg_command()
+async def _(
+        m: aiogram.types.Message,
+        middleware_data_tg_bot: MiddlewareDataTgBot,
+        **kwargs
+):
+    kb_builder = InlineKeyboardBuilder()
+    kb_builder.row(InlineKeyboardButton(
+        text="Raise fake error",
+        callback_data=_CD().pack()
+    ))
+    await m.answer(
+        text=get_cached_admin_tg_bot_blank().good(),
+        reply_markup=kb_builder.as_markup()
+    )
+
+
+@tg_bot_router.callback_query(_CD.filter())
+async def _(
+        m: aiogram.types.Message,
+        middleware_data_tg_bot: MiddlewareDataTgBot,
+        **kwargs
+):
+    raise Exception("fake error")
---
+++ packages/arpakitlib/arpakitlib-1.8.84.tar.gz/arpakitlib-1.8.84/arpakitlib/_arpakit_project_template_v_5/project/tg_bot/router/general/start.py
@@ -0,0 +1,34 @@
+import logging
+
+import aiogram
+from aiogram import Router
+from aiogram.filters import Command, or_f
+
+from project.tg_bot.blank.client import get_cached_client_tg_bot_blank
+from project.tg_bot.const import GeneralTgBotCommands
+from project.tg_bot.filter_.message_text import MessageTextTgBotFilter
+from project.tg_bot.middleware.common import MiddlewareDataTgBot
+
+tg_bot_router = Router()
+_logger = logging.getLogger(__name__)
+
+
+@tg_bot_router.message(
+    or_f(
+        Command(GeneralTgBotCommands.start),
+        MessageTextTgBotFilter([
+            GeneralTgBotCommands.start,
+            "начать",
+            "старт",
+            "привет",
+            "запуск",
+        ], ignore_case=True)
+    ),
+
+)
+async def _(
+        m: aiogram.types.Message,
+        middleware_data_tg_bot: MiddlewareDataTgBot,
+        **kwargs
+):
+    await m.answer(text=get_cached_client_tg_bot_blank().welcome())
