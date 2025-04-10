3fd4de46cf03f1c3b5a97a274bc2c6805b1a100f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-10 03:18:35 +0000

    Add package NeuralNetworkTools/1.0/NeuralNetworkTools-1.0.tar.gz

---
+++ packages/NeuralNetworkTools/NeuralNetworkTools-1.0.tar.gz/NeuralNetworkTools-1.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='NeuralNetworkTools',
+    version='1.0',
+    description='A simple libary for self made Neural network. Include relu and relu derivative, loss and softmax. WARING NUMPY IS REQURED',
+    license='MIT',
+    packages=['NeuralNetworkTools'],
+    author='foloip',
+    author_email='NeuralNetworkTools@gmail.com',
+    keywords=['sis'],
+    url='https://github.com/Foloip'
+)
