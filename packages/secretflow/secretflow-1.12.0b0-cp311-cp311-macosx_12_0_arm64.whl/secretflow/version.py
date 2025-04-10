6a6cb502a2e7daa9e04f10c67e344246d66047e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-10 03:18:53 +0000

    Add package secretflow/1.12.0b0/secretflow-1.12.0b0-cp311-cp311-macosx_12_0_arm64.whl

---
+++ packages/secretflow/secretflow-1.12.0b0-cp311-cp311-macosx_12_0_arm64.whl/secretflow/version.py
@@ -0,0 +1,38 @@
+# Copyright 2023 Ant Group Co., Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+__version__ = "1.12.0b0"
+__commit_id__ = "08ea340ee4c6999d5b66c58084b8f02876e9a897"
+__docker_version__ = "$$DOCKER_VERSION$$"
+__build_time__ = "Apr 09 2025, 10:48:28"
+__mode__ = "full"
+
+
+def is_lite_version():
+    return __mode__ == "full"
+
+
+def build_message():
+    msg = []
+    msg.append(f"Secretflow {__mode__} {__version__}")
+
+    if "$$" not in __commit_id__:
+        msg.append(f"Build time ({__build_time__}) with commit id: {__commit_id__}")
+    else:
+        msg.append(f"Build time ({__build_time__})")
+
+    if "$$" not in __docker_version__:
+        msg.append(f"Distribution inside docker: {__docker_version__}")
+
+    return "\n".join(msg)
