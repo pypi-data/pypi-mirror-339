36586f5847d93b77df19e47141272930ae5b81dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-10 05:05:38 +0000

    Add package siapy/0.7.1/siapy-0.7.1.tar.gz

---
+++ packages/siapy/siapy-0.7.1.tar.gz/siapy-0.7.1/siapy/core/types.py
@@ -0,0 +1,42 @@
+from typing import Any, Sequence
+
+import numpy as np
+import pandas as pd
+import spectral as sp
+import xarray as xr
+from numpy.typing import ArrayLike
+from PIL.Image import Image
+
+from siapy.entities import SpectralImage, SpectralImageSet
+
+__all__ = [
+    "SpectralLibType",
+    "XarrayType",
+    "ImageType",
+    "ImageSizeType",
+    "ImageDataType",
+    "ImageContainerType",
+    "ArrayLike1dType",
+    "ArrayLike2dType",
+]
+
+SpectralLibType = sp.io.envi.BilFile | sp.io.envi.BipFile | sp.io.envi.BsqFile
+XarrayType = xr.DataArray | xr.Dataset
+ImageType = SpectralImage | np.ndarray | Image
+ImageSizeType = int | tuple[int, ...]
+ImageDataType = (
+    np.uint8
+    | np.int16
+    | np.int32
+    | np.float32
+    | np.float64
+    | np.complex64
+    | np.complex128
+    | np.uint16
+    | np.uint32
+    | np.int64
+    | np.uint64
+)
+ImageContainerType = SpectralImage | SpectralImageSet
+ArrayLike1dType = np.ndarray | pd.Series | Sequence[Any] | ArrayLike
+ArrayLike2dType = np.ndarray | pd.DataFrame | Sequence[Any] | ArrayLike
