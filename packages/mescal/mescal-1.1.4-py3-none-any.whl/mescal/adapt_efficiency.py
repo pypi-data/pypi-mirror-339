94f32e06d8aa805a133cb1346bcfddc1b7e78bbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-10 07:07:22 +0000

    Add package mescal/1.1.4/mescal-1.1.4-py3-none-any.whl

---
+++ packages/mescal/mescal-1.1.4-py3-none-any.whl/mescal/adapt_efficiency.py
@@ -0,0 +1,296 @@
+import pandas as pd
+import ast
+from .database import Dataset
+
+
+def correct_esm_and_lca_efficiency_differences(
+        self,
+        removed_flows: pd.DataFrame,
+        double_counting_removal: pd.DataFrame,
+        write_efficiency_report: bool = True,
+) -> None:
+    """
+    Correct the efficiency differences between ESM technologies and their construction LCI datasets
+
+    :param removed_flows: dataframe containing the name, code and amount of removed flows during double counting
+        removal
+    :param double_counting_removal: dataframe containing the scaled amounts of removed flows during double counting
+        removal
+    :param write_efficiency_report: if True, write the efficiency differences in a csv file
+    :return: None
+    """
+
+    # Store frequently accessed instance variables in local variables inside a method if they don't need to be modified
+    db_dict_code = self.main_database.db_as_dict_code
+    efficiency = self.efficiency
+    unit_conversion = self.unit_conversion
+    mapping_esm_flows_to_CPC_cat = self.mapping_esm_flows_to_CPC_cat
+
+    try:
+        efficiency.Flow = efficiency.Flow.apply(ast.literal_eval)
+    except ValueError:
+        pass
+
+    efficiency['ESM efficiency'] = efficiency.apply(
+        self.compute_efficiency_esm,
+        axis=1,
+    )
+    efficiency['LCA input unit'] = efficiency.apply(
+        self.get_lca_input_flow_unit_or_product,
+        axis=1,
+        output_type='unit',
+        removed_flows=removed_flows
+    )
+    efficiency.drop(efficiency[efficiency['LCA input unit'].isnull()].index, inplace=True)
+    efficiency['LCA input product'] = efficiency.apply(
+        self.get_lca_input_flow_unit_or_product,
+        axis=1,
+        output_type='product',
+        removed_flows=removed_flows
+    )
+    efficiency.drop(efficiency[efficiency['LCA input product'].isnull()].index, inplace=True)
+    efficiency['LCA input quantity'] = efficiency.apply(
+        self.get_lca_input_quantity,
+        axis=1,
+        double_counting_removal=double_counting_removal
+    )
+    efficiency.drop(efficiency[efficiency['LCA input quantity'] == 0].index, inplace=True)
+    efficiency = efficiency.merge(
+        right=unit_conversion[unit_conversion.Type == 'Operation'][['Name', 'Value', 'LCA', 'ESM']],
+        how='left',
+        on='Name'
+    )
+    efficiency.rename(
+        columns={'Value': 'Output conversion factor', 'LCA': 'LCA output unit', 'ESM': 'ESM output unit'},
+        inplace=True)
+    efficiency = efficiency.merge(
+        right=unit_conversion[
+            (unit_conversion.Type == 'Other')
+            & (unit_conversion.ESM == 'kilowatt hour')
+            ][['Name', 'Value', 'LCA']],
+        how='left',
+        left_on=['LCA input product', 'LCA input unit'],
+        right_on=['Name', 'LCA'],
+        suffixes=('', '_to_remove')
+    )
+    efficiency.drop(columns=['Name_to_remove', 'LCA'], inplace=True)
+    efficiency.rename(columns={'Value': 'Input conversion factor'}, inplace=True)
+
+    missing_units = efficiency[efficiency['Input conversion factor'].isna()][
+        ['LCA input product', 'LCA input unit']].values.tolist()
+    missing_units = [tuple(x) + ('kilowatt hour',) for x in set(tuple(x) for x in missing_units)]
+    if len(missing_units) > 0:
+        raise ValueError(f'No conversion factor found for the following units (product, unit from, unit to): '
+                         f'{missing_units}')
+
+    efficiency['LCA efficiency'] = efficiency['Input conversion factor'] / (
+            efficiency['Output conversion factor'] * efficiency['LCA input quantity'])
+
+    efficiency['efficiency_ratio'] = efficiency['LCA efficiency'] / efficiency['ESM efficiency']
+
+    for i in range(len(efficiency)):
+
+        act_to_adapt_list = []  # there might be several activities to adapt for one technology in case of market
+        tech = efficiency['Name'].iloc[i]
+        flows_list = efficiency['Flow'].iloc[i]
+
+        CPC_list = []  # list of CPC categories corresponding to the fuel flow(s) of the technology
+        for flow in flows_list:
+            CPC_list += mapping_esm_flows_to_CPC_cat[mapping_esm_flows_to_CPC_cat['Flow'] == flow]['CPC'].values[0]
+
+        df_removed_flows = removed_flows[removed_flows.Name == tech]  # flows removed during double counting removal
+        for j in range(len(df_removed_flows)):
+            (main_act_database, main_act_code, removed_act_database, removed_act_code) = df_removed_flows[
+                ['Database', 'Code', 'Removed flow database', 'Removed flow code']].iloc[j]
+            act_exc = db_dict_code[removed_act_database, removed_act_code]
+            if 'classifications' in act_exc:
+                if 'CPC' in dict(act_exc['classifications']):
+                    if dict(act_exc['classifications'])['CPC'] in CPC_list:
+                        # if this flow (that was removed during double counting removal) is a fuel flow of the
+                        # technology, the biosphere flows the activity will be adjusted
+                        act_to_adapt = db_dict_code[main_act_database, main_act_code]
+                        if act_to_adapt not in act_to_adapt_list:
+                            # in case there are several fuel flows in the same activity
+                            act_to_adapt_list.append(act_to_adapt)
+
+        if len(act_to_adapt_list) == 0:
+            self.logger.warning(
+                f'No flow of type(s) {flows_list} found for {tech}. The efficiency of this technology cannot be '
+                f'adjusted.'
+            )
+
+        for act in act_to_adapt_list:
+            efficiency_ratio = efficiency['efficiency_ratio'].iloc[i]
+            act = self.adapt_biosphere_flows_to_efficiency_difference(act, efficiency_ratio, tech)
+
+    if write_efficiency_report:
+        # saving the efficiency differences in a csv file
+        efficiency.to_csv(f'{self.results_path_file}efficiency_differences.csv', index=False)
+
+
+def compute_efficiency_esm(
+        self,
+        row: pd.Series,
+) -> float:
+    """
+    Retrieve a technology efficiency from the ESM model file
+
+    :param row: series containing the name of the ESM technology and the name of the ESM flow to check for efficiency
+        difference
+    :return: efficiency of the technology with respect to the given flow
+    """
+    # Store frequently accessed instance variables in local variables inside a method if they don't need to be modified
+    model = self.model
+
+    flows_list = row.Flow
+    input_amount = 0
+
+    for flow in flows_list:
+        try:
+            input_amount += model[(model.Name == row.Name) & (model.Flow == flow)].Amount.values[0]
+        except IndexError:
+            # This allows the user to put a fuel that is not an input in the ESM but in the LCI dataset in the
+            # efficiency file. This is useful in case of mismatch (different fuel consumed between ESM nd LCI dataset).
+            # However, direct emissions must then be adjusted (tech_specifics file).
+            self.logger.warning(f'Flow of type {flow} found for {row.Name} in efficiency file, but not in model file.')
+            input_amount += 0
+    if input_amount == 0:
+        raise ValueError(f'No flow of type(s) {flows_list} found for {row.Name} in the model file')
+    return -1.0 / input_amount  # had negative sign as it is an input
+
+
+def get_lca_input_flow_unit_or_product(
+        self,
+        row: pd.Series,
+        output_type: str,
+        removed_flows: pd.DataFrame
+) -> str | None:
+    """
+    Retrieve the unit or product of the removed flow (taken from the double counting removal report)
+
+    :param row: series containing the name of the ESM technology and the name of the ESM flow to check for efficiency
+        difference
+    :param output_type: can be either 'unit' or 'product'
+    :param removed_flows: dataframe containing the name and amount of removed flows during double counting removal
+    :return: the unit or product of the flow removed for the ESM (technology, flow) couple
+    """
+
+    # Store frequently accessed instance variables in local variables inside a method if they don't need to be modified
+    db_dict_code = self.main_database.db_as_dict_code
+    mapping_esm_flows_to_CPC_cat = self.mapping_esm_flows_to_CPC_cat
+
+    unit_list = []
+    name_list = []
+    CPC_list = []
+
+    flows_list = row.Flow
+
+    for flow in flows_list:
+        CPC_list += mapping_esm_flows_to_CPC_cat[mapping_esm_flows_to_CPC_cat['Flow'] == flow]['CPC'].values[0]
+    df_removed_flows = removed_flows[removed_flows.Name == row.Name]
+
+    for i in range(len(df_removed_flows)):
+        removed_act_database, removed_act_code = df_removed_flows[
+            ['Removed flow database', 'Removed flow code']].iloc[i]
+        act_exc = db_dict_code[removed_act_database, removed_act_code]
+        if 'classifications' in act_exc:
+            if 'CPC' in dict(act_exc['classifications']):
+                if dict(act_exc['classifications'])['CPC'] in CPC_list:
+                    # in carculator, the 'fuel' product may be diesel, NG or H2
+                    if act_exc['reference product'] == 'fuel':
+                        if 'cng' in act_exc['name']:
+                            name_list.append('natural gas')
+                        elif 'methane' in act_exc['name']:
+                            name_list.append('natural gas')
+                        elif 'diesel' in act_exc['name']:
+                            name_list.append('diesel')
+                        elif 'hydrogen' in act_exc['name']:
+                            name_list.append('hydrogen')
+                        else:
+                            raise ValueError(f'Unknown fuel type in carculator for flow {row.Flow} in {row.Name}: '
+                                             f'{act_exc["name"]}')
+                    else:
+                        name_list.append(act_exc['reference product'].split(',')[0])
+                    unit_list.append(act_exc['unit'])
+
+    if output_type == 'unit':
+        if len(set(unit_list)) > 1:
+            raise ValueError(f'Several units possible for flow {row.Flow} in {row.Name}: {set(unit_list)}')
+        elif len(set(unit_list)) == 0:
+            self.logger.warning(
+                f'No flow found for type(s) {row.Flow} in {row.Name}. The efficiency of this technology cannot be '
+                f'adjusted.'
+            )
+            return None
+        else:
+            return list(set(unit_list))[0]
+
+    elif output_type == 'product':
+        if len(set(name_list)) > 1:
+            self.logger.warning(
+                f'Several names possible for the same type of flow in {row.Name}: {set(name_list)}. '
+                f'Kept the first one.')
+            return list(set(name_list))[0]
+        elif len(set(name_list)) == 0:
+            self.logger.warning(
+                f'No flow found for type(s) {row.Flow} in {row.Name}. The efficiency of this technology cannot be '
+                f'adjusted.')
+            return None
+        else:
+            return list(set(name_list))[0]
+
+    else:
+        raise ValueError(f'output_type must be either "unit" or "product"')
+
+@staticmethod
+def adapt_biosphere_flows_to_efficiency_difference(
+        act: dict,
+        efficiency_ratio: float,
+        tech: str
+) -> dict:
+    """
+    Adapt the biosphere flows of an activity to correct the efficiency difference between ESM and LCA
+
+    :param act: LCI dataset to adapt
+    :param efficiency_ratio: ratio between the LCA and ESM efficiencies
+    :param tech: name of the technology in the ESM
+    :return: the adapted LCI dataset
+    """
+    for exc in Dataset(act).get_biosphere_flows():
+        if exc['unit'] in ['square meter-year', 'square meter', 'megajoule']:
+            # we exclude land occupation, land transformation and energy elementary flows
+            pass
+        else:
+            exc['amount'] *= efficiency_ratio
+            exc['comment'] = (f'EF multiplied by {round(efficiency_ratio, 4)} (efficiency). '
+                              + exc.get('comment', ''))
+
+    act['comment'] = (f'Biosphere flows adjusted by a factor {round(efficiency_ratio, 4)} to correct efficiency '
+                      f'difference between ESM and LCA. ' + act.get('comment', ''))
+    return act
+
+
+def get_lca_input_quantity(
+        self,
+        row: pd.Series,
+        double_counting_removal: pd.DataFrame
+) -> float:
+    """
+    Retrieve the quantity of the flow removed from the double counting removal report
+
+    :param row: series containing the name of the ESM technology and the name of the ESM flow to check for efficiency
+        difference
+    :param double_counting_removal: dataframe containing the scaled amounts removed during double counting removal
+    :return: quantity of the flow removed
+    """
+    amount = 0
+    flows_list = row.Flow
+    for flow in flows_list:
+        try:
+            amount += double_counting_removal[(double_counting_removal.Name == row.Name)
+                                              & (double_counting_removal.Flow == flow)].Amount.values[0]
+        except IndexError:
+            self.logger.warning(f'No flow of type {flow} has been removed in {row.Name}.')
+            amount += 0
+    else:
+        return amount
---
+++ packages/mescal/mescal-1.1.4-py3-none-any.whl/mescal/normalization.py
@@ -0,0 +1,238 @@
+import pandas as pd
+import ast
+from pathlib import Path
+
+
+def tech_type(tech: str) -> str:
+    """
+    Returns the short name of the technology type
+
+    :param tech: type of technology
+    :return: short name of the technology type
+    """
+    if tech == 'Construction':
+        return 'constr'
+    elif tech == 'Operation':
+        return 'op'
+    elif tech == 'Resource':
+        return 'res'
+    else:
+        raise ValueError(f"Unknown technology type: {tech}")
+
+
+def lcia_methods_short_names(lcia_method: str) -> str:
+    """
+    Returns the short name of the LCIA method
+
+    :param lcia_method: name of the LCIA method
+    :return: short name for the LCIA method
+    """
+    if lcia_method == 'IMPACT World+ Damage 2.0.1':
+        return 'endpoint'
+    elif lcia_method == 'IMPACT World+ Damage 2.0.1 - Total only':
+        return 'endpoint_tot'
+    elif lcia_method == 'IMPACT World+ Midpoint 2.0.1':
+        return 'midpoint'
+    elif lcia_method == 'IMPACT World+ Footprint 2.0.1':
+        return 'footprint'
+    if lcia_method == 'IMPACT World+ Damage 2.0.1_regionalized':
+        return 'endpoint_reg'
+    elif lcia_method == 'IMPACT World+ Damage 2.0.1_regionalized - Total only':
+        return 'endpoint_tot_reg'
+    elif lcia_method == 'IMPACT World+ Midpoint 2.0.1_regionalized':
+        return 'midpoint_reg'
+    elif lcia_method == 'IMPACT World+ Footprint 2.0.1_regionalized':
+        return 'footprint_reg'
+    elif lcia_method == 'PB LCIA':
+        return 'pb_lcia'
+    else:
+        raise ValueError(f"Unknown LCIA method: {lcia_method}")
+
+
+def from_str_to_tuple(df: pd.DataFrame, col: str) -> pd.DataFrame:
+    """
+    Convert a column of strings to tuples
+
+    :param df: dataframe containing the column to be converted
+    :param col: column to be converted
+    :return: dataframe with the column converted to tuples
+    """
+    if type(df[col].iloc[0]) is tuple:
+        pass
+    elif type(df[col].iloc[0]) is str:
+        df[col] = df[col].apply(lambda x: ast.literal_eval(x))
+    else:
+        raise ValueError(f"Unknown type for {col}: {type(df[col].iloc[0])}")
+
+    return df
+
+
+def restrict_lcia_metrics(
+        df: pd.DataFrame,
+        lcia_methods: list[str],
+        specific_lcia_categories: list[str] = None,
+        specific_lcia_abbrev: list[str] = None,
+) -> pd.DataFrame:
+    """
+    Restrict the dataframe to the LCIA method specified
+
+    :param df: dataframe containing the LCA metrics
+    :param lcia_methods: general LCIA method to be used
+    :param specific_lcia_categories: specific LCIA categories to be used
+    :param specific_lcia_abbrev: specific LCIA abbreviations to be used
+    :return: dataframe containing the LCA metrics for the specified LCIA method
+    """
+
+    df = df[df.apply(lambda x: x.Impact_category[0] in lcia_methods, axis=1)]
+
+    if specific_lcia_categories is not None:
+        df = df[df.apply(lambda x: x.Impact_category[-1] in specific_lcia_categories, axis=1)]
+
+    if specific_lcia_abbrev is not None:
+        df = df[df.apply(lambda x: x.Abbrev in specific_lcia_abbrev, axis=1)]
+
+    return df
+
+
+@staticmethod
+def normalize_lca_metrics(
+        R: pd.DataFrame,
+        mip_gap: float,
+        impact_abbrev: pd.DataFrame,
+        lcia_methods: list[str],
+        specific_lcia_categories: list[str] = None,
+        specific_lcia_abbrev: list[str] = None,
+        assessment_type: str = 'esm',
+        max_per_cat: pd.DataFrame = None,
+        path: str = 'results/',
+        file_name: str = 'techs_lcia',
+        metadata: dict = None,
+        output: str = 'write',
+) -> None | tuple[pd.DataFrame, dict]:
+    """
+    Create a .dat file containing the normalized LCA metrics for AMPL and a csv file containing the normalization
+    factors
+
+    :param path: path to results folder
+    :param file_name: name of the .dat file
+    :param R: dataframe containing the LCA indicators results
+    :param mip_gap: normalized values that are lower than the MIP gap are set to 0 (to improve numerical stability)
+    :param lcia_methods: LCIA method to be used
+    :param specific_lcia_categories: specific LCIA categories to be used
+    :param specific_lcia_abbrev: specific LCIA abbreviations to be used
+    :param assessment_type: type of assessment, can be 'esm' for the full LCA database, or 'direct emissions' for the
+        computation of territorial emissions only
+    :param max_per_cat: dataframe containing the maximum value of each AoP, needed if assessment_type is 'direct
+        emissions'
+    :param impact_abbrev: dataframe containing the impact categories abbreviations
+    :param metadata: dictionary containing the metadata. Can contain keys 'ecoinvent_version, 'year', 'spatialized',
+        'regionalized', 'iam', 'ssp_rcp', 'lcia_method'.
+    :param output: if 'write', writes the .dat file in 'path', if 'return', normalizes pandas dataframe, if 'both' does
+        both operations.
+    :return: None or the normalized pandas dataframe and the refactor dictionary (depending on the value of 'output')
+    """
+
+    if assessment_type == 'direct emissions' and max_per_cat is None:
+        raise ValueError("If assessment_type is 'direct emissions', max_per_cat must be provided. Run this method with "
+                         "assessment_type='esm' first to get the max_per_cat dataframe.")
+
+    if assessment_type == 'esm':
+        metric_type = 'lcia'
+    elif assessment_type == 'direct emissions':
+        metric_type = 'direct'
+    else:
+        raise ValueError(f"Unknown assessment type: {assessment_type}. Must be 'esm' or 'direct emissions'.")
+
+    if metadata is None:
+        metadata = {}
+
+    R = from_str_to_tuple(R, 'Impact_category')
+    impact_abbrev = from_str_to_tuple(impact_abbrev, 'Impact_category')
+
+    impact_abbrev = restrict_lcia_metrics(
+        df=impact_abbrev,
+        lcia_methods=lcia_methods,
+        specific_lcia_categories=specific_lcia_categories,
+        specific_lcia_abbrev=specific_lcia_abbrev,
+    )
+
+    R = pd.merge(R, impact_abbrev, on='Impact_category')
+
+    if assessment_type == 'esm':
+        refactor = {}
+        R_scaled = R[R['Type'] != 'Construction']
+        for aop in R['AoP'].unique():
+            # Scale the construction metrics to be at the same order of magnitude as the operation and resource metrics
+            lcia_op_max = R[((R['Type'] == 'Operation') |
+                             (R['Type'] == 'Resource')) & (R['AoP'] == aop)]['Value'].max()
+            lcia_constr_max = R[(R['Type'] == 'Construction') & (R['AoP'] == aop)]['Value'].max()
+            refactor[aop] = lcia_op_max / lcia_constr_max
+            R_constr_imp = R[(R['Type'] == 'Construction') & (R['AoP'] == aop)]
+            R_constr_imp['Value'] *= refactor[aop]
+            R_scaled = pd.concat([R_scaled, R_constr_imp])  # R matrix but with refactor applied to construction metrics
+            R_scaled['max_AoP'] = R_scaled.groupby('AoP')['Value'].transform('max')
+
+    else:  # assessment_type == 'direct emissions'
+        refactor = None  # not needed for direct emissions as they are for operation datasets only
+        max_per_cat_dict = {}
+        for i in range(len(max_per_cat)):
+            max_per_cat_dict[max_per_cat['AoP'][i]] = max_per_cat['max_AoP'][i]
+        R_scaled = R.copy()
+        R_scaled['max_AoP'] = R_scaled.apply(lambda x: max_per_cat_dict[x['AoP']], axis=1)
+
+    R_scaled['Value_norm'] = R_scaled['Value'] / R_scaled['max_AoP']
+    R_scaled['Value_norm'] = R_scaled['Value_norm'].apply(lambda x: x if abs(x) > mip_gap else 0)
+
+    if (output == 'write') | (output == 'both'):
+
+        Path(path).mkdir(parents=True, exist_ok=True)  # Create the folder if it does not exist
+
+        with open(f'{path}{file_name}.dat', 'w') as f:
+
+            # Write metadata at the beginning of the file
+            if 'ecoinvent_version' in metadata:
+                f.write(f"# Ecoinvent version: {metadata['ecoinvent_version']}\n")
+            if 'spatialized' in metadata:
+                f.write(f"# Spatialized database: {metadata['spatialized']}\n")
+            if 'regionalized' in metadata:
+                f.write(f"# Regionalized database: {metadata['regionalized']}\n")
+            if 'year' in metadata:
+                f.write(f"# Selected year in premise: {metadata['year']}\n")
+            if 'iam' in metadata:
+                f.write(f"# Selected IAM in premise: {metadata['iam']}\n")
+            if 'ssp_rcp' in metadata:
+                f.write(f"# Selected SSP-RCP scenario in premise: {metadata['ssp_rcp']}\n")
+            if 'lcia_method' in metadata:
+                f.write(f"# LCIA method: {metadata['lcia_method']}\n")
+            f.write("\n")
+
+            if assessment_type == 'esm':
+                # Set of LCA indicators and AoPs
+                f.write(f"set INDICATORS := {' '.join(R_scaled['Abbrev'].unique())};\n\n")
+
+                # Declare the refactor parameters values
+                f.write('# Parameters to set the operation and infrastructure indicators at the same order of '
+                        'magnitude\n')
+                for cat in R_scaled['Abbrev'].unique():
+                    aop = R[R['Abbrev'] == cat]['AoP'].iloc[0]
+                    f.write(f"let refactor['{cat}'] := {refactor[aop]};\n")
+                f.write('\n')
+
+            # Declare the LCA indicators values
+            for i in range(len(R_scaled)):
+                # Declaring the LCIA parameters
+                f.write(f"let {metric_type}_{tech_type(R_scaled.Type.iloc[i])}['{R_scaled.Abbrev.iloc[i]}','{R_scaled.Name.iloc[i]}'] "
+                        f":= {R_scaled.Value_norm.iloc[i]}; # normalized {R_scaled.Unit.iloc[i]}\n")
+
+        # To come back to the original values, we save the maximum value of each AoP
+        if assessment_type == 'esm':
+            R_scaled[['Abbrev', 'AoP', 'max_AoP']].drop_duplicates().to_csv(f'{path}{file_name}_max.csv', index=False)
+
+        if output == 'both':
+            return R_scaled, refactor
+
+    elif output == 'return':
+        return R_scaled, refactor
+
+    else:
+        raise ValueError(f"The output parameter must be either 'write', 'return' or 'both'")
