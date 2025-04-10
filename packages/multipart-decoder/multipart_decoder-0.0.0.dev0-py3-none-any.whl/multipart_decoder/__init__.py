9d834ca48704611c39838a711a6e12de89be1c1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-10 03:19:06 +0000

    Add package multipart-decoder/0.0.0.dev0/multipart_decoder-0.0.0.dev0-py3-none-any.whl

---
+++ packages/multipart-decoder/multipart_decoder-0.0.0.dev0-py3-none-any.whl/multipart_decoder/__init__.py
@@ -0,0 +1,8 @@
+"""
+multipart_decoder - A lightweight Python package for parsing multipart/form-data content.
+"""
+
+from .decoder import MultipartFormParser
+
+__version__ = "0.0.1"
+__all__ = ["MultipartFormParser"]
