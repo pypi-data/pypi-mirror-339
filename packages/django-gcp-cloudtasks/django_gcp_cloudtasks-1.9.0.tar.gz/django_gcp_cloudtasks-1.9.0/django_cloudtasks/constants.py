dde2d46a3582508559855597eddf7b9597ca190c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-10 06:07:11 +0000

    Add package django-gcp-cloudtasks/1.9.0/django_gcp_cloudtasks-1.9.0.tar.gz

---
+++ packages/django-gcp-cloudtasks/django_gcp_cloudtasks-1.9.0.tar.gz/django_gcp_cloudtasks-1.9.0/django_cloudtasks/constants.py
@@ -0,0 +1,43 @@
+# constants.py
+
+import os
+
+PROJECT_ID = os.getenv("TASKAPP_GCP_PROJECT")
+LOCATION_ID = os.getenv("TASKAPP_GCP_LOCATION")
+QUEUE_ID = os.getenv("TASKAPP_GCP_QUEUE")
+CLOUD_RUN_URL = os.getenv("TASKAPP_CLOUD_RUN_URL")
+AUTH_TOKEN = os.getenv("TASKAPP_AUTH_TOKEN")
+DEBUG_MODE = os.getenv("TASKAPP_DEBUG_MODE", "False").lower() == "true"
+
+MAX_TASK_RETRIES = 2
+
+class ChainStatus:
+    PENDING = "PENDING"
+    RUNNING = "RUNNING"
+    SUCCESS = "SUCCESS"
+    FAILED = "FAILED"
+
+    CHOICES = [
+        (PENDING, "Pending"),
+        (RUNNING, "Running"),
+        (SUCCESS, "Success"),
+        (FAILED, "Failed"),
+    ]
+
+
+class TaskStatus:
+    PENDING = "PENDING"
+    SCHEDULED = "SCHEDULED"
+    SUCCESS = "SUCCESS"
+    FAILED = "FAILED"
+    ERROR = "ERROR"
+    REVOKED = "REVOKED"
+
+    CHOICES = [
+        (PENDING, "Pending"),
+        (SCHEDULED, "Scheduled"),
+        (SUCCESS, "Success"),
+        (FAILED, "Failed"),
+        (ERROR, "Error"),
+        (REVOKED, "Revoked"),
+    ]
