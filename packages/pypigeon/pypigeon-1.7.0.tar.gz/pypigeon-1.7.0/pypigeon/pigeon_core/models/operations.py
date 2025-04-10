4142fd407a96ab2473aaa1a179b89217a8d0423c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-10 05:06:18 +0000

    Add package pypigeon/1.7.0/pypigeon-1.7.0.tar.gz

---
+++ packages/pypigeon/pypigeon-1.7.0.tar.gz/pypigeon-1.7.0/pypigeon/pigeon_core/models/operations.py
@@ -0,0 +1,15 @@
+from enum import Enum
+
+
+class Operations(str, Enum):
+    ACCOUNT = "account"
+    ADMIN_CDE = "admin.cde"
+    ADMIN_CONFIG = "admin.config"
+    ADMIN_GRANTADMIN = "admin.grantadmin"
+    ADMIN_METADATA = "admin.metadata"
+    ADMIN_TASKS = "admin.tasks"
+    ADMIN_USERS = "admin.users"
+    GROUP = "group"
+
+    def __str__(self) -> str:
+        return str(self.value)
