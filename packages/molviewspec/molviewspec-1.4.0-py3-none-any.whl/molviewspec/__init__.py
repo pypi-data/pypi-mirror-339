1cd62fef41c5959c27626b544f4a54c3fe331979
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-10 03:19:02 +0000

    Add package molviewspec/1.4.0/molviewspec-1.4.0-py3-none-any.whl

---
+++ packages/molviewspec/molviewspec-1.4.0-py3-none-any.whl/molviewspec/__init__.py
@@ -0,0 +1,15 @@
+"""MolViewSpec"""
+
+__version__ = "1.4.0"
+
+from molviewspec.builder import create_builder
+from molviewspec.nodes import (
+    ComponentExpression,
+    GlobalMetadata,
+    PrimitiveComponentExpressions,
+    Snapshot,
+    SnapshotMetadata,
+    State,
+    States,
+    validate_state_tree,
+)
