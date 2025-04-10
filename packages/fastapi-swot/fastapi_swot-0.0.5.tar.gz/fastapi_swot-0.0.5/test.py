ae358c434c1902c0fc28938f69eb5f746780ff6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-10 06:06:27 +0000

    Add package fastapi-swot/0.0.5/fastapi_swot-0.0.5.tar.gz

---
+++ packages/fastapi-swot/fastapi_swot-0.0.5.tar.gz/fastapi_swot-0.0.5/test.py
@@ -0,0 +1,5 @@
+from fastapi_swot.swot import is_academic
+
+print(is_academic('21010569@vnu.edu.vn'))
+print(is_academic('son161002559@lms.utc.edu.vn'))
+print(is_academic('harrydang.tech@gmail.com'))
