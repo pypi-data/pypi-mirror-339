cdbd88ad483fd54480e8329c20e078c1f7cdfb82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-10 14:07:04 +0000

    Add package pbesa/4.0.10/pbesa-4.0.10.tar.gz

---
+++ packages/pbesa/pbesa-4.0.10.tar.gz/pbesa-4.0.10/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+
+
+setup(
+    name='pbesa',
+    version='4.0.10',
+    packages=find_packages(),
+    install_requires=[
+        'pymongo==4.6.3',
+        'requests==2.32.3',
+        'azure-ai-projects==1.0.0b6',
+        'azure-ai-inference==1.0.0b9',
+        'azure-identity==1.20.0'
+    ],
+)
