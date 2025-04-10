d44255ec8ba560a2981cacdc3fcd8019ab0027be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-10 04:06:12 +0000

    Add package pixiu/0.161.0/pixiu-0.161.0.tar.gz

---
+++ packages/pixiu/pixiu-0.161.0.tar.gz/pixiu-0.161.0/setup.py
@@ -0,0 +1,63 @@
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'README.md')) as f:
+    README = f.read()
+with open(os.path.join(here, 'CHANGES.md')) as f:
+    CHANGES = f.read()
+
+requires = [
+    'python-dateutil',
+    'RestrictedPython',
+    'numpy',
+    'pandas',
+    'pandas_ta',
+    'TA-Lib',
+    'pymongo',
+    'tabulate',
+    'hashids',
+    'pytz',
+    'dash',
+    'plotly',
+    'pyjson5',
+    'jinja2',
+    'black'
+]
+
+tests_require = [
+    'WebTest >= 1.3.1',  # py3 compat
+    'pytest',
+    'pytest-cov',
+]
+
+setup(
+    name='pixiu',
+    version='0.161.0',
+    description='PiXiu - A trading backtesting tool similar to MT4/MT5',
+    long_description=README + '\n\n' + CHANGES,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: Apache Software License'
+    ],
+    author='tradepython',
+    author_email='tradepython@icloud.com',
+    url='https://github.com/tradepython/pixiu',
+    project_urls={
+        "Bug Tracker": "https://github.com/tradepython/pixiu/issues",
+    },
+    keywords='Trading Backtest Forex Stocks MT4 MT5 MetaTrade',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    extras_require={
+        'testing': tests_require,
+    },
+    install_requires=requires,
+    entry_points={
+        'console_scripts': ['pixiu=pixiu.main:main'],
+    },
+    python_requires=">=3.8",
+)
---
+++ packages/pixiu/pixiu-0.161.0.tar.gz/pixiu-0.161.0/tests/scripts/v1/ts_order_buylimit.py
@@ -0,0 +1,112 @@
+POINT = SymbolInfo("point")
+DIGITS = int(SymbolInfo("digits"))
+
+ood = GetOpenedOrderUIDs()
+if len(ood) == 0:
+    pod = GetPendingOrderUIDs()
+    assertIsNotNone(pod)
+    if len(pod) == 0:
+        #
+        volume = 0.01
+
+        # Buy all params
+        stop_loss = round(Bid() - 15 * POINT, DIGITS)
+        take_profit = round(Ask() + 30 * POINT, DIGITS)
+        comment = "test buylimit"
+        magic_number = 4301250
+        # failed
+        price = Ask()
+        errid, result = Buy(volume=0.01, type=OrderType.LIMIT, price=price, stop_loss=stop_loss,
+                            take_profit=take_profit, magic_number=magic_number,
+                            symbol=Symbol(), slippage=3, arrow_color="white")
+        assertEqual(errid, EID_EAT_INVALID_LIMIT_ORDER_OPEN_PRICE)
+
+        # failed
+        price = round(Ask() + 10 * POINT, DIGITS)
+        errid, result = Buy(volume=0.01, type=OrderType.LIMIT, price=price, stop_loss=stop_loss,
+                            take_profit=take_profit, magic_number=magic_number,
+                            symbol=Symbol(), slippage=3, arrow_color="white")
+        assertEqual(errid, EID_EAT_INVALID_LIMIT_ORDER_OPEN_PRICE)
+
+        #test open, modify, close
+        price = valid_buylimit_price
+        errid, result = Buy(volume=0.01, type=OrderType.LIMIT, price=price, stop_loss=stop_loss,
+                            take_profit=take_profit, magic_number=magic_number,
+                            symbol=Symbol(), slippage=3, arrow_color="white")
+        assertEqual(errid, 0)
+        assertIsNotNone(result)
+        errid = exec_command()
+        assertEqual(errid, 0)
+        oo = GetPendingOrderUIDs()
+        assertTrue(result['order_uid'] in oo)
+        order = GetOrder(result['order_uid'])
+        assertEqual(order.status, OrderStatus.PENDING)
+
+        #modify
+        stop_loss = round(stop_loss - 15 * POINT, DIGITS)
+        take_profit = round(take_profit + 30 * POINT, DIGITS)
+        errid, result = ModifyOrder(result['order_uid'], price=Ask()+1*POINT, stop_loss=stop_loss, take_profit=take_profit)
+        assertEqual(errid, EID_EAT_INVALID_LIMIT_ORDER_OPEN_PRICE)
+        errid, result = ModifyOrder(result['order_uid'], price=Ask()-1*POINT, stop_loss=stop_loss, take_profit=take_profit)
+        assertEqual(errid, 0)
+        assertIsNotNone(result['order_uid'])
+        errid = exec_command()
+        assertEqual(errid, 0)
+
+        #
+        order = GetOrder(result['order_uid'])
+        assertEqual(order.symbol, Symbol())
+        assertEqual(order.uid, result['order_uid'])
+        assertEqual(order.volume, volume)
+        assertEqual(order.stop_loss, stop_loss)
+        assertEqual(order.take_profit, take_profit)
+        assertIsNone(order.close_time)
+        assertIsNone(order.close_price)
+        assertEqual(order.status, OrderStatus.PENDING)
+
+        #close
+        errid, ct = CloseOrder(result['order_uid'], volume=volume, price=Bid())
+        assertEqual(errid, 0)
+        assertEqual(ct['order_uid'], result['order_uid'])
+        errid = exec_command()
+        assertEqual(errid, 0)
+        order = GetOrder(result['order_uid'])
+        assertEqual(order.status, OrderStatus.CANCELLED)
+
+        # The order was closed ?
+        oo = GetPendingOrderUIDs()
+        assertFalse(result['order_uid'] in oo)
+
+        #
+        price = valid_buylimit_price
+        errid, result = Buy(volume=0.01, type=OrderType.LIMIT, price=price, stop_loss=stop_loss,
+                            take_profit=take_profit, magic_number=magic_number,
+                            symbol=Symbol(), slippage=3, arrow_color="white")
+        assertEqual(errid, 0)
+        assertIsNotNone(result['order_uid'])
+        errid = exec_command()
+        assertEqual(errid, 0)
+        order = GetOrder(result['order_uid'])
+        assertEqual(order.status, OrderStatus.PENDING)
+
+else:
+    pass
+ctime = Time()
+if ctime == valid_buylimit_time:
+    pod = GetPendingOrderUIDs()
+    assertEqual(len(pod), 0)
+    assertEqual(len(ood), 1)
+    # order = GetOrder(result['order_uid'])
+    # assertEqual(order.symbol, Symbol())
+    # assertEqual(order.uid, result['order_uid'])
+    # assertEqual(order.open_time, ctime)
+    # assertEqual(order.volume, volume)
+    # assertEqual(order.stop_loss, stop_loss)
+    # assertEqual(order.take_profit, take_profit)
+    # assertEqual(order.magic_number, magic_number)
+    # assertIsNone(order.close_time)
+    # assertEqual(order.close_price, nan)
+
+    set_test_result("OK")
+    StopTester()
+
