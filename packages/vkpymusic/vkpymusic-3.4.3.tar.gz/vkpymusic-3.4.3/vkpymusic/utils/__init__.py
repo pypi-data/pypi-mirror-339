b784c5e22688bb4a227cd02099356fa57f949b8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-10 03:18:36 +0000

    Add package vkpymusic/3.4.3/vkpymusic-3.4.3.tar.gz

---
+++ packages/vkpymusic/vkpymusic-3.4.3.tar.gz/vkpymusic-3.4.3/vkpymusic/utils/__init__.py
@@ -0,0 +1,15 @@
+"""
+This module contains utilities for conversion and logging.
+
+Classes:
+    Converter: A class for performing various conversion operations.
+    get_logger: A function for getting or creating a logger.
+"""
+
+from .converter import Converter
+from .logger import create_logger
+
+__all__ = [
+    "Converter",
+    "create_logger",
+]
