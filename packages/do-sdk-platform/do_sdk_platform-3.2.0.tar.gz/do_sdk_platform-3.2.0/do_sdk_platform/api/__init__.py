50d4805c965186a4367609cff81f4bd3c6eaaaa1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-10 06:06:15 +0000

    Add package do-sdk-platform/3.2.0/do_sdk_platform-3.2.0.tar.gz

---
+++ packages/do-sdk-platform/do_sdk_platform-3.2.0.tar.gz/do_sdk_platform-3.2.0/do_sdk_platform/api/__init__.py
@@ -0,0 +1,25 @@
+# flake8: noqa
+
+# import apis into api package
+from do_sdk_platform.api.accounts_api import AccountsApi
+from do_sdk_platform.api.admin_api import AdminApi
+from do_sdk_platform.api.billing_api import BillingApi
+from do_sdk_platform.api.catalog_api import CatalogApi
+from do_sdk_platform.api.clusters_api import ClustersApi
+from do_sdk_platform.api.computebenches_api import ComputebenchesApi
+from do_sdk_platform.api.credits_api import CreditsApi
+from do_sdk_platform.api.default_api import DefaultApi
+from do_sdk_platform.api.functions_api import FunctionsApi
+from do_sdk_platform.api.health_api import HealthApi
+from do_sdk_platform.api.invoices_api import InvoicesApi
+from do_sdk_platform.api.organizations_api import OrganizationsApi
+from do_sdk_platform.api.pipelines_api import PipelinesApi
+from do_sdk_platform.api.secrets_api import SecretsApi
+from do_sdk_platform.api.ssh_api import SshApi
+from do_sdk_platform.api.subscriptions_api import SubscriptionsApi
+from do_sdk_platform.api.tools_api import ToolsApi
+from do_sdk_platform.api.users_api import UsersApi
+from do_sdk_platform.api.volumes_api import VolumesApi
+from do_sdk_platform.api.workflow_tasks_api import WorkflowTasksApi
+from do_sdk_platform.api.workflows_api import WorkflowsApi
+
