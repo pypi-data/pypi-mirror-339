d37bc6c459723192827fe4cb4c6430c6c78799b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-10 06:06:18 +0000

    Add package multidict/6.4.1/multidict-6.4.1.tar.gz

---
+++ packages/multidict/multidict-6.4.1.tar.gz/multidict-6.4.1/setup.py
@@ -0,0 +1,44 @@
+import os
+import platform
+import sys
+
+from setuptools import Extension, setup
+
+NO_EXTENSIONS = bool(os.environ.get("MULTIDICT_NO_EXTENSIONS"))
+
+if sys.implementation.name != "cpython":
+    NO_EXTENSIONS = True
+
+CFLAGS = ["-O3"]
+# CFLAGS = ["-O0", "-g3", "-DDEBUG"]
+if platform.system() != "Windows":
+    CFLAGS.extend(
+        [
+            "-std=c99",
+            "-Wall",
+            "-Wsign-compare",
+            "-Wconversion",
+            "-fno-strict-aliasing",
+            "-pedantic",
+        ]
+    )
+
+extensions = [
+    Extension(
+        "multidict._multidict",
+        ["multidict/_multidict.c"],
+        extra_compile_args=CFLAGS,
+    ),
+]
+
+
+if not NO_EXTENSIONS:
+    print("*********************")
+    print("* Accelerated build *")
+    print("*********************")
+    setup(ext_modules=extensions)
+else:
+    print("*********************")
+    print("* Pure Python build *")
+    print("*********************")
+    setup()
