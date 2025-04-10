46a5d48b9ea49835d0e15f5d57509db44685c40c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-10 04:07:01 +0000

    Add package dbt-sl-sdk/0.10.0/dbt_sl_sdk-0.10.0.tar.gz

---
+++ packages/dbt-sl-sdk/dbt_sl_sdk-0.10.0.tar.gz/dbt_sl_sdk-0.10.0/dbtsl/models/metric.py
@@ -0,0 +1,44 @@
+from dataclasses import dataclass, field
+from enum import Enum
+from typing import List, Optional
+
+from dbtsl.models.base import BaseModel, FlexibleEnumMeta, GraphQLFragmentMixin
+from dbtsl.models.dimension import Dimension
+from dbtsl.models.entity import Entity
+from dbtsl.models.measure import Measure
+from dbtsl.models.time import TimeGranularity
+
+
+class MetricType(Enum, metaclass=FlexibleEnumMeta):
+    """The type of a Metric."""
+
+    UNKNOWN = "UNKNOWN"
+    SIMPLE = "SIMPLE"
+    RATIO = "RATIO"
+    CUMULATIVE = "CUMULATIVE"
+    DERIVED = "DERIVED"
+    CONVERSION = "CONVERSION"
+
+
+QUERYABLE_GRANULARITIES_DEPRECATION = (
+    "Since the introduction of custom time granularities, `Metric.queryable_granularities` is deprecated. "
+    "Use `queryable_time_granularities` instead."
+)
+
+
+@dataclass(frozen=True)
+class Metric(BaseModel, GraphQLFragmentMixin):
+    """A metric."""
+
+    name: str
+    description: Optional[str]
+    type: MetricType
+    dimensions: List[Dimension]
+    measures: List[Measure]
+    entities: List[Entity]
+    queryable_granularities: List[TimeGranularity] = field(
+        metadata={BaseModel.DEPRECATED: QUERYABLE_GRANULARITIES_DEPRECATION}
+    )
+    queryable_time_granularities: List[str]
+    label: str
+    requires_metric_time: bool
