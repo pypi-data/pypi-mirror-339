232db3f52e1ea60ad977dc25a1b63dead97c2ab2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-10 03:18:35 +0000

    Add package pokerengine/1.9.3/pokerengine-1.9.3-cp313-cp313-manylinux_2_41_x86_64.whl

---
+++ packages/pokerengine/pokerengine-1.9.3-cp313-cp313-manylinux_2_41_x86_64.whl/pokerengine/__init__.py
@@ -0,0 +1,33 @@
+"""
+Poker Library
+"""
+
+from __future__ import annotations
+
+from pokerengine.pokerengine_core import __doc__, __version__
+
+from . import (
+    card,
+    constants,
+    engine,
+    enums,
+    enums_schema,
+    evaluation,
+    exceptions,
+    pokerengine,
+    schema,
+)
+
+__all__ = (
+    "__doc__",
+    "__version__",
+    "card",
+    "constants",
+    "engine",
+    "enums",
+    "enums_schema",
+    "evaluation",
+    "exceptions",
+    "pokerengine",
+    "schema",
+)
