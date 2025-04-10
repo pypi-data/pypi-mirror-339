d0a9cb1e26bf8850c9a928a69b3e3bad6673b0d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-10 03:18:44 +0000

    Add package einstein-models/0.1.4/einstein-models-0.1.4.tar.gz

---
+++ packages/einstein-models/einstein-models-0.1.4.tar.gz/einstein-models-0.1.4/einstein_models/__init__.py
@@ -0,0 +1,8 @@
+"""
+Einstein Models Python SDK
+"""
+
+from .einstein_models import ModelsAI
+from .data.chat_messages import Messages
+
+__all__ = ['ModelsAI', 'Messages']
---
+++ packages/einstein-models/einstein-models-0.1.4.tar.gz/einstein-models-0.1.4/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="einstein-models",
+    version="0.1.4",
+    packages=find_packages(),
+    package_data={
+        'einstein_models': ['data/*.py'],
+        'einstein_models.models': ['models/*.py'],
+        'einstein_models.rest': ['rest/*.py'],
+        'einstein_models.constant': ['constants/*.py'],
+
+    },
+    install_requires=[
+        "requests",
+        "python-dotenv",
+    ],
+    author="Amir Khan",
+    author_email="amir.khan@salesforce.com",
+    description="Python SDK for Salesforce Einstein Models API",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/amirkhan-ak-sf/einstein-models",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+) 
