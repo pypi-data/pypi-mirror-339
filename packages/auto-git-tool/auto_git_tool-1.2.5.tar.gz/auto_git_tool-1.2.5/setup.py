ec0a3b6c8a0c75ea857c2150ab4c423acd28b1f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-10 04:05:46 +0000

    Add package auto-git-tool/1.2.5/auto_git_tool-1.2.5.tar.gz

---
+++ packages/auto-git-tool/auto_git_tool-1.2.5.tar.gz/auto_git_tool-1.2.5/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="auto-git-tool",
+    version="1.2.5",
+    packages=find_packages(),
+    install_requires=[
+        'jst-aicommit',
+        'rich',
+        
+    ],
+    entry_points={
+        'console_scripts': [
+            'autogit=auto_git.cli:aic',  
+        ],
+    },
+    url="https://github.com/Husanjonazamov/auto-git",
+    author="Husanjon Azamov",
+    author_email="azamovhusanboy@gmail.com",
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
