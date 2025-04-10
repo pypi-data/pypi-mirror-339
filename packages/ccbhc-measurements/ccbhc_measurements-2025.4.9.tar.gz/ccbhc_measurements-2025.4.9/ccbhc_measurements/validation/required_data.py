df5871ac644d3b1275ff93acb020235fe1d8cfce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-10 07:06:47 +0000

    Add package ccbhc-measurements/2025.4.9/ccbhc_measurements-2025.4.9.tar.gz

---
+++ packages/ccbhc-measurements/ccbhc_measurements-2025.4.9.tar.gz/ccbhc_measurements-2025.4.9/ccbhc_measurements/validation/required_data.py
@@ -0,0 +1,45 @@
+DEP_REM_sub_1 = [
+    "PHQ9",
+    "Diagnostic_History",
+    "Demographic_Data",
+    "Insurance_History"
+]
+ASC_sub_1 = [
+    "Alcohol_Encounters",
+    "Diagnostic_History",
+    "ASC_Demographic_Data",
+    "Insurance_History"
+]
+ASC_sub_2 = [
+    None
+]
+
+
+def get_required_dataframes(submeasure_name: str) -> list[str]:
+    """
+    Gets the required Dataframes for a given submeasure
+
+    Parameters
+    ----------
+    submeasure_name
+        Name of the submeasure
+
+    Returns
+    -------
+    list[str]
+        Names of required Dataframes
+
+    Raises
+    ------
+    ValueError
+        If the submeasure is unknown
+    """
+    match submeasure_name:
+        case "DEP_REM_sub_1":
+            return DEP_REM_sub_1
+        case "ASC_sub_1":
+            return ASC_sub_1
+        case "ASC_sub_2":
+            return ASC_sub_2
+    raise ValueError(f"Unknown submeasure: {submeasure_name}")
+    
