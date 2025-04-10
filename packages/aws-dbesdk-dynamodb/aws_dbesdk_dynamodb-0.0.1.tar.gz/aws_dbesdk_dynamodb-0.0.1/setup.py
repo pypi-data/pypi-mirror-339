4f819686cdb3986121cd072ff6ee28374e98972a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-10 05:06:26 +0000

    Add package aws-dbesdk-dynamodb/0.0.1/aws_dbesdk_dynamodb-0.0.1.tar.gz

---
+++ packages/aws-dbesdk-dynamodb/aws_dbesdk_dynamodb-0.0.1.tar.gz/aws_dbesdk_dynamodb-0.0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name="aws-dbesdk-dynamodb",  # Replace with the name you want to reserve
+    version="0.0.1",
+    description="Upcoming Python package.",
+    long_description="This package is under development. Expected release in the future.",
+    author="Amazon Web Services",
+    maintainer="Amazon Web Services",
+    author_email="aws-crypto-tools@amazon.com",
+    classifiers=[
+        "Development Status :: 1 - Planning",
+    ],
+)
