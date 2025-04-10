e5fb075f1c95cd3e48530eea9750c5144713037a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-10 03:19:01 +0000

    Add package anndata/0.12.0rc1/anndata-0.12.0rc1.tar.gz

---
+++ packages/anndata/anndata-0.12.0rc1.tar.gz/anndata-0.12.0rc1/src/anndata/__init__.py
@@ -0,0 +1,70 @@
+"""Annotated multivariate observation data."""
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from typing import Any
+
+from ._core.anndata import AnnData
+from ._core.extensions import register_anndata_namespace
+from ._core.merge import concat
+from ._core.raw import Raw
+from ._settings import settings
+from ._version import __version__
+from ._warnings import (
+    ExperimentalFeatureWarning,
+    ImplicitModificationWarning,
+    OldFormatWarning,
+    WriteWarning,
+)
+from .io import read_h5ad, read_zarr
+from .utils import module_get_attr_redirect
+
+# Submodules need to be imported last
+from . import abc, experimental, typing, io, types  # noqa: E402 isort: skip
+
+# We use these in tests by attribute access
+from . import logging  # noqa: F401, E402 isort: skip
+
+_DEPRECATED_IO = (
+    "read_loom",
+    "read_hdf",
+    "read_excel",
+    "read_umi_tools",
+    "read_csv",
+    "read_text",
+    "read_mtx",
+)
+_DEPRECATED = dict((method, f"io.{method}") for method in _DEPRECATED_IO)
+
+
+def __getattr__(attr_name: str) -> Any:
+    return module_get_attr_redirect(attr_name, deprecated_mapping=_DEPRECATED)
+
+
+__all__ = [
+    # Attributes
+    "__version__",
+    "settings",
+    # Submodules
+    "abc",
+    "experimental",
+    "typing",
+    "types",
+    "io",
+    # Classes
+    "AnnData",
+    "Raw",
+    # Functions
+    "concat",
+    "read_zarr",
+    "read_h5ad",
+    "register_anndata_namespace",
+    # Warnings
+    "OldFormatWarning",
+    "WriteWarning",
+    "ImplicitModificationWarning",
+    "ExperimentalFeatureWarning",
+]
