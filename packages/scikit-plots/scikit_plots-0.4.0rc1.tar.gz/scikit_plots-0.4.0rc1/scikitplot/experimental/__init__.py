0fb8beb3656bc0f1749cc73fdfa3c15ada2bfff2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-10 06:07:26 +0000

    Add package scikit-plots/0.4.0rc1/scikit_plots-0.4.0rc1.tar.gz

---
+++ packages/scikit-plots/scikit_plots-0.4.0rc1.tar.gz/scikit_plots-0.4.0rc1/scikitplot/experimental/__init__.py
@@ -0,0 +1,24 @@
+"""
+Experimental functions and utilities by Python and Cython/Pybind11 Bindings.
+
+This package contains experimental functions and utilities for use with
+Cython/Pybind11 Bindings and Python implementations. The functions provided here
+are optimized for performance and are implemented using Cython/Pybind11 Bindings
+to leverage its speed advantages.
+
+This package is intended for experimental use. Functionality and APIs may
+change in future releases.
+"""
+
+# scikitplot/experimental/__init__.py
+
+# Your package/module initialization code goes here
+from ._logsumexp import *
+
+# Optionally import other modules if available
+try:
+    from ._cy_experimental import *
+    from ._py_experimental import *
+except ImportError:
+    # Optional modules not available
+    pass
