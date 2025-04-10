9bb1c211fc5938f1face3037d0d9c370a2eea84e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-10 14:07:05 +0000

    Add package regridding/0.1.4/regridding-0.1.4.tar.gz

---
+++ packages/regridding/regridding-0.1.4.tar.gz/regridding-0.1.4/regridding/__init__.py
@@ -0,0 +1,11 @@
+"""
+Resample arrays defined on curvilinear grids.
+"""
+
+from . import math
+from . import geometry
+from ._find_indices import *
+from ._weights import *
+from ._interp_ndarray import *
+from ._regrid import *
+from ._fill import *
---
+++ packages/regridding/regridding-0.1.4.tar.gz/regridding-0.1.4/regridding/math.py
@@ -0,0 +1,18 @@
+import numba
+
+__all__ = [
+    "sign",
+]
+
+
+@numba.njit(cache=True, inline="always", error_model="numpy")
+def sign(x: float):
+    """
+    Numba-compiled version of the `sign function <https://en.wikipedia.org/wiki/Sign_function>`_
+
+    Parameters
+    ----------
+    x
+        the value to find the sign of
+    """
+    return bool(x > 0) - bool(x < 0)
