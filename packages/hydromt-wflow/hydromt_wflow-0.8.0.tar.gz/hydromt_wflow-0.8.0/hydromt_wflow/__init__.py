26644a272572678c3412d443f8b4e7f8e4f4db52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-10 03:18:59 +0000

    Add package hydromt-wflow/0.8.0/hydromt_wflow-0.8.0.tar.gz

---
+++ packages/hydromt-wflow/hydromt_wflow-0.8.0.tar.gz/hydromt_wflow-0.8.0/hydromt_wflow/__init__.py
@@ -0,0 +1,17 @@
+"""hydroMT plugin for wflow models."""
+
+__version__ = "0.8.0"
+
+from .naming import *
+from .utils import *
+from .wflow import *
+from .wflow_sediment import *
+
+# PCRaster functions
+try:
+    import pcraster as pcr
+
+    HAS_PCRASTER = True
+    from .pcrm import *
+except ImportError:
+    HAS_PCRASTER = False
