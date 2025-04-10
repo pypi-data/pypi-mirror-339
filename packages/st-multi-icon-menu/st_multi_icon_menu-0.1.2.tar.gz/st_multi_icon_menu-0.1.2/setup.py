51969c7f8018e3154d75e1dfba865ebf43eabf32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-10 03:19:05 +0000

    Add package st-multi-icon-menu/0.1.2/st_multi_icon_menu-0.1.2.tar.gz

---
+++ packages/st-multi-icon-menu/st_multi_icon_menu-0.1.2.tar.gz/st_multi_icon_menu-0.1.2/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open('README.md') as f:
+    long_desc = f.read()
+
+setuptools.setup(
+    name="st_multi_icon_menu",
+    version="0.1.2",
+    author="Urban Ottosson",
+    author_email="urban@ottosson.org",
+    description="Streamlit Component for ANT Menu with Bootstrap icon support",
+    long_description=long_desc,
+    long_description_content_type='text/markdown',
+    url="https://github.com/locupleto/st_multi_icon_menu",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[],
+    python_requires=">=3.6",
+    install_requires=[
+        # By definition, a Custom Component depends on Streamlit.
+        # If your component has other Python dependencies, list
+        # them here.
+        "streamlit >= 0.63",
+    ],
+)
