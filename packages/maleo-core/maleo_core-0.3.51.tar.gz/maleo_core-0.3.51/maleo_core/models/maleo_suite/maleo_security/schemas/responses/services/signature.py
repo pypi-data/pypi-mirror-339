8f8c723be098afc89ef3be6f611aecceafb579b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-10 03:19:07 +0000

    Add package maleo-core/0.3.51/maleo_core-0.3.51.tar.gz

---
+++ packages/maleo-core/maleo_core-0.3.51.tar.gz/maleo_core-0.3.51/maleo_core/models/maleo_suite/maleo_security/schemas/responses/services/signature.py
@@ -0,0 +1,28 @@
+from maleo_core.models.base.schemas.responses.general import BaseGeneralResponsesSchemas
+from maleo_core.models.maleo_suite.maleo_security.transfers.results.general.signature import MaleoSecuritySignatureGeneralResults
+
+class MaleoSecuritySignatureServiceResponsesSchemas:
+    #* ----- ----- Response ----- ----- *#
+    class SignSingleResponse(BaseGeneralResponsesSchemas.SingleData):
+        code:str = "SEC-SGN-001"
+        message:str = "Succesfully signed single message"
+        description:str = "The given message successfully signed"
+        data:MaleoSecuritySignatureGeneralResults.SingleSign
+
+    class SignMultipleResponse(BaseGeneralResponsesSchemas.SingleData):
+        code:str = "SEC-SGN-002"
+        message:str = "Succesfully signed multiple messages"
+        description:str = "The given messages successfully signed"
+        data:MaleoSecuritySignatureGeneralResults.MultipleSign
+
+    class VerifySingleResponse(BaseGeneralResponsesSchemas.SingleData):
+        code:str = "SEC-SGN-003"
+        message:str = "Succesfully verified single signature"
+        description:str = "The given signature successfully verified"
+        data:MaleoSecuritySignatureGeneralResults.SingleVerify
+
+    class VerifyMultipleResponse(BaseGeneralResponsesSchemas.SingleData):
+        code:str = "SEC-SGN-004"
+        message:str = "Succesfully verified multiple signatures"
+        description:str = "The given signatures successfully verified"
+        data:MaleoSecuritySignatureGeneralResults.MultipleVerify
